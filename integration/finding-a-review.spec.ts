6692ade48ee49f2f686acade240bcd96cc4a9371
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-05-09 13:40:43 +0100

    Display reviews as a card
    Refs #3, https://inclusive-components.design/cards/

--- integration/finding-a-review.spec.ts
+++ integration/finding-a-review.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test'
 
 test('can find and view a review', async ({ page }) => {
   await page.goto('/preprints/doi-10.1101-2022.01.13.476201')
-  await page.click('text=Jingfang Hao')
+  await page.click('text=Read the review by Jingfang Hao et al')
 
   const review = page.locator('main')
 
