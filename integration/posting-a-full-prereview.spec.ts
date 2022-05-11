91db95d4304d1f8c1e668f1d3928e149c643d703
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-05-11 11:32:26 +0100

    Add 'Write a PREreview form'
    Adds a form containing a simple textbox; it's impossible to submit the form.
    
    Refs #4

---
+++ integration/posting-a-full-prereview.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from '@playwright/test'
+
+test('can post a full PREreview', async ({ page }) => {
+  await page.goto('/preprints/doi-10.1101-2022.01.13.476201')
+  await page.click('text="Write a PREreview"')
+
+  await page.fill('text=PREreview', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+  await page.click('text="Post PREreview"')
+
+  const h1 = page.locator('h1')
+
+  test.fixme(true, 'Success message does not appear')
+
+  await expect(h1).toContainText('PREreview posted')
+
+  await page.click('text="Back to preprint"')
+
+  const review = page.locator('main article').first()
+
+  await expect(review).toContainText('Lorem ipsum dolor sit amet')
+})
