0cbf965bcc8c0746fcaad461455353d9d484ea7e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-05-13 14:10:56 +0100

    Separate creating the server from running it

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,56 @@
+import express from 'express'
 import { constant, pipe } from 'fp-ts/function'
+import fs from 'fs/promises'
+import http from 'http'
 import { NotFound } from 'http-errors'
 import { route } from 'hyper-ts-routing'
 import * as RM from 'hyper-ts/lib/ReaderMiddleware'
+import { toRequestHandler } from 'hyper-ts/lib/express'
+import * as L from 'logger-fp-ts'
+import path from 'path'
 import { handleError } from './http-error'
 import { router } from './router'
 
+export type AppEnv = L.LoggerEnv
+
 const routerMiddleware = pipe(route(router, constant(new NotFound())), RM.fromMiddleware, RM.iflatten)
 
-export const appMiddleware = pipe(routerMiddleware, RM.orElseMiddlewareK(handleError))
+const appMiddleware = pipe(routerMiddleware, RM.orElseMiddlewareK(handleError))
+
+export const app = (deps: AppEnv) => {
+  const app = express()
+    .disable('x-powered-by')
+    .use((req, res, next) => {
+      pipe({ method: req.method, url: req.url }, L.infoP('Received HTTP request'))(deps)()
+
+      res.once('finish', () => {
+        pipe({ status: res.statusCode }, L.infoP('Sent HTTP response'))(deps)()
+      })
+
+      res.once('close', () => {
+        if (res.writableFinished) {
+          return
+        }
+
+        pipe({ status: res.statusCode }, L.warnP('HTTP response may not have been completely sent'))(deps)()
+      })
+
+      next()
+    })
+    .use(async (req, res, next) => {
+      const file = req.url + '.html'
+      await fs
+        .access(path.join('static', file))
+        .then(() => (req.url = file))
+        .catch(() => {
+          // do nothing
+        })
+
+      next()
+    })
+    .use(express.static('static'))
+    .use(express.urlencoded({ extended: true }))
+    .use(pipe(appMiddleware(deps), toRequestHandler))
+
+  return http.createServer(app)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,17 @@
 import { createTerminus } from '@godaddy/terminus'
 import { SystemClock } from 'clock-ts'
-import express from 'express'
 import * as RTC from 'fp-ts-contrib/ReaderTask'
 import * as C from 'fp-ts/Console'
 import { pipe } from 'fp-ts/function'
-import fs from 'fs/promises'
-import http from 'http'
-import { toRequestHandler } from 'hyper-ts/lib/express'
 import * as L from 'logger-fp-ts'
-import path from 'path'
-import { appMiddleware } from './app'
-
-type AppEnv = L.LoggerEnv
+import { AppEnv, app } from './app'
 
 const deps: AppEnv = {
   clock: SystemClock,
   logger: pipe(C.log, L.withShow(L.getColoredShow(L.ShowLogEntry))),
 }
 
-const app = express()
-  .disable('x-powered-by')
-  .use((req, res, next) => {
-    pipe({ method: req.method, url: req.url }, L.infoP('Received HTTP request'))(deps)()
-
-    res.once('finish', () => {
-      pipe({ status: res.statusCode }, L.infoP('Sent HTTP response'))(deps)()
-    })
-
-    res.once('close', () => {
-      if (res.writableFinished) {
-        return
-      }
-
-      pipe({ status: res.statusCode }, L.warnP('HTTP response may not have been completely sent'))(deps)()
-    })
-
-    next()
-  })
-  .use(async (req, res, next) => {
-    const file = req.url + '.html'
-    await fs
-      .access(path.join('static', file))
-      .then(() => (req.url = file))
-      .catch(() => {
-        // do nothing
-      })
-
-    next()
-  })
-  .use(express.static('static'))
-  .use(express.urlencoded({ extended: true }))
-  .use(pipe(appMiddleware(deps), toRequestHandler))
-
-const server = http.createServer(app)
+const server = app(deps)
 
 server.on('listening', () => {
   L.debug('Server listening')(deps)()
