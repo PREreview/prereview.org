32df6a93c682cbdf3806f32043ee130d2190e2c7
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-06-27 14:36:25 +0100

    Show the preprint abstract
    Refs #107

--- src/preprint.ts
+++ src/preprint.ts
@@ -63,6 +63,25 @@ function createPage(reviews: Records) {
             <li><a href="https://orcid.org/0000-0001-5124-3000">Wojciech Nawrocki</a></li>
             <li><a href="https://orcid.org/0000-0003-3469-834X">Roberta Croce</a></li>
           </ol>
+
+          <h3 class="visually-hidden">Abstract</h3>
+
+          <p>
+            Non-photochemical quenching (NPQ) is the process that protects photosynthetic organisms from photodamage by
+            dissipating the energy absorbed in excess as heat. In the model green alga
+            <i>Chlamydomonas reinhardtii</i>, NPQ was abolished in the knock-out mutants of the pigment-protein
+            complexes LHCSR3 and LHCBM1. However, while LHCSR3 was shown to be a pH sensor and switching to a quenched
+            conformation at low pH, the role of LHCBM1 in NPQ has not been elucidated yet. In this work, we combine
+            biochemical and physiological measurements to study short-term high light acclimation of <i>npq5</i>, the
+            mutant lacking LHCBM1. We show that while in low light in the absence of this complex, the antenna size of
+            PSII is smaller than in its presence, this effect is marginal in high light, implying that a reduction of
+            the antenna is not responsible for the low NPQ. We also show that the mutant expresses LHCSR3 at the WT
+            level in high light, indicating that the absence of this complex is also not the reason. Finally, NPQ
+            remains low in the mutant even when the pH is artificially lowered to values that can switch LHCSR3 to the
+            quenched conformation. It is concluded that both LHCSR3 and LHCBM1 need to be present for the induction of
+            NPQ and that LHCBM1 is the interacting partner of LHCSR3. This interaction can either enhance the quenching
+            capacity of LHCSR3 or connect this complex with the PSII supercomplex.
+          </p>
         </article>
       </aside>
 
