884e78a6f898b2d4bf2b1b2be9adc4a800329af8
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-08-30 14:37:44 +0100

    Remove redundant property
    Refs #249, 54db266852965eb8b285aeebd7c8cf0cdc2f81b4

--- test/fc.ts
+++ test/fc.ts
@@ -150,7 +150,6 @@ export const preprint = (): fc.Arbitrary<Preprint> =>
     id: preprintId(),
     language: languageCode(),
     posted: plainDate(),
-    server: fc.constantFrom('biorxiv', 'medrxiv'),
     title: html(),
     url: url(),
   })
