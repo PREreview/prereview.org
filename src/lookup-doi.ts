eebd28e6b220dd7d8464eff58d7111e6b3234b7f
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-09-07 15:37:49 +0100

    Allows DOIs to be URLs
    Refs #249

--- src/lookup-doi.ts
+++ src/lookup-doi.ts
@@ -1,6 +1,6 @@
-import { hasRegistrant, isDoi } from 'doi-ts'
+import { hasRegistrant, parse } from 'doi-ts'
 import { format } from 'fp-ts-routing'
-import { compose } from 'fp-ts/Refinement'
+import * as E from 'fp-ts/Either'
 import { pipe } from 'fp-ts/function'
 import * as M from 'hyper-ts/lib/Middleware'
 import * as D from 'io-ts/Decoder'
@@ -8,7 +8,11 @@ import { get } from 'spectacles-ts'
 import { seeOther } from './middleware'
 import { homeMatch, preprintMatch } from './routes'
 
-const DoiD = D.fromRefinement(pipe(isDoi, compose(hasRegistrant('1101', '1590'))), 'DOI')
+const DoiD = pipe(
+  D.string,
+  D.parse(s => E.fromOption(() => D.error(s, 'DOI'))(parse(s))),
+  D.refine(hasRegistrant('1101', '1590'), 'DOI'),
+)
 
 const LookupDoiD = pipe(
   D.struct({
