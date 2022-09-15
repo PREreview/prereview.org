5eaa4518bbaf832b3d717e4c8f5e6239df475121
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-09-15 18:31:59 +0100

    Prevent the question from being off the screen when fixing an error
    When following the link in the error summary, browsers will scroll down to the form control, which might mean the question is off the screen. This change intercepts the click and scrolls to the question while retaining focus on the control.
    
    Refs #286, https://github.com/alphagov/govuk-frontend/blob/cd2e1a7dbca67bc15af2fffcc22e3ef7a030adac/src/govuk/components/error-summary/error-summary.mjs#L52-L69

--- assets/error-summary.ts
+++ assets/error-summary.ts
@@ -5,7 +5,91 @@ class ErrorSummary extends HTMLElement {
     super()
 
     forceFocus(this)
+
+    this.addEventListener('click', event => {
+      const { target } = event
+
+      if (!(target instanceof HTMLAnchorElement)) {
+        return
+      }
+
+      if (focusTarget(target)) {
+        event.preventDefault()
+      }
+    })
   }
+}
+
+function focusTarget(target: HTMLAnchorElement) {
+  const inputId = getFragmentFromUrl(target.href)
+
+  if (!inputId) {
+    return false
+  }
+
+  const input = document.getElementById(inputId)
+
+  if (!(input instanceof HTMLInputElement) && !(input instanceof HTMLTextAreaElement) && !input?.isContentEditable) {
+    return false
+  }
+
+  const legendOrLabel = getAssociatedLegendOrLabel(input)
+
+  if (!legendOrLabel) {
+    return false
+  }
+
+  input.focus({ preventScroll: true })
+  legendOrLabel.scrollIntoView()
+
+  return true
+}
+
+function getFragmentFromUrl(url: string) {
+  if (url.indexOf('#') === -1) {
+    return undefined
+  }
+
+  return url.split('#').pop()
+}
+
+function getAssociatedLegendOrLabel(input: HTMLElement) {
+  if (input instanceof HTMLInputElement || input instanceof HTMLTextAreaElement) {
+    const fieldset = input.closest('fieldset')
+
+    if (fieldset) {
+      const legends = fieldset.getElementsByTagName('legend')
+
+      if (legends.length) {
+        const candidateLegend = legends[0]
+
+        if (input.type === 'checkbox' || input.type === 'radio') {
+          return candidateLegend
+        }
+
+        const legendTop = candidateLegend.getBoundingClientRect().top
+        const inputRect = input.getBoundingClientRect()
+
+        if (inputRect.height && window.innerHeight) {
+          const inputBottom = inputRect.top + inputRect.height
+
+          if (inputBottom - legendTop < window.innerHeight / 2) {
+            return candidateLegend
+          }
+        }
+      }
+    }
+  }
+
+  const labelledBy = input.getAttribute('aria-labelledby')
+
+  if (labelledBy) {
+    return document.getElementById(labelledBy)
+  }
+
+  const id = input.getAttribute('id')
+
+  return id ? document.querySelector<HTMLLabelElement>(`label[for="${id}"]`) : input.closest('label')
 }
 
 window.customElements.define(ErrorSummary.element, ErrorSummary)
