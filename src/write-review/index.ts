95b1bc2df09ecab231d1bf0fad0e029fdcfd1a38
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-09-21 14:37:08 +0100

    Extract the code-of-conduct step into its own module
    Refs #319, 956369188a95c233c328bc8851153750468bcbf9

--- src/write-review/index.ts
+++ src/write-review/index.ts
@@ -15,12 +15,11 @@ import { Orcid } from 'orcid-id-ts'
 import { getLangDir } from 'rtl-detect'
 import { P, match } from 'ts-pattern'
 import { SubmittedDeposition } from 'zenodo-ts'
-import { Html, html, plainText, rawHtml, sanitizeHtml, sendHtml } from '../html'
+import { Html, html, plainText, sanitizeHtml, sendHtml } from '../html'
 import { notFound, seeOther } from '../middleware'
 import { page } from '../page'
 import {
   preprintMatch,
-  writeReviewCompetingInterestsMatch,
   writeReviewConductMatch,
   writeReviewMatch,
   writeReviewPersonaMatch,
@@ -28,8 +27,8 @@ import {
   writeReviewReviewMatch,
 } from '../routes'
 import { User, getUserFromSession } from '../user'
-import { CodeOfConductFormC, CompletedForm, CompletedFormC } from './completed-form'
-import { Form, deleteForm, getForm, saveForm, showNextForm, updateForm } from './form'
+import { CompletedForm, CompletedFormC } from './completed-form'
+import { deleteForm, getForm, showNextForm } from './form'
 import { Preprint, getPreprint } from './preprint'
 
 export type NewPrereview = {
@@ -60,22 +59,7 @@ export { writeReviewAddAuthors } from './write-review-add-authors'
 
 export { writeReviewCompetingInterests } from './write-review-competing-interests'
 
-export const writeReviewConduct = flow(
-  RM.fromReaderTaskEitherK(getPreprint),
-  RM.ichainW(preprint =>
-    pipe(
-      RM.right({ preprint }),
-      RM.apS('user', getUserFromSession()),
-      RM.bindW('form', ({ user }) => RM.rightReaderTask(getForm(user.orcid, preprint.doi))),
-      RM.apSW('method', RM.decodeMethod(E.right)),
-      RM.ichainW(state =>
-        match(state).with({ method: 'POST' }, handleCodeOfConductForm).otherwise(showCodeOfConductForm),
-      ),
-      RM.orElseMiddlewareK(() => seeOther(format(writeReviewMatch.formatter, { doi: preprint.doi }))),
-    ),
-  ),
-  RM.orElseW(() => notFound),
-)
+export { writeReviewConduct } from './write-review-conduct'
 
 export const writeReviewPost = flow(
   RM.fromReaderTaskEitherK(getPreprint),
@@ -113,18 +97,6 @@ const handlePostForm = ({ form, preprint, user }: { form: CompletedForm; preprin
     RM.orElseW(() => showFailureMessage(preprint)),
   )
 
-const showCodeOfConductForm = flow(
-  fromReaderK(({ form, preprint }: { form: Form; preprint: Preprint }) => codeOfConductForm(preprint, form)),
-  RM.ichainFirst(() => RM.status(Status.OK)),
-  RM.ichainMiddlewareK(sendHtml),
-)
-
-const showCodeOfConductErrorForm = flow(
-  fromReaderK((preprint: Preprint) => codeOfConductForm(preprint, {}, true)),
-  RM.ichainFirst(() => RM.status(Status.BadRequest)),
-  RM.ichainMiddlewareK(sendHtml),
-)
-
 const showPostForm = flow(
   fromReaderK(({ form, preprint, user }: { form: CompletedForm; preprint: Preprint; user: User }) =>
     postForm(preprint, form, user),
@@ -132,15 +104,6 @@ const showPostForm = flow(
   RM.ichainFirst(() => RM.status(Status.OK)),
   RM.ichainMiddlewareK(sendHtml),
 )
-
-const handleCodeOfConductForm = ({ form, preprint, user }: { form: Form; preprint: Preprint; user: User }) =>
-  pipe(
-    RM.decodeBody(CodeOfConductFormC.decode),
-    RM.map(updateForm(form)),
-    RM.chainFirstReaderTaskK(saveForm(user.orcid, preprint.doi)),
-    RM.ichainMiddlewareKW(showNextForm(preprint.doi)),
-    RM.orElseW(() => showCodeOfConductErrorForm(preprint)),
-  )
 
 const createRecord = (newPrereview: NewPrereview) =>
   RTE.asksReaderTaskEither(RTE.fromTaskEitherK(({ createRecord }: CreateRecordEnv) => createRecord(newPrereview)))
@@ -272,107 +235,6 @@ function postForm(preprint: Preprint, review: CompletedForm, user: User) {
       </main>
     `,
     js: ['single-use-form.js', 'error-summary.js'],
-  })
-}
-
-function codeOfConductForm(preprint: Preprint, form: Form, error = false) {
-  return page({
-    title: plainText`${error ? 'Error: ' : ''}Code of Conduct – PREreview of “${preprint.title}”`,
-    content: html`
-      <nav>
-        <a href="${format(writeReviewCompetingInterestsMatch.formatter, { doi: preprint.doi })}" class="back">Back</a>
-      </nav>
-
-      <main>
-        <form method="post" action="${format(writeReviewConductMatch.formatter, { doi: preprint.doi })}" novalidate>
-          ${error
-            ? html`
-                <error-summary aria-labelledby="error-summary-title" role="alert">
-                  <h2 id="error-summary-title">There is a problem</h2>
-                  <ul>
-                    <li>
-                      <a href="#conduct-yes">Confirm that you are following the Code&nbsp;of&nbsp;Conduct</a>
-                    </li>
-                  </ul>
-                </error-summary>
-              `
-            : ''}
-
-          <div ${rawHtml(error ? 'class="error"' : '')}>
-            <fieldset
-              role="group"
-              aria-describedby="conduct-tip"
-              ${rawHtml(error ? 'aria-invalid="true" aria-errormessage="conduct-error"' : '')}
-            >
-              <legend>
-                <h1>Code of Conduct</h1>
-              </legend>
-
-              <div id="conduct-tip" role="note">
-                As a member of our community, we expect you to abide by the
-                <a href="https://content.prereview.org/coc/">PREreview Code&nbsp;of&nbsp;Conduct</a>.
-              </div>
-
-              <details>
-                <summary>Examples of expected behaviors</summary>
-
-                <div>
-                  <ul>
-                    <li>Using welcoming and inclusive language.</li>
-                    <li>Providing feedback that is constructive, i.e. useful, to the receiver.</li>
-                    <li>Being respectful of differing viewpoints and experiences.</li>
-                    <li>Gracefully accepting constructive criticism.</li>
-                    <li>Focusing on what is best for the community.</li>
-                    <li>Showing empathy towards other community members.</li>
-                  </ul>
-                </div>
-              </details>
-
-              <details>
-                <summary>Examples of unacceptable behaviors</summary>
-
-                <div>
-                  <ul>
-                    <li>Trolling, insulting or derogatory comments, and personal or political attacks.</li>
-                    <li>Providing unconstructive or disruptive feedback on PREreview.</li>
-                    <li>Public or private harassment.</li>
-                    <li>
-                      Publishing others’ confidential information, such as a physical or electronic address, without
-                      explicit permission.
-                    </li>
-                    <li>Use of sexualized language or imagery and unwelcome sexual attention or advances.</li>
-                    <li>Other conduct which could reasonably be considered inappropriate in a professional setting.</li>
-                  </ul>
-                </div>
-              </details>
-
-              ${error
-                ? html`
-                    <div class="error-message" id="conduct-error">
-                      <span class="visually-hidden">Error:</span> Confirm that you are following the
-                      Code&nbsp;of&nbsp;Conduct
-                    </div>
-                  `
-                : ''}
-
-              <label>
-                <input
-                  name="conduct"
-                  id="conduct-yes"
-                  type="checkbox"
-                  value="yes"
-                  ${rawHtml(form.conduct === 'yes' ? 'checked' : '')}
-                />
-                <span>I’m following the Code&nbsp;of&nbsp;Conduct</span>
-              </label>
-            </fieldset>
-          </div>
-
-          <button>Continue</button>
-        </form>
-      </main>
-    `,
-    js: ['error-summary.js'],
   })
 }
 
---
+++ src/write-review/write-review-conduct.ts
@@ -0,0 +1,161 @@
+import { format } from 'fp-ts-routing'
+import * as E from 'fp-ts/Either'
+import { Reader } from 'fp-ts/Reader'
+import { flow, pipe } from 'fp-ts/function'
+import { Status, StatusOpen } from 'hyper-ts'
+import * as RM from 'hyper-ts/lib/ReaderMiddleware'
+import { match } from 'ts-pattern'
+import { html, plainText, rawHtml, sendHtml } from '../html'
+import { notFound, seeOther } from '../middleware'
+import { page } from '../page'
+import { writeReviewCompetingInterestsMatch, writeReviewConductMatch, writeReviewMatch } from '../routes'
+import { User, getUserFromSession } from '../user'
+import { CodeOfConductFormC } from './completed-form'
+import { Form, getForm, saveForm, showNextForm, updateForm } from './form'
+import { Preprint, getPreprint } from './preprint'
+
+export const writeReviewConduct = flow(
+  RM.fromReaderTaskEitherK(getPreprint),
+  RM.ichainW(preprint =>
+    pipe(
+      RM.right({ preprint }),
+      RM.apS('user', getUserFromSession()),
+      RM.bindW('form', ({ user }) => RM.rightReaderTask(getForm(user.orcid, preprint.doi))),
+      RM.apSW('method', RM.decodeMethod(E.right)),
+      RM.ichainW(state =>
+        match(state).with({ method: 'POST' }, handleCodeOfConductForm).otherwise(showCodeOfConductForm),
+      ),
+      RM.orElseMiddlewareK(() => seeOther(format(writeReviewMatch.formatter, { doi: preprint.doi }))),
+    ),
+  ),
+  RM.orElseW(() => notFound),
+)
+
+const showCodeOfConductForm = flow(
+  fromReaderK(({ form, preprint }: { form: Form; preprint: Preprint }) => codeOfConductForm(preprint, form)),
+  RM.ichainFirst(() => RM.status(Status.OK)),
+  RM.ichainMiddlewareK(sendHtml),
+)
+
+const showCodeOfConductErrorForm = flow(
+  fromReaderK((preprint: Preprint) => codeOfConductForm(preprint, {}, true)),
+  RM.ichainFirst(() => RM.status(Status.BadRequest)),
+  RM.ichainMiddlewareK(sendHtml),
+)
+
+const handleCodeOfConductForm = ({ form, preprint, user }: { form: Form; preprint: Preprint; user: User }) =>
+  pipe(
+    RM.decodeBody(CodeOfConductFormC.decode),
+    RM.map(updateForm(form)),
+    RM.chainFirstReaderTaskK(saveForm(user.orcid, preprint.doi)),
+    RM.ichainMiddlewareKW(showNextForm(preprint.doi)),
+    RM.orElseW(() => showCodeOfConductErrorForm(preprint)),
+  )
+
+function codeOfConductForm(preprint: Preprint, form: Form, error = false) {
+  return page({
+    title: plainText`${error ? 'Error: ' : ''}Code of Conduct – PREreview of “${preprint.title}”`,
+    content: html`
+      <nav>
+        <a href="${format(writeReviewCompetingInterestsMatch.formatter, { doi: preprint.doi })}" class="back">Back</a>
+      </nav>
+
+      <main>
+        <form method="post" action="${format(writeReviewConductMatch.formatter, { doi: preprint.doi })}" novalidate>
+          ${error
+            ? html`
+                <error-summary aria-labelledby="error-summary-title" role="alert">
+                  <h2 id="error-summary-title">There is a problem</h2>
+                  <ul>
+                    <li>
+                      <a href="#conduct-yes">Confirm that you are following the Code&nbsp;of&nbsp;Conduct</a>
+                    </li>
+                  </ul>
+                </error-summary>
+              `
+            : ''}
+
+          <div ${rawHtml(error ? 'class="error"' : '')}>
+            <fieldset
+              role="group"
+              aria-describedby="conduct-tip"
+              ${rawHtml(error ? 'aria-invalid="true" aria-errormessage="conduct-error"' : '')}
+            >
+              <legend>
+                <h1>Code of Conduct</h1>
+              </legend>
+
+              <div id="conduct-tip" role="note">
+                As a member of our community, we expect you to abide by the
+                <a href="https://content.prereview.org/coc/">PREreview Code&nbsp;of&nbsp;Conduct</a>.
+              </div>
+
+              <details>
+                <summary>Examples of expected behaviors</summary>
+
+                <div>
+                  <ul>
+                    <li>Using welcoming and inclusive language.</li>
+                    <li>Providing feedback that is constructive, i.e. useful, to the receiver.</li>
+                    <li>Being respectful of differing viewpoints and experiences.</li>
+                    <li>Gracefully accepting constructive criticism.</li>
+                    <li>Focusing on what is best for the community.</li>
+                    <li>Showing empathy towards other community members.</li>
+                  </ul>
+                </div>
+              </details>
+
+              <details>
+                <summary>Examples of unacceptable behaviors</summary>
+
+                <div>
+                  <ul>
+                    <li>Trolling, insulting or derogatory comments, and personal or political attacks.</li>
+                    <li>Providing unconstructive or disruptive feedback on PREreview.</li>
+                    <li>Public or private harassment.</li>
+                    <li>
+                      Publishing others’ confidential information, such as a physical or electronic address, without
+                      explicit permission.
+                    </li>
+                    <li>Use of sexualized language or imagery and unwelcome sexual attention or advances.</li>
+                    <li>Other conduct which could reasonably be considered inappropriate in a professional setting.</li>
+                  </ul>
+                </div>
+              </details>
+
+              ${error
+                ? html`
+                    <div class="error-message" id="conduct-error">
+                      <span class="visually-hidden">Error:</span> Confirm that you are following the
+                      Code&nbsp;of&nbsp;Conduct
+                    </div>
+                  `
+                : ''}
+
+              <label>
+                <input
+                  name="conduct"
+                  id="conduct-yes"
+                  type="checkbox"
+                  value="yes"
+                  ${rawHtml(form.conduct === 'yes' ? 'checked' : '')}
+                />
+                <span>I’m following the Code&nbsp;of&nbsp;Conduct</span>
+              </label>
+            </fieldset>
+          </div>
+
+          <button>Continue</button>
+        </form>
+      </main>
+    `,
+    js: ['error-summary.js'],
+  })
+}
+
+// https://github.com/DenisFrezzato/hyper-ts/pull/85
+function fromReaderK<R, A extends ReadonlyArray<unknown>, B, I = StatusOpen, E = never>(
+  f: (...a: A) => Reader<R, B>,
+): (...a: A) => RM.ReaderMiddleware<R, I, I, E, B> {
+  return (...a) => RM.rightReader(f(...a))
+}
