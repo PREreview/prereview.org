7a3468d0ae0d0e09c636c004a2898cd29a0235c2
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-09-21 14:51:09 +0100

    Order exports in the same manner as imports
    Unfortunately there doesn't seem to be a way to automate this yet.
    
    Refs #319, 956369188a95c233c328bc8851153750468bcbf9, https://github.com/trivago/prettier-plugin-sort-imports/issues/159

--- src/write-review/index.ts
+++ src/write-review/index.ts
@@ -1,21 +1,10 @@
-export { NewPrereview, CreateRecordEnv } from './write-review-post'
-
-export { GetPreprintTitleEnv } from './preprint'
-
 export { FormStoreEnv } from './form'
-
+export { GetPreprintTitleEnv } from './preprint'
 export { writeReview } from './write-review'
-
-export { writeReviewReview } from './write-review-review'
-
-export { writeReviewPersona } from './write-review-persona'
-
-export { writeReviewAuthors } from './write-review-authors'
-
 export { writeReviewAddAuthors } from './write-review-add-authors'
-
+export { writeReviewAuthors } from './write-review-authors'
 export { writeReviewCompetingInterests } from './write-review-competing-interests'
-
 export { writeReviewConduct } from './write-review-conduct'
-
-export { writeReviewPost } from './write-review-post'
+export { writeReviewPersona } from './write-review-persona'
+export { CreateRecordEnv, NewPrereview, writeReviewPost } from './write-review-post'
+export { writeReviewReview } from './write-review-review'
