296cbc00d30be73d066b85ab6653e31249e27c6b
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-10-13 14:25:30 +0100

    Make the add-more-authors form errors typesafe
    Refs #319, a8746835d8422f08ec42c7fe950b72d46b7f978d

--- src/write-review/write-review-authors.ts
+++ src/write-review/write-review-authors.ts
@@ -1,12 +1,15 @@
 import { format } from 'fp-ts-routing'
 import * as E from 'fp-ts/Either'
 import { Reader } from 'fp-ts/Reader'
+import * as RR from 'fp-ts/ReadonlyRecord'
 import { flow, pipe } from 'fp-ts/function'
 import { Status, StatusOpen } from 'hyper-ts'
 import * as RM from 'hyper-ts/lib/ReaderMiddleware'
 import * as D from 'io-ts/Decoder'
+import { get } from 'spectacles-ts'
 import { P, match } from 'ts-pattern'
 import { canAddAuthors } from '../feature-flags'
+import { MissingE, missingE } from '../form'
 import { html, plainText, rawHtml, sendHtml } from '../html'
 import { notFound, seeOther, serviceUnavailable } from '../middleware'
 import { page } from '../page'
@@ -42,20 +45,30 @@ export const writeReviewAuthors = flow(
 )
 
 const showAuthorsForm = flow(
-  fromReaderK(({ form, preprint }: { form: Form; preprint: Preprint }) => authorsForm(preprint, form)),
+  fromReaderK(({ form, preprint }: { form: Form; preprint: Preprint }) =>
+    authorsForm(preprint, { moreAuthors: E.right(form.moreAuthors) }),
+  ),
   RM.ichainFirst(() => RM.status(Status.OK)),
   RM.ichainMiddlewareK(sendHtml),
 )
 
-const showAuthorsErrorForm = flow(
-  fromReaderK((preprint: Preprint) => authorsForm(preprint, {}, true)),
-  RM.ichainFirst(() => RM.status(Status.BadRequest)),
-  RM.ichainMiddlewareK(sendHtml),
-)
+const showAuthorsErrorForm = (preprint: Preprint) =>
+  flow(
+    fromReaderK((form: AuthorsForm) => authorsForm(preprint, form)),
+    RM.ichainFirst(() => RM.status(Status.BadRequest)),
+    RM.ichainMiddlewareK(sendHtml),
+  )
 
 const handleAuthorsForm = ({ form, preprint, user }: { form: Form; preprint: Preprint; user: User }) =>
   pipe(
-    RM.decodeBody(AuthorsFormD.decode),
+    RM.decodeBody(body => E.right({ moreAuthors: pipe(MoreAuthorsFieldD.decode(body), E.mapLeft(missingE)) })),
+    RM.chainEitherK(fields =>
+      pipe(
+        E.Do,
+        E.apS('moreAuthors', fields.moreAuthors),
+        E.mapLeft(() => fields),
+      ),
+    ),
     RM.map(updateForm(form)),
     RM.chainFirstReaderTaskK(saveForm(user.orcid, preprint.doi)),
     RM.bindTo('form'),
@@ -70,16 +83,23 @@ const handleAuthorsForm = ({ form, preprint, user }: { form: Form; preprint: Pre
         )
         .otherwise(flow(({ form }) => form, showNextForm(preprint.doi))),
     ),
-    RM.orElseW(() => showAuthorsErrorForm(preprint)),
+    RM.orElseW(showAuthorsErrorForm(preprint)),
   )
 
-type AuthorsForm = D.TypeOf<typeof AuthorsFormD>
+const MoreAuthorsFieldD = pipe(
+  D.struct({
+    moreAuthors: D.literal('yes', 'no'),
+  }),
+  D.map(get('moreAuthors')),
+)
 
-const AuthorsFormD = D.struct({
-  moreAuthors: D.literal('yes', 'no'),
-})
+type AuthorsForm = {
+  readonly moreAuthors: E.Either<MissingE, 'yes' | 'no' | undefined>
+}
 
-function authorsForm(preprint: Preprint, form: Partial<AuthorsForm>, error = false) {
+function authorsForm(preprint: Preprint, form: AuthorsForm) {
+  const error = pipe(form, RR.some<E.Either<unknown, unknown>>(E.isLeft))
+
   return page({
     title: plainText`${error ? 'Error: ' : ''}Did you write the PREreview with anyone else? – PREreview of “${
       preprint.title
@@ -96,28 +116,41 @@ function authorsForm(preprint: Preprint, form: Partial<AuthorsForm>, error = fal
                 <error-summary aria-labelledby="error-summary-title" role="alert">
                   <h2 id="error-summary-title">There is a problem</h2>
                   <ul>
-                    <li>
-                      <a href="#more-authors-no">Select yes if you wrote the PREreview with someone else</a>
-                    </li>
+                    ${E.isLeft(form.moreAuthors)
+                      ? html`
+                          <li>
+                            <a href="#more-authors-no">
+                              ${match(form.moreAuthors.left)
+                                .with(
+                                  { _tag: 'MissingE' },
+                                  () => 'Select yes if you wrote the PREreview with someone else',
+                                )
+                                .exhaustive()}
+                            </a>
+                          </li>
+                        `
+                      : ''}
                   </ul>
                 </error-summary>
               `
             : ''}
 
-          <div ${rawHtml(error ? 'class="error"' : '')}>
+          <div ${rawHtml(E.isLeft(form.moreAuthors) ? 'class="error"' : '')}>
             <fieldset
               role="group"
-              ${rawHtml(error ? 'aria-invalid="true" aria-errormessage="more-authors-error"' : '')}
+              ${rawHtml(E.isLeft(form.moreAuthors) ? 'aria-invalid="true" aria-errormessage="more-authors-error"' : '')}
             >
               <legend>
                 <h1>Did you write the PREreview with anyone&nbsp;else?</h1>
               </legend>
 
-              ${error
+              ${E.isLeft(form.moreAuthors)
                 ? html`
                     <div class="error-message" id="more-authors-error">
-                      <span class="visually-hidden">Error:</span> Select yes if you wrote the PREreview with someone
-                      else
+                      <span class="visually-hidden">Error:</span>
+                      ${match(form.moreAuthors.left)
+                        .with({ _tag: 'MissingE' }, () => 'Select yes if you wrote the PREreview with someone else')
+                        .exhaustive()}
                     </div>
                   `
                 : ''}
@@ -130,7 +163,9 @@ function authorsForm(preprint: Preprint, form: Partial<AuthorsForm>, error = fal
                       id="more-authors-no"
                       type="radio"
                       value="no"
-                      ${rawHtml(form.moreAuthors === 'no' ? 'checked' : '')}
+                      ${match(form.moreAuthors)
+                        .with(E.right('no' as const), () => 'checked')
+                        .otherwise(() => '')}
                     />
                     <span>No, by myself</span>
                   </label>
@@ -141,7 +176,9 @@ function authorsForm(preprint: Preprint, form: Partial<AuthorsForm>, error = fal
                       name="moreAuthors"
                       type="radio"
                       value="yes"
-                      ${rawHtml(form.moreAuthors === 'yes' ? 'checked' : '')}
+                      ${match(form.moreAuthors)
+                        .with(E.right('yes' as const), () => 'checked')
+                        .otherwise(() => '')}
                     />
                     <span>Yes</span>
                   </label>
