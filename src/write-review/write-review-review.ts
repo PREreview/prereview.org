d811a673d9c34c5d48e91a75b841b2d48ba0a6c7
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-10-13 13:59:02 +0100

    Make the write-your-PREreview form errors typesafe
    Refs #319, a8746835d8422f08ec42c7fe950b72d46b7f978d

--- src/write-review/write-review-review.ts
+++ src/write-review/write-review-review.ts
@@ -1,16 +1,19 @@
 import { format } from 'fp-ts-routing'
 import * as E from 'fp-ts/Either'
 import { Reader } from 'fp-ts/Reader'
-import { flow, pipe } from 'fp-ts/function'
+import * as RR from 'fp-ts/ReadonlyRecord'
+import { flow, identity, pipe } from 'fp-ts/function'
 import { Status, StatusOpen } from 'hyper-ts'
 import * as RM from 'hyper-ts/lib/ReaderMiddleware'
 import * as D from 'io-ts/Decoder'
-import { match } from 'ts-pattern'
+import { get } from 'spectacles-ts'
+import { P, match } from 'ts-pattern'
+import { MissingE, missingE } from '../form'
 import { html, plainText, rawHtml, sendHtml } from '../html'
 import { notFound, seeOther, serviceUnavailable } from '../middleware'
 import { page } from '../page'
 import { preprintMatch, writeReviewMatch, writeReviewReviewMatch } from '../routes'
-import { NonEmptyStringC } from '../string'
+import { NonEmptyString, NonEmptyStringC } from '../string'
 import { User, getUserFromSession } from '../user'
 import { Form, getForm, saveForm, showNextForm, updateForm } from './form'
 import { Preprint, getPreprint } from './preprint'
@@ -35,34 +38,51 @@ export const writeReviewReview = flow(
   ),
 )
 
-const handleReviewForm = ({ form, preprint, user }: { form: Form; preprint: Preprint; user: User }) =>
-  pipe(
-    RM.decodeBody(ReviewFormD.decode),
-    RM.map(updateForm(form)),
-    RM.chainFirstReaderTaskK(saveForm(user.orcid, preprint.doi)),
-    RM.ichainMiddlewareKW(showNextForm(preprint.doi)),
-    RM.orElseW(() => showReviewErrorForm(preprint)),
-  )
-
-type ReviewForm = D.TypeOf<typeof ReviewFormD>
-
-const ReviewFormD = D.struct({
-  review: NonEmptyStringC,
-})
-
 const showReviewForm = flow(
-  fromReaderK(({ form, preprint }: { form: Form; preprint: Preprint }) => reviewForm(preprint, form)),
+  fromReaderK(({ form, preprint }: { form: Form; preprint: Preprint }) =>
+    reviewForm(preprint, { review: E.right(form.review) }),
+  ),
   RM.ichainFirst(() => RM.status(Status.OK)),
   RM.ichainMiddlewareK(sendHtml),
 )
 
-const showReviewErrorForm = flow(
-  fromReaderK((preprint: Preprint) => reviewForm(preprint, {}, true)),
-  RM.ichainFirst(() => RM.status(Status.BadRequest)),
-  RM.ichainMiddlewareK(sendHtml),
+const showReviewErrorForm = (preprint: Preprint) =>
+  flow(
+    fromReaderK((form: ReviewForm) => reviewForm(preprint, form)),
+    RM.ichainFirst(() => RM.status(Status.BadRequest)),
+    RM.ichainMiddlewareK(sendHtml),
+  )
+
+const handleReviewForm = ({ form, preprint, user }: { form: Form; preprint: Preprint; user: User }) =>
+  pipe(
+    RM.decodeBody(body => E.right({ review: pipe(ReviewFieldD.decode(body), E.mapLeft(missingE)) })),
+    RM.chainEitherK(fields =>
+      pipe(
+        E.Do,
+        E.apS('review', fields.review),
+        E.mapLeft(() => fields),
+      ),
+    ),
+    RM.map(updateForm(form)),
+    RM.chainFirstReaderTaskK(saveForm(user.orcid, preprint.doi)),
+    RM.ichainMiddlewareKW(showNextForm(preprint.doi)),
+    RM.orElseW(showReviewErrorForm(preprint)),
+  )
+
+const ReviewFieldD = pipe(
+  D.struct({
+    review: NonEmptyStringC,
+  }),
+  D.map(get('review')),
 )
 
-function reviewForm(preprint: Preprint, form: Partial<ReviewForm>, error = false) {
+type ReviewForm = {
+  readonly review: E.Either<MissingE, NonEmptyString | undefined>
+}
+
+function reviewForm(preprint: Preprint, form: ReviewForm) {
+  const error = pipe(form, RR.some<E.Either<unknown, unknown>>(E.isLeft))
+
   return page({
     title: plainText`${error ? 'Error: ' : ''}Write your PREreview of “${preprint.title}”`,
     content: html`
@@ -77,21 +97,32 @@ function reviewForm(preprint: Preprint, form: Partial<ReviewForm>, error = false
                 <error-summary aria-labelledby="error-summary-title" role="alert">
                   <h2 id="error-summary-title">There is a problem</h2>
                   <ul>
-                    <li>
-                      <a href="#review">Enter your PREreview</a>
-                    </li>
+                    ${E.isLeft(form.review)
+                      ? html`
+                          <li>
+                            <a href="#review">
+                              ${match(form.review.left)
+                                .with({ _tag: 'MissingE' }, () => 'Enter your PREreview')
+                                .exhaustive()}
+                            </a>
+                          </li>
+                        `
+                      : ''}
                   </ul>
                 </error-summary>
               `
             : ''}
 
-          <div ${rawHtml(error ? 'class="error"' : '')}>
+          <div ${rawHtml(E.isLeft(form.review) ? 'class="error"' : '')}>
             <h1><label id="review-label" for="review">Write your PREreview</label></h1>
 
-            ${error
+            ${E.isLeft(form.review)
               ? html`
                   <div class="error-message" id="review-error">
-                    <span class="visually-hidden">Error:</span> Enter your PREreview
+                    <span class="visually-hidden">Error:</span>
+                    ${match(form.review.left)
+                      .with({ _tag: 'MissingE' }, () => 'Enter your PREreview')
+                      .exhaustive()}
                   </div>
                 `
               : ''}
@@ -101,9 +132,11 @@ function reviewForm(preprint: Preprint, form: Partial<ReviewForm>, error = false
                 id="review"
                 name="review"
                 rows="20"
-                ${rawHtml(error ? 'aria-invalid="true" aria-errormessage="review-error"' : '')}
+                ${rawHtml(E.isLeft(form.review) ? 'aria-invalid="true" aria-errormessage="review-error"' : '')}
               >
-${rawHtml(form.review ?? '')}</textarea
+${match(form.review)
+                  .with(E.right(P.select(P.string)), identity)
+                  .otherwise(() => '')}</textarea
               >
             </html-editor>
           </div>
