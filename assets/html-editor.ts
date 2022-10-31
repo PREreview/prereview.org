58273e578cc8bfbbc49bcb58384e0f32350e4bf4
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-10-31 14:08:17 +0000

    Don't wrap a web component in an async function
    Refs #421

--- assets/html-editor.ts
+++ assets/html-editor.ts
@@ -1,74 +1,74 @@
-void (async () => {
-  const [{ Editor }, { Link }, { Subscript }, { Superscript }, { Typography }, { default: StarterKit }] =
-    await Promise.all([
-      import('@tiptap/core'),
-      import('@tiptap/extension-link'),
-      import('@tiptap/extension-subscript'),
-      import('@tiptap/extension-superscript'),
-      import('@tiptap/extension-typography'),
-      import('@tiptap/starter-kit'),
-    ])
+class HtmlEditor extends HTMLElement {
+  static element = 'html-editor' as const
 
-  class HtmlEditor extends HTMLElement {
-    static element = 'html-editor' as const
-
-    constructor() {
-      super()
-
-      const textArea = this.firstElementChild
-
-      if (!(textArea instanceof HTMLTextAreaElement)) {
-        throw new Error('No text area')
-      }
-
-      const editor = new Editor({
-        editorProps: {
-          attributes: {
-            'aria-labelledby': replaceLabels(textArea)
-              .map(label => label.id)
-              .join(' '),
-            'aria-multiline': 'true',
-            id: textArea.id,
-            role: 'textbox',
-            ...extractAttributes(textArea, ['aria-errormessage', 'aria-invalid']),
-          },
-        },
-        extensions: [
-          StarterKit.configure({
-            blockquote: false,
-            code: false,
-            codeBlock: false,
-            hardBreak: false,
-            heading: {
-              levels: [1, 2, 3],
-            },
-            horizontalRule: false,
-            strike: false,
-          }),
-          Link.configure({
-            openOnClick: false,
-          }),
-          Subscript,
-          Superscript,
-          Typography,
-        ],
-        content: new DOMParser().parseFromString(textArea.innerHTML, 'text/html').documentElement.textContent,
-      })
-
-      editor.on('update', ({ editor }) => {
-        const html = editor.getHTML()
-
-        textArea.innerText = html !== '<p></p>' ? html : ''
-      })
-
-      textArea.insertAdjacentElement('afterend', editor.options.element)
-      textArea.hidden = true
-      removeAttributes(textArea, ['aria-errormessage', 'aria-invalid', 'id'])
-    }
+  constructor() {
+    super()
   }
 
-  window.customElements.define(HtmlEditor.element, HtmlEditor)
-})()
+  async connectedCallback() {
+    const [{ Editor }, { Link }, { Subscript }, { Superscript }, { Typography }, { default: StarterKit }] =
+      await Promise.all([
+        import('@tiptap/core'),
+        import('@tiptap/extension-link'),
+        import('@tiptap/extension-subscript'),
+        import('@tiptap/extension-superscript'),
+        import('@tiptap/extension-typography'),
+        import('@tiptap/starter-kit'),
+      ])
+
+    const textArea = this.firstElementChild
+
+    if (!(textArea instanceof HTMLTextAreaElement)) {
+      throw new Error('No text area')
+    }
+
+    const editor = new Editor({
+      editorProps: {
+        attributes: {
+          'aria-labelledby': replaceLabels(textArea)
+            .map(label => label.id)
+            .join(' '),
+          'aria-multiline': 'true',
+          id: textArea.id,
+          role: 'textbox',
+          ...extractAttributes(textArea, ['aria-errormessage', 'aria-invalid']),
+        },
+      },
+      extensions: [
+        StarterKit.configure({
+          blockquote: false,
+          code: false,
+          codeBlock: false,
+          hardBreak: false,
+          heading: {
+            levels: [1, 2, 3],
+          },
+          horizontalRule: false,
+          strike: false,
+        }),
+        Link.configure({
+          openOnClick: false,
+        }),
+        Subscript,
+        Superscript,
+        Typography,
+      ],
+      content: new DOMParser().parseFromString(textArea.innerHTML, 'text/html').documentElement.textContent,
+    })
+
+    editor.on('update', ({ editor }) => {
+      const html = editor.getHTML()
+
+      textArea.innerText = html !== '<p></p>' ? html : ''
+    })
+
+    textArea.insertAdjacentElement('afterend', editor.options.element)
+    textArea.hidden = true
+    removeAttributes(textArea, ['aria-errormessage', 'aria-invalid', 'id'])
+  }
+}
+
+window.customElements.define(HtmlEditor.element, HtmlEditor)
 
 function replaceLabels(source: HTMLTextAreaElement) {
   return [...source.labels].map(label => {
