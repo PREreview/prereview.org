c4392f916bc55b6541053bd3514713a4e9cd964c
Chris Wilkinson <c.wilkinson@elifesciences.org>
2022-11-14 15:20:54 +0000

    Provide some details on what makes a good review when writing
    This commit adds some details to the 'Write your PREreview' form, which should complement the template (and will still be visible as the user writes their review).
    
    The text will need some iteration, but this is a good start.
    
    Refs #441, eea0b81c7903bb780830bd32d4e144f0f34a5df1

--- src/write-review/write-review-review.ts
+++ src/write-review/write-review-review.ts
@@ -223,6 +223,38 @@ function writeReviewForm(preprint: Preprint, form: WriteReviewForm) {
           <div ${rawHtml(E.isLeft(form.review) ? 'class="error"' : '')}>
             <h1><label id="review-label" for="review">Write your PREreview</label></h1>
 
+            <div id="review-tip" role="note">
+              We want to support you in contributing high-quality feedback on PREreview. Check out our
+              <a href="https://content.prereview.org/resources/">tips and resources for reviewers</a>.
+            </div>
+
+            <details>
+              <summary>Examples of good reviewer behavior</summary>
+
+              <div>
+                <ul>
+                  <li>Being respectful of the authors and their work.</li>
+                  <li>Being humble and aware of how you would like to receive feedback from others.</li>
+                  <li>Giving clear, constructive, and actionable feedback that can improve the preprint.</li>
+                </ul>
+              </div>
+            </details>
+
+            <details>
+              <summary>Examples of helpful review sections</summary>
+
+              <div>
+                <ol>
+                  <li>Begin with a summary of the research and how it contributes to the field of study.</li>
+                  <li>Next, share your positive feedback, including the approach’s strengths and results.</li>
+                  <li>
+                    Finally, share major and minor concerns and related clear, constructive, and actionable suggestions
+                    for addressing them.
+                  </li>
+                </ol>
+              </div>
+            </details>
+
             ${E.isLeft(form.review)
               ? html`
                   <div class="error-message" id="review-error">
@@ -239,14 +271,16 @@ function writeReviewForm(preprint: Preprint, form: WriteReviewForm) {
                 .with(
                   E.right(undefined),
                   () => html`
-                    <textarea id="review" name="review" rows="20">${template}</textarea>
+                    <textarea id="review" name="review" rows="20" aria-describedby="review-tip">${template}</textarea>
                     <textarea hidden disabled>${markdownIt().render(template)}</textarea>
                   `,
                 )
                 .with(
                   E.right(P.select(P.not(undefined))),
                   review => html`
-                    <textarea id="review" name="review" rows="20">${turndown.turndown(review.toString())}</textarea>
+                    <textarea id="review" name="review" rows="20" aria-describedby="review-tip">
+${turndown.turndown(review.toString())}</textarea
+                    >
                     <textarea hidden disabled>${review}</textarea>
                   `,
                 )
@@ -257,6 +291,7 @@ function writeReviewForm(preprint: Preprint, form: WriteReviewForm) {
                       id="review"
                       name="review"
                       rows="20"
+                      aria-describedby="review-tip"
                       aria-invalid="true"
                       aria-errormessage="review-error"
                     ></textarea>
