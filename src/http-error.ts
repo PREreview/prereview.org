9ae97485ac992d2991ea743d9f779276939a47f1
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-01-24 15:59:39 +0000

    Add broken link details to the not-found page
    Refs #607, https://design-system.service.gov.uk/patterns/page-not-found-pages/

--- src/http-error.ts
+++ src/http-error.ts
@@ -30,6 +30,11 @@ function notFoundPage() {
         <p>If you typed the web address, check it is correct.</p>
 
         <p>If you pasted the web address, check you copied the entire address.</p>
+
+        <p>
+          If the web address is correct or you selected a link or button, please
+          <a href="mailto:contact@prereview.org">get in touch</a>.
+        </p>
       </main>
     `,
     skipLinks: [[html`Skip to main content`, '#main-content']],
