1ca07f40a83aa090d5f6fba29b4b71b8f708291c
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-02-24 11:57:04 +0000

    Test the SkipLink component
    Refs #687

---
+++ test/assets/skip-link.test.ts
@@ -0,0 +1,15 @@
+import { defineCE, expect, fixture } from '@open-wc/testing'
+import * as _ from '../../assets/skip-link'
+
+describe('when a link is clicked', () => {
+  it('sets focus to the target', async () => {
+    const element = defineCE(class extends _.SkipLink {})
+    const skipLink = await fixture<_.SkipLink>(`<${element}><a href="#target"/></${element}>`)
+    const target = await fixture<HTMLDivElement>('<div id="target"/>')
+
+    skipLink.querySelector('a')?.click()
+
+    expect(target).to.have.focus
+    expect(skipLink.ownerDocument.location.hash).to.be.empty
+  })
+})
