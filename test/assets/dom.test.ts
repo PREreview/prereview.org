ceb675fdb548a073c043f4701b6aef12841b6bc7
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-02-24 10:57:41 +0000

    Test the forceFocus function
    Refs #687

--- test/assets/dom.test.ts
+++ test/assets/dom.test.ts
@@ -1,4 +1,4 @@
-import { expect } from '@open-wc/testing'
+import { expect, fixture } from '@open-wc/testing'
 import * as _ from '../../assets/dom'
 
 describe('preventDefault', () => {
@@ -8,5 +8,37 @@ describe('preventDefault', () => {
     _.preventDefault(event)
 
     expect(event.defaultPrevented).to.be.true
+  })
+})
+
+describe('forceFocus', () => {
+  describe('when the element has a tabindex', () => {
+    it('leaves the tabindex unchanged', async () => {
+      const element = await fixture<HTMLDivElement>('<div tabindex="2"></div>')
+
+      _.forceFocus(element)
+
+      expect(element).to.have.attribute('tabindex', '2')
+      expect(element).to.have.focus
+
+      element.blur()
+
+      expect(element).to.have.attribute('tabindex', '2')
+    })
+  })
+
+  describe('when the element does not have a tabindex', () => {
+    it('sets a temporary tabindex', async () => {
+      const element = await fixture<HTMLDivElement>('<div></div>')
+
+      _.forceFocus(element)
+
+      expect(element).to.have.attribute('tabindex', '-1')
+      expect(element).to.have.focus
+
+      element.blur()
+
+      expect(element).not.to.have.attribute('tabindex')
+    })
   })
 })
