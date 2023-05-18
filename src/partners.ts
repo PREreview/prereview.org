ef5e916bad97c37e8398c30a542071dd816bfd9d
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-05-18 13:29:06 +0100

    Don't delay loading the images
    I copied the 'loading' attribute from images on the homepage, but this isn't suitable here as they appear at the top of the page.
    
    Refs #931, 4e4cb9f357556df16bb7514e7fd62a9d1938d1f8

--- src/partners.ts
+++ src/partners.ts
@@ -27,49 +27,37 @@ function createPage(user?: User) {
         <ol class="logos">
           <li>
             <a href="https://info.africarxiv.org/">
-              <img src="${assets['africarxiv.svg']}" width="518" height="551" loading="lazy" alt="AfricArXiv" />
+              <img src="${assets['africarxiv.svg']}" width="518" height="551" alt="AfricArXiv" />
             </a>
           </li>
           <li>
             <a href="https://asapbio.org/">
-              <img src="${assets['asapbio.svg']}" width="1851" height="308" loading="lazy" alt="ASAPbio" />
+              <img src="${assets['asapbio.svg']}" width="1851" height="308" alt="ASAPbio" />
             </a>
           </li>
           <li>
             <a href="https://www.coar-repositories.org/">
-              <img src="${assets['coar.svg']}" width="440" height="343" loading="lazy" alt="COAR" />
+              <img src="${assets['coar.svg']}" width="440" height="343" alt="COAR" />
             </a>
           </li>
           <li>
             <a href="https://www.cshl.edu/">
-              <img
-                src="${assets['cshl.svg']}"
-                width="280"
-                height="134"
-                loading="lazy"
-                alt="Cold Spring Harbor Laboratory"
-              />
+              <img src="${assets['cshl.svg']}" width="280" height="134" alt="Cold Spring Harbor Laboratory" />
             </a>
           </li>
           <li>
             <a href="https://eiderafricaltd.org/">
-              <img src="${assets['eider-africa.svg']}" width="895" height="927" loading="lazy" alt="Eider Africa" />
+              <img src="${assets['eider-africa.svg']}" width="895" height="927" alt="Eider Africa" />
             </a>
           </li>
           <li>
             <a href="https://elifesciences.org/">
-              <img src="${assets['elife.svg']}" width="129" height="44" loading="lazy" alt="eLife" />
+              <img src="${assets['elife.svg']}" width="129" height="44" alt="eLife" />
             </a>
           </li>
           <li>
             <a href="https://www.healthra.org/">
-              <img
-                src="${assets['healthra.svg']}"
-                width="564"
-                height="224"
-                loading="lazy"
-                alt="Health Research Alliance"
-              />
+              <img src="${assets['healthra.svg']}" width="564" height="224" alt="Health Research Alliance" />
             </a>
           </li>
           <li>
@@ -78,7 +66,6 @@ function createPage(user?: User) {
                 src="${assets['ofrg.svg']}"
                 width="1327"
                 height="311"
-                loading="lazy"
                 alt="Open Research Funders Group"
                 class="wide"
               />
@@ -86,17 +73,17 @@ function createPage(user?: User) {
           </li>
           <li>
             <a href="https://www.peerref.com/">
-              <img src="${assets['peerref.svg']}" width="2019" height="326" loading="lazy" alt="PeerRef" />
+              <img src="${assets['peerref.svg']}" width="2019" height="326" alt="PeerRef" />
             </a>
           </li>
           <li>
             <a href="https://scielo.org/">
-              <img src="${assets['scielo.svg']}" width="538" height="562" loading="lazy" alt="SciELO" />
+              <img src="${assets['scielo.svg']}" width="538" height="562" alt="SciELO" />
             </a>
           </li>
           <li>
             <a href="https://sciety.org/">
-              <img src="${assets['sciety.svg']}" width="119" height="36" loading="lazy" alt="Sciety" />
+              <img src="${assets['sciety.svg']}" width="119" height="36" alt="Sciety" />
             </a>
           </li>
         </ol>
