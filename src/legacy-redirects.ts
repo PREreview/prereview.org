9c311af0ba666c323295b38b2ea69ad0ff0de1b9
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-05-25 16:12:31 +0100

    Generate new paths safely
    Refs #961

--- src/legacy-redirects.ts
+++ src/legacy-redirects.ts
@@ -1,6 +1,14 @@
+import type { Doi } from 'doi-ts'
 import express from 'express'
+import { format } from 'fp-ts-routing'
 import { Status } from 'hyper-ts'
+import { preprintReviewsMatch } from './routes'
 
 export const legacyRedirects = express().use(/^\/preprints\/arxiv-([A-z0-9.+-]+?)(?:v[0-9]+)?$/, (req, res) => {
-  res.redirect(Status.MovedPermanently, `/preprints/doi-10.48550-arxiv.${req.params['0']}`)
+  res.redirect(
+    Status.MovedPermanently,
+    format(preprintReviewsMatch.formatter, {
+      id: { type: 'arxiv', value: `10.48550/arxiv.${req.params['0']}` as Doi<'48550'> },
+    }),
+  )
 })
