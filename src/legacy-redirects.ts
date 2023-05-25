a09a5cf8de7fc929d7824a337446deb81918bbe1
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-05-25 16:14:16 +0100

    Redirect legacy paths
    Refs #961

--- src/legacy-redirects.ts
+++ src/legacy-redirects.ts
@@ -2,13 +2,56 @@ import type { Doi } from 'doi-ts'
 import express from 'express'
 import { format } from 'fp-ts-routing'
 import { Status } from 'hyper-ts'
-import { preprintReviewsMatch } from './routes'
+import { isUuid } from 'uuid-ts'
+import {
+  findAPreprintMatch,
+  logInMatch,
+  logOutMatch,
+  preprintReviewsMatch,
+  preprintReviewsUuidMatch,
+  reviewAPreprintMatch,
+  reviewsMatch,
+} from './routes'
 
-export const legacyRedirects = express().use(/^\/preprints\/arxiv-([A-z0-9.+-]+?)(?:v[0-9]+)?$/, (req, res) => {
-  res.redirect(
-    Status.MovedPermanently,
-    format(preprintReviewsMatch.formatter, {
-      id: { type: 'arxiv', value: `10.48550/arxiv.${req.params['0']}` as Doi<'48550'> },
-    }),
-  )
-})
+export const legacyRedirects = express()
+  .use('/login', (req, res) => {
+    res.redirect(Status.MovedPermanently, format(logInMatch.formatter, {}))
+  })
+  .use('/logout', (req, res) => {
+    res.redirect(Status.MovedPermanently, format(logOutMatch.formatter, {}))
+  })
+  .use(/^\/preprints\/arxiv-([A-z0-9.+-]+?)(?:v[0-9]+)?$/, (req, res) => {
+    res.redirect(
+      Status.MovedPermanently,
+      format(preprintReviewsMatch.formatter, {
+        id: { type: 'arxiv', value: `10.48550/arxiv.${req.params['0']}` as Doi<'48550'> },
+      }),
+    )
+  })
+  .use(/^\/preprints\/([A-z0-9-]+)\/full-reviews/, (req, res, next) => {
+    if (isUuid(req.params['0'])) {
+      res.redirect(Status.MovedPermanently, format(preprintReviewsUuidMatch.formatter, { uuid: req.params['0'] }))
+    } else {
+      next()
+    }
+  })
+  .use('/reviews/new', (req, res) => {
+    res.redirect(Status.MovedPermanently, format(reviewAPreprintMatch.formatter, {}))
+  })
+  .use(/^\/reviews$/, (req, res, next) => {
+    if (!req.query.page) {
+      res.redirect(Status.MovedPermanently, format(reviewsMatch.formatter, { page: 1 }))
+    } else {
+      next()
+    }
+  })
+  .use(/^\/validate\/([A-z0-9-]+)$/, (req, res, next) => {
+    if (isUuid(req.params['0'])) {
+      res.redirect(Status.MovedPermanently, format(preprintReviewsUuidMatch.formatter, { uuid: req.params['0'] }))
+    } else {
+      next()
+    }
+  })
+  .use('/validate', (req, res) => {
+    res.redirect(Status.MovedPermanently, format(findAPreprintMatch.formatter, {}))
+  })
--- test/legacy-redirects.test.ts
+++ test/legacy-redirects.test.ts
@@ -5,9 +5,22 @@ import supertest from 'supertest'
 import * as _ from '../src/legacy-redirects'
 
 test.each([
+  ['/login', '/log-in'],
+  ['/logout', '/log-out'],
   ['/preprints/arxiv-2204.09673', '/preprints/doi-10.48550-arxiv.2204.09673'],
   ['/preprints/arxiv-1312.0906', '/preprints/doi-10.48550-arxiv.1312.0906'],
-])('legacyRedirects', async (path, expected) => {
+  [
+    '/preprints/a5ff6309-cba7-4eb3-9e2c-b1eb4c391983/full-reviews/e7dc4769-827b-4b79-b38e-b0cf22758ec5',
+    '/preprints/a5ff6309-cba7-4eb3-9e2c-b1eb4c391983',
+  ],
+  [
+    '/preprints/96da7e82-2e5a-433b-b72e-2b5726220fe7/full-reviews/bdfb53db-491d-45c5-880f-31c88173ed28',
+    '/preprints/96da7e82-2e5a-433b-b72e-2b5726220fe7',
+  ],
+  ['/reviews', '/reviews?page=1'],
+  ['/reviews/new', '/review-a-preprint'],
+  ['/validate/838df174-081f-4701-b314-cf568c8d6839', '/preprints/838df174-081f-4701-b314-cf568c8d6839'],
+])('legacyRedirects (%s)', async (path, expected) => {
   const response = await supertest(_.legacyRedirects).get(path)
 
   expect(response.status).toStrictEqual(Status.MovedPermanently)
