f2153b13a3e3e2473a76f8f4c12efd6f8698942d
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-06-30 09:43:09 +0100

    Add a type for a club's PREreviews
    Note that the data is never successfully loaded.
    
    Refs #1051, 1fad194c72afe4d8b2e0c96b1f0bc3dc37021bca

--- src/club.ts
+++ src/club.ts
@@ -1,16 +1,41 @@
+import { Temporal } from '@js-temporal/polyfill'
+import { format } from 'fp-ts-routing'
+import type { Reader } from 'fp-ts/Reader'
 import * as RTE from 'fp-ts/ReaderTaskEither'
+import * as RA from 'fp-ts/ReadonlyArray'
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
 import type * as TE from 'fp-ts/TaskEither'
 import * as b from 'fp-ts/boolean'
 import { flow, pipe } from 'fp-ts/function'
-import { Status } from 'hyper-ts'
+import { Status, type StatusOpen } from 'hyper-ts'
 import * as RM from 'hyper-ts/lib/ReaderMiddleware'
+import type { LanguageCode } from 'iso-639-1'
+import { getLangDir } from 'rtl-detect'
 import { match } from 'ts-pattern'
 import { canSeeClubs } from './feature-flags'
-import { sendHtml } from './html'
+import { type Html, html, plainText, rawHtml, sendHtml } from './html'
 import { notFound, serviceUnavailable } from './middleware'
+import { page } from './page'
+import type { PreprintId } from './preprint-id'
+import { reviewMatch } from './routes'
+import { renderDate } from './time'
+import { type User, maybeGetUser } from './user'
+
+import PlainDate = Temporal.PlainDate
+
+export type Prereviews = ReadonlyArray<{
+  readonly id: number
+  readonly reviewers: RNEA.ReadonlyNonEmptyArray<string>
+  readonly published: PlainDate
+  readonly preprint: {
+    readonly id: PreprintId
+    readonly language: LanguageCode
+    readonly title: Html
+  }
+}>
 
 export interface GetPrereviewsEnv {
-  getPrereviews: (id: 'asapbio-metabolism') => TE.TaskEither<'unavailable', never>
+  getPrereviews: (id: 'asapbio-metabolism') => TE.TaskEither<'unavailable', Prereviews>
 }
 
 const getPrereviews = (id: 'asapbio-metabolism') =>
@@ -30,13 +55,124 @@ export const club = (id: 'asapbio-metabolism') =>
     ),
   )
 
-const showClubPage = flow(
-  RM.fromReaderTaskEitherK(getPrereviews),
-  RM.ichainFirst(() => RM.status(Status.OK)),
-  RM.ichainMiddlewareKW(sendHtml),
-  RM.orElseW(error =>
-    match(error)
-      .with('unavailable', () => serviceUnavailable)
-      .exhaustive(),
-  ),
-)
+const showClubPage = (id: 'asapbio-metabolism') =>
+  pipe(
+    RM.fromReaderTaskEither(getPrereviews(id)),
+    RM.bindTo('prereviews'),
+    RM.apSW(
+      'name',
+      RM.of(
+        match(id)
+          .with('asapbio-metabolism', () => 'ASAPbio Metabolism Crowd')
+          .exhaustive(),
+      ),
+    ),
+    RM.apSW('user', maybeGetUser),
+    chainReaderKW(createPage),
+    RM.ichainFirst(() => RM.status(Status.OK)),
+    RM.ichainMiddlewareKW(sendHtml),
+    RM.orElseW(error =>
+      match(error)
+        .with('unavailable', () => serviceUnavailable)
+        .exhaustive(),
+    ),
+  )
+
+function createPage({ name, prereviews, user }: { name: string; prereviews: Prereviews; user?: User }) {
+  return page({
+    title: plainText`${name}’s PREreviews`,
+    content: html`
+      <main id="main-content">
+        <h1>${name}’s PREreviews</h1>
+
+        ${pipe(
+          prereviews,
+          RA.match(
+            () => html`
+              <div class="inset">
+                <p>The ${name} hasn’t published a PREreview yet.</p>
+
+                <p>When they do, it’ll appear here.</p>
+              </div>
+            `,
+            prereviews => html`
+              <ol class="cards">
+                ${prereviews.map(
+                  prereview => html`
+                    <li>
+                      <article>
+                        <a href="${format(reviewMatch.formatter, { id: prereview.id })}">
+                          ${formatList('en')(prereview.reviewers)} reviewed
+                          <cite dir="${getLangDir(prereview.preprint.language)}" lang="${prereview.preprint.language}"
+                            >${prereview.preprint.title}</cite
+                          >
+                        </a>
+
+                        <dl>
+                          <dt>Review published</dt>
+                          <dd>${renderDate(prereview.published)}</dd>
+                          <dt>Preprint server</dt>
+                          <dd>
+                            ${match(prereview.preprint.id.type)
+                              .with('africarxiv', () => 'AfricArXiv Preprints')
+                              .with('arxiv', () => 'arXiv')
+                              .with('biorxiv', () => 'bioRxiv')
+                              .with('chemrxiv', () => 'ChemRxiv')
+                              .with('eartharxiv', () => 'EarthArXiv')
+                              .with('ecoevorxiv', () => 'EcoEvoRxiv')
+                              .with('edarxiv', () => 'EdArXiv')
+                              .with('engrxiv', () => 'engrXiv')
+                              .with('medrxiv', () => 'medRxiv')
+                              .with('metaarxiv', () => 'MetaArXiv')
+                              .with('osf', () => 'OSF Preprints')
+                              .with('philsci', () => 'PhilSci-Archive')
+                              .with('preprints.org', () => 'Preprints.org')
+                              .with('psyarxiv', () => 'PsyArXiv')
+                              .with('research-square', () => 'Research Square')
+                              .with('scielo', () => 'SciELO Preprints')
+                              .with('science-open', () => 'ScienceOpen Preprints')
+                              .with('socarxiv', () => 'SocArXiv')
+                              .with('zenodo', () => 'Zenodo')
+                              .exhaustive()}
+                          </dd>
+                        </dl>
+                      </article>
+                    </li>
+                  `,
+                )}
+              </ol>
+            `,
+          ),
+        )}
+      </main>
+    `,
+    skipLinks: [[html`Skip to main content`, '#main-content']],
+    user,
+  })
+}
+
+function formatList(
+  ...args: ConstructorParameters<typeof Intl.ListFormat>
+): (list: RNEA.ReadonlyNonEmptyArray<Html | string>) => Html {
+  const formatter = new Intl.ListFormat(...args)
+
+  return flow(
+    RNEA.map(item => html`<b>${item}</b>`.toString()),
+    list => formatter.format(list),
+    rawHtml,
+  )
+}
+
+// https://github.com/DenisFrezzato/hyper-ts/pull/85
+function fromReaderK<R, A extends ReadonlyArray<unknown>, B, I = StatusOpen, E = never>(
+  f: (...a: A) => Reader<R, B>,
+): (...a: A) => RM.ReaderMiddleware<R, I, I, E, B> {
+  return (...a) => RM.rightReader(f(...a))
+}
+
+// https://github.com/DenisFrezzato/hyper-ts/pull/85
+function chainReaderKW<R2, A, B>(
+  f: (a: A) => Reader<R2, B>,
+): <R1, I, E>(ma: RM.ReaderMiddleware<R1, I, I, E, A>) => RM.ReaderMiddleware<R1 & R2, I, I, E, B> {
+  return RM.chainW(fromReaderK(f))
+}
--- test/club.test.ts
+++ test/club.test.ts
@@ -1,35 +1,71 @@
 import { test } from '@fast-check/jest'
-import { describe, expect } from '@jest/globals'
+import { describe, expect, jest } from '@jest/globals'
 import * as E from 'fp-ts/Either'
 import * as TE from 'fp-ts/TaskEither'
 import { MediaType, Status } from 'hyper-ts'
 import * as M from 'hyper-ts/lib/Middleware'
+import type { Mock } from 'jest-mock'
 import * as _ from '../src/club'
 import * as fc from './fc'
 import { runMiddleware } from './middleware'
 
 describe('club', () => {
-  test.prop([
-    fc.connection({ method: fc.requestMethod().filter(method => method !== 'POST') }),
-    fc.either(fc.constant('no-session' as const), fc.user()),
-  ])('when clubs can be seen', async (connection, user) => {
-    const actual = await runMiddleware(
-      _.club('asapbio-metabolism')({
-        canSeeClubs: true,
-        getPrereviews: () => TE.left('unavailable'),
-        getUser: () => M.fromEither(user),
-      }),
-      connection,
-    )()
+  describe('when clubs can be seen', () => {
+    test.prop([
+      fc.connection({ method: fc.requestMethod().filter(method => method !== 'POST') }),
+      fc.either(fc.constant('no-session' as const), fc.user()),
+      fc.array(
+        fc.record({
+          id: fc.integer(),
+          reviewers: fc.nonEmptyArray(fc.string()),
+          published: fc.plainDate(),
+          preprint: fc.preprintTitle(),
+        }),
+      ),
+    ])('when the data can be loaded', async (connection, user, prereviews) => {
+      const getPrereviews: Mock<_.GetPrereviewsEnv['getPrereviews']> = jest.fn(_ => TE.right(prereviews))
 
-    expect(actual).toStrictEqual(
-      E.right([
-        { type: 'setStatus', status: Status.ServiceUnavailable },
-        { type: 'setHeader', name: 'Cache-Control', value: 'no-store, must-revalidate' },
-        { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
-        { type: 'setBody', body: expect.anything() },
-      ]),
-    )
+      const actual = await runMiddleware(
+        _.club('asapbio-metabolism')({
+          canSeeClubs: true,
+          getPrereviews,
+          getUser: () => M.fromEither(user),
+        }),
+        connection,
+      )()
+
+      expect(actual).toStrictEqual(
+        E.right([
+          { type: 'setStatus', status: Status.OK },
+          { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
+          { type: 'setBody', body: expect.anything() },
+        ]),
+      )
+      expect(getPrereviews).toHaveBeenCalledWith('asapbio-metabolism')
+    })
+
+    test.prop([
+      fc.connection({ method: fc.requestMethod().filter(method => method !== 'POST') }),
+      fc.either(fc.constant('no-session' as const), fc.user()),
+    ])('when the PREreviews are unavailable', async (connection, user) => {
+      const actual = await runMiddleware(
+        _.club('asapbio-metabolism')({
+          canSeeClubs: true,
+          getPrereviews: () => TE.left('unavailable'),
+          getUser: () => M.fromEither(user),
+        }),
+        connection,
+      )()
+
+      expect(actual).toStrictEqual(
+        E.right([
+          { type: 'setStatus', status: Status.ServiceUnavailable },
+          { type: 'setHeader', name: 'Cache-Control', value: 'no-store, must-revalidate' },
+          { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
+          { type: 'setBody', body: expect.anything() },
+        ]),
+      )
+    })
   })
 
   test.prop([
