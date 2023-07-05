725c12e293045dbce2ffb55f05e33b49d4be5853
Sean Wiseman <seanwiseman2012@gmail.com>
2023-07-05 11:32:53 +0100

    Improve user journey for unauthenticated users accessing the form.
    - ask them to login
    - redirect them to my-details after login
    
    Refs #1069

--- src/change-career-stage.ts
+++ src/change-career-stage.ts
@@ -2,14 +2,23 @@ import * as b from 'fp-ts/boolean'
 import { pipe } from 'fp-ts/function'
 import * as RM from 'hyper-ts/lib/ReaderMiddleware'
 import { canEditProfile } from './feature-flags'
+import { logInAndRedirect } from './log-in'
 import { notFound, serviceUnavailable } from './middleware'
+import { myDetailsMatch } from './routes'
+import { getUser } from './user'
 
 export const changeCareerStage = pipe(
   RM.rightReader(canEditProfile),
   RM.ichainW(
     b.match(
       () => notFound,
-      () => serviceUnavailable,
+      () => showChangeCareerStage,
     ),
   ),
+)
+
+const showChangeCareerStage = pipe(
+  getUser,
+  RM.ichainW(() => serviceUnavailable),
+  RM.orElseW(() => logInAndRedirect(myDetailsMatch.formatter, {})),
 )
--- test/change-career-stage.test.ts
+++ test/change-career-stage.test.ts
@@ -1,48 +1,109 @@
 import { test } from '@fast-check/jest'
 import { describe, expect } from '@jest/globals'
+import { format } from 'fp-ts-routing'
 import * as E from 'fp-ts/Either'
 import { MediaType, Status } from 'hyper-ts'
 import * as M from 'hyper-ts/lib/Middleware'
 import * as _ from '../src/change-career-stage'
+import { myDetailsMatch } from '../src/routes'
 import * as fc from './fc'
 import { runMiddleware } from './middleware'
 
 describe('changeCareerStage', () => {
-  test.prop([fc.connection(), fc.either(fc.constant('no-session' as const), fc.user())])(
-    'when profiles can be edited',
-    async (connection, user) => {
-      const actual = await runMiddleware(
-        _.changeCareerStage({ canEditProfile: true, getUser: () => M.fromEither(user) }),
-        connection,
-      )()
+  test.prop([
+    fc.record({
+      authorizeUrl: fc.url(),
+      clientId: fc.string(),
+      clientSecret: fc.string(),
+      redirectUri: fc.url(),
+      tokenUrl: fc.url(),
+    }),
+    fc.origin(),
+    fc.connection(),
+    fc.user(),
+  ])('when profiles can be edited and there is a logged in user', async (oauth, publicUrl, connection, user) => {
+    const actual = await runMiddleware(
+      _.changeCareerStage({ canEditProfile: true, getUser: () => M.fromEither(E.right(user)), publicUrl, oauth }),
+      connection,
+    )()
 
-      expect(actual).toStrictEqual(
-        E.right([
-          { type: 'setStatus', status: Status.ServiceUnavailable },
-          { type: 'setHeader', name: 'Cache-Control', value: 'no-store, must-revalidate' },
-          { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
-          { type: 'setBody', body: expect.anything() },
-        ]),
-      )
-    },
-  )
+    expect(actual).toStrictEqual(
+      E.right([
+        { type: 'setStatus', status: Status.ServiceUnavailable },
+        { type: 'setHeader', name: 'Cache-Control', value: 'no-store, must-revalidate' },
+        { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
+        { type: 'setBody', body: expect.anything() },
+      ]),
+    )
+  })
 
-  test.prop([fc.connection(), fc.either(fc.constant('no-session' as const), fc.user())])(
-    "when profiles can't be edited",
-    async (connection, user) => {
-      const actual = await runMiddleware(
-        _.changeCareerStage({ canEditProfile: false, getUser: () => M.fromEither(user) }),
-        connection,
-      )()
+  test.prop([
+    fc.record({
+      authorizeUrl: fc.url(),
+      clientId: fc.string(),
+      clientSecret: fc.string(),
+      redirectUri: fc.url(),
+      tokenUrl: fc.url(),
+    }),
+    fc.origin(),
+    fc.connection(),
+  ])('when profiles can be edited but the user is not logged in', async (oauth, publicUrl, connection) => {
+    const actual = await runMiddleware(
+      _.changeCareerStage({
+        canEditProfile: true,
+        getUser: () => M.fromEither(E.left('no-session')),
+        publicUrl,
+        oauth,
+      }),
+      connection,
+    )()
 
-      expect(actual).toStrictEqual(
-        E.right([
-          { type: 'setStatus', status: Status.NotFound },
-          { type: 'setHeader', name: 'Cache-Control', value: 'no-store, must-revalidate' },
-          { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
-          { type: 'setBody', body: expect.anything() },
-        ]),
-      )
-    },
-  )
+    expect(actual).toStrictEqual(
+      E.right([
+        { type: 'setStatus', status: Status.Found },
+        {
+          type: 'setHeader',
+          name: 'Location',
+          value: new URL(
+            `?${new URLSearchParams({
+              client_id: oauth.clientId,
+              response_type: 'code',
+              redirect_uri: oauth.redirectUri.href,
+              scope: '/authenticate',
+              state: new URL(format(myDetailsMatch.formatter, {}), publicUrl).toString(),
+            }).toString()}`,
+            oauth.authorizeUrl,
+          ).href,
+        },
+        { type: 'endResponse' },
+      ]),
+    )
+  })
+
+  test.prop([
+    fc.record({
+      authorizeUrl: fc.url(),
+      clientId: fc.string(),
+      clientSecret: fc.string(),
+      redirectUri: fc.url(),
+      tokenUrl: fc.url(),
+    }),
+    fc.origin(),
+    fc.connection(),
+    fc.either(fc.constant('no-session' as const), fc.user()),
+  ])("when profiles can't be edited", async (oauth, publicUrl, connection, user) => {
+    const actual = await runMiddleware(
+      _.changeCareerStage({ canEditProfile: false, getUser: () => M.fromEither(user), publicUrl, oauth }),
+      connection,
+    )()
+
+    expect(actual).toStrictEqual(
+      E.right([
+        { type: 'setStatus', status: Status.NotFound },
+        { type: 'setHeader', name: 'Cache-Control', value: 'no-store, must-revalidate' },
+        { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
+        { type: 'setBody', body: expect.anything() },
+      ]),
+    )
+  })
 })
