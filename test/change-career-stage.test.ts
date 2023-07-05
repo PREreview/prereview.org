4aff88ef157c8de0e5a41248573a3f0fe0586932
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-07-05 14:35:33 +0100

    Reduce the length of test names with groups
    Refs #1069

--- test/change-career-stage.test.ts
+++ test/change-career-stage.test.ts
@@ -10,46 +10,45 @@ import * as fc from './fc'
 import { runMiddleware } from './middleware'
 
 describe('changeCareerStage', () => {
-  test.prop([
-    fc.record({
-      authorizeUrl: fc.url(),
-      clientId: fc.string(),
-      clientSecret: fc.string(),
-      redirectUri: fc.url(),
-      tokenUrl: fc.url(),
-    }),
-    fc.origin(),
-    fc.connection({ method: fc.requestMethod().filter(method => method !== 'POST') }),
-    fc.user(),
-  ])('when profiles can be edited and there is a logged in user', async (oauth, publicUrl, connection, user) => {
-    const actual = await runMiddleware(
-      _.changeCareerStage({ canEditProfile: true, getUser: () => M.fromEither(E.right(user)), publicUrl, oauth }),
-      connection,
-    )()
+  describe('when profiles can be edited', () => {
+    test.prop([
+      fc.record({
+        authorizeUrl: fc.url(),
+        clientId: fc.string(),
+        clientSecret: fc.string(),
+        redirectUri: fc.url(),
+        tokenUrl: fc.url(),
+      }),
+      fc.origin(),
+      fc.connection({ method: fc.requestMethod().filter(method => method !== 'POST') }),
+      fc.user(),
+    ])('when there is a logged in user', async (oauth, publicUrl, connection, user) => {
+      const actual = await runMiddleware(
+        _.changeCareerStage({ canEditProfile: true, getUser: () => M.fromEither(E.right(user)), publicUrl, oauth }),
+        connection,
+      )()
 
-    expect(actual).toStrictEqual(
-      E.right([
-        { type: 'setStatus', status: Status.OK },
-        { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
-        { type: 'setBody', body: expect.anything() },
-      ]),
-    )
-  })
+      expect(actual).toStrictEqual(
+        E.right([
+          { type: 'setStatus', status: Status.OK },
+          { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
+          { type: 'setBody', body: expect.anything() },
+        ]),
+      )
+    })
 
-  test.prop([
-    fc.record({
-      authorizeUrl: fc.url(),
-      clientId: fc.string(),
-      clientSecret: fc.string(),
-      redirectUri: fc.url(),
-      tokenUrl: fc.url(),
-    }),
-    fc.origin(),
-    fc.connection({ method: fc.constant('POST') }),
-    fc.user(),
-  ])(
-    'when profiles can be edited and there is a logged in user and the form has been submitted',
-    async (oauth, publicUrl, connection, user) => {
+    test.prop([
+      fc.record({
+        authorizeUrl: fc.url(),
+        clientId: fc.string(),
+        clientSecret: fc.string(),
+        redirectUri: fc.url(),
+        tokenUrl: fc.url(),
+      }),
+      fc.origin(),
+      fc.connection({ method: fc.constant('POST') }),
+      fc.user(),
+    ])('when the form has been submitted', async (oauth, publicUrl, connection, user) => {
       const actual = await runMiddleware(
         _.changeCareerStage({ canEditProfile: true, getUser: () => M.right(user), publicUrl, oauth }),
         connection,
@@ -63,50 +62,50 @@ describe('changeCareerStage', () => {
           { type: 'setBody', body: expect.anything() },
         ]),
       )
-    },
-  )
+    })
 
-  test.prop([
-    fc.record({
-      authorizeUrl: fc.url(),
-      clientId: fc.string(),
-      clientSecret: fc.string(),
-      redirectUri: fc.url(),
-      tokenUrl: fc.url(),
-    }),
-    fc.origin(),
-    fc.connection(),
-  ])('when profiles can be edited but the user is not logged in', async (oauth, publicUrl, connection) => {
-    const actual = await runMiddleware(
-      _.changeCareerStage({
-        canEditProfile: true,
-        getUser: () => M.left('no-session'),
-        publicUrl,
-        oauth,
+    test.prop([
+      fc.record({
+        authorizeUrl: fc.url(),
+        clientId: fc.string(),
+        clientSecret: fc.string(),
+        redirectUri: fc.url(),
+        tokenUrl: fc.url(),
       }),
-      connection,
-    )()
+      fc.origin(),
+      fc.connection(),
+    ])('when the user is not logged in', async (oauth, publicUrl, connection) => {
+      const actual = await runMiddleware(
+        _.changeCareerStage({
+          canEditProfile: true,
+          getUser: () => M.left('no-session'),
+          publicUrl,
+          oauth,
+        }),
+        connection,
+      )()
 
-    expect(actual).toStrictEqual(
-      E.right([
-        { type: 'setStatus', status: Status.Found },
-        {
-          type: 'setHeader',
-          name: 'Location',
-          value: new URL(
-            `?${new URLSearchParams({
-              client_id: oauth.clientId,
-              response_type: 'code',
-              redirect_uri: oauth.redirectUri.href,
-              scope: '/authenticate',
-              state: new URL(format(myDetailsMatch.formatter, {}), publicUrl).toString(),
-            }).toString()}`,
-            oauth.authorizeUrl,
-          ).href,
-        },
-        { type: 'endResponse' },
-      ]),
-    )
+      expect(actual).toStrictEqual(
+        E.right([
+          { type: 'setStatus', status: Status.Found },
+          {
+            type: 'setHeader',
+            name: 'Location',
+            value: new URL(
+              `?${new URLSearchParams({
+                client_id: oauth.clientId,
+                response_type: 'code',
+                redirect_uri: oauth.redirectUri.href,
+                scope: '/authenticate',
+                state: new URL(format(myDetailsMatch.formatter, {}), publicUrl).toString(),
+              }).toString()}`,
+              oauth.authorizeUrl,
+            ).href,
+          },
+          { type: 'endResponse' },
+        ]),
+      )
+    })
   })
 
   test.prop([
