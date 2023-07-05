e09b8bdb9607f42bf27acecc790558fa1a7ef7e5
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-07-05 11:01:25 +0100

    Ensure we hide the change-career-stage page when the feature flag is disabled
    Refs #1069

--- src/change-career-stage.ts
+++ src/change-career-stage.ts
@@ -1,3 +1,15 @@
-import { serviceUnavailable } from './middleware'
+import * as b from 'fp-ts/boolean'
+import { pipe } from 'fp-ts/function'
+import * as RM from 'hyper-ts/lib/ReaderMiddleware'
+import { canEditProfile } from './feature-flags'
+import { notFound, serviceUnavailable } from './middleware'
 
-export const changeCareerStage = serviceUnavailable
+export const changeCareerStage = pipe(
+  RM.rightReader(canEditProfile),
+  RM.ichainW(
+    b.match(
+      () => notFound,
+      () => serviceUnavailable,
+    ),
+  ),
+)
--- test/change-career-stage.test.ts
+++ test/change-career-stage.test.ts
@@ -1,5 +1,5 @@
 import { test } from '@fast-check/jest'
-import { expect } from '@jest/globals'
+import { describe, expect } from '@jest/globals'
 import * as E from 'fp-ts/Either'
 import { MediaType, Status } from 'hyper-ts'
 import * as M from 'hyper-ts/lib/Middleware'
@@ -7,18 +7,42 @@ import * as _ from '../src/change-career-stage'
 import * as fc from './fc'
 import { runMiddleware } from './middleware'
 
-test.prop([fc.connection(), fc.either(fc.constant('no-session' as const), fc.user())])(
-  'changeCareerStage',
-  async (connection, user) => {
-    const actual = await runMiddleware(_.changeCareerStage({ getUser: () => M.fromEither(user) }), connection)()
+describe('changeCareerStage', () => {
+  test.prop([fc.connection(), fc.either(fc.constant('no-session' as const), fc.user())])(
+    'when profiles can be edited',
+    async (connection, user) => {
+      const actual = await runMiddleware(
+        _.changeCareerStage({ canEditProfile: true, getUser: () => M.fromEither(user) }),
+        connection,
+      )()
 
-    expect(actual).toStrictEqual(
-      E.right([
-        { type: 'setStatus', status: Status.ServiceUnavailable },
-        { type: 'setHeader', name: 'Cache-Control', value: 'no-store, must-revalidate' },
-        { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
-        { type: 'setBody', body: expect.anything() },
-      ]),
-    )
-  },
-)
+      expect(actual).toStrictEqual(
+        E.right([
+          { type: 'setStatus', status: Status.ServiceUnavailable },
+          { type: 'setHeader', name: 'Cache-Control', value: 'no-store, must-revalidate' },
+          { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
+          { type: 'setBody', body: expect.anything() },
+        ]),
+      )
+    },
+  )
+
+  test.prop([fc.connection(), fc.either(fc.constant('no-session' as const), fc.user())])(
+    "when profiles can't be edited",
+    async (connection, user) => {
+      const actual = await runMiddleware(
+        _.changeCareerStage({ canEditProfile: false, getUser: () => M.fromEither(user) }),
+        connection,
+      )()
+
+      expect(actual).toStrictEqual(
+        E.right([
+          { type: 'setStatus', status: Status.NotFound },
+          { type: 'setHeader', name: 'Cache-Control', value: 'no-store, must-revalidate' },
+          { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
+          { type: 'setBody', body: expect.anything() },
+        ]),
+      )
+    },
+  )
+})
