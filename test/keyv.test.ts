f9b0ffa44d18e32e03a8549e30539a0450de7f5f
Sean Wiseman <seanwiseman2012@gmail.com>
2023-07-10 11:14:48 +0100

    Sketched out a test list.
    Refs #1069

---
+++ test/keyv.test.ts
@@ -0,0 +1,11 @@
+import { describe, test } from '@jest/globals'
+
+describe('getCareerStage', () => {
+  test.todo('when the key contains a career stage')
+
+  test.todo('when the key contains something other than career stage')
+
+  test.todo('when the key is not found')
+
+  test.todo('when the key cannot be accessed')
+})
