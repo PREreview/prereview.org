06ec88a8f6036238427d97969b92f85bc79c0c39
Sean Wiseman <seanwiseman2012@gmail.com>
2023-07-10 11:55:11 +0100

    Implement remaining tests.
    Refs #1069

--- test/keyv.test.ts
+++ test/keyv.test.ts
@@ -6,11 +6,34 @@ import * as _ from '../src/keyv'
 import * as fc from './fc'
 
 describe('getCareerStage', () => {
-  test.todo('when the key contains a career stage')
+  test.prop([fc.orcid(), fc.careerStage()])('when the key contains a career stage', async (orcid, careerStage) => {
+    const store = new Keyv()
+    await store.set(orcid, careerStage)
 
-  test.todo('when the key contains something other than career stage')
+    const actual = await _.getCareerStage(orcid)({ careerStageStore: store })()
 
-  test.todo('when the key is not found')
+    expect(actual).toStrictEqual(E.right(careerStage))
+  })
+
+  test.prop([fc.orcid(), fc.anything()])(
+    'when the key contains something other than career stage',
+    async (orcid, value) => {
+      const store = new Keyv()
+      await store.set(orcid, value)
+
+      const actual = await _.getCareerStage(orcid)({ careerStageStore: store })()
+
+      expect(actual).toStrictEqual(E.left('not-found'))
+    },
+  )
+
+  test.prop([fc.orcid()])('when the key is not found', async orcid => {
+    const store = new Keyv()
+
+    const actual = await _.getCareerStage(orcid)({ careerStageStore: store })()
+
+    expect(actual).toStrictEqual(E.left('not-found'))
+  })
 
   test.prop([fc.orcid(), fc.anything()])('when the key cannot be accessed', async (orcid, error) => {
     const store = new Keyv()
