85ed84c5cfe4f975e62d7212de64ef85bb7a9876
Sean Wiseman <seanwiseman2012@gmail.com>
2023-07-10 11:46:03 +0100

    Implement a test.
    This commit implements the test for when Keyv cannot be accessed. We looked at methods for forcing a failure but could not find one easily. So landed on overriding the target method which is hacky but isolated to this one test.
    
    Refs #1069

--- test/keyv.test.ts
+++ test/keyv.test.ts
@@ -1,4 +1,9 @@
-import { describe, test } from '@jest/globals'
+import { test } from '@fast-check/jest'
+import { describe, expect } from '@jest/globals'
+import * as E from 'fp-ts/Either'
+import Keyv from 'keyv'
+import * as _ from '../src/keyv'
+import * as fc from './fc'
 
 describe('getCareerStage', () => {
   test.todo('when the key contains a career stage')
@@ -7,5 +12,12 @@ describe('getCareerStage', () => {
 
   test.todo('when the key is not found')
 
-  test.todo('when the key cannot be accessed')
+  test.prop([fc.orcid(), fc.anything()])('when the key cannot be accessed', async (orcid, error) => {
+    const store = new Keyv()
+    store.get = () => Promise.reject(error)
+
+    const actual = await _.getCareerStage(orcid)({ careerStageStore: store })()
+
+    expect(actual).toStrictEqual(E.left('unavailable'))
+  })
 })
