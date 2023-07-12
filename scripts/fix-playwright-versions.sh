3a6a210d2d2535c71e8e545484edafa92903f4f2
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-07-12 10:38:46 +0100

    Match the Playwright Docker image version
    Refs #1092, #1093, db397a966e0fb7882fb1ecb9ad984b773e1b38b5

--- scripts/fix-playwright-versions.sh
+++ scripts/fix-playwright-versions.sh
@@ -8,3 +8,9 @@ echo "Found @playwright/test $version"
 echo "Updating all copies of playwright-core"
 npm install playwright@"$version" playwright-core@"$version" --save-dev --save-exact --ignore-scripts
 npm uninstall playwright playwright-core --save-dev
+
+echo "Updating Dockerfile"
+sed --in-place --regexp-extended "s/mcr\.microsoft\.com\/playwright:v.+?-jammy/mcr.microsoft.com\/playwright:v$version-jammy/g" Dockerfile
+
+echo "Updating CI"
+sed --in-place --regexp-extended "s/mcr\.microsoft\.com\/playwright:v.+?-jammy/mcr.microsoft.com\/playwright:v$version-jammy/g" .github/workflows/ci.yml
