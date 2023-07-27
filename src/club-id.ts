021e8f058d99b99f7343646581a83b8b154b4398
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-07-27 13:01:22 +0100

    Remove duplication
    Refs #1124

--- src/club-id.ts
+++ src/club-id.ts
@@ -1,1 +1,11 @@
+import { pipe } from 'fp-ts/function'
+import * as C from 'io-ts/Codec'
+import * as D from 'io-ts/Decoder'
+
 export type ClubId = 'asapbio-metabolism'
+
+export const ClubIdC = C.fromDecoder(pipe(D.string, D.refine(isClubId, 'ClubID')))
+
+export function isClubId(value: string): value is ClubId {
+  return value === 'asapbio-metabolism'
+}
---
+++ test/club-id.test.ts
@@ -0,0 +1,43 @@
+import { test } from '@fast-check/jest'
+import { describe, expect } from '@jest/globals'
+import * as D from 'io-ts/Decoder'
+import * as _ from '../src/club-id'
+import * as fc from './fc'
+
+describe('ClubIdC', () => {
+  describe('decode', () => {
+    test.prop([fc.clubId()])('with a club ID', string => {
+      const actual = _.ClubIdC.decode(string)
+
+      expect(actual).toStrictEqual(D.success(string))
+    })
+
+    test.prop([fc.string()])('with a non-club ID', string => {
+      const actual = _.ClubIdC.decode(string)
+
+      expect(actual).toStrictEqual(D.failure(string, 'ClubID'))
+    })
+
+    test.prop([fc.anything().filter(value => typeof value !== 'string')])('with a non-string', value => {
+      const actual = _.ClubIdC.decode(value)
+
+      expect(actual).toStrictEqual(D.failure(value, 'string'))
+    })
+  })
+
+  test.prop([fc.clubId()])('encode', clubId => {
+    const actual = _.ClubIdC.encode(clubId)
+
+    expect(actual).toStrictEqual(clubId)
+  })
+})
+
+describe('isClubId', () => {
+  test.prop([fc.clubId()])('with a club ID', string => {
+    expect(_.isClubId(string)).toBe(true)
+  })
+
+  test.prop([fc.string()])('with a non-club ID', string => {
+    expect(_.isClubId(string)).toBe(false)
+  })
+})
