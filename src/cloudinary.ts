79013432618901a617770a1dc3f2040dc65c231c
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-08-24 11:15:22 +0100

    Make it easier to add more hard-coded profile images
    Refs #1182

--- src/cloudinary.ts
+++ src/cloudinary.ts
@@ -1,29 +1,33 @@
 import { v2 as cloudinary } from 'cloudinary'
 import * as TE from 'fp-ts/TaskEither'
+import { flow } from 'fp-ts/function'
 import type { Orcid } from 'orcid-id-ts'
 import { match } from 'ts-pattern'
 import { URL } from 'url'
 
-export const getAvatarFromCloudinary = (orcid: Orcid) =>
-  match(orcid)
-    .with('0000-0003-4921-6155' as Orcid, () =>
-      TE.right(
-        new URL(
-          cloudinary.url('prereview-profile/dvyalmcsaz6bwri1iux4', {
-            cloud_name: 'prereview',
-            force_version: false,
-            secure: true,
-            transformation: {
-              crop: 'thumb',
-              fetch_format: 'auto',
-              quality: 'auto',
-              gravity: 'face',
-              width: 300,
-              height: 300,
-              zoom: 0.666,
-            },
-          }),
-        ),
+export const getAvatarFromCloudinary = flow(
+  (orcid: Orcid) =>
+    match(orcid)
+      .returnType<TE.TaskEither<'not-found', string>>()
+      .with('0000-0003-4921-6155' as Orcid, () => TE.right('prereview-profile/dvyalmcsaz6bwri1iux4'))
+      .otherwise(() => TE.left('not-found' as const)),
+  TE.map(
+    imageId =>
+      new URL(
+        cloudinary.url(imageId, {
+          cloud_name: 'prereview',
+          force_version: false,
+          secure: true,
+          transformation: {
+            crop: 'thumb',
+            fetch_format: 'auto',
+            quality: 'auto',
+            gravity: 'face',
+            width: 300,
+            height: 300,
+            zoom: 0.666,
+          },
+        }),
       ),
-    )
-    .otherwise(() => TE.left('not-found' as const))
+  ),
+)
