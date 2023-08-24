5c43152696e8d26de5933a23fe288bde62cfa813
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-08-24 11:10:45 +0100

    Remove Cloudinary URL format hardcoding
    Refs #1182, https://cloudinary.com/documentation/node_integration

--- src/cloudinary.ts
+++ src/cloudinary.ts
@@ -1,13 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary'
 import * as TE from 'fp-ts/TaskEither'
 import type { Orcid } from 'orcid-id-ts'
 import { match } from 'ts-pattern'
+import { URL } from 'url'
 
 export const getAvatarFromCloudinary = (orcid: Orcid) =>
   match(orcid)
     .with('0000-0003-4921-6155' as Orcid, () =>
       TE.right(
         new URL(
-          'https://res.cloudinary.com/prereview/image/upload/c_thumb,f_auto,g_face,h_300,q_auto,w_300,z_0.666/prereview-profile/dvyalmcsaz6bwri1iux4',
+          cloudinary.url('prereview-profile/dvyalmcsaz6bwri1iux4', {
+            cloud_name: 'prereview',
+            force_version: false,
+            secure: true,
+            transformation: {
+              crop: 'thumb',
+              fetch_format: 'auto',
+              quality: 'auto',
+              gravity: 'face',
+              width: 300,
+              height: 300,
+              zoom: 0.666,
+            },
+          }),
         ),
       ),
     )
