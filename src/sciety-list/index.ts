b58370fb43a60ba92a0614b7ec66be199fc60b52
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-10-04 12:08:05 +0100

    Start getting real data
    Refs: #1290

--- src/sciety-list/index.ts
+++ src/sciety-list/index.ts
@@ -1,38 +1,47 @@
-import * as TE from 'fp-ts/TaskEither'
+import * as RTE from 'fp-ts/ReaderTaskEither'
+import * as RA from 'fp-ts/ReadonlyArray'
 import { pipe } from 'fp-ts/function'
 import { Status } from 'hyper-ts'
 import * as RM from 'hyper-ts/ReaderMiddleware'
 import * as D from 'io-ts/Decoder'
-import { notFound } from '../middleware'
+import type { LoggerEnv } from 'logger-fp-ts'
+import type { ZenodoEnv } from 'zenodo-ts'
+import type { GetPreprintTitleEnv } from '../preprint'
 import type { NonEmptyString } from '../string'
+import { getRecentPrereviewsFromZenodo } from '../zenodo'
 
 export interface ScietyListEnv {
   scietyListToken: NonEmptyString
 }
 
-const getAllPrereviews = () => TE.of(hardcoded)
+interface Prereview {
+  preprint: string
+  createdAt: string
+  doi: string
+  authors: ReadonlyArray<{ name: string }>
+}
 
-const hardcoded = [
-  {
-    preprint: 'doi:10.1101/2023.07.21.23292937',
-    createdAt: '2023-09-25T15:12:29.000Z',
-    doi: '10.5281/zenodo.8377098',
-    authors: [
-      {
-        name: 'Louis Fisher',
-      },
-      {
-        name: 'Brian MacKenna',
-      },
-      {
-        name: 'Helen Curtis',
-      },
-      {
-        name: 'Rose Higgins',
-      },
-    ],
-  },
-]
+const getAllPrereviews = (): RTE.ReaderTaskEither<
+  ZenodoEnv & GetPreprintTitleEnv & LoggerEnv,
+  never,
+  ReadonlyArray<Prereview>
+> =>
+  pipe(
+    getRecentPrereviewsFromZenodo(1),
+    RTE.map(data => data.recentPrereviews),
+    RTE.map(
+      RA.map(prereview => ({
+        preprint: prereview.preprint.id.value.toString(),
+        createdAt: prereview.published.toString(),
+        doi: `10.5281/zenode.${prereview.id}`,
+        authors: pipe(
+          prereview.reviewers,
+          RA.map(reviewer => ({ name: reviewer })),
+        ),
+      })),
+    ),
+    RTE.orElseW(() => RTE.of([])),
+  )
 
 const isAllowed = pipe(
   RM.ask<ScietyListEnv>(),
@@ -46,12 +55,12 @@ export const scietyList = pipe(
   RM.ichainW(isAllowed =>
     isAllowed
       ? pipe(
-          RM.fromTaskEither(getAllPrereviews()),
-          RM.ichainFirst(() => RM.status(Status.OK)),
-          RM.ichainFirst(() => RM.contentType('application/json')),
-          RM.ichainFirst(RM.closeHeaders),
-          RM.ichain(prereviews => RM.send(JSON.stringify(prereviews))),
+          RM.fromReaderTaskEither(getAllPrereviews()),
+          RM.ichainFirstW(() => RM.status(Status.OK)),
+          RM.ichainFirstW(() => RM.contentType('application/json')),
+          RM.ichainFirstW(RM.closeHeaders),
+          RM.ichainW(prereviews => RM.send(JSON.stringify(prereviews))),
         )
-      : pipe(notFound),
+      : pipe(RM.status(Status.Forbidden), RM.ichain(RM.closeHeaders), RM.ichain(RM.end)),
   ),
 )
