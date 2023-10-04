745976c9af5437f8456f6b551d3f1586c7fd20ca
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-10-04 10:52:01 +0100

    Making it easier to produce a list by not grouping by preprint
    This change deviates from the legacy API shape to remove redundant nesting and filtering. As a result, the Sciety ingestion will need to change (hopefully becoming simpler).
    
    Refs #1290

--- src/sciety-list/index.ts
+++ src/sciety-list/index.ts
@@ -2,34 +2,27 @@ import { pipe } from 'fp-ts/function'
 import { Status } from 'hyper-ts'
 import * as RM from 'hyper-ts/ReaderMiddleware'
 
-const hardcoded = {
-  data: [
-    {
-      handle: 'doi:10.1101/2023.07.21.23292937',
-      fullReviews: [
-        {
-          createdAt: '2023-09-25T15:12:29.000Z',
-          isPublished: true,
-          doi: '10.5281/zenodo.8377098',
-          authors: [
-            {
-              name: 'Louis Fisher',
-            },
-            {
-              name: 'Brian MacKenna',
-            },
-            {
-              name: 'Helen Curtis',
-            },
-            {
-              name: 'Rose Higgins',
-            },
-          ],
-        },
-      ],
-    },
-  ],
-}
+const hardcoded = [
+  {
+    preprint: 'doi:10.1101/2023.07.21.23292937',
+    createdAt: '2023-09-25T15:12:29.000Z',
+    doi: '10.5281/zenodo.8377098',
+    authors: [
+      {
+        name: 'Louis Fisher',
+      },
+      {
+        name: 'Brian MacKenna',
+      },
+      {
+        name: 'Helen Curtis',
+      },
+      {
+        name: 'Rose Higgins',
+      },
+    ],
+  },
+]
 
 export const scietyList = pipe(
   RM.status(Status.OK),
