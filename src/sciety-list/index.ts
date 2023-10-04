b75c37be9b02022d7c0e804debb9e696e1b79bf5
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-10-04 10:45:57 +0100

    Hardcode a single PREreview matching the old API shape
    Refs: #1290

--- src/sciety-list/index.ts
+++ src/sciety-list/index.ts
@@ -2,8 +2,38 @@ import { pipe } from 'fp-ts/function'
 import { Status } from 'hyper-ts'
 import * as RM from 'hyper-ts/ReaderMiddleware'
 
+const hardcoded = {
+  data: [
+    {
+      handle: 'doi:10.1101/2023.07.21.23292937',
+      fullReviews: [
+        {
+          createdAt: '2023-09-25T15:12:29.000Z',
+          isPublished: true,
+          doi: '10.5281/zenodo.8377098',
+          authors: [
+            {
+              name: 'Louis Fisher',
+            },
+            {
+              name: 'Brian MacKenna',
+            },
+            {
+              name: 'Helen Curtis',
+            },
+            {
+              name: 'Rose Higgins',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
 export const scietyList = pipe(
-  RM.status(Status.Forbidden),
+  RM.status(Status.OK),
+  RM.ichain(() => RM.contentType('application/json')),
   RM.ichain(RM.closeHeaders),
-  RM.ichain(() => RM.send('forbidden')),
+  RM.ichain(() => RM.send(JSON.stringify(hardcoded))),
 )
