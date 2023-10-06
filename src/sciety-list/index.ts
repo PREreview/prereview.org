d57950f8f84d32e2250228969ce48e610a650bdc
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-10-06 15:30:27 +0100

    Remove a ternary
    Refs #1290

--- src/sciety-list/index.ts
+++ src/sciety-list/index.ts
@@ -1,10 +1,11 @@
 import * as RTE from 'fp-ts/ReaderTaskEither'
 import * as RA from 'fp-ts/ReadonlyArray'
-import { pipe } from 'fp-ts/function'
+import { constVoid, pipe } from 'fp-ts/function'
 import { Status } from 'hyper-ts'
 import * as RM from 'hyper-ts/ReaderMiddleware'
 import * as D from 'io-ts/Decoder'
 import type { LoggerEnv } from 'logger-fp-ts'
+import { match } from 'ts-pattern'
 import type { ZenodoEnv } from 'zenodo-ts'
 import type { GetPreprintTitleEnv } from '../preprint'
 import type { NonEmptyString } from '../string'
@@ -46,21 +47,19 @@ const getAllPrereviews = (): RTE.ReaderTaskEither<
 const isAllowed = pipe(
   RM.ask<ScietyListEnv>(),
   RM.chain(env => RM.decodeHeader('Authorization', D.literal(`Bearer ${env.scietyListToken}`).decode)),
-  RM.map(() => true),
-  RM.orElse(() => RM.of(false)),
+  RM.bimap(() => 'forbidden' as const, constVoid),
 )
 
 export const scietyList = pipe(
   isAllowed,
-  RM.ichainW(isAllowed =>
-    isAllowed
-      ? pipe(
-          RM.fromReaderTaskEither(getAllPrereviews()),
-          RM.ichainFirstW(() => RM.status(Status.OK)),
-          RM.ichainFirstW(() => RM.contentType('application/json')),
-          RM.ichainFirstW(RM.closeHeaders),
-          RM.ichainW(prereviews => RM.send(JSON.stringify(prereviews))),
-        )
-      : pipe(RM.status(Status.Forbidden), RM.ichain(RM.closeHeaders), RM.ichain(RM.end)),
+  RM.chainReaderTaskEitherKW(getAllPrereviews),
+  RM.ichainFirst(() => RM.status(Status.OK)),
+  RM.ichainFirst(() => RM.contentType('application/json')),
+  RM.ichainFirst(() => RM.closeHeaders()),
+  RM.ichain(prereviews => RM.send(JSON.stringify(prereviews))),
+  RM.orElseW(error =>
+    match(error)
+      .with('forbidden', () => pipe(RM.status(Status.Forbidden), RM.ichain(RM.closeHeaders), RM.ichain(RM.end)))
+      .exhaustive(),
   ),
 )
