6405f93c81c3ba931cee8e66090afa521bae87ec
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-11-08 10:30:11 +0000

    Allow us to run locally without a Mailjet configuration
    Refs: #1376

--- src/index.ts
+++ src/index.ts
@@ -62,11 +62,16 @@ const server = app({
   },
   languagesStore: new Keyv({ namespace: 'languages', store: keyvStore }),
   locationStore: new Keyv({ namespace: 'location', store: keyvStore }),
-  mailjetApi: {
-    key: env.MAILJET_API_KEY,
-    secret: env.MAILJET_API_SECRET,
-    sandbox: env.MAILJET_API_SANDBOX,
-  },
+  mailjetApi:
+    typeof env.MAILJET_API_KEY === 'string' &&
+    typeof env.MAILJET_API_SANDBOX === 'boolean' &&
+    typeof env.MAILJET_API_SECRET === 'string'
+      ? {
+          key: env.MAILJET_API_KEY,
+          secret: env.MAILJET_API_SECRET,
+          sandbox: env.MAILJET_API_SANDBOX,
+        }
+      : undefined,
   oauth: {
     authorizeUrl: new URL('https://orcid.org/oauth/authorize'),
     clientId: env.ORCID_CLIENT_ID,
