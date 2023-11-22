4906887750e3b94429f648946f878f6c44a1fbfd
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-11-22 12:15:09 +0000

    Fix how the new single project is set up
    Refs #1405, 9a3b8c57d41e3f10995c046675ab83f3bcc8d644

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,41 +9,43 @@ export default defineConfig({
   outputDir: 'integration-results',
   preserveOutput: 'failures-only',
   projects: [
-    {
-      name: 'Desktop Chrome',
-      use: { ...devices['Desktop Chrome'], contextOptions: { reducedMotion: 'reduce' as const } },
-    },
-    {
-      name: 'Desktop Chrome (high contrast)',
-      use: {
-        ...devices['Desktop Chrome'],
-        colorScheme: 'dark' as const,
-        contextOptions: { forcedColors: 'active' as const, reducedMotion: 'reduce' as const },
+    ...[
+      {
+        name: 'Desktop Chrome',
+        use: { ...devices['Desktop Chrome'], contextOptions: { reducedMotion: 'reduce' as const } },
       },
-    },
-    {
-      name: 'Mobile Chrome (high contrast)',
-      use: {
-        ...devices['Pixel 5'],
-        colorScheme: 'dark' as const,
-        contextOptions: { forcedColors: 'active' as const, reducedMotion: 'reduce' as const },
+      {
+        name: 'Desktop Chrome (high contrast)',
+        use: {
+          ...devices['Desktop Chrome'],
+          colorScheme: 'dark' as const,
+          contextOptions: { forcedColors: 'active' as const, reducedMotion: 'reduce' as const },
+        },
       },
-    },
-    {
-      name: 'Desktop Firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'iPhone 11',
-      use: { ...devices['iPhone 11'] },
-    },
+      {
+        name: 'Mobile Chrome (high contrast)',
+        use: {
+          ...devices['Pixel 5'],
+          colorScheme: 'dark' as const,
+          contextOptions: { forcedColors: 'active' as const, reducedMotion: 'reduce' as const },
+        },
+      },
+      {
+        name: 'Desktop Firefox',
+        use: { ...devices['Desktop Firefox'] },
+      },
+      {
+        name: 'iPhone 11',
+        use: { ...devices['iPhone 11'] },
+      },
+    ].flatMap(env => [env, { name: `${env.name} (no JavaScript)`, use: { ...env.use, javaScriptEnabled: false } }]),
     {
       name: 'Desktop Firefox Visual Regression',
       use: { ...devices['Desktop Firefox'] },
       testDir: 'visual-regression',
       snapshotDir: path.resolve('visual-regression', 'snapshots'),
     },
-  ].flatMap(env => [env, { name: `${env.name} (no JavaScript)`, use: { ...env.use, javaScriptEnabled: false } }]),
+  ],
   snapshotDir: path.resolve('integration', 'snapshots'),
   testDir: 'integration',
   use: {
