a7574c96f863169e82fdb87638dd84f3c3fef656
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2023-11-22 12:03:51 +0000

    Remove knowledge of the running app
    Incidentally makes tests faster.
    
    Refs: 1405

--- visual-regression/skip-link.spec.ts
+++ visual-regression/skip-link.spec.ts
@@ -1,10 +1,22 @@
-import { expect, test } from '../integration/base'
+import { test as baseTest, expect } from '@playwright/test'
+import path from 'path'
 import { html, plainText } from '../src/html'
 import { page as templatePage } from '../src/page'
 
-test('skip-link', async ({ page }) => {
-  await page.goto('/')
+const test = baseTest.extend({
+  page: async ({ page }, use) => {
+    await page.route('**/*', (route, request) => {
+      if (request.url() === 'http://example.com/') {
+        return route.fulfill({ status: 200, headers: { 'Content-type': 'text/html; charset=utf-8' } })
+      }
+      return route.fulfill({ path: path.join('dist/assets', new URL(request.url()).pathname) })
+    })
+    await page.goto('http://example.com')
+    await use(page)
+  },
+})
 
+test('skip-link', async ({ page }) => {
   const pageHtml = templatePage({
     content: html`<p id="main">hello</p>`,
     title: plainText('Something'),
