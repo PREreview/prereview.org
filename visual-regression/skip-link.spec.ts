c98b3c06b4c4d2e7e04772c5c8f51b2088bad1d3
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-11-22 11:28:25 +0000

    Isolate what we're trying to visually-regression test
    Refs #1405

--- visual-regression/skip-link.spec.ts
+++ visual-regression/skip-link.spec.ts
@@ -1,7 +1,17 @@
 import { expect, test } from '../integration/base'
+import { html, plainText } from '../src/html'
+import { page as templatePage } from '../src/page'
 
 test('skip-link', async ({ page }) => {
   await page.goto('/')
+
+  const pageHtml = templatePage({
+    content: html`<p id="main">hello</p>`,
+    title: plainText('Something'),
+    skipLinks: [[html`Skip to main content`, '#main']],
+  })({})
+
+  await page.setContent(pageHtml.toString())
   await page.keyboard.press('Tab')
 
   await expect(page.getByRole('link', { name: 'Skip to main content' })).toBeFocused()
