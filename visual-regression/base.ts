12717f0d938a29c111ef6df83c9caec56f02bc00
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-11-29 11:26:43 +0000

    Remove duplication
    Refs #1405

---
+++ visual-regression/base.ts
@@ -0,0 +1,17 @@
+import { test as baseTest } from '@playwright/test'
+import path from 'path'
+
+export { expect } from '@playwright/test'
+
+export const test = baseTest.extend({
+  page: async ({ page }, use) => {
+    await page.route('**/*', (route, request) => {
+      if (request.url() === 'http://example.com/') {
+        return route.fulfill({ status: 200, headers: { 'Content-type': 'text/html; charset=utf-8' } })
+      }
+      return route.fulfill({ path: path.join('dist/assets', new URL(request.url()).pathname) })
+    })
+    await page.goto('http://example.com')
+    await use(page)
+  },
+})
--- visual-regression/page.spec.ts
+++ visual-regression/page.spec.ts
@@ -1,20 +1,6 @@
-import { test as baseTest, expect } from '@playwright/test'
-import path from 'path'
 import { html, plainText } from '../src/html'
 import { page as templatePage } from '../src/page'
-
-const test = baseTest.extend({
-  page: async ({ page }, use) => {
-    await page.route('**/*', (route, request) => {
-      if (request.url() === 'http://example.com/') {
-        return route.fulfill({ status: 200, headers: { 'Content-type': 'text/html; charset=utf-8' } })
-      }
-      return route.fulfill({ path: path.join('dist/assets', new URL(request.url()).pathname) })
-    })
-    await page.goto('http://example.com')
-    await use(page)
-  },
-})
+import { expect, test } from './base'
 
 test('page layout looks right', async ({ page }) => {
   const pageHtml = templatePage({
