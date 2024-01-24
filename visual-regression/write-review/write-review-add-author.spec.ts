3eb4ed4ca3b0cef973fd83d01565c3e6cacb70ec
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-01-24 10:35:55 +0000

    Convert remaining tests of templating to only test the template rather than the middleware
    Refs: #1498

--- visual-regression/write-review/write-review-add-author.spec.ts
+++ visual-regression/write-review/write-review-add-author.spec.ts
@@ -1,15 +1,10 @@
 import type { Doi } from 'doi-ts'
 import * as E from 'fp-ts/Either'
-import type { JsonRecord } from 'fp-ts/Json'
-import * as TE from 'fp-ts/TaskEither'
-import Keyv from 'keyv'
-import type { Orcid } from 'orcid-id-ts'
+import { invalidE, missingE } from '../../src/form'
 import { html, plainText } from '../../src/html'
 import { page as templatePage } from '../../src/page'
-import type { Pseudonym } from '../../src/types/pseudonym'
-import { writeReviewAddAuthor } from '../../src/write-review'
+import type { NonEmptyString } from '../../src/types/string'
 import { addAuthorForm } from '../../src/write-review/add-author-page/add-author-form'
-import { saveForm } from '../../src/write-review/form'
 import { expect, test } from '../base'
 
 test('content looks right', async ({ page }) => {
@@ -46,41 +41,20 @@ test('content looks right', async ({ page }) => {
 })
 
 test('content looks right when fields are missing', async ({ page }) => {
-  const formStore = new Keyv<JsonRecord>()
-  await saveForm('0000-0002-1825-0097' as Orcid, {
-    type: 'biorxiv',
-    value: '10.1101/2022.01.13.476201' as Doi<'1101'>,
-  })({ moreAuthors: 'yes' })({ formStore })()
-
-  const response = await writeReviewAddAuthor({
-    body: { name: '', emailAddress: '' },
-    id: {
-      type: 'biorxiv-medrxiv',
-      value: '10.1101/2022.01.13.476201' as Doi<'1101'>,
+  const response = addAuthorForm({
+    form: {
+      name: E.left(missingE()),
+      emailAddress: E.left(missingE()),
     },
-    method: 'POST',
-    user: {
-      name: 'Josiah Carberry',
-      orcid: '0000-0002-1825-0097' as Orcid,
-      pseudonym: 'Orange Panda' as Pseudonym,
+    preprint: {
+      id: {
+        type: 'biorxiv',
+        value: '10.1101/2022.01.13.476201' as Doi<'1101'>,
+      },
+      title: html`The role of LHCBM1 in non-photochemical quenching in <i>Chlamydomonas reinhardtii</i>`,
+      language: 'en',
     },
-  })({
-    canInviteAuthors: () => true,
-    formStore,
-    getPreprintTitle: () =>
-      TE.right({
-        id: {
-          type: 'biorxiv',
-          value: '10.1101/2022.01.13.476201' as Doi<'1101'>,
-        },
-        title: html`The role of LHCBM1 in non-photochemical quenching in <i>Chlamydomonas reinhardtii</i>`,
-        language: 'en',
-      }),
-  })()
-
-  if (response._tag !== 'StreamlinePageResponse') {
-    throw new Error('incorrect page response')
-  }
+  })
 
   const content = html` <main id="${response.skipToLabel}">${response.main}</main>`
 
@@ -95,42 +69,20 @@ test('content looks right when fields are missing', async ({ page }) => {
 })
 
 test('content looks right when fields are invalid', async ({ page }) => {
-  const formStore = new Keyv<JsonRecord>()
-  await saveForm('0000-0002-1825-0097' as Orcid, {
-    type: 'biorxiv',
-    value: '10.1101/2022.01.13.476201' as Doi<'1101'>,
-  })({ moreAuthors: 'yes' })({ formStore })()
-
-  const response = await writeReviewAddAuthor({
-    body: { name: 'a name', emailAddress: 'not an email address' },
-    id: {
-      type: 'biorxiv-medrxiv',
-      value: '10.1101/2022.01.13.476201' as Doi<'1101'>,
+  const response = addAuthorForm({
+    form: {
+      name: E.right('a name' as NonEmptyString),
+      emailAddress: E.left(invalidE('not an email address')),
     },
-    method: 'POST',
-    user: {
-      name: 'Josiah Carberry',
-      orcid: '0000-0002-1825-0097' as Orcid,
-      pseudonym: 'Orange Panda' as Pseudonym,
+    preprint: {
+      id: {
+        type: 'biorxiv',
+        value: '10.1101/2022.01.13.476201' as Doi<'1101'>,
+      },
+      title: html`The role of LHCBM1 in non-photochemical quenching in <i>Chlamydomonas reinhardtii</i>`,
+      language: 'en',
     },
-  })({
-    canInviteAuthors: () => true,
-    formStore,
-    getPreprintTitle: () =>
-      TE.right({
-        id: {
-          type: 'biorxiv',
-          value: '10.1101/2022.01.13.476201' as Doi<'1101'>,
-        },
-        title: html`The role of LHCBM1 in non-photochemical quenching in <i>Chlamydomonas reinhardtii</i>`,
-        language: 'en',
-      }),
-  })()
-
-  if (response._tag !== 'StreamlinePageResponse') {
-    console.log(response)
-    throw new Error('incorrect page response')
-  }
+  })
 
   const content = html` <main id="${response.skipToLabel}">${response.main}</main>`
 
