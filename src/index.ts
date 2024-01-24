c9d1ede69d3da3a3087fc6b0e5bc2d0ab65ea414
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-01-24 09:44:04 +0000

    Allow people to try inviting further authors on our sandbox instance
    Refs: #1498

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,7 @@ const server = app({
   ...loggerEnv,
   allowSiteCrawlers: env.ALLOW_SITE_CRAWLERS,
   authorInviteStore: new Keyv({ namespace: 'author-invite', store: keyvStore }),
-  canInviteAuthors: () => false,
+  canInviteAuthors: () => env.CAN_INVITE_AUTHORS,
   cloudinaryApi: { cloudName: 'prereview', key: env.CLOUDINARY_API_KEY, secret: env.CLOUDINARY_API_SECRET },
   contactEmailAddressStore: new Keyv({ namespace: 'contact-email-address', store: keyvStore }),
   fathomId: env.FATHOM_SITE_ID,
