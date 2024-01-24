3a8d0d0c99c0c6dac361fe406221b49637eebb72
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-01-24 10:55:16 +0000

    Remove duplication
    Refs: #1498

--- visual-regression/write-review/write-review-add-author.spec.ts
+++ visual-regression/write-review/write-review-add-author.spec.ts
@@ -1,9 +1,11 @@
+import type { Page } from '@playwright/test'
 import type { Doi } from 'doi-ts'
 import * as E from 'fp-ts/Either'
 import { invalidE, missingE } from '../../src/form'
 import { html, plainText } from '../../src/html'
 import { page as templatePage } from '../../src/page'
 import type { PreprintTitle } from '../../src/preprint'
+import type { StreamlinePageResponse } from '../../src/response'
 import type { NonEmptyString } from '../../src/types/string'
 import { addAuthorForm } from '../../src/write-review/add-author-page/add-author-form'
 import { expect, test } from '../base'
@@ -26,6 +28,41 @@ test('content looks right', async ({ page }) => {
     preprint,
   })
 
+  const { nav, main } = await showPage(response, page)
+
+  await expect(nav).toHaveScreenshot()
+  await expect(main).toHaveScreenshot()
+})
+
+test('content looks right when fields are missing', async ({ page }) => {
+  const response = addAuthorForm({
+    form: {
+      name: E.left(missingE()),
+      emailAddress: E.left(missingE()),
+    },
+    preprint,
+  })
+
+  const { main } = await showPage(response, page)
+
+  await expect(main).toHaveScreenshot()
+})
+
+test('content looks right when fields are invalid', async ({ page }) => {
+  const response = addAuthorForm({
+    form: {
+      name: E.right('a name' as NonEmptyString),
+      emailAddress: E.left(invalidE('not an email address')),
+    },
+    preprint,
+  })
+
+  const { main } = await showPage(response, page)
+
+  await expect(main).toHaveScreenshot()
+})
+
+async function showPage(response: StreamlinePageResponse, page: Page) {
   const content = html`
     ${response.nav ? html` <nav data-testid="nav">${response.nav}</nav>` : ''}
 
@@ -39,48 +76,8 @@ test('content looks right', async ({ page }) => {
 
   await page.setContent(pageHtml.toString())
 
-  await expect(page.getByTestId('nav')).toHaveScreenshot()
-  await expect(page.getByRole('main')).toHaveScreenshot()
-})
-
-test('content looks right when fields are missing', async ({ page }) => {
-  const response = addAuthorForm({
-    form: {
-      name: E.left(missingE()),
-      emailAddress: E.left(missingE()),
-    },
-    preprint,
-  })
-
-  const content = html` <main id="${response.skipToLabel}">${response.main}</main>`
-
-  const pageHtml = templatePage({
-    content,
-    title: plainText('Something'),
-  })({})
-
-  await page.setContent(pageHtml.toString())
-
-  await expect(page.getByRole('main')).toHaveScreenshot()
-})
-
-test('content looks right when fields are invalid', async ({ page }) => {
-  const response = addAuthorForm({
-    form: {
-      name: E.right('a name' as NonEmptyString),
-      emailAddress: E.left(invalidE('not an email address')),
-    },
-    preprint,
-  })
-
-  const content = html` <main id="${response.skipToLabel}">${response.main}</main>`
-
-  const pageHtml = templatePage({
-    content,
-    title: plainText('Something'),
-  })({})
-
-  await page.setContent(pageHtml.toString())
-
-  await expect(page.getByRole('main')).toHaveScreenshot()
-})
+  return {
+    nav: page.getByTestId('nav'),
+    main: page.getByRole('main'),
+  }
+}
