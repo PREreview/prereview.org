0c22b86f8b0c6f2b09fe1851f2dfbf031ff3a07a
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-02-16 16:10:55 +0000

    Fix a copy-and-paste gone wrong
    Refs #1470, fac54ac5fccc9eb63cce1bcd3ce20265288b45c9

--- test/my-details-page/change-career-stage.test.ts
+++ test/my-details-page/change-career-stage.test.ts
@@ -1,111 +1,151 @@
 import { test } from '@fast-check/jest'
 import { describe, expect, jest } from '@jest/globals'
 import { format } from 'fp-ts-routing'
+import * as TE from 'fp-ts/TaskEither'
 import { Status } from 'hyper-ts'
-import * as _ from '../../src/my-details-page/change-avatar'
-import { changeAvatarMatch, myDetailsMatch } from '../../src/routes'
+import * as _ from '../../src/my-details-page/change-career-stage'
+import { changeCareerStageMatch, myDetailsMatch } from '../../src/routes'
 import * as fc from '../fc'
 import { shouldNotBeCalled } from '../should-not-be-called'
 
-describe('changeAvatar', () => {
-  describe('when avatars can be uploaded', () => {
-    test.prop([fc.record({ buffer: fc.string().map(string => Buffer.from(string)) }), fc.user()])(
-      'when the form has been submitted',
-      async (file, user) => {
-        const actual = await _.changeAvatar({ body: { avatar: [file] }, method: 'POST', user })({
-          canUploadAvatar: () => true,
-        })()
-
-        expect(actual).toStrictEqual({
-          _tag: 'PageResponse',
-          status: Status.ServiceUnavailable,
-          title: expect.stringContaining('problems'),
-          main: expect.stringContaining('problems'),
-          skipToLabel: 'main',
-          js: [],
-        })
-      },
-    )
-
-    test.prop([fc.user()])('when the avatar is too big', async user => {
-      const actual = await _.changeAvatar({ body: { avatar: 'TOO_BIG' }, method: 'POST', user })({
-        canUploadAvatar: () => true,
-      })()
-
-      expect(actual).toStrictEqual({
-        _tag: 'PageResponse',
-        canonical: format(changeAvatarMatch.formatter, {}),
-        status: Status.BadRequest,
-        title: expect.stringContaining('Error: Upload an avatar'),
-        nav: expect.stringContaining('Back'),
-        main: expect.stringContaining('avatar'),
-        skipToLabel: 'form',
-        js: ['error-summary.js'],
-      })
-    })
-
-    test.prop([
-      fc.oneof(fc.anything(), fc.record({ avatar: fc.constant('ERROR') }, { withDeletedKeys: true })),
-      fc.user(),
-    ])('when the avatar is missing', async (body, user) => {
-      const actual = await _.changeAvatar({ body, method: 'POST', user })({
-        canUploadAvatar: () => true,
-      })()
-
-      expect(actual).toStrictEqual({
-        _tag: 'PageResponse',
-        canonical: format(changeAvatarMatch.formatter, {}),
-        status: Status.BadRequest,
-        title: expect.stringContaining('Error: Upload an avatar'),
-        nav: expect.stringContaining('Back'),
-        main: expect.stringContaining('avatar'),
-        skipToLabel: 'form',
-        js: ['error-summary.js'],
-      })
-    })
-
-    test.prop([fc.anything(), fc.string().filter(method => method !== 'POST'), fc.user()])(
-      'when the form needs to be submitted',
-      async (body, method, user) => {
-        const actual = await _.changeAvatar({ body, method, user })({
-          canUploadAvatar: () => true,
-        })()
-
-        expect(actual).toStrictEqual({
-          _tag: 'PageResponse',
-          canonical: format(changeAvatarMatch.formatter, {}),
-          status: Status.OK,
-          title: expect.stringContaining('avatar'),
-          nav: expect.stringContaining('Back'),
-          main: expect.stringContaining('avatar'),
-          skipToLabel: 'form',
-          js: [],
-        })
-      },
-    )
-  })
-
-  test.prop([fc.anything(), fc.string(), fc.user()])("when avatars can't be uploaded", async (body, method, user) => {
-    const canUploadAvatar = jest.fn<_.Env['canUploadAvatar']>(_ => false)
-
-    const actual = await _.changeAvatar({ body, method, user })({
-      canUploadAvatar,
+describe('changeCareerStage', () => {
+  test.prop([
+    fc.anything(),
+    fc.string().filter(method => method !== 'POST'),
+    fc.user(),
+    fc.either(fc.constantFrom('not-found' as const, 'unavailable' as const), fc.careerStage()),
+  ])('when there is a logged in user', async (body, method, user, careerStage) => {
+    const actual = await _.changeCareerStage({ body, method, user })({
+      deleteCareerStage: shouldNotBeCalled,
+      getCareerStage: () => TE.fromEither(careerStage),
+      saveCareerStage: shouldNotBeCalled,
     })()
 
     expect(actual).toStrictEqual({
       _tag: 'PageResponse',
-      status: Status.NotFound,
-      title: expect.stringContaining('not found'),
-      main: expect.stringContaining('not found'),
-      skipToLabel: 'main',
+      canonical: format(changeCareerStageMatch.formatter, {}),
+      status: Status.OK,
+      title: expect.stringContaining('career stage'),
+      nav: expect.stringContaining('Back'),
+      main: expect.stringContaining('career stage'),
+      skipToLabel: 'form',
       js: [],
     })
-    expect(canUploadAvatar).toHaveBeenCalledWith(user)
   })
 
+  describe('when the form has been submitted', () => {
+    test.prop([fc.careerStageValue(), fc.user(), fc.careerStage()])(
+      'there is a career stage already',
+      async (careerStage, user, existingCareerStage) => {
+        const saveCareerStage = jest.fn<_.Env['saveCareerStage']>(_ => TE.right(undefined))
+
+        const actual = await _.changeCareerStage({ body: { careerStage }, method: 'POST', user })({
+          deleteCareerStage: shouldNotBeCalled,
+          getCareerStage: () => TE.right(existingCareerStage),
+          saveCareerStage,
+        })()
+
+        expect(actual).toStrictEqual({
+          _tag: 'RedirectResponse',
+          status: Status.SeeOther,
+          location: format(myDetailsMatch.formatter, {}),
+        })
+        expect(saveCareerStage).toHaveBeenCalledWith(user.orcid, {
+          value: careerStage,
+          visibility: existingCareerStage.visibility,
+        })
+      },
+    )
+
+    test.prop([fc.careerStageValue(), fc.user()])(
+      "when there isn't a career stage already",
+      async (careerStage, user) => {
+        const saveCareerStage = jest.fn<_.Env['saveCareerStage']>(_ => TE.right(undefined))
+
+        const actual = await _.changeCareerStage({ body: { careerStage }, method: 'POST', user })({
+          deleteCareerStage: shouldNotBeCalled,
+          getCareerStage: () => TE.left('not-found'),
+          saveCareerStage,
+        })()
+
+        expect(actual).toStrictEqual({
+          _tag: 'RedirectResponse',
+          status: Status.SeeOther,
+          location: format(myDetailsMatch.formatter, {}),
+        })
+        expect(saveCareerStage).toHaveBeenCalledWith(user.orcid, { value: careerStage, visibility: 'restricted' })
+      },
+    )
+  })
+
+  test.prop([
+    fc.oneof(fc.careerStageValue(), fc.constant('skip')),
+    fc.user(),
+    fc.either(fc.constantFrom('not-found' as const, 'unavailable' as const), fc.careerStage()),
+  ])(
+    'when the form has been submitted but the career stage cannot be saved',
+    async (careerStage, user, existingCareerStage) => {
+      const actual = await _.changeCareerStage({ body: { careerStage }, method: 'POST', user })({
+        deleteCareerStage: () => TE.left('unavailable'),
+        getCareerStage: () => TE.fromEither(existingCareerStage),
+        saveCareerStage: () => TE.left('unavailable'),
+      })()
+
+      expect(actual).toStrictEqual({
+        _tag: 'PageResponse',
+        status: Status.ServiceUnavailable,
+        title: expect.stringContaining('problems'),
+        main: expect.stringContaining('problems'),
+        skipToLabel: 'main',
+        js: [],
+      })
+    },
+  )
+
+  test.prop([fc.user()])('when the form has been skipped', async user => {
+    const deleteCareerStage = jest.fn<_.Env['deleteCareerStage']>(_ => TE.right(undefined))
+
+    const actual = await _.changeCareerStage({ body: { careerStage: 'skip' }, method: 'POST', user })({
+      deleteCareerStage,
+      getCareerStage: shouldNotBeCalled,
+      saveCareerStage: shouldNotBeCalled,
+    })()
+
+    expect(actual).toStrictEqual({
+      _tag: 'RedirectResponse',
+      status: Status.SeeOther,
+      location: format(myDetailsMatch.formatter, {}),
+    })
+    expect(deleteCareerStage).toHaveBeenCalledWith(user.orcid)
+  })
+
+  test.prop([fc.record({ careerStage: fc.lorem() }, { withDeletedKeys: true }), fc.user()])(
+    'when the form has been submitted without setting career stage',
+    async (body, user) => {
+      const actual = await _.changeCareerStage({ body, method: 'POST', user })({
+        deleteCareerStage: shouldNotBeCalled,
+        getCareerStage: shouldNotBeCalled,
+        saveCareerStage: shouldNotBeCalled,
+      })()
+
+      expect(actual).toStrictEqual({
+        _tag: 'PageResponse',
+        canonical: format(changeCareerStageMatch.formatter, {}),
+        status: Status.BadRequest,
+        title: expect.stringContaining('career stage'),
+        nav: expect.stringContaining('Back'),
+        main: expect.stringContaining('career stage'),
+        skipToLabel: 'form',
+        js: ['error-summary.js'],
+      })
+    },
+  )
+
   test.prop([fc.anything(), fc.string()])('when the user is not logged in', async (body, method) => {
-    const actual = await _.changeAvatar({ body, method })({
-      canUploadAvatar: shouldNotBeCalled,
+    const actual = await _.changeCareerStage({ body, method, user: undefined })({
+      deleteCareerStage: shouldNotBeCalled,
+      getCareerStage: shouldNotBeCalled,
+      saveCareerStage: shouldNotBeCalled,
     })()
 
     expect(actual).toStrictEqual({
