8d70113b670b4189546bf8968ef5c401b890ba57
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-03-18 14:02:02 +0000

    Improve how it reads
    Refs #1602

--- src/club-details.ts
+++ src/club-details.ts
@@ -275,13 +275,13 @@ const clubs: RR.ReadonlyRecord<ClubId, Club> = {
   'reviewing-dental-articles-club': {
     name: 'Reviewing Dental Articles Club',
     description: html`<p>
-        Comprehensively train revolutionary professionals in Health Sciences, produce knowledge and technologies and
-        innovations; develop university extension and exercise methodological guidance of study plans and programs for
-        postgraduate courses and modalities.
+        The Reviewing Dental Articles Club exists to comprehensively train revolutionary professionals in Health
+        Sciences, produce knowledge and technologies and innovations, and develop university extension and exercise
+        methodological guidance of study plans and programs for postgraduate courses and modalities.
       </p>
       <p>
-        Help the authors of the stomatology union in improving the writing and publication of articles related to this
-        science.
+        It also exists to help the authors of the stomatology union in improving the writing and publication of articles
+        related to this science.
       </p> `,
     leads: [{ name: 'Alain Manuel Chaple Gil', orcid: '0000-0002-8571-4429' as Orcid }],
     contact: 'alain.chaple@uautonoma.cl' as EmailAddress,
