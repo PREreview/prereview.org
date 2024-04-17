eeffeff88431319e87ee200305e7e6fdc18a8277
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-04-17 11:12:33 +0100

    Sketch a test list
    Refs #1661

---
+++ test/prereview-coar-notify.test.ts
@@ -0,0 +1,12 @@
+import { test } from '@fast-check/jest'
+import { describe } from '@jest/globals'
+
+describe('publishToPrereviewCoarNotifyInbox', () => {
+  test.todo('publishing succeeds')
+
+  describe('publishing fails', () => {
+    test.todo('with a network error')
+
+    test.todo('with an unexpected status')
+  })
+})
