bd27bda462892e2186600b19e01d10b78c1ab4d2
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-04-17 14:31:17 +0100

    Publish the request
    Refs #1661, b585bcd0641276133789eb959b6c033eb7fdb98a

--- integration/requesting-a-prereview.spec.ts
+++ integration/requesting-a-prereview.spec.ts
@@ -1,14 +1,20 @@
+import { Status } from 'hyper-ts'
 import { areLoggedIn, canLogIn, canRequestReviews, expect, test } from './base'
 
-test.extend(canLogIn).extend(areLoggedIn).extend(canRequestReviews)('can request a PREreview', async ({ page }) => {
-  await page.goto('/preprints/doi-10.1101-2024.02.07.578830/request-a-prereview')
+test.extend(canLogIn).extend(areLoggedIn).extend(canRequestReviews)(
+  'can request a PREreview',
+  async ({ fetch, page }) => {
+    await page.goto('/preprints/doi-10.1101-2024.02.07.578830/request-a-prereview')
 
-  await expect(page.getByRole('heading', { level: 1 })).toHaveText('Request a PREreview')
+    await expect(page.getByRole('heading', { level: 1 })).toHaveText('Request a PREreview')
 
-  await page.getByRole('button', { name: 'Start now' }).click()
+    await page.getByRole('button', { name: 'Start now' }).click()
 
-  await expect(page.getByRole('heading', { level: 1 })).toHaveText('Check your request')
-  await expect(page.getByRole('main')).toContainText('Published name Josiah Carberry')
+    await expect(page.getByRole('heading', { level: 1 })).toHaveText('Check your request')
+    await expect(page.getByRole('main')).toContainText('Published name Josiah Carberry')
 
-  await page.getByRole('button', { name: 'Request PREreview' }).click()
-})
+    fetch.postOnce('https://coar-notify-sandbox.prereview.org/inbox', { status: Status.Created })
+
+    await page.getByRole('button', { name: 'Request PREreview' }).click()
+  },
+)
