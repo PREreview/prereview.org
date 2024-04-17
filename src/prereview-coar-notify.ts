7c497de5d700b15dd37c333c1b35f3623cc12952
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-04-17 11:35:04 +0100

    Introduce a seam to constrain possible payloads using types
    Refs #1661

--- src/prereview-coar-notify.ts
+++ src/prereview-coar-notify.ts
@@ -8,7 +8,36 @@ import { type GenerateUuidEnv, generateUuid } from './types/uuid'
 
 const hardcodedCoarNotifyUrl = 'https://coar-notify-sandbox.prereview.org'
 
-const constructCoarPayload = ({ coarNotifyUrl }: { coarNotifyUrl: string }) =>
+interface CoarReviewActionOfferPayload {
+  id: string
+  '@context': ['https://www.w3.org/ns/activitystreams', 'https://purl.org/coar/notify']
+  type: ['Offer', 'coar-notify:ReviewAction']
+  origin: {
+    id: string
+    inbox: string
+    type: 'Service'
+  }
+  target: {
+    id: string
+    inbox: string
+    type: 'Service'
+  }
+  object: {
+    id: '10.1101/2024.02.07.578830'
+    'ietf:cite-as': 'https://doi.org/10.1101/2024.02.07.578830'
+  }
+  actor: {
+    id: 'https://prereview.org'
+    type: 'Person'
+    name: 'A PREreviewer'
+  }
+}
+
+const constructCoarPayload = ({
+  coarNotifyUrl,
+}: {
+  coarNotifyUrl: string
+}): RIO.ReaderIO<GenerateUuidEnv, CoarReviewActionOfferPayload> =>
   pipe(
     generateUuid,
     RIO.map(uuid => ({
