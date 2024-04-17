04970b8bf041ac5cdf6c753bd0fadac57c4875d9
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-04-17 11:23:58 +0100

    Write a characterisation test
    Refs: #1661

--- test/prereview-coar-notify.test.ts
+++ test/prereview-coar-notify.test.ts
@@ -1,8 +1,22 @@
 import { test } from '@fast-check/jest'
-import { describe } from '@jest/globals'
+import { describe, expect } from '@jest/globals'
+import * as E from 'fp-ts/Either'
+import { Status } from 'hyper-ts'
+import * as _ from '../src/prereview-coar-notify'
+import * as fc from './fc'
 
 describe('publishToPrereviewCoarNotifyInbox', () => {
-  test.todo('publishing succeeds')
+  test.prop([fc.fetchResponse({ status: fc.constant(Status.Created) }), fc.uuid()])(
+    'publishing succeeds',
+    async (response, uuid) => {
+      const result = await _.publishToPrereviewCoarNotifyInbox()({
+        fetch: () => Promise.resolve(response),
+        generateUuid: () => uuid,
+      })()
+
+      expect(result).toStrictEqual(E.right(undefined))
+    },
+  )
 
   describe('publishing fails', () => {
     test.todo('with a network error')
