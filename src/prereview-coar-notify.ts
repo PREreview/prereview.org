9a6a3b60f30f3cc93a5881cc52c3a6916887d52c
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-04-17 10:30:45 +0100

    Create basic POST request and handling structure
    Refs #1661

--- src/prereview-coar-notify.ts
+++ src/prereview-coar-notify.ts
@@ -1,5 +1,19 @@
-import type * as F from 'fetch-fp-ts'
+import * as F from 'fetch-fp-ts'
 import * as RTE from 'fp-ts/ReaderTaskEither'
+import { identity, pipe } from 'fp-ts/function'
+import { Status } from 'hyper-ts'
+import { timeoutRequest } from './fetch'
 
 export const publishToPrereviewCoarNotifyInbox = (): RTE.ReaderTaskEither<F.FetchEnv, 'unavailable', void> =>
-  RTE.left('unavailable')
+  pipe(
+    'http://www.example.com/',
+    F.Request('POST'),
+    F.setBody(JSON.stringify({}), 'application/json'),
+    F.send,
+    RTE.local(timeoutRequest(2000)),
+    RTE.filterOrElseW(F.hasStatus(Status.Created), identity),
+    RTE.bimap(
+      () => 'unavailable' as const,
+      () => undefined,
+    ),
+  )
