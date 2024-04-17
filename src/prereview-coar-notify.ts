b585bcd0641276133789eb959b6c033eb7fdb98a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-04-17 10:42:53 +0100

    Publish a request for a PREreview to our sandbox
    Refs: #1661

--- src/prereview-coar-notify.ts
+++ src/prereview-coar-notify.ts
@@ -4,11 +4,36 @@ import { identity, pipe } from 'fp-ts/function'
 import { Status } from 'hyper-ts'
 import { timeoutRequest } from './fetch'
 
+const hardcodedBody = {
+  id: '78965d36-830d-47c3-b0b7-01ed47cbec28',
+  '@context': ['https://www.w3.org/ns/activitystreams', 'https://purl.org/coar/notify'],
+  type: ['Offer', 'coar-notify:ReviewAction'],
+  origin: {
+    id: 'https://coar-notify-sandbox.prereview.org',
+    inbox: 'https://coar-notify-sandbox.prereview.org/inbox',
+    type: 'Service',
+  },
+  target: {
+    id: 'https://coar-notify-sandbox.prereview.org',
+    inbox: 'https://coar-notify-sandbox.prereview.org/inbox',
+    type: 'Service',
+  },
+  object: {
+    id: '10.1101/2024.02.07.578830',
+    'ietf:cite-as': 'https://doi.org/10.1101/2024.02.07.578830',
+  },
+  actor: {
+    id: 'https://prereview.org',
+    type: 'Person',
+    name: 'A PREreviewer',
+  },
+}
+
 export const publishToPrereviewCoarNotifyInbox = (): RTE.ReaderTaskEither<F.FetchEnv, 'unavailable', void> =>
   pipe(
-    'http://www.example.com/',
+    'https://coar-notify-sandbox.prereview.org/inbox',
     F.Request('POST'),
-    F.setBody(JSON.stringify({}), 'application/json'),
+    F.setBody(JSON.stringify(hardcodedBody), 'application/json'),
     F.send,
     RTE.local(timeoutRequest(2000)),
     RTE.filterOrElseW(F.hasStatus(Status.Created), identity),
