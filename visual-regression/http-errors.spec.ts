3daa3289a0321c624e329cba2327a587e5b11e16
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-05-02 15:21:14 +0100

    Add missing visual-regression tests
    Refs #1689

--- visual-regression/http-errors.spec.ts
+++ visual-regression/http-errors.spec.ts
@@ -1,7 +1,19 @@
 import { html } from '../src/html'
-import { noPermissionPage } from '../src/http-error'
+import { havingProblemsPage, noPermissionPage, pageNotFound } from '../src/http-error'
 import { page as templatePage } from '../src/page'
 import { expect, test } from './base'
+
+test('page-not-found page content looks right', async ({ showPage }) => {
+  const content = await showPage(pageNotFound)
+
+  await expect(content).toHaveScreenshot()
+})
+
+test('having-problems page content looks right', async ({ showPage }) => {
+  const content = await showPage(havingProblemsPage)
+
+  await expect(content).toHaveScreenshot()
+})
 
 test('no-permission page content looks right', async ({ page }) => {
   const pageHtml = templatePage({
