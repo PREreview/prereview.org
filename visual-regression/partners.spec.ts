26baf59f06d6c63a412319a6e2a190812b6ce70e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-05-10 10:45:27 +0100

    Match other visual regression tests
    Refs #1658

--- visual-regression/partners.spec.ts
+++ visual-regression/partners.spec.ts
@@ -1,17 +1,8 @@
-import { html, plainText } from '../src/html'
-import { page as templatePage } from '../src/page'
 import { partners } from '../src/partners'
 import { expect, test } from './base'
 
-test('main content looks right', async ({ page }) => {
-  const partnersPage = partners
+test('content looks right', async ({ showPage }) => {
+  const content = await showPage(partners)
 
-  const pageHtml = templatePage({
-    content: html`<main>${partnersPage.main}</main>`,
-    title: plainText('Something'),
-  })({})
-
-  await page.setContent(pageHtml.toString())
-
-  await expect(page.getByRole('main')).toHaveScreenshot()
+  await expect(content).toHaveScreenshot()
 })
