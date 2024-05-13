0ee3a7a20068d90c3b8f30fa90526c4d1f355320
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-05-13 11:36:14 +0100

    Remove knowledge of templating dependencies from a page
    Note they still exist on the error page.
    
    Refs #1700

--- src/write-review/write-review-methods-appropriate.ts
+++ src/write-review/write-review-methods-appropriate.ts
@@ -1,7 +1,7 @@
 import { format } from 'fp-ts-routing'
 import * as E from 'fp-ts/Either'
 import { flow, identity, pipe } from 'fp-ts/function'
-import { Status } from 'hyper-ts'
+import { type ResponseEnded, Status, type StatusOpen } from 'hyper-ts'
 import * as RM from 'hyper-ts/ReaderMiddleware'
 import * as D from 'io-ts/Decoder'
 import type { Encoder } from 'io-ts/Encoder'
@@ -17,7 +17,7 @@ import {
 } from '../form'
 import { html, plainText, rawHtml, sendHtml } from '../html'
 import { getMethod, notFound, seeOther, serviceUnavailable } from '../middleware'
-import { page } from '../page'
+import { type FathomEnv, type PhaseEnv, type TemplatePageEnv, templatePage } from '../page'
 import { type PreprintTitle, getPreprintTitle } from '../preprint'
 import {
   writeReviewIntroductionMatchesMatch,
@@ -26,7 +26,7 @@ import {
   writeReviewReviewTypeMatch,
 } from '../routes'
 import { NonEmptyStringC } from '../types/string'
-import { type User, getUser } from '../user'
+import { type GetUserEnv, type User, getUser } from '../user'
 import { type Form, getForm, redirectToNextForm, saveForm, updateForm } from './form'
 
 export const writeReviewMethodsAppropriate = flow(
@@ -92,6 +92,15 @@ const handleMethodsAppropriateForm = ({ form, preprint, user }: { form: Form; pr
     RM.ichainMiddlewareKW(redirectToNextForm(preprint.id)),
     RM.orElseW(error =>
       match(error)
+        .returnType<
+          RM.ReaderMiddleware<
+            GetUserEnv & FathomEnv & PhaseEnv & TemplatePageEnv,
+            StatusOpen,
+            ResponseEnded,
+            never,
+            void
+          >
+        >()
         .with('form-unavailable', () => serviceUnavailable)
         .with({ methodsAppropriate: P.any }, showMethodsAppropriateErrorForm(preprint, user))
         .exhaustive(),
@@ -147,7 +156,7 @@ type MethodsAppropriateForm = Fields<typeof methodsAppropriateFields>
 function methodsAppropriateForm(preprint: PreprintTitle, form: MethodsAppropriateForm, user: User) {
   const error = hasAnError(form)
 
-  return page({
+  return templatePage({
     title: plainText`${error ? 'Error: ' : ''}Are the methods well-suited for this research? – PREreview of “${
       preprint.title
     }”`,
