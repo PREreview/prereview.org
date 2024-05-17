f0a98c57ce94ba79bc719de8e66f35067bba152e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-05-17 13:40:37 +0100

    Let us know when failures happen
    Refs #1712

--- src/openalex/index.ts
+++ src/openalex/index.ts
@@ -1,10 +1,24 @@
 import * as RTE from 'fp-ts/ReaderTaskEither'
 import * as RA from 'fp-ts/ReadonlyArray'
 import { flow } from 'fp-ts/function'
+import * as D from 'io-ts/Decoder'
+import * as L from 'logger-fp-ts'
+import { P, match } from 'ts-pattern'
 import { fieldIdFromOpenAlexId } from './ids'
 import { getFields, getWorkByDoi } from './work'
 
 export const getFieldsFromOpenAlex = flow(
   getWorkByDoi,
+  RTE.orElseFirstW(
+    flow(
+      error =>
+        match(error)
+          .with({ _tag: 'NetworkError', error: P.select() }, error => ({ error: error.message }))
+          .with({ _tag: 'UnableToDecodeBody', error: P.select() }, error => ({ error: D.draw(error) }))
+          .with({ _tag: 'UnexpectedStatusCode', actual: P.select() }, status => ({ status }))
+          .exhaustive(),
+      RTE.fromReaderIOK(L.errorP('Failed to get fields from OpenAlex')),
+    ),
+  ),
   RTE.bimap(() => 'unavailable' as const, flow(getFields, RA.filterMap(fieldIdFromOpenAlexId))),
 )
