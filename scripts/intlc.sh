83dfa8f430684daa8a0ce0d1315b99542321438b
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-07-22 15:00:06 +0100

    Extract a button's text to a locale file and make it available to the assets
    Note the file is unused.
    
    Refs #1817

--- scripts/intlc.sh
+++ scripts/intlc.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 modules=$(find "locales/en-US" -name "*.json" -exec basename "{}" .json \;)
+declare -a targets=("assets" "src")
 
 compile_module() {
   module="$1"
 
-  directory="src/locales"
-  mkdir -p "$directory"
+  for target in "${targets[@]}"; do
+    directory="${target}/locales"
+    mkdir -p "$directory"
 
-  intlc compile "locales/en-US/$module.json" -l "en-US" > "$directory/$module.ts"
+    intlc compile "locales/en-US/$module.json" -l "en-US" > "$directory/$module.ts"
+  done
 }
 
 for module in $modules; do compile_module "$module" & done
