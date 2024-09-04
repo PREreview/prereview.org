0db3be53efae8a6611aed02ea3642743447e10aa
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-09-04 11:18:23 +0100

    Extract and name to improve legibility
    Refs: #1834

--- src/Redis.ts
+++ src/Redis.ts
@@ -6,30 +6,31 @@ import { DeprecatedEnvVars } from './env.js'
 
 export class DeprecatedLoggerEnv extends Context.Tag('DeprecatedLoggerEnv')<DeprecatedLoggerEnv, L.LoggerEnv>() {}
 
-export const redisLifecycle = Effect.acquireRelease(
+const makeRedis = Effect.gen(function* () {
+  const env = yield* DeprecatedEnvVars
+  const loggerEnv = yield* DeprecatedLoggerEnv
+  const redis = new IoRedis(env.REDIS_URI.href, { commandTimeout: 2 * 1000, enableAutoPipelining: true })
+
+  redis.on('connect', () => L.debug('Redis connected')(loggerEnv)())
+  redis.on('close', () => L.debug('Redis connection closed')(loggerEnv)())
+  redis.on('reconnecting', () => L.info('Redis reconnecting')(loggerEnv)())
+  redis.removeAllListeners('error')
+  redis.on('error', (error: Error) => L.errorP('Redis connection error')({ error: error.message })(loggerEnv)())
+
+  return redis
+})
+
+const teardownRedis = (redis: IoRedis) =>
   Effect.gen(function* () {
-    const env = yield* DeprecatedEnvVars
     const loggerEnv = yield* DeprecatedLoggerEnv
-    const redis = new IoRedis(env.REDIS_URI.href, { commandTimeout: 2 * 1000, enableAutoPipelining: true })
+    yield* Effect.promise(() =>
+      redis
+        .quit()
+        .then(() => L.debug('Redis disconnected')(loggerEnv)())
+        .catch((error: unknown) =>
+          L.warnP('Redis unable to disconnect')({ error: toError(error).message })(loggerEnv)(),
+        ),
+    )
+  })
 
-    redis.on('connect', () => L.debug('Redis connected')(loggerEnv)())
-    redis.on('close', () => L.debug('Redis connection closed')(loggerEnv)())
-    redis.on('reconnecting', () => L.info('Redis reconnecting')(loggerEnv)())
-    redis.removeAllListeners('error')
-    redis.on('error', (error: Error) => L.errorP('Redis connection error')({ error: error.message })(loggerEnv)())
-
-    return redis
-  }),
-  redis =>
-    Effect.gen(function* () {
-      const loggerEnv = yield* DeprecatedLoggerEnv
-      yield* Effect.promise(() =>
-        redis
-          .quit()
-          .then(() => L.debug('Redis disconnected')(loggerEnv)())
-          .catch((error: unknown) =>
-            L.warnP('Redis unable to disconnect')({ error: toError(error).message })(loggerEnv)(),
-          ),
-      )
-    }),
-)
+export const redisLifecycle = Effect.acquireRelease(makeRedis, teardownRedis)
