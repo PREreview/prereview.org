63df21ea43625befd15cafc463683a4631e0c64f
Chris Wilkinson <chris@prereview.org>
2024-09-10 14:49:30 +0100

    Remove redundant config change
    The Zenodo Sandbox API seems to load correctly.
    
    Refs #1834, f9ea0d7cecec15a4d33a7046cf74ae966e892910

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,14 @@ import { DeprecatedEnvVars, DeprecatedLoggerEnv, Express, Redis } from './Contex
 import { makeDeprecatedEnvVars, makeDeprecatedLoggerEnv } from './DeprecatedServices.js'
 import { ExpressHttpApp } from './ExpressHttpApp.js'
 import { expressServer } from './ExpressServer.js'
-import { mitigateZenodoSandboxIpv6Issue } from './MitigateZenodoSandboxIpv6Issue.js'
 import { redisLifecycle } from './Redis.js'
 import { verifyCache } from './VerifyCache.js'
 
 pipe(
-  mitigateZenodoSandboxIpv6Issue,
-  Effect.andThen(pipe(ExpressHttpApp, HttpServer.serve(), Layer.merge(Layer.effectDiscard(verifyCache)), Layer.launch)),
+  ExpressHttpApp,
+  HttpServer.serve(),
+  Layer.merge(Layer.effectDiscard(verifyCache)),
+  Layer.launch,
   Effect.provide(NodeHttpServer.layerConfig(() => createServer(), { port: Config.succeed(3000) })),
   Effect.provideServiceEffect(Express, expressServer),
   Effect.provideServiceEffect(Redis, redisLifecycle),
