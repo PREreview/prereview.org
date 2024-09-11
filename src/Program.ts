1b7cd8559f3fa3a6880e707e75e9a7ddb7f54428
Chris Wilkinson <chris@prereview.org>
2024-09-11 15:21:13 +0100

    Ensure Effect responses have the X-Robots-Tag header
    Refs #1834

--- src/Program.ts
+++ src/Program.ts
@@ -1,9 +1,22 @@
-import { Headers, HttpMiddleware, HttpServer, HttpServerRequest } from '@effect/platform'
-import { Effect, Layer, pipe } from 'effect'
+import { Headers, HttpMiddleware, HttpServer, HttpServerRequest, HttpServerResponse } from '@effect/platform'
+import { Config, Effect, Layer, pipe } from 'effect'
 import { Express } from './Context.js'
 import { ExpressHttpApp } from './ExpressHttpApp.js'
 import { expressServer } from './ExpressServer.js'
 import { Router } from './Router.js'
+
+const addXRobotsTagHeader = HttpMiddleware.make(app =>
+  Effect.gen(function* () {
+    const allowSiteCrawlers = yield* Config.withDefault(Config.boolean('ALLOW_SITE_CRAWLERS'), false)
+    const response = yield* app
+
+    if (allowSiteCrawlers) {
+      return response
+    }
+
+    return HttpServerResponse.setHeader(response, 'X-Robots-Tag', 'none, noarchive')
+  }),
+)
 
 const annotateLogsWithRequestId = HttpMiddleware.make(app =>
   pipe(
@@ -18,6 +31,7 @@ const annotateLogsWithRequestId = HttpMiddleware.make(app =>
 export const Program = pipe(
   Router,
   Effect.catchTag('RouteNotFound', () => ExpressHttpApp),
+  addXRobotsTagHeader,
   HttpServer.serve(annotateLogsWithRequestId),
   Layer.provide(Layer.effect(Express, expressServer)),
 )
