f9caeb1b786348b2be4d29fe7ecf42ba8aeb1008
Chris Wilkinson <chris@prereview.org>
2024-09-11 14:25:02 +0100

    Use Effect's logger
    Refs #1834

--- src/VerifyCache.ts
+++ src/VerifyCache.ts
@@ -1,22 +1,24 @@
 import cacache from 'cacache'
-import { Effect } from 'effect'
-import * as E from 'fp-ts/lib/Either.js'
-import type { JsonRecord } from 'fp-ts/lib/Json.js'
-import * as L from 'logger-fp-ts'
-import { DeprecatedEnvVars, DeprecatedLoggerEnv } from './Context.js'
+import { Effect, Inspectable, pipe } from 'effect'
+import { DeprecatedEnvVars } from './Context.js'
 
 export const verifyCache = Effect.gen(function* () {
   const env = yield* DeprecatedEnvVars
-  const loggerEnv = yield* DeprecatedLoggerEnv
 
   if (!env.VERIFY_CACHE) {
     return yield* Effect.void
   }
-  yield* Effect.promise(() =>
-    Promise.resolve()
-      .then(() => L.debug('Verifying cache')(loggerEnv)())
-      .then(() => cacache.verify('data/cache', { concurrency: 5 }))
-      .then((stats: JsonRecord) => L.debugP('Cache verified')(stats)(loggerEnv)())
-      .catch((error: unknown) => L.errorP('Failed to verify cache')({ error: E.toError(error).message })(loggerEnv)()),
+
+  yield* pipe(
+    Effect.logDebug('Verifying cache'),
+    Effect.andThen(Effect.tryPromise(() => cacache.verify('data/cache', { concurrency: 5 }))),
+    Effect.tap(stats => pipe(Effect.logDebug('Cache verified'), Effect.annotateLogs('stats', stats))),
+    Effect.tapError(({ error }) =>
+      pipe(
+        Effect.logWarning('Failed to verify cache'),
+        Effect.annotateLogs('error', error instanceof Error ? error.message : Inspectable.toStringUnknown(error)),
+      ),
+    ),
+    Effect.orElse(() => Effect.void),
   )
 })
