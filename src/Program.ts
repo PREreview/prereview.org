a15c0bef720ebd20213d0854254ae34b4759d7c6
Chris Wilkinson <chris@prereview.org>
2024-09-11 13:25:48 +0100

    Add a place to add Effect routes
    Refs #1834

--- src/Program.ts
+++ src/Program.ts
@@ -1,7 +1,12 @@
-import { HttpServer } from '@effect/platform'
-import { Layer, pipe } from 'effect'
+import { HttpRouter, HttpServer } from '@effect/platform'
+import { Effect, Layer, pipe } from 'effect'
 import { Express } from './Context.js'
 import { ExpressHttpApp } from './ExpressHttpApp.js'
 import { expressServer } from './ExpressServer.js'
 
-export const Program = pipe(ExpressHttpApp, HttpServer.serve(), Layer.provide(Layer.effect(Express, expressServer)))
+export const Program = pipe(
+  HttpRouter.empty,
+  Effect.catchTag('RouteNotFound', () => ExpressHttpApp),
+  HttpServer.serve(),
+  Layer.provide(Layer.effect(Express, expressServer)),
+)
