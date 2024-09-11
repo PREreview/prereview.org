3d92e859466be0599551753ff86d8a8d13c602e7
Chris Wilkinson <chris@prereview.org>
2024-09-11 15:01:18 +0100

    Add the request ID to the Effect logger
    Refs #1834

--- src/Program.ts
+++ src/Program.ts
@@ -1,13 +1,23 @@
-import { HttpServer } from '@effect/platform'
+import { Headers, HttpMiddleware, HttpServer, HttpServerRequest } from '@effect/platform'
 import { Effect, Layer, pipe } from 'effect'
 import { Express } from './Context.js'
 import { ExpressHttpApp } from './ExpressHttpApp.js'
 import { expressServer } from './ExpressServer.js'
 import { Router } from './Router.js'
 
+const annotateLogsWithRequestId = HttpMiddleware.make(app =>
+  pipe(
+    HttpServerRequest.HttpServerRequest,
+    Effect.map(req => req.headers),
+    Effect.flatMap(Headers.get('Fly-Request-Id')),
+    Effect.orElseSucceed(() => null),
+    Effect.andThen(requestId => Effect.annotateLogs(app, 'requestId', requestId)),
+  ),
+)
+
 export const Program = pipe(
   Router,
   Effect.catchTag('RouteNotFound', () => ExpressHttpApp),
-  HttpServer.serve(),
+  HttpServer.serve(annotateLogsWithRequestId),
   Layer.provide(Layer.effect(Express, expressServer)),
 )
