e0b83c5fd3500e6eac8650c4888886978e462a8e
Chris Wilkinson <chris@prereview.org>
2024-09-18 11:09:12 +0100

    Prepare events for storage somewhere
    Refs #1941, https://effect.website/docs/guides/schema/classes

--- src/Feedback/Events.ts
+++ src/Feedback/Events.ts
@@ -1,20 +1,33 @@
-import type { Doi } from 'doi-ts'
-import { Data } from 'effect'
-import type { Orcid } from 'orcid-id-ts'
-import type { Html } from '../html.js'
+import { Schema } from '@effect/schema'
+import { isDoi, type Doi } from 'doi-ts'
+import { pipe } from 'effect'
+import { isOrcid, type Orcid } from 'orcid-id-ts'
+import { rawHtml, type Html } from '../html.js'
 
-export class FeedbackWasStarted extends Data.TaggedClass('FeedbackWasStarted')<{
-  prereviewId: number
-  authorId: Orcid
-}> {}
+const DoiSchema: Schema.Schema<Doi, string> = pipe(Schema.String, Schema.filter(isDoi))
 
-export class FeedbackWasEntered extends Data.TaggedClass('FeedbackWasEntered')<{
-  feedback: Html
-}> {}
+const OrcidSchema: Schema.Schema<Orcid, string> = pipe(Schema.String, Schema.filter(isOrcid))
 
-export class FeedbackWasPublished extends Data.TaggedClass('FeedbackWasPublished')<{
-  id: number
-  doi: Doi
-}> {}
+const HtmlSchema: Schema.Schema<Html, string> = Schema.transform(Schema.String, Schema.Any, {
+  strict: true,
+  decode: rawHtml,
+  encode: String,
+})
 
-export type FeedbackEvent = FeedbackWasStarted | FeedbackWasEntered | FeedbackWasPublished
+export class FeedbackWasStarted extends Schema.TaggedClass<FeedbackWasStarted>()('FeedbackWasStarted', {
+  prereviewId: Schema.Number,
+  authorId: OrcidSchema,
+}) {}
+
+export class FeedbackWasEntered extends Schema.TaggedClass<FeedbackWasEntered>()('FeedbackWasEntered', {
+  feedback: HtmlSchema,
+}) {}
+
+export class FeedbackWasPublished extends Schema.TaggedClass<FeedbackWasPublished>()('FeedbackWasPublished', {
+  id: Schema.Number,
+  doi: DoiSchema,
+}) {}
+
+export type FeedbackEvent = typeof FeedbackEvent.Type
+
+export const FeedbackEvent = Schema.Union(FeedbackWasStarted, FeedbackWasEntered, FeedbackWasPublished)
