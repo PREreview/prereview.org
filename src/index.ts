75a8229dcb14b3ace9a4b92a63178daaf7864925
Chris Wilkinson <chris@prereview.org>
2024-09-20 13:21:17 +0100

    Allow the PREreview team to write feedback
    Refs #1941, 7f5af11e64821ea10611b16340d965baa52d83c4

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,27 @@ import { ExpressConfigLive } from './ExpressServer.js'
 import { Program } from './Program.js'
 import { redisLifecycle } from './Redis.js'
 import { verifyCache } from './VerifyCache.js'
+import { CanWriteFeedback } from './feature-flags.js'
+import type { User } from './user.js'
+
+const isPrereviewTeam = (user?: User) =>
+  user
+    ? [
+        '0000-0001-8511-8689',
+        '0000-0002-1472-1824',
+        '0000-0002-3708-3546',
+        '0000-0002-6109-0367',
+        '0000-0002-6750-9341',
+        '0000-0003-4921-6155',
+        '0000-0002-5753-2556',
+      ].includes(user.orcid)
+    : false
 
 pipe(
   Program,
   Layer.merge(Layer.effectDiscard(verifyCache)),
   Layer.launch,
+  Effect.provideService(CanWriteFeedback, isPrereviewTeam),
   Effect.provide(NodeHttpServer.layerConfig(() => createServer(), { port: Config.succeed(3000) })),
   Effect.provideServiceEffect(ExpressConfig, ExpressConfigLive),
   Effect.provideServiceEffect(Redis, redisLifecycle),
