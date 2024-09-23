90166090b36e788d60708df6ef464f7135aa7d7b
Chris Wilkinson <chris@prereview.org>
2024-09-23 15:39:49 +0100

    Create a query for finding the unpublished feedback for a user
    Note this loads all the events in memory; this will have to be replaced at some point, but it's probably fine for quite a while.
    
    Refs #1941

---
+++ src/Feedback/Queries.ts
@@ -0,0 +1,35 @@
+import { Array, Equal, Option, pipe, Record } from 'effect'
+import type { Orcid } from 'orcid-id-ts'
+import type { Uuid } from 'uuid-ts'
+import type { FeedbackEvent } from './Events.js'
+import { EvolveFeedback } from './Evolve.js'
+import { FeedbackNotStarted, type FeedbackState } from './State.js'
+
+export const GetAllUnpublishedFeedbackByAnAuthorForAPrereview =
+  (events: ReadonlyArray<{ readonly event: FeedbackEvent; readonly resourceId: Uuid }>) =>
+  ({ authorId, prereviewId }: { readonly authorId: Orcid; readonly prereviewId: number }) =>
+    pipe(
+      Array.reduce(
+        events,
+        {} as Record.ReadonlyRecord<Uuid, ReadonlyArray<FeedbackEvent>>,
+        (candidates, { event, resourceId }) =>
+          pipe(
+            Record.modifyOption(candidates, resourceId, Array.append(event)),
+            Option.getOrElse(() => {
+              if (
+                event._tag === 'FeedbackWasStarted' &&
+                Equal.equals(event.authorId, authorId) &&
+                Equal.equals(event.prereviewId, prereviewId)
+              ) {
+                return Record.set(candidates, resourceId, Array.of(event))
+              }
+
+              return candidates
+            }),
+          ),
+      ),
+      Record.map(
+        Array.reduce(new FeedbackNotStarted() as FeedbackState, (state, event) => EvolveFeedback(state)(event)),
+      ),
+      Record.filter(state => state._tag === 'FeedbackInProgress' || state._tag === 'FeedbackReadyForPublishing'),
+    )
