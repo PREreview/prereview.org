d146ee45945a4003f979ad66a01f4748cdb5a44d
Chris Wilkinson <chris@prereview.org>
2024-10-07 13:12:39 +0100

    Remove a memory leak
    `express.use()` is stateful, so a middleware was added on each request handled. This change creates a fresh Express app on each request instead, which feels wasteful but should remove the leak.
    
    Refs #1834, e33d15d3a4d274596ae8410094d91f799180fdba

--- src/ExpressHttpApp.ts
+++ src/ExpressHttpApp.ts
@@ -1,12 +1,12 @@
 import { type HttpApp, HttpMiddleware, HttpServerRequest, HttpServerResponse } from '@effect/platform'
 import { NodeHttpServerRequest } from '@effect/platform-node'
 import { Effect } from 'effect'
-import type { ErrorRequestHandler } from 'express'
+import express, { type ErrorRequestHandler } from 'express'
 import { Express } from './Context.js'
 
 export const ExpressHttpApp: HttpApp.Default<never, Express | HttpServerRequest.HttpServerRequest> = Effect.gen(
   function* () {
-    const express = yield* Express
+    const expressApp = yield* Express
     const request = yield* HttpServerRequest.HttpServerRequest
 
     const nodeRequest = NodeHttpServerRequest.toIncomingMessage(request)
@@ -15,13 +15,15 @@ export const ExpressHttpApp: HttpApp.Default<never, Express | HttpServerRequest.
     return yield* Effect.async<HttpServerResponse.HttpServerResponse>(resume => {
       nodeResponse.once('close', () => resume(Effect.succeed(HttpServerResponse.empty())))
 
-      express.use(((error, req, res, next) => {
-        if (error instanceof Error && 'code' in error && error.code === 'ERR_HTTP_HEADERS_SENT') {
-          return next()
-        }
+      express()
+        .use(expressApp)
+        .use(((error, req, res, next) => {
+          if (error instanceof Error && 'code' in error && error.code === 'ERR_HTTP_HEADERS_SENT') {
+            return next()
+          }
 
-        next(error)
-      }) satisfies ErrorRequestHandler)(nodeRequest, nodeResponse)
+          next(error)
+        }) satisfies ErrorRequestHandler)(nodeRequest, nodeResponse)
     }).pipe(HttpMiddleware.withLoggerDisabled)
   },
 )
