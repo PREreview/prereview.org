d29a25444d7261d810223ad775dd1b41d94e87b1
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2024-10-31 15:22:09 +0000

    Improve readability of test by separating boilerplate from test specific setup
    Refs: #2060

--- test/Feedback/React.test.ts
+++ test/Feedback/React.test.ts
@@ -1,6 +1,6 @@
 import { test } from '@fast-check/jest'
 import { describe, expect, jest } from '@jest/globals'
-import { Effect, Either, TestContext } from 'effect'
+import { Effect, Either, pipe, TestContext } from 'effect'
 import * as Feedback from '../../src/Feedback/index.js'
 import * as _ from '../../src/Feedback/React.js'
 import * as fc from '../fc.js'
@@ -40,13 +40,16 @@ describe('OnFeedbackPublicationWasRequested', () => {
     fc.feedbackError(),
   ])("when the feedback can't be updated", (feedbackId, event, feedback, id, doi, error) =>
     Effect.gen(function* () {
-      const actual = yield* Effect.either(_.OnFeedbackPublicationWasRequested({ feedbackId, event }))
+      const actual = yield* pipe(
+        _.OnFeedbackPublicationWasRequested({ feedbackId, event }),
+        Effect.provideService(Feedback.HandleFeedbackCommand, () => Effect.fail(error)),
+        Effect.either,
+      )
 
       expect(actual).toStrictEqual(Either.left(new Feedback.UnableToHandleCommand({ cause: error })))
     }).pipe(
       Effect.provideService(Feedback.GetFeedback, () => Effect.succeed(feedback)),
       Effect.provideService(Feedback.PublishFeedbackWithADoi, () => Effect.succeed([doi, id])),
-      Effect.provideService(Feedback.HandleFeedbackCommand, () => Effect.fail(error)),
       Effect.provide(TestContext.TestContext),
       Effect.runPromise,
     ),
@@ -56,15 +59,18 @@ describe('OnFeedbackPublicationWasRequested', () => {
     "when the feedback can't be published",
     (feedbackId, event, feedback) =>
       Effect.gen(function* () {
-        const actual = yield* Effect.either(_.OnFeedbackPublicationWasRequested({ feedbackId, event }))
+        const actual = yield* pipe(
+          _.OnFeedbackPublicationWasRequested({ feedbackId, event }),
+          Effect.provideService(Feedback.PublishFeedbackWithADoi, () =>
+            Effect.fail(new Feedback.UnableToPublishFeedback({})),
+          ),
+          Effect.provideService(Feedback.HandleFeedbackCommand, shouldNotBeCalled),
+          Effect.either,
+        )
 
         expect(actual).toStrictEqual(Either.left(new Feedback.UnableToPublishFeedback({})))
       }).pipe(
         Effect.provideService(Feedback.GetFeedback, () => Effect.succeed(feedback)),
-        Effect.provideService(Feedback.PublishFeedbackWithADoi, () =>
-          Effect.fail(new Feedback.UnableToPublishFeedback({})),
-        ),
-        Effect.provideService(Feedback.HandleFeedbackCommand, shouldNotBeCalled),
         Effect.provide(TestContext.TestContext),
         Effect.runPromise,
       ),
@@ -72,15 +78,15 @@ describe('OnFeedbackPublicationWasRequested', () => {
 
   test.prop([fc.uuid(), fc.feedbackPublicationWasRequested()])("when the feedback can't be read", (feedbackId, event) =>
     Effect.gen(function* () {
-      const actual = yield* Effect.either(_.OnFeedbackPublicationWasRequested({ feedbackId, event }))
+      const actual = yield* pipe(
+        _.OnFeedbackPublicationWasRequested({ feedbackId, event }),
+        Effect.provideService(Feedback.GetFeedback, () => Effect.fail(new Feedback.UnableToQuery({}))),
+        Effect.provideService(Feedback.PublishFeedbackWithADoi, shouldNotBeCalled),
+        Effect.provideService(Feedback.HandleFeedbackCommand, shouldNotBeCalled),
+        Effect.either,
+      )
 
       expect(actual).toStrictEqual(Either.left(new Feedback.UnableToQuery({})))
-    }).pipe(
-      Effect.provideService(Feedback.GetFeedback, () => Effect.fail(new Feedback.UnableToQuery({}))),
-      Effect.provideService(Feedback.PublishFeedbackWithADoi, shouldNotBeCalled),
-      Effect.provideService(Feedback.HandleFeedbackCommand, shouldNotBeCalled),
-      Effect.provide(TestContext.TestContext),
-      Effect.runPromise,
-    ),
+    }).pipe(Effect.provide(TestContext.TestContext), Effect.runPromise),
   )
 })
