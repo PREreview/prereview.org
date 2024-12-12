44b054c9f0c79cadfc960efb7a696700a4c5db6c
Chris Wilkinson <chris@prereview.org>
2024-12-12 09:40:39 +0000

    Remove unused module
    Refs #2116, 61478edcd620947bc0a522c03ba1327f5b04f436

--- src/CrowdinInContextSecurityPolicies.ts
+++
@@ -1,6 +0,0 @@
-export const crowdInContextSecurityPolicies = {
-  'script-src': ["'self'", 'cdn.usefathom.com', 'cdn.crowdin.com', "'nonce-8IBTHwOdqNKAWeKl7plt8g=='", "'unsafe-eval'"],
-  'img-src': '*.crowdin.com',
-  'frame-src': ['crowdin.com', 'accounts.crowdin.com'],
-  crossOriginEmbedderPolicy: 'unsafe-none',
-}
