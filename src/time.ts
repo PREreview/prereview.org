1de2ed7c60cab24792226ab204d3d5fd5ffdd78a
Chris Wilkinson <chris@prereview.org>
2025-01-06 11:44:24 +0000

    Prefer Effect's matcher
    Refs #1834

--- src/time.ts
+++ src/time.ts
@@ -1,35 +1,33 @@
 import { Temporal } from '@js-temporal/polyfill'
-import { P, match } from 'ts-pattern'
-import { type Html, html } from './html.js'
+import { Match, pipe } from 'effect'
+import { html } from './html.js'
 
 import PlainDate = Temporal.PlainDate
 import PlainYearMonth = Temporal.PlainYearMonth
 
 export type PartialDate = PlainDate | PlainYearMonth | number
 
-export const renderDate =
-  (locale: string) =>
-  (date: PartialDate): Html =>
-    match(date)
-      .with(
-        P.number,
-        year =>
-          html`<time datetime="${year}"
-            >${new PlainDate(year, 1, 1).toLocaleString(locale, { year: 'numeric' })}</time
-          >`,
-      )
-      .when(
-        isPlainYearMonth,
-        date =>
-          html`<time datetime="${date.toString()}"
-            >${date.toLocaleString(locale, { calendar: date.calendarId, month: 'long', year: 'numeric' })}</time
-          >`,
-      )
-      .when(
-        isPlainDate,
-        date => html`<time datetime="${date.toString()}">${date.toLocaleString(locale, { dateStyle: 'long' })}</time>`,
-      )
-      .exhaustive()
+export const renderDate = (locale: string) =>
+  pipe(
+    Match.type<PartialDate>(),
+    Match.when(
+      Match.number,
+      year =>
+        html`<time datetime="${year}">${new PlainDate(year, 1, 1).toLocaleString(locale, { year: 'numeric' })}</time>`,
+    ),
+    Match.when(
+      isPlainYearMonth,
+      date =>
+        html`<time datetime="${date.toString()}"
+          >${date.toLocaleString(locale, { calendar: date.calendarId, month: 'long', year: 'numeric' })}</time
+        >`,
+    ),
+    Match.when(
+      isPlainDate,
+      date => html`<time datetime="${date.toString()}">${date.toLocaleString(locale, { dateStyle: 'long' })}</time>`,
+    ),
+    Match.exhaustive,
+  )
 
 function isPlainDate(value: unknown): value is PlainDate {
   return typeof value === 'object' && value?.constructor.name === 'PlainDate'
