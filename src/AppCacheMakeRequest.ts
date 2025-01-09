781d073607ec1d7c86afa8094e007cb2b5d49f38
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-01-09 14:41:54 +0000

    introduce concept of cache staleness

--- src/AppCacheMakeRequest.ts
+++ src/AppCacheMakeRequest.ts
@@ -1,27 +1,31 @@
 import type { HttpClientRequest, HttpClientResponse } from '@effect/platform'
 import crypto from 'crypto'
-import { Effect, pipe } from 'effect'
+import { DateTime, Effect, pipe } from 'effect'
 
-const cache = new Map<string, HttpClientResponse.HttpClientResponse>()
+const cache = new Map<string, { staleAt: DateTime.DateTime; response: HttpClientResponse.HttpClientResponse }>()
 
 export const AppCacheMakeRequest: <E, R>(
   effect: Effect.Effect<HttpClientResponse.HttpClientResponse, E, R>,
   request: HttpClientRequest.HttpClientRequest,
 ) => Effect.Effect<HttpClientResponse.HttpClientResponse, E, R> = (effect, request) =>
   Effect.gen(function* () {
+    const timestamp = yield* DateTime.now
     const key = keyForRequest(request)
-
     const response = cache.get(key)
 
     if (response) {
-      yield* Effect.logDebug('Cache hit')
-      return response
+      if (DateTime.lessThan(timestamp, response.staleAt)) {
+        yield* Effect.logDebug('Cache hit')
+        return response.response
+      }
+      yield* Effect.logDebug('Cache stale')
+    } else {
+      yield* Effect.logDebug('Cache miss')
     }
 
     return yield* pipe(
-      Effect.logDebug('Cache miss'),
-      Effect.andThen(effect),
-      Effect.tap(response => cache.set(key, response)),
+      effect,
+      Effect.tap(response => cache.set(key, { staleAt: DateTime.addDuration(timestamp, '10 seconds'), response })),
     )
   })
 
