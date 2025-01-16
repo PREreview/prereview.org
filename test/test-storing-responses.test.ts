7a7e1fc9fa25517fc31dc8be976c23783324412f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-01-16 14:25:44 +0000

    Manually transform response to allow serialisation

--- test/test-storing-responses.test.ts
+++ test/test-storing-responses.test.ts
@@ -1,14 +1,37 @@
-import { HttpClient } from '@effect/platform'
+import { Headers, HttpClient } from '@effect/platform'
 import { NodeHttpClient } from '@effect/platform-node'
 import { expect, test } from '@jest/globals'
-import { Effect } from 'effect'
+import { Effect, pipe, Schema } from 'effect'
+
+const StoredResponseSchema = Schema.Struct({
+  status: Schema.Number,
+  headers: Schema.Record({ key: Schema.String, value: Schema.String }),
+  body: Schema.String,
+})
 
 test('test', () =>
   Effect.gen(function* () {
     const client = yield* HttpClient.HttpClient
 
     const originalResponse = yield* client.get('https://api.crossref.org/works/10.1101/2024.11.04.24316579')
-    const testResponse = yield* client.get('https://api.crossref.org/works/10.1101/2024.11.04.24316579')
+    const testResponse = yield* pipe(
+      originalResponse,
+      response =>
+        Effect.gen(function* () {
+          return {
+            status: response.status,
+            headers: response.headers,
+            body: yield* response.text,
+          }
+        }),
+      Effect.andThen(Schema.encode(StoredResponseSchema)),
+      Effect.andThen(Schema.decode(StoredResponseSchema)),
+      Effect.andThen(stored => ({
+        ...stored,
+        headers: Headers.fromInput(stored.headers),
+        text: Effect.succeed(stored.body),
+      })),
+    )
 
     expect(testResponse.status).toStrictEqual(originalResponse.status)
     expect(testResponse.headers).toStrictEqual(originalResponse.headers)
