93232e1dd279bf6f9d5ba8177b0fe846adbfd818
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-01-24 09:58:01 +0000

    Partially implement a happy test
    Refs: #2186

--- test/CachingHttpClient.test.ts
+++ test/CachingHttpClient.test.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpClientRequest, HttpClientResponse } from '@effect/platform'
 import { test } from '@fast-check/jest'
-import { describe } from '@jest/globals'
+import { describe, expect } from '@jest/globals'
+import { Effect, pipe, TestContext } from 'effect'
+import * as _ from '../src/CachingHttpClient/index.js'
+import * as HttpCache from '../src/HttpCache.js'
+import * as fc from './fc.js'
+
+const stubbedClient = (response: HttpClientResponse.HttpClientResponse): HttpClient.HttpClient.With<never, never> =>
+  HttpClient.makeWith(() => Effect.succeed(response), Effect.succeed)
 
 describe('there is no cache entry', () => {
   describe('the request succeeds', () => {
-    test.todo('able to cache it')
+    test.failing.prop([fc.url()])('able to cache it', url =>
+      Effect.gen(function* () {
+        const cache = new Map()
+        const successfulResponse = HttpClientResponse.fromWeb(HttpClientRequest.get(url), new Response())
+        const client = yield* pipe(
+          _.CachingHttpClient,
+          Effect.provideService(HttpClient.HttpClient, stubbedClient(successfulResponse)),
+          Effect.provide(HttpCache.layerInMemory),
+        )
+
+        const actualResponse = yield* client.get(url)
+        expect(actualResponse).toStrictEqual(successfulResponse)
+        expect(cache.size).toBe(1)
+      }).pipe(Effect.scoped, Effect.provide(TestContext.TestContext), Effect.runPromise),
+    )
 
     test.todo('not able to cache it')
   })
