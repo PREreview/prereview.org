82df0855702465af16dc2661ddb21cff6c956c24
Chris Wilkinson <chris@prereview.org>
2025-01-27 13:22:46 +0000

    Extract request-a-PREreview strings
    Refs #1817

--- src/request-a-prereview-page/request-a-prereview-page.ts
+++ src/request-a-prereview-page/request-a-prereview-page.ts
@@ -1,26 +1,31 @@
 import { format } from 'fp-ts-routing'
+import { identity } from 'fp-ts/lib/function.js'
 import { Status } from 'hyper-ts'
 import { html, plainText, rawHtml } from '../html.js'
+import { type SupportedLocale, translate } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 import { homeMatch, requestAPrereviewMatch } from '../routes.js'
 import type * as Form from './form.js'
 
-export const requestAPrereviewPage = (form: Form.IncompleteForm) => {
+export const requestAPrereviewPage = (form: Form.IncompleteForm, locale: SupportedLocale) => {
   const error = form._tag === 'InvalidForm'
+  const t = translate(locale)
 
   return PageResponse({
     status: error ? Status.BadRequest : Status.OK,
-    title: plainText`${error ? 'Error: ' : ''}Which preprint would you like reviewed?`,
-    nav: html`<a href="${format(homeMatch.formatter, {})}" class="back"><span>Back</span></a>`,
+    title: plainText(t('request-a-prereview-page', 'requestTitle')({ error: error ? identity : () => '' })),
+    nav: html`<a href="${format(homeMatch.formatter, {})}" class="back"
+      ><span>${t('request-a-prereview-page', 'back')()}</span></a
+    >`,
     main: html`
       <form method="post" action="${format(requestAPrereviewMatch.formatter, {})}" novalidate>
         ${error
           ? html`
               <error-summary aria-labelledby="error-summary-title" role="alert">
-                <h2 id="error-summary-title">There is a problem</h2>
+                <h2 id="error-summary-title">${t('request-a-prereview-page', 'errorSummaryTitle')()}</h2>
                 <ul>
                   <li>
-                    <a href="#preprint">Enter the preprint DOI or URL</a>
+                    <a href="#preprint">${t('request-a-prereview-page', 'errorEnterPreprint')({ error: () => '' })}</a>
                   </li>
                 </ul>
               </error-summary>
@@ -28,18 +33,31 @@ export const requestAPrereviewPage = (form: Form.IncompleteForm) => {
           : ''}
 
         <div ${rawHtml(error ? 'class="error"' : '')}>
-          <h1><label id="preprint-label" for="preprint">Which preprint would you like reviewed?</label></h1>
+          <h1>
+            <label id="preprint-label" for="preprint"
+              >${t('request-a-prereview-page', 'requestTitle')({ error: () => '' })}</label
+            >
+          </h1>
 
-          <p id="preprint-tip" role="note">Use the preprint DOI or URL.</p>
+          <p id="preprint-tip" role="note">${t('request-a-prereview-page', 'useDoiUrl')()}</p>
 
           <details>
-            <summary><span>What is a DOI?</span></summary>
+            <summary><span>${t('request-a-prereview-page', 'whatIsDoi')()}</span></summary>
 
             <div>
               <p>
-                A <a href="https://www.doi.org/"><dfn>DOI</dfn></a> is a unique identifier that you can find on many
-                preprints. For example, <q class="select-all" translate="no">10.1101/2022.10.06.511170</q> or
-                <q class="select-all" translate="no">https://doi.org/10.1101/2022.10.06.511170</q>.
+                ${rawHtml(
+                  t(
+                    'request-a-prereview-page',
+                    'whatIsDoiText',
+                  )({
+                    doi: text => html`<a href="https://www.doi.org/"><dfn>${text}</dfn></a>`.toString(),
+                    example: html`<q class="select-all" translate="no">10.1101/2022.10.06.511170</q>`.toString(),
+                    exampleUrl: html`<q class="select-all" translate="no"
+                      >https://doi.org/10.1101/2022.10.06.511170</q
+                    >`.toString(),
+                  }),
+                )}
               </p>
             </div>
           </details>
@@ -47,8 +65,12 @@ export const requestAPrereviewPage = (form: Form.IncompleteForm) => {
           ${error
             ? html`
                 <div class="error-message" id="preprint-error">
-                  <span class="visually-hidden">Error:</span>
-                  Enter the preprint DOI or URL
+                  ${rawHtml(
+                    t(
+                      'request-a-prereview-page',
+                      'errorEnterPreprint',
+                    )({ error: text => html`<span class="visually-hidden">${text}</span>`.toString() }),
+                  )}
                 </div>
               `
             : ''}
@@ -65,7 +87,7 @@ export const requestAPrereviewPage = (form: Form.IncompleteForm) => {
           />
         </div>
 
-        <button>Continue</button>
+        <button>${t('request-a-prereview-page', 'continueButton')()}</button>
       </form>
     `,
     skipToLabel: 'form',
