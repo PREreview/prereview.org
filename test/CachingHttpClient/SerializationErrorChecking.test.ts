1fea3cded8bad49c0c9cbff98955451341c3f17a
Chris Wilkinson <chris@prereview.org>
2025-01-30 13:25:15 +0000

    Write a test list
    Refs #2186

---
+++ test/CachingHttpClient/SerializationErrorChecking.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from '@jest/globals'
+
+describe('when the cached response matches the original', () => {
+  it.todo('the response is left in the cache')
+})
+
+describe('when the cached response does not match the original', () => {
+  it.todo('the response is removed from the cache')
+})
