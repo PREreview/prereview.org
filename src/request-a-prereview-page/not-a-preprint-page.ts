963633ac6e7ac6a13737408dbcca4142619bc885
Chris Wilkinson <chris@prereview.org>
2025-02-03 15:29:49 +0000

    Advertise support for Jxiv
    Refs #2209

--- src/request-a-prereview-page/not-a-preprint-page.ts
+++ src/request-a-prereview-page/not-a-preprint-page.ts
@@ -12,8 +12,8 @@ export const notAPreprintPage = PageResponse({
 
     <p>
       We support preprints from AfricArXiv, arXiv, Authorea, bioRxiv, ChemRxiv, EarthArXiv, EcoEvoRxiv, EdArXiv,
-      engrXiv, medRxiv, MetaArXiv, OSF, PhilSci-Archive, Preprints.org, PsyArXiv, PsychArchives, Research&nbsp;Square,
-      SciELO, ScienceOpen, SocArXiv, TechRxiv and Zenodo.
+      engrXiv, Jxiv, medRxiv, MetaArXiv, OSF, PhilSci-Archive, Preprints.org, PsyArXiv, PsychArchives,
+      Research&nbsp;Square, SciELO, ScienceOpen, SocArXiv, TechRxiv and Zenodo.
     </p>
 
     <p>If this is a preprint, please <a href="mailto:help@prereview.org">get in touch</a>.</p>
