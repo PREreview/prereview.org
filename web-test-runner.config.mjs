4c15f1040bb17fe928804ec4e97724506e4a5f51
Chris Wilkinson <chris@prereview.org>
2025-02-03 10:03:18 +0000

    Remove redundant messages from the build output

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -12,6 +12,7 @@ export default {
     // playwrightLauncher({ product: 'firefox' }),
   ],
   files: 'test/assets/**/*.test.ts',
+  filterBrowserLogs: log => log.type !== 'Lit is in dev mode',
   nodeResolve: true,
   plugins: [url({}), esbuildPlugin({ ts: true })],
 }
