af066ccdac6d9fdb4ea348ee73ae40215729902e
Chris Wilkinson <chris@prereview.org>
2025-02-14 09:59:51 +0000

    Make a test pass
    Refs #2186

--- test/CachingHttpClient/PersistedToRedis.test.ts
+++ test/CachingHttpClient/PersistedToRedis.test.ts
@@ -1,9 +1,32 @@
-import { describe, it } from '@jest/globals'
+import { Headers, HttpClientRequest } from '@effect/platform'
+import { describe, expect, it } from '@jest/globals'
+import { DateTime, Effect, Either, Schema, TestContext } from 'effect'
+import { CacheValueFromStringSchema } from '../../src/CachingHttpClient/HttpCache.js'
+import * as _ from '../../src/CachingHttpClient/PersistedToRedis.js'
+import type * as Redis from '../../src/Redis.js'
 
 describe('getFromRedis', () => {
   describe('there is a value for a given key', () => {
     describe('the value can be read', () => {
-      it.todo('succeeds')
+      it('succeeds', () =>
+        Effect.gen(function* () {
+          const request = HttpClientRequest.get('http://example.com')
+          const decodableValue = yield* Schema.encode(CacheValueFromStringSchema)({
+            staleAt: yield* DateTime.now,
+            response: {
+              status: 200,
+              headers: Headers.empty,
+              body: '',
+            },
+          })
+          const redis = {
+            get: () => Promise.resolve(decodableValue),
+          } as unknown as typeof Redis.HttpCacheRedis.Service
+
+          const result = yield* Effect.either(_.getFromRedis(redis)(request))
+
+          expect(result).toStrictEqual(Either.right(expect.anything()))
+        }).pipe(Effect.provide(TestContext.TestContext), Effect.runPromise))
     })
 
     describe('the value can not be read', () => {
