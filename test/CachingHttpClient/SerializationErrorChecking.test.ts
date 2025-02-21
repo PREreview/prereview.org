a1f761a4c658b9de82321c15d46b6c28abddfca9
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-02-21 11:13:40 +0000

    Implement happy path todo test for SerializationErrorChecking
    Refs: #2186

--- test/CachingHttpClient/SerializationErrorChecking.test.ts
+++ test/CachingHttpClient/SerializationErrorChecking.test.ts
@@ -1,7 +1,29 @@
-import { describe, it } from '@jest/globals'
+import { HttpClientRequest, HttpClientResponse } from '@effect/platform'
+import { it } from '@fast-check/jest'
+import { describe } from '@jest/globals'
+import { expect } from '@playwright/test'
+import { DateTime, Effect, pipe, TestContext } from 'effect'
+import { HttpCache } from '../../src/CachingHttpClient/HttpCache.js'
+import { layerInMemory } from '../../src/CachingHttpClient/InMemory.js'
+import * as fc from '../fc.js'
 
 describe('when the cached response matches the original', () => {
-  it.todo('the response is left in the cache')
+  it.prop([fc.url()])('the response is left in the cache', url =>
+    Effect.gen(function* () {
+      const cacheStorage = new Map()
+
+      const response = HttpClientResponse.fromWeb(HttpClientRequest.get(url), new Response())
+      const staleAt = yield* DateTime.now
+
+      yield* pipe(
+        HttpCache,
+        Effect.andThen(httpCache => httpCache.set(response, staleAt)),
+        Effect.provide(layerInMemory(cacheStorage)),
+      )
+
+      expect(cacheStorage.size).toBe(1)
+    }).pipe(Effect.provide(TestContext.TestContext), Effect.runPromise),
+  )
 })
 
 describe('when the cached response does not match the original', () => {
