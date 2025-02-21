827e8352d938dbab7e73b19d807dca0bdf2f5997
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-02-21 11:21:50 +0000

    Specify missing behaviour for serilization checking with a failing test
    Refs: #2186

--- test/CachingHttpClient/SerializationErrorChecking.test.ts
+++ test/CachingHttpClient/SerializationErrorChecking.test.ts
@@ -27,5 +27,25 @@ describe('when the cached response matches the original', () => {
 })
 
 describe('when the cached response does not match the original', () => {
-  it.todo('the response is removed from the cache')
+  it.failing.prop([fc.url()])('the response is removed from the cache', url =>
+    Effect.gen(function* () {
+      const cacheStorage = new Map()
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      const healthySetMethod = cacheStorage.set
+      cacheStorage.set = function (key, value) {
+        return healthySetMethod.call(this, key, `${value} extra nonsense`)
+      }
+
+      const response = HttpClientResponse.fromWeb(HttpClientRequest.get(url), new Response())
+      const staleAt = yield* DateTime.now
+
+      yield* pipe(
+        HttpCache,
+        Effect.andThen(httpCache => httpCache.set(response, staleAt)),
+        Effect.provide(layerInMemory(cacheStorage)),
+      )
+
+      expect(cacheStorage.size).toBe(0)
+    }).pipe(Effect.provide(TestContext.TestContext), Effect.runPromise),
+  )
 })
