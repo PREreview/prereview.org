7a9434c10d7b8f4a509a27f17e4a7d23ab65483a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-02-21 12:03:00 +0000

    Improve legibility prior to implementing missing behaviour
    Refs: #2186

--- src/CachingHttpClient/SerializationErrorChecking.ts
+++ src/CachingHttpClient/SerializationErrorChecking.ts
@@ -3,50 +3,82 @@ import { diff } from 'deep-object-diff'
 import { type DateTime, Effect, pipe } from 'effect'
 import type * as HttpCache from './HttpCache.js'
 
+const diffResponses = (
+  responseA: HttpClientResponse.HttpClientResponse,
+  responseB: HttpClientResponse.HttpClientResponse,
+) =>
+  Effect.gen(function* () {
+    const diffableA = {
+      status: responseA.status,
+      headers: { ...responseA.headers },
+      body: yield* responseA.text,
+    }
+    const diffableB = {
+      status: responseB.status,
+      headers: { ...responseB.headers },
+      body: yield* responseB.text,
+    }
+    return diff(diffableA, diffableB)
+  })
+
+const isDifferent = (
+  responseA: HttpClientResponse.HttpClientResponse,
+  responseB: HttpClientResponse.HttpClientResponse,
+) =>
+  Effect.gen(function* () {
+    const difference = yield* diffResponses(responseA, responseB)
+    return Object.keys(difference).length !== 0
+  })
+
+const loggableDiff = (
+  responseA: HttpClientResponse.HttpClientResponse,
+  responseB: HttpClientResponse.HttpClientResponse,
+) =>
+  Effect.gen(function* () {
+    function replacer(_: unknown, value: unknown) {
+      if (value == undefined) {
+        return null
+      }
+      return value
+    }
+
+    const difference = yield* diffResponses(responseA, responseB)
+    return JSON.parse(JSON.stringify(difference, replacer)) as unknown
+  })
+
 export const serializationErrorChecking = (
   httpCache: typeof HttpCache.HttpCache.Service,
 ): typeof HttpCache.HttpCache.Service => ({
   ...httpCache,
   set: (response: HttpClientResponse.HttpClientResponse, staleAt: DateTime.Utc) =>
-    pipe(
-      httpCache.set(response, staleAt),
-      Effect.tap(() =>
-        Effect.gen(function* () {
-          const logAnnotations = {
-            url: response.request.url,
-            urlParams: UrlParams.toString(response.request.urlParams),
-            method: response.request.method,
-          }
-          const cachedValue = yield* pipe(
-            httpCache.get(response.request),
-            Effect.orElseSucceed(() => undefined),
+    Effect.gen(function* () {
+      yield* httpCache.set(response, staleAt)
+
+      const logAnnotations = {
+        url: response.request.url,
+        urlParams: UrlParams.toString(response.request.urlParams),
+        method: response.request.method,
+      }
+
+      Effect.gen(function* () {
+        const cachedValue = yield* pipe(
+          httpCache.get(response.request),
+          Effect.orElseSucceed(() => undefined),
+        )
+
+        if (cachedValue === undefined) {
+          yield* pipe('Cache entry not found after setting it', Effect.logError, Effect.annotateLogs(logAnnotations))
+          return
+        }
+
+        if (yield* isDifferent(response, cachedValue.response)) {
+          yield* pipe(
+            'Cached response does not equal original',
+            Effect.logError,
+            Effect.annotateLogs({ ...logAnnotations, diff: loggableDiff(response, cachedValue.response) }),
           )
-          if (cachedValue === undefined) {
-            return yield* Effect.logError('cache entry not found').pipe(Effect.annotateLogs(logAnnotations))
-          }
-          const origResponse = {
-            status: response.status,
-            headers: { ...response.headers },
-            body: yield* response.text,
-          }
-          const cachedResponse = {
-            status: cachedValue.response.status,
-            headers: cachedValue.response.headers,
-            body: yield* cachedValue.response.text,
-          }
-          const difference = diff(origResponse, cachedResponse)
-          function replacer(_: unknown, value: unknown) {
-            if (value == undefined) {
-              return null
-            }
-            return value
-          }
-          if (Object.keys(difference).length !== 0) {
-            return yield* Effect.logError('cached response does not equal original').pipe(
-              Effect.annotateLogs({ ...logAnnotations, diff: JSON.parse(JSON.stringify(difference, replacer)) }),
-            )
-          }
-        }),
-      ),
-    ),
+          return
+        }
+      })
+    }),
 })
