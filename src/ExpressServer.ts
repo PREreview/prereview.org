4b93b4a5c0de5b0574507cc4b22be4ca17379980
Chris Wilkinson <chris@prereview.org>
2025-02-24 11:25:57 +0000

    Remove redundant environment variable
    Refs #2173

--- src/ExpressServer.ts
+++ src/ExpressServer.ts
@@ -62,7 +62,7 @@ export const ExpressConfigLive = Effect.gen(function* () {
     canRequestReviews: () => true,
     canUploadAvatar: () => true,
     canUseSearchQueries: () => true,
-    canSeeAlternativeCompetingInterestsForm: env.CAN_SEE_ALTERNATIVE_COMPETING_INTERESTS_FORM,
+    canSeeAlternativeCompetingInterestsForm: true,
     cloudinaryApi: { cloudName: 'prereview', key: env.CLOUDINARY_API_KEY, secret: env.CLOUDINARY_API_SECRET },
     coarNotifyToken: env.COAR_NOTIFY_TOKEN,
     coarNotifyUrl: env.COAR_NOTIFY_URL,
