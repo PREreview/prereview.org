2d9e70732eca0a06a0fc636d426e747b5272dacb
Chris Wilkinson <chris@prereview.org>
2025-03-06 10:29:03 +0000

    Get ready to extract strings
    Refs #1817

--- src/app-router.ts
+++ src/app-router.ts
@@ -569,7 +569,14 @@ const router: P.Parser<RM.ReaderMiddleware<RouterEnv, StatusOpen, ResponseEnded,
         pipe(
           RM.of({}),
           RM.apS('user', maybeGetUser),
-          RM.apSW('response', RM.fromReaderTask(funding)),
+          RM.apS(
+            'locale',
+            RM.asks((env: RouterEnv) => env.locale),
+          ),
+          RM.bindW(
+            'response',
+            RM.fromReaderTaskK(({ locale }) => funding(locale)),
+          ),
           RM.ichainW(handleResponse),
         ),
       ),
