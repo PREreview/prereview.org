b99f73a9cf360c9e53e890bf3ef70dc63df70746
Chris Wilkinson <chris@prereview.org>
2025-03-06 11:02:49 +0000

    Extract resources string
    Refs #1817

--- src/resources.ts
+++ src/resources.ts
@@ -2,9 +2,9 @@ import { pipe } from 'effect'
 import { format } from 'fp-ts-routing'
 import * as RTE from 'fp-ts/lib/ReaderTaskEither.js'
 import { getPageFromGhost } from './GhostPage.js'
-import { type Html, fixHeadingLevels, html, plainText } from './html.js'
+import { fixHeadingLevels, html, plainText, type Html } from './html.js'
 import { havingProblemsPage } from './http-error.js'
-import type { SupportedLocale } from './locales/index.js'
+import { translate, type SupportedLocale } from './locales/index.js'
 import { PageResponse } from './response.js'
 import { resourcesMatch } from './routes.js'
 
@@ -17,11 +17,13 @@ export const resources = (locale: SupportedLocale) =>
     RTE.matchW(() => havingProblemsPage, createPage),
   )
 
-function createPage({ content }: { content: Html; locale: SupportedLocale }) {
+function createPage({ content, locale }: { content: Html; locale: SupportedLocale }) {
+  const t = translate(locale)
+
   return PageResponse({
-    title: plainText`Resources`,
+    title: plainText(t('resources', 'resources')()),
     main: html`
-      <h1>Resources</h1>
+      <h1>${t('resources', 'resources')()}</h1>
 
       ${fixHeadingLevels(1, content)}
     `,
