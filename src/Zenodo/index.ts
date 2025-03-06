0eda8aa4574b1ece858eb4071a670d511cb5c1eb
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-03-06 14:35:50 +0000

    Start implementing the new adapter
    Refs #2282

--- src/Zenodo/index.ts
+++ src/Zenodo/index.ts
@@ -1,7 +1,21 @@
+import { type HttpClient, UrlParams } from '@effect/platform'
 import type * as Doi from 'doi-ts'
-import type { Effect } from 'effect'
+import { Effect, pipe } from 'effect'
 import type * as ReviewPage from '../review-page/index.js'
+import { getCommunityRecords } from './CommunityRecords.js'
 
-export declare const getCommentsForPrereviewFromZenodo: (
+export const getCommentsForPrereviewFromZenodo = (
   id: Doi.Doi,
-) => Effect.Effect<ReadonlyArray<ReviewPage.Comment>, 'unavailable'>
+): Effect.Effect<ReadonlyArray<ReviewPage.Comment>, 'unavailable', HttpClient.HttpClient> =>
+  pipe(
+    UrlParams.fromInput({
+      q: `related.identifier:"${id}"`,
+      size: '100',
+      sort: 'publication-desc',
+      resource_type: 'publication::publication-other',
+      access_status: 'open',
+    }),
+    getCommunityRecords,
+    Effect.orElseFail(() => 'unavailable' as const),
+    Effect.andThen(() => Effect.fail('unavailable' as const)),
+  )
