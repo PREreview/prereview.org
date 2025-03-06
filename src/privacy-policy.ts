294bc096e0bc41ee01455b23430d3404039cc014
Chris Wilkinson <chris@prereview.org>
2025-03-06 10:55:29 +0000

    Extract privacy-policy string
    Refs #1817

--- src/privacy-policy.ts
+++ src/privacy-policy.ts
@@ -2,9 +2,9 @@ import { pipe } from 'effect'
 import { format } from 'fp-ts-routing'
 import * as RTE from 'fp-ts/lib/ReaderTaskEither.js'
 import { getPageFromGhost } from './GhostPage.js'
-import { type Html, fixHeadingLevels, html, plainText } from './html.js'
+import { fixHeadingLevels, html, plainText, type Html } from './html.js'
 import { havingProblemsPage } from './http-error.js'
-import type { SupportedLocale } from './locales/index.js'
+import { translate, type SupportedLocale } from './locales/index.js'
 import { PageResponse } from './response.js'
 import { privacyPolicyMatch } from './routes.js'
 
@@ -16,11 +16,13 @@ export const privacyPolicy = (locale: SupportedLocale) =>
     RTE.matchW(() => havingProblemsPage, createPage),
   )
 
-function createPage({ content }: { content: Html; locale: SupportedLocale }) {
+function createPage({ content, locale }: { content: Html; locale: SupportedLocale }) {
+  const t = translate(locale)
+
   return PageResponse({
-    title: plainText`Privacy Policy`,
+    title: plainText(t('privacy-policy', 'privacyPolicy')()),
     main: html`
-      <h1>Privacy Policy</h1>
+      <h1>${t('privacy-policy', 'privacyPolicy')()}</h1>
 
       ${fixHeadingLevels(1, content)}
     `,
