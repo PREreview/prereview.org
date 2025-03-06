67789f17e8d60dd7625b02329902f2b3f48c1bc6
Chris Wilkinson <chris@prereview.org>
2025-03-06 15:23:53 +0000

    Partially implement transformation
    Refs #2282

--- src/Zenodo/TransformRecordToCommentWithoutText.ts
+++ src/Zenodo/TransformRecordToCommentWithoutText.ts
@@ -1,7 +1,17 @@
-import type { Either } from 'effect'
+import { toTemporalInstant } from '@js-temporal/polyfill'
+import { Either } from 'effect'
 import type { Record } from 'zenodo-ts'
 import type * as ReviewPage from '../review-page/index.js'
 
 export type CommentWithoutText = Omit<ReviewPage.Comment, 'text'> & { textUrl: URL }
 
-export declare const transformRecordToCommentWithoutText: (record: Record) => Either.Either<CommentWithoutText>
+export const transformRecordToCommentWithoutText = (record: Record): Either.Either<CommentWithoutText> =>
+  Either.right({
+    authors: { named: record.metadata.creators },
+    doi: record.metadata.doi,
+    language: 'en',
+    id: record.id,
+    license: 'CC-BY-4.0',
+    published: toTemporalInstant.call(record.metadata.publication_date).toZonedDateTimeISO('UTC').toPlainDate(),
+    textUrl: new URL('http://example.com'),
+  })
