b10c92fac9dc486422079c5ad79d1a124a166f2f
Chris Wilkinson <chris@prereview.org>
2025-03-06 10:25:08 +0000

    Get ready to extract strings
    Refs #1817

--- src/app-router.ts
+++ src/app-router.ts
@@ -533,7 +533,14 @@ const router: P.Parser<RM.ReaderMiddleware<RouterEnv, StatusOpen, ResponseEnded,
         pipe(
           RM.of({}),
           RM.apS('user', maybeGetUser),
-          RM.apSW('response', RM.fromReaderTask(ediaStatement)),
+          RM.apS(
+            'locale',
+            RM.asks((env: RouterEnv) => env.locale),
+          ),
+          RM.bindW(
+            'response',
+            RM.fromReaderTaskK(({ locale }) => ediaStatement(locale)),
+          ),
           RM.ichainW(handleResponse),
         ),
       ),
--- src/edia-statement.ts
+++ src/edia-statement.ts
@@ -4,15 +4,19 @@ import * as RTE from 'fp-ts/lib/ReaderTaskEither.js'
 import { getPageFromGhost } from './GhostPage.js'
 import { type Html, fixHeadingLevels, html, plainText } from './html.js'
 import { havingProblemsPage } from './http-error.js'
+import type { SupportedLocale } from './locales/index.js'
 import { PageResponse } from './response.js'
 import { ediaStatementMatch } from './routes.js'
 
-export const ediaStatement = pipe(
-  getPageFromGhost('6154aa157741400e8722bb17'),
-  RTE.matchW(() => havingProblemsPage, createPage),
-)
+export const ediaStatement = (locale: SupportedLocale) =>
+  pipe(
+    RTE.Do,
+    RTE.let('locale', () => locale),
+    RTE.apS('content', getPageFromGhost('6154aa157741400e8722bb17')),
+    RTE.matchW(() => havingProblemsPage, createPage),
+  )
 
-function createPage(content: Html) {
+function createPage({ content }: { content: Html; locale: SupportedLocale }) {
   return PageResponse({
     title: plainText`Equity, Diversity, Inclusion, and Accessibility Statement`,
     main: html`
