b555ae08468f86d98dca56f351f389188a8a1856
Chris Wilkinson <chris@prereview.org>
2025-03-06 10:45:32 +0000

    Get ready to extract strings
    Refs #1817

--- src/app-router.ts
+++ src/app-router.ts
@@ -620,7 +620,14 @@ const router: P.Parser<RM.ReaderMiddleware<RouterEnv, StatusOpen, ResponseEnded,
         pipe(
           RM.of({}),
           RM.apS('user', maybeGetUser),
-          RM.apSW('response', RM.fromReaderTask(liveReviews)),
+          RM.apS(
+            'locale',
+            RM.asks((env: RouterEnv) => env.locale),
+          ),
+          RM.bindW(
+            'response',
+            RM.fromReaderTaskK(({ locale }) => liveReviews(locale)),
+          ),
           RM.ichainW(handleResponse),
         ),
       ),
--- src/live-reviews.ts
+++ src/live-reviews.ts
@@ -4,15 +4,19 @@ import * as RTE from 'fp-ts/lib/ReaderTaskEither.js'
 import { getPageFromGhost } from './GhostPage.js'
 import { type Html, fixHeadingLevels, html, plainText } from './html.js'
 import { havingProblemsPage } from './http-error.js'
+import type { SupportedLocale } from './locales/index.js'
 import { PageResponse } from './response.js'
 import { liveReviewsMatch } from './routes.js'
 
-export const liveReviews = pipe(
-  getPageFromGhost('6154aa157741400e8722bb10'),
-  RTE.matchW(() => havingProblemsPage, createPage),
-)
+export const liveReviews = (locale: SupportedLocale) =>
+  pipe(
+    RTE.Do,
+    RTE.let('locale', () => locale),
+    RTE.apS('content', getPageFromGhost('6154aa157741400e8722bb10')),
+    RTE.matchW(() => havingProblemsPage, createPage),
+  )
 
-function createPage(content: Html) {
+function createPage({ content }: { content: Html; locale: SupportedLocale }) {
   return PageResponse({
     title: plainText`Live Reviews`,
     main: html`
