697043c7bc9fb86832346c8a70a9f6f645dbd10e
Chris Wilkinson <chris@prereview.org>
2025-03-06 10:31:35 +0000

    Extract how-we're-funded string
    Refs #1817

--- src/funding.ts
+++ src/funding.ts
@@ -2,9 +2,9 @@ import { pipe } from 'effect'
 import { format } from 'fp-ts-routing'
 import * as RTE from 'fp-ts/lib/ReaderTaskEither.js'
 import { getPageFromGhost } from './GhostPage.js'
-import { type Html, fixHeadingLevels, html, plainText } from './html.js'
+import { fixHeadingLevels, html, plainText, type Html } from './html.js'
 import { havingProblemsPage } from './http-error.js'
-import type { SupportedLocale } from './locales/index.js'
+import { translate, type SupportedLocale } from './locales/index.js'
 import { PageResponse } from './response.js'
 import { fundingMatch } from './routes.js'
 
@@ -16,11 +16,13 @@ export const funding = (locale: SupportedLocale) =>
     RTE.matchW(() => havingProblemsPage, createPage),
   )
 
-function createPage({ content }: { content: Html; locale: SupportedLocale }) {
+function createPage({ content, locale }: { content: Html; locale: SupportedLocale }) {
+  const t = translate(locale)
+
   return PageResponse({
-    title: plainText`How we’re funded`,
+    title: plainText(t('funding', 'howWeFunded')()),
     main: html`
-      <h1>How we’re funded</h1>
+      <h1>${t('funding', 'howWeFunded')()}</h1>
 
       ${fixHeadingLevels(1, content)}
     `,
