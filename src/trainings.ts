70b35725c340e388a3428732f9f3660cd3749838
Chris Wilkinson <chris@prereview.org>
2025-03-06 11:04:37 +0000

    Get ready to extract strings
    Refs #1817

--- src/trainings.ts
+++ src/trainings.ts
@@ -1,20 +1,23 @@
 import { Effect } from 'effect'
 import { format } from 'fp-ts-routing'
+import { Locale } from './Context.js'
 import { GetPageFromGhost } from './GhostPage.js'
 import { HavingProblemsPage } from './HavingProblemsPage/index.js'
 import { type Html, fixHeadingLevels, html, plainText } from './html.js'
+import type { SupportedLocale } from './locales/index.js'
 import { PageResponse } from './response.js'
 import { trainingsMatch } from './routes.js'
 
 export const TrainingsPage = Effect.gen(function* () {
   const getPageFromGhost = yield* GetPageFromGhost
+  const locale = yield* Locale
 
   const content = yield* getPageFromGhost('64639b5007fb34a92c7f8518')
 
-  return createPage(content)
+  return createPage({ content, locale })
 }).pipe(Effect.catchAll(() => HavingProblemsPage))
 
-function createPage(content: Html) {
+function createPage({ content }: { content: Html; locale: SupportedLocale }) {
   return PageResponse({
     title: plainText`Trainings`,
     main: html`
