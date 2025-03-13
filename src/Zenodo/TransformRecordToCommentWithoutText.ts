d27f47e5cd75668e509cbd22c0dc052b4abef41f
Chris Wilkinson <chris@prereview.org>
2025-03-13 13:37:23 +0000

    Sketch out a more specific type for reducing the amount of logic in the transformation
    Refs #2282

--- src/Zenodo/TransformRecordToCommentWithoutText.ts
+++ src/Zenodo/TransformRecordToCommentWithoutText.ts
@@ -1,9 +1,36 @@
-import { toTemporalInstant } from '@js-temporal/polyfill'
-import { Either } from 'effect'
+import { type Temporal, toTemporalInstant } from '@js-temporal/polyfill'
+import type * as Doi from 'doi-ts'
+import { type Array, Either } from 'effect'
+import type * as Orcid from 'orcid-id-ts'
 import type { Record } from 'zenodo-ts'
 import type * as ReviewPage from '../review-page/index.js'
+import type * as Iso639 from '../types/iso639.js'
 
 export type CommentWithoutText = Omit<ReviewPage.Comment, 'text'> & { textUrl: URL }
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface ZenodoRecordForAComment {
+  id: number
+  files: Array.NonEmptyArray<{
+    key: string
+    links: {
+      self: URL
+    }
+  }>
+  metadata: {
+    access_right: 'open'
+    creators: Array.NonEmptyReadonlyArray<{
+      name: string
+      orcid?: Orcid.Orcid
+    }>
+    doi: Doi.Doi
+    language: Iso639.Iso6393Code
+    license: {
+      id: 'CC-BY-4.0'
+    }
+    publication_date: Temporal.PlainDate
+  }
+}
 
 export const transformRecordToCommentWithoutText = (record: Record): Either.Either<CommentWithoutText> =>
   Either.right({
