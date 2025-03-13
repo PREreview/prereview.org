0aa321f905293ad147fc72f5100f1a11f216bcc8
Chris Wilkinson <chris@prereview.org>
2025-03-13 11:37:40 +0000

    Extract unknown-PhilSci-preprint strings
    Refs #1817

--- src/review-a-preprint-page/unknown-philsci-preprint-page.ts
+++ src/review-a-preprint-page/unknown-philsci-preprint-page.ts
@@ -1,31 +1,42 @@
 import { format } from 'fp-ts-routing'
 import { Status } from 'hyper-ts'
-import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { html, plainText, rawHtml } from '../html.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 import { reviewAPreprintMatch } from '../routes.js'
 import type { PhilsciPreprintId } from '../types/preprint-id.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const createUnknownPhilsciPreprintPage = (preprint: PhilsciPreprintId, locale: SupportedLocale) =>
   PageResponse({
     status: Status.BadRequest,
-    title: plainText`Sorry, we don’t know this preprint`,
+    title: plainText(translate(locale, 'review-a-preprint', 'dontKnowPreprint')()),
     main: html`
-      <h1>Sorry, we don’t know this preprint</h1>
+      <h1>${translate(locale, 'review-a-preprint', 'dontKnowPreprint')()}</h1>
 
       <p>
-        We think the URL
-        <q class="select-all" translate="no">https://philsci-archive.pitt.edu/${preprint.value}/</q> could be a
-        PhilSci-Archive preprint, but we can’t find any details.
+        ${rawHtml(
+          translate(
+            locale,
+            'review-a-preprint',
+            'urlCouldBePhilsci',
+          )({
+            url: html`<q class="select-all" translate="no"
+              >https://philsci-archive.pitt.edu/${preprint.value}/</q
+            >`.toString(),
+          }),
+        )}
       </p>
 
-      <p>If you typed the URL, check it is correct.</p>
+      <p>${translate(locale, 'review-a-preprint', 'checkCorrectUrl')()}</p>
 
-      <p>If you pasted the URL, check you copied the entire address.</p>
+      <p>${translate(locale, 'review-a-preprint', 'checkPastedUrl')()}</p>
 
-      <p>If the URL is correct, please <a href="mailto:help@prereview.org">get in touch</a>.</p>
+      <p>${rawHtml(translate(locale, 'review-a-preprint', 'urlIsCorrect')({ contact: mailToHelp }))}</p>
 
-      <a href="${format(reviewAPreprintMatch.formatter, {})}" class="button">Back</a>
+      <a href="${format(reviewAPreprintMatch.formatter, {})}" class="button"
+        >${translate(locale, 'review-a-preprint', 'back')()}</a
+      >
     `,
   })
+
+const mailToHelp = (text: string) => html`<a href="mailto:help@prereview.org">${text}</a>`.toString()
