f3f4e3b9bfdb577de33cd3336d372ef5b25a46dd
Chris Wilkinson <chris@prereview.org>
2025-03-13 11:26:17 +0000

    Extract unsupported-DOI strings
    Refs #1817

--- src/review-a-preprint-page/unsupported-doi-page.ts
+++ src/review-a-preprint-page/unsupported-doi-page.ts
@@ -1,30 +1,25 @@
 import { format } from 'fp-ts-routing'
 import { Status } from 'hyper-ts'
-import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { html, plainText, rawHtml } from '../html.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 import { reviewAPreprintMatch } from '../routes.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const unsupportedDoiPage = (locale: SupportedLocale) =>
   PageResponse({
     status: Status.BadRequest,
-    title: plainText`Sorry, we don’t support this DOI`,
+    title: plainText(translate(locale, 'review-a-preprint', 'unsupportedDoi')()),
     main: html`
-      <h1>Sorry, we don’t support this DOI</h1>
+      <h1>${translate(locale, 'review-a-preprint', 'unsupportedDoi')()}</h1>
 
-      <p>
-        We support preprints from Advance, AfricArXiv, Arcadia&nbsp;Science, arXiv, Authorea, bioRxiv, ChemRxiv,
-        Curvenote, EarthArXiv, EcoEvoRxiv, EdArXiv, engrXiv, Jxiv, medRxiv, MetaArXiv, OSF, PhilSci-Archive,
-        Preprints.org, PsyArXiv, PsychArchives, Research&nbsp;Square, SciELO, ScienceOpen, SocArXiv, TechRxiv, VeriXiv
-        and Zenodo.
-      </p>
+      <p>${translate(locale, 'review-a-preprint', 'supportPreprintsFrom')()}</p>
 
-      <p>
-        If this DOI is for a preprint on a server we don’t support, please
-        <a href="mailto:help@prereview.org">get in touch</a>.
-      </p>
+      <p>${rawHtml(translate(locale, 'review-a-preprint', 'doiIsForPreprint')({ contact: mailToHelp }))}</p>
 
-      <a href="${format(reviewAPreprintMatch.formatter, {})}" class="button">Back</a>
+      <a href="${format(reviewAPreprintMatch.formatter, {})}" class="button"
+        >${translate(locale, 'review-a-preprint', 'back')()}</a
+      >
     `,
   })
+
+const mailToHelp = (text: string) => html`<a href="mailto:help@prereview.org">${text}</a>`.toString()
