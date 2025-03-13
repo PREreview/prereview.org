986745b87e8182042ea2a993414a5368875578bb
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-03-13 14:00:08 +0000

    Start to implement the Schema
    Currently wrapped in an anonymous function to avoid initialisation during tests causing a failure due to use unimplemented Schemas.
    
    Refs: #2282

--- src/Zenodo/TransformRecordToCommentWithoutText.ts
+++ src/Zenodo/TransformRecordToCommentWithoutText.ts
@@ -1,6 +1,6 @@
 import { type Temporal, toTemporalInstant } from '@js-temporal/polyfill'
 import type * as Doi from 'doi-ts'
-import { type Array, Either } from 'effect'
+import { type Array, Either, Schema } from 'effect'
 import type * as Orcid from 'orcid-id-ts'
 import type { Record } from 'zenodo-ts'
 import type * as ReviewPage from '../review-page/index.js'
@@ -8,10 +8,47 @@ import type * as Iso639 from '../types/iso639.js'
 
 export type CommentWithoutText = Omit<ReviewPage.Comment, 'text'> & { textUrl: URL }
 
+declare const OrcidSchema: Schema.Schema<Orcid.Orcid, unknown>
+
+declare const DoiSchema: Schema.Schema<Doi.Doi, unknown>
+
+declare const Iso6393Schema: Schema.Schema<Iso639.Iso6393Code, unknown>
+
+declare const PlainDateSchema: Schema.Schema<Temporal.PlainDate, unknown>
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const ZenodoRecordForACommentSchema = () =>
+  Schema.Struct({
+    id: Schema.Number,
+    files: Schema.NonEmptyArray(
+      Schema.Struct({
+        key: Schema.String,
+        links: Schema.Struct({
+          self: Schema.URL,
+        }),
+      }),
+    ),
+    metadata: Schema.Struct({
+      access_right: Schema.Literal('open'),
+      creators: Schema.NonEmptyArray(
+        Schema.Struct({
+          name: Schema.String,
+          orcid: Schema.optionalWith(OrcidSchema, { exact: true }),
+        }),
+      ),
+      doi: DoiSchema,
+      language: Iso6393Schema,
+      license: Schema.Struct({
+        id: Schema.Literal('CC-BY-4.0'),
+      }),
+      publication_date: PlainDateSchema,
+    }),
+  })
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface ZenodoRecordForAComment {
   id: number
-  files: Array.NonEmptyArray<{
+  files: Array.NonEmptyReadonlyArray<{
     key: string
     links: {
       self: URL
