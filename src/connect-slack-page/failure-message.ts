112b16475a2231ba3f328560aa516b90f6ebc30a
Chris Wilkinson <chris@prereview.org>
2025-03-17 12:46:37 +0000

    Extract having-problems strings
    Refs #1817

--- src/connect-slack-page/failure-message.ts
+++ src/connect-slack-page/failure-message.ts
@@ -1,18 +1,17 @@
 import { Status } from 'hyper-ts'
 import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const failureMessage = (locale: SupportedLocale) =>
   PageResponse({
-    title: plainText`Sorry, we’re having problems`,
+    title: plainText(translate(locale, 'connect-slack-page', 'havingProblems')()),
     status: Status.ServiceUnavailable,
     main: html`
-      <h1>Sorry, we’re having problems</h1>
+      <h1>${translate(locale, 'connect-slack-page', 'havingProblems')()}</h1>
 
-      <p>We’re unable to connect your account right now.</p>
+      <p>${translate(locale, 'connect-slack-page', 'unableToConnect')()}</p>
 
-      <p>Please try again later.</p>
+      <p>${translate(locale, 'connect-slack-page', 'tryAgainLater')()}</p>
     `,
   })
