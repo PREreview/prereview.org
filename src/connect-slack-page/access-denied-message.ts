1fca3e4ff77411d1785cd63a18e2056a8422380d
Chris Wilkinson <chris@prereview.org>
2025-03-17 12:42:15 +0000

    Extract denied-access strings
    Refs #1817

--- src/connect-slack-page/access-denied-message.ts
+++ src/connect-slack-page/access-denied-message.ts
@@ -1,18 +1,17 @@
 import { Status } from 'hyper-ts'
 import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const accessDeniedMessage = (locale: SupportedLocale) =>
   PageResponse({
-    title: plainText`Sorry, we can’t connect your account`,
+    title: plainText(translate(locale, 'connect-slack-page', 'cannotConnect')()),
     status: Status.Forbidden,
     main: html`
-      <h1>Sorry, we can’t connect your account</h1>
+      <h1>${translate(locale, 'connect-slack-page', 'cannotConnect')()}</h1>
 
-      <p>You have denied PREreview access to your Community Slack account.</p>
+      <p>${translate(locale, 'connect-slack-page', 'youDeniedAccess')()}</p>
 
-      <p>Please try again.</p>
+      <p>${translate(locale, 'connect-slack-page', 'tryAgain')()}</p>
     `,
   })
