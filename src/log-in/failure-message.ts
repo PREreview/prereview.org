663b95c729293bdd7553735f87ea0018e8afd4e2
Chris Wilkinson <chris@prereview.org>
2025-03-18 13:16:44 +0000

    Extract failure-message strings
    Refs #1817

--- src/log-in/failure-message.ts
+++ src/log-in/failure-message.ts
@@ -1,18 +1,17 @@
 import { Status } from 'hyper-ts'
 import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const failureMessage = (locale: SupportedLocale) =>
   PageResponse({
-    title: plainText`Sorry, we’re having problems`,
+    title: plainText(translate(locale, 'log-in', 'havingProblems')()),
     status: Status.ServiceUnavailable,
     main: html`
-      <h1>Sorry, we’re having problems</h1>
+      <h1>${translate(locale, 'log-in', 'havingProblems')()}</h1>
 
-      <p>We’re unable to log you in right now.</p>
+      <p>${translate(locale, 'log-in', 'unableToLogYouIn')()}</p>
 
-      <p>Please try again later.</p>
+      <p>${translate(locale, 'log-in', 'tryAgainLater')()}</p>
     `,
   })
