f1562db73773f2fe3248fe6f89f98714952ae37a
Chris Wilkinson <chris@prereview.org>
2025-03-18 13:20:30 +0000

    Extract access-denied strings
    Refs #1817

--- src/log-in/access-denied-message.ts
+++ src/log-in/access-denied-message.ts
@@ -1,18 +1,17 @@
 import { Status } from 'hyper-ts'
 import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const accessDeniedMessage = (locale: SupportedLocale) =>
   PageResponse({
-    title: plainText`Sorry, we can’t log you in`,
+    title: plainText(translate(locale, 'log-in', 'cannotLogYouIn')()),
     status: Status.Forbidden,
     main: html`
-      <h1>Sorry, we can’t log you in</h1>
+      <h1>${translate(locale, 'log-in', 'cannotLogYouIn')()}</h1>
 
-      <p>You have denied PREreview access to your ORCID&nbsp;iD.</p>
+      <p>${translate(locale, 'log-in', 'youDeniedAccess')()}</p>
 
-      <p>Please try again.</p>
+      <p>${translate(locale, 'log-in', 'tryAgain')()}</p>
     `,
   })
