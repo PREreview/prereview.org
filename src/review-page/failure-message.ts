f0bae82e788e3daec753b45073b34dbe3b06e2a7
Chris Wilkinson <chris@prereview.org>
2025-03-19 14:12:36 +0000

    Extract having-problems strings
    Refs #1817

--- src/review-page/failure-message.ts
+++ src/review-page/failure-message.ts
@@ -1,18 +1,17 @@
 import { Status } from 'hyper-ts'
 import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const failureMessage = (locale: SupportedLocale) =>
   PageResponse({
     status: Status.ServiceUnavailable,
-    title: plainText`Sorry, we’re having problems`,
+    title: plainText(translate(locale, 'review-page', 'havingProblems')()),
     main: html`
-      <h1>Sorry, we’re having problems</h1>
+      <h1>${translate(locale, 'review-page', 'havingProblems')()}</h1>
 
-      <p>We’re unable to show the PREreview now.</p>
+      <p>${translate(locale, 'review-page', 'unableToShow')()}</p>
 
-      <p>Please try again later.</p>
+      <p>${translate(locale, 'review-page', 'tryAgainLater')()}</p>
     `,
   })
