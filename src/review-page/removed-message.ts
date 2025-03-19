0e05f505329da2a9ff161f73f499204a8f02ccf9
Chris Wilkinson <chris@prereview.org>
2025-03-19 14:09:42 +0000

    Extract PREreview-removed strings
    Refs #1817

--- src/review-page/removed-message.ts
+++ src/review-page/removed-message.ts
@@ -1,16 +1,15 @@
 import { Status } from 'hyper-ts'
 import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const removedMessage = (locale: SupportedLocale) =>
   PageResponse({
     status: Status.Gone,
-    title: plainText`PREreview removed`,
+    title: plainText(translate(locale, 'review-page', 'prereviewRemoved')()),
     main: html`
-      <h1>PREreview removed</h1>
+      <h1>${translate(locale, 'review-page', 'prereviewRemoved')()}</h1>
 
-      <p>We’ve removed this PREreview.</p>
+      <p>${translate(locale, 'review-page', 'weHaveRemoved')()}</p>
     `,
   })
