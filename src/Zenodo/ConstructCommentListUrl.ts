f1b3cfb8f40497ed2c2610fed17f4924b0c4690e
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-03-20 15:51:46 +0000

    Make test pass by copying existing implementation
    Refs: #2317

--- src/Zenodo/ConstructCommentListUrl.ts
+++ src/Zenodo/ConstructCommentListUrl.ts
@@ -1,7 +1,18 @@
+import { Url, UrlParams } from '@effect/platform'
 import type * as Doi from 'doi-ts'
 import { Effect } from 'effect'
 import { ZenodoOrigin } from './CommunityRecords.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const constructCommentListUrl = (prereviewDoi: Doi.Doi): Effect.Effect<URL, never, ZenodoOrigin> =>
-  Effect.succeed(new URL('example.com'))
+  Effect.gen(function* () {
+    const zenodoOrigin = yield* ZenodoOrigin
+    const zenodoCommunityRecordsApiUrl = new URL('/api/communities/prereview-reviews/records', zenodoOrigin)
+    const params = UrlParams.fromInput({
+      q: `related.identifier:"${prereviewDoi}"`,
+      size: '100',
+      sort: 'publication-desc',
+      resource_type: 'publication::publication-other',
+      access_status: 'open',
+    })
+    return Url.setUrlParams(zenodoCommunityRecordsApiUrl, params)
+  })
--- test/Zenodo/ConstructCommentListUrl.test.ts
+++ test/Zenodo/ConstructCommentListUrl.test.ts
@@ -6,14 +6,13 @@ import * as Doi from '../../src/types/Doi.js'
 import * as EffectTest from '../EffectTest.js'
 
 describe('ConstructCommentListUrl', () => {
-  it.failing('constructs a valid url', () =>
+  it('constructs a valid url', () =>
     Effect.gen(function* () {
       const prereviewDoi = Doi.Doi('10.1101/12345')
       const expectedUrl =
-        'http://zenodo.test/api/communities/prereview-reviews/records?q=related.identifier:"10.1101/12345"&size=100&sort=publication-desc&resource_type=publication::publication-other&access_status=open'
+        'http://zenodo.test/api/communities/prereview-reviews/records?q=related.identifier%3A%2210.1101%2F12345%22&size=100&sort=publication-desc&resource_type=publication%3A%3Apublication-other&access_status=open'
       const result = yield* _.constructCommentListUrl(prereviewDoi)
 
-      expect(result).toBe(expectedUrl)
-    }).pipe(Effect.provideService(ZenodoOrigin, new URL('http://zenodo.test')), EffectTest.run),
-  )
+      expect(result.href).toStrictEqual(expectedUrl)
+    }).pipe(Effect.provideService(ZenodoOrigin, new URL('http://zenodo.test')), EffectTest.run))
 })
