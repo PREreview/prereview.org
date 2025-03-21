7d2c5a8920c59880eaf19a2d72da60d8dca82985
Chris Wilkinson <chris@prereview.org>
2025-03-21 10:45:14 +0000

    Use the Effect HttpClient as a fetch instance when the feature is enabled
    Refs #2321

--- src/FetchHttpClient.ts
+++ src/FetchHttpClient.ts
@@ -1,6 +1,15 @@
-import { FetchHttpClient } from '@effect/platform'
-import { Config, Effect } from 'effect'
+import {
+  FetchHttpClient,
+  HttpBody,
+  type HttpClient,
+  type HttpClientError,
+  HttpClientRequest,
+  type HttpClientResponse,
+  HttpMethod,
+} from '@effect/platform'
+import { Config, Effect, flow, pipe, Runtime } from 'effect'
 import fetch from 'make-fetch-happen'
+import * as CachingHttpClient from './CachingHttpClient/index.js'
 import { PublicUrl } from './public-url.js'
 
 export const Fetch = FetchHttpClient.Fetch
@@ -13,7 +22,7 @@ export const makeFetch = Effect.gen(function* () {
   )
 
   if (disableLegacyVolumeBasedCache) {
-    yield* Effect.logDebug('Legacy cache should be disabled')
+    return yield* transmogrifyHttpClient
   }
 
   return fetch.defaults({
@@ -23,3 +32,52 @@ export const makeFetch = Effect.gen(function* () {
     },
   }) as unknown as typeof globalThis.fetch
 })
+
+const transmogrifyHttpClient: Effect.Effect<
+  typeof globalThis.fetch,
+  never,
+  HttpClient.HttpClient | CachingHttpClient.HttpCache
+> = Effect.gen(function* () {
+  const client = yield* CachingHttpClient.CachingHttpClient('10 seconds')
+  const runtime = yield* Effect.runtime()
+
+  return flow(
+    convertRequest,
+    Effect.andThen(client.execute),
+    Effect.andThen(convertResponse),
+    Effect.scoped,
+    Runtime.runPromise(runtime),
+  )
+})
+
+const convertRequest = (
+  input: string | URL | globalThis.Request,
+  init?: RequestInit,
+): Effect.Effect<HttpClientRequest.HttpClientRequest, Error> =>
+  Effect.gen(function* () {
+    const request = new Request(input, init)
+    const method = request.method
+
+    if (!HttpMethod.isHttpMethod(method)) {
+      return yield* Effect.fail(new Error('Unsupported method'))
+    }
+
+    if (!HttpMethod.hasBody(method)) {
+      return HttpClientRequest.make(method)(request.url, { headers: request.headers })
+    }
+
+    const body = yield* Effect.tryPromise({
+      try: () => request.text().then(text => HttpBody.text(text, request.headers.get('Content-Type') ?? undefined)),
+      catch: () => new Error('Unable to read body'),
+    })
+
+    return HttpClientRequest.make(method)(request.url, { headers: request.headers, body })
+  })
+
+const convertResponse = (
+  response: HttpClientResponse.HttpClientResponse,
+): Effect.Effect<Response, HttpClientError.ResponseError> =>
+  pipe(
+    response.text,
+    Effect.andThen(body => new Response(body, { headers: response.headers, status: response.status })),
+  )
