188410b62cad4b016048101d90a047bde22122bb
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-03-21 10:25:00 +0000

    Introduce a no-op feature flag
    Refs: #2321

--- src/FetchHttpClient.ts
+++ src/FetchHttpClient.ts
@@ -1,5 +1,5 @@
 import { FetchHttpClient } from '@effect/platform'
-import { Effect } from 'effect'
+import { Config, Effect } from 'effect'
 import fetch from 'make-fetch-happen'
 import { PublicUrl } from './public-url.js'
 
@@ -7,6 +7,14 @@ export const Fetch = FetchHttpClient.Fetch
 
 export const makeFetch = Effect.gen(function* () {
   const publicUrl = yield* PublicUrl
+  const disableLegacyVolumeBasedCache = yield* Config.withDefault(
+    Config.boolean('DISABLE_LEGACY_VOLUME_BASED_CACHE'),
+    false,
+  )
+
+  if (disableLegacyVolumeBasedCache) {
+    yield* Effect.logDebug('Legacy cache should be disabled')
+  }
 
   return fetch.defaults({
     cachePath: 'data/cache',
