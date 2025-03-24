c870422b74ea3683a1738b728d88c00e0d693e55
Chris Wilkinson <chris@prereview.org>
2025-03-24 15:50:57 +0000

    Extract not-a-preprint strings
    Refs #1817

--- src/request-a-prereview-page/not-a-preprint-page.ts
+++ src/request-a-prereview-page/not-a-preprint-page.ts
@@ -1,27 +1,25 @@
 import { format } from 'fp-ts-routing'
 import { Status } from 'hyper-ts'
-import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { html, plainText, rawHtml } from '../html.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 import { requestAPrereviewMatch } from '../routes.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const notAPreprintPage = (locale: SupportedLocale) =>
   PageResponse({
     status: Status.BadRequest,
-    title: plainText`Sorry, we only support preprints`,
+    title: plainText(translate(locale, 'request-a-prereview-page', 'notAPreprint')()),
     main: html`
-      <h1>Sorry, we only support preprints</h1>
+      <h1>${translate(locale, 'request-a-prereview-page', 'notAPreprint')()}</h1>
 
-      <p>
-        We support preprints from Advance, AfricArXiv, Arcadia&nbsp;Science, arXiv, Authorea, bioRxiv, ChemRxiv,
-        Curvenote, EarthArXiv, EcoEvoRxiv, EdArXiv, engrXiv, Jxiv, medRxiv, MetaArXiv, OSF, PhilSci-Archive,
-        Preprints.org, PsyArXiv, PsychArchives, Research&nbsp;Square, SciELO, ScienceOpen, SocArXiv, TechRxiv, VeriXiv
-        and Zenodo.
-      </p>
+      <p>${translate(locale, 'request-a-prereview-page', 'supportPreprintsFrom')()}</p>
 
-      <p>If this is a preprint, please <a href="mailto:help@prereview.org">get in touch</a>.</p>
+      <p>${rawHtml(translate(locale, 'request-a-prereview-page', 'isAPreprint')({ contact: mailToHelp }))}</p>
 
-      <a href="${format(requestAPrereviewMatch.formatter, {})}" class="button">Back</a>
+      <a href="${format(requestAPrereviewMatch.formatter, {})}" class="button"
+        >${translate(locale, 'request-a-prereview-page', 'back')()}</a
+      >
     `,
   })
+
+const mailToHelp = (text: string) => html`<a href="mailto:help@prereview.org">${text}</a>`.toString()
