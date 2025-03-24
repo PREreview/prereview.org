d91752563646b425c7cf1071909a69e8bd50fa93
Chris Wilkinson <chris@prereview.org>
2025-03-24 16:37:42 +0000

    Extract change-career-stage strings
    Refs #1817

--- src/my-details-page/change-career-stage-form-page.ts
+++ src/my-details-page/change-career-stage-form-page.ts
@@ -1,17 +1,16 @@
-import type { Option } from 'effect'
+import { identity, type Option } from 'effect'
 import { format } from 'fp-ts-routing'
 import { Status } from 'hyper-ts'
 import { match } from 'ts-pattern'
 import type { CareerStage } from '../career-stage.js'
 import { html, plainText, rawHtml } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 import { changeCareerStageMatch, myDetailsMatch } from '../routes.js'
 
 export const createFormPage = ({
   careerStage,
   error = false,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   locale,
 }: {
   careerStage: Option.Option<CareerStage>
@@ -20,17 +19,21 @@ export const createFormPage = ({
 }) =>
   PageResponse({
     status: error ? Status.BadRequest : Status.OK,
-    title: plainText`${error ? 'Error: ' : ''}What career stage are you at?`,
-    nav: html`<a href="${format(myDetailsMatch.formatter, {})}" class="back"><span>Back</span></a>`,
+    title: plainText(translate(locale, 'my-details', 'whatCareerStage')({ error: error ? identity : () => '' })),
+    nav: html`<a href="${format(myDetailsMatch.formatter, {})}" class="back"
+      ><span>${translate(locale, 'my-details', 'back')()}</span></a
+    >`,
     main: html`
       <form method="post" action="${format(changeCareerStageMatch.formatter, {})}" novalidate>
         ${error
           ? html`
               <error-summary aria-labelledby="error-summary-title" role="alert">
-                <h2 id="error-summary-title">There is a problem</h2>
+                <h2 id="error-summary-title">${translate(locale, 'my-details', 'thereIsAProblem')()}</h2>
                 <ul>
                   <li>
-                    <a href="#career-stage-early"> Select which career stage you are at </a>
+                    <a href="#career-stage-early"
+                      >${translate(locale, 'my-details', 'selectCareerStageError')({ error: () => '' })}</a
+                    >
                   </li>
                 </ul>
               </error-summary>
@@ -40,14 +43,13 @@ export const createFormPage = ({
         <div ${error ? rawHtml('class="error"') : ''}>
           <fieldset role="group" ${error ? rawHtml('aria-invalid="true" aria-errormessage="career-stage-error"') : ''}>
             <legend>
-              <h1>What career stage are you at?</h1>
+              <h1>${translate(locale, 'my-details', 'whatCareerStage')({ error: () => '' })}</h1>
             </legend>
 
             ${error
               ? html`
                   <div class="error-message" id="career-stage-error">
-                    <span class="visually-hidden">Error:</span>
-                    Select which career stage you are at
+                    ${rawHtml(translate(locale, 'my-details', 'selectCareerStageError')({ error: visuallyHidden }))}
                   </div>
                 `
               : ''}
@@ -64,7 +66,7 @@ export const createFormPage = ({
                       .with({ value: { value: 'early' } }, () => 'checked')
                       .otherwise(() => '')}
                   />
-                  <span>Early</span>
+                  <span>${translate(locale, 'my-details', 'early')()}</span>
                 </label>
               </li>
               <li>
@@ -77,7 +79,7 @@ export const createFormPage = ({
                       .with({ value: { value: 'mid' } }, () => 'checked')
                       .otherwise(() => '')}
                   />
-                  <span>Mid</span>
+                  <span>${translate(locale, 'my-details', 'mid')()}</span>
                 </label>
               </li>
               <li>
@@ -90,24 +92,26 @@ export const createFormPage = ({
                       .with({ value: { value: 'late' } }, () => 'checked')
                       .otherwise(() => '')}
                   />
-                  <span>Late</span>
+                  <span>${translate(locale, 'my-details', 'late')()}</span>
                 </label>
               </li>
               <li>
-                <span>or</span>
+                <span>${translate(locale, 'my-details', 'or')()}</span>
                 <label>
                   <input name="careerStage" type="radio" value="skip" />
-                  <span>Prefer not to say</span>
+                  <span>${translate(locale, 'my-details', 'preferNotToSay')()}y</span>
                 </label>
               </li>
             </ol>
           </fieldset>
         </div>
 
-        <button>Save and continue</button>
+        <button>${translate(locale, 'my-details', 'saveAndContinueButton')()}</button>
       </form>
     `,
     skipToLabel: 'form',
     canonical: format(changeCareerStageMatch.formatter, {}),
     js: error ? ['error-summary.js'] : [],
   })
+
+const visuallyHidden = (text: string) => html`<span class="visually-hidden">${text}</span>`.toString()
