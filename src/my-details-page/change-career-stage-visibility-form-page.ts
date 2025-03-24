f206a55f0aebdd5f3661804a43f6c4a1eda220dd
Chris Wilkinson <chris@prereview.org>
2025-03-24 16:48:21 +0000

    Extract change-career-stage-visibility strings
    Refs #1817

--- src/my-details-page/change-career-stage-visibility-form-page.ts
+++ src/my-details-page/change-career-stage-visibility-form-page.ts
@@ -2,20 +2,21 @@ import { format } from 'fp-ts-routing'
 import { match } from 'ts-pattern'
 import type { CareerStage } from '../career-stage.js'
 import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import { PageResponse } from '../response.js'
 import { changeCareerStageVisibilityMatch, myDetailsMatch } from '../routes.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const createFormPage = ({ careerStage, locale }: { careerStage: CareerStage; locale: SupportedLocale }) =>
   PageResponse({
-    title: plainText`Who can see your career stage?`,
-    nav: html`<a href="${format(myDetailsMatch.formatter, {})}" class="back"><span>Back</span></a>`,
+    title: plainText(translate(locale, 'my-details', 'seeCareerStage')()),
+    nav: html`<a href="${format(myDetailsMatch.formatter, {})}" class="back"
+      ><span>${translate(locale, 'my-details', 'back')()}</span></a
+    >`,
     main: html`
       <form method="post" action="${format(changeCareerStageVisibilityMatch.formatter, {})}" novalidate>
         <fieldset role="group">
           <legend>
-            <h1>Who can see your career stage?</h1>
+            <h1>${translate(locale, 'my-details', 'seeCareerStage')()}</h1>
           </legend>
 
           <ol>
@@ -31,9 +32,11 @@ export const createFormPage = ({ careerStage, locale }: { careerStage: CareerSta
                     .with('public', () => 'checked')
                     .otherwise(() => '')}
                 />
-                <span>Everyone</span>
+                <span>${translate(locale, 'my-details', 'everyone')()}</span>
               </label>
-              <p id="career-stage-visibility-tip-public" role="note">We’ll show it on your public profile.</p>
+              <p id="career-stage-visibility-tip-public" role="note">
+                ${translate(locale, 'my-details', 'showOnPublic')()}
+              </p>
             </li>
             <li>
               <label>
@@ -47,14 +50,16 @@ export const createFormPage = ({ careerStage, locale }: { careerStage: CareerSta
                     .with('restricted', () => 'checked')
                     .otherwise(() => '')}
                 />
-                <span>Only PREreview</span>
+                <span>${translate(locale, 'my-details', 'onlyPrereview')()}</span>
               </label>
-              <p id="career-stage-visibility-tip-restricted" role="note">We won’t share it with anyone else.</p>
+              <p id="career-stage-visibility-tip-restricted" role="note">
+                ${translate(locale, 'my-details', 'willNotShare')()}
+              </p>
             </li>
           </ol>
         </fieldset>
 
-        <button>Save and continue</button>
+        <button>${translate(locale, 'my-details', 'saveAndContinueButton')()}</button>
       </form>
     `,
     skipToLabel: 'form',
