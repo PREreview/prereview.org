47054ab595811b55a89529e2522540e903abfd77
Chris Wilkinson <chris@prereview.org>
2025-03-27 15:00:51 +0000

    Extract change-location strings
    Refs #1817

--- src/my-details-page/change-location-form-page.ts
+++ src/my-details-page/change-location-form-page.ts
@@ -2,19 +2,20 @@ import { Option } from 'effect'
 import { format } from 'fp-ts-routing'
 import { P, match } from 'ts-pattern'
 import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import type { Location } from '../location.js'
 import { PageResponse } from '../response.js'
 import { changeLocationMatch, myDetailsMatch } from '../routes.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const createFormPage = (location: Option.Option<Location>, locale: SupportedLocale) =>
   PageResponse({
-    title: plainText`Where are you based?`,
-    nav: html`<a href="${format(myDetailsMatch.formatter, {})}" class="back"><span>Back</span></a>`,
+    title: plainText(translate(locale, 'my-details', 'whereBased')()),
+    nav: html`<a href="${format(myDetailsMatch.formatter, {})}" class="back"
+      ><span>${translate(locale, 'my-details', 'back')()}</span></a
+    >`,
     main: html`
       <form method="post" action="${format(changeLocationMatch.formatter, {})}" novalidate>
-        <h1><label for="location">Where are you based?</label></h1>
+        <h1><label for="location">${translate(locale, 'my-details', 'whereBased')()}</label></h1>
 
         <input
           name="location"
@@ -26,7 +27,7 @@ export const createFormPage = (location: Option.Option<Location>, locale: Suppor
             .exhaustive()}
         />
 
-        <button>Save and continue</button>
+        <button>${translate(locale, 'my-details', 'saveAndContinueButton')()}</button>
       </form>
     `,
     skipToLabel: 'form',
--- src/my-details-page/change-location-visibility-form-page.ts
+++ src/my-details-page/change-location-visibility-form-page.ts
@@ -1,21 +1,22 @@
 import { format } from 'fp-ts-routing'
 import { match } from 'ts-pattern'
 import { html, plainText } from '../html.js'
-import type { SupportedLocale } from '../locales/index.js'
+import { translate, type SupportedLocale } from '../locales/index.js'
 import type { Location } from '../location.js'
 import { PageResponse } from '../response.js'
 import { changeLocationVisibilityMatch, myDetailsMatch } from '../routes.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const createFormPage = ({ locale, location }: { locale: SupportedLocale; location: Location }) =>
   PageResponse({
-    title: plainText`Who can see your location?`,
-    nav: html`<a href="${format(myDetailsMatch.formatter, {})}" class="back"><span>Back</span></a>`,
+    title: plainText(translate(locale, 'my-details', 'seeLocation')()),
+    nav: html`<a href="${format(myDetailsMatch.formatter, {})}" class="back"
+      ><span>${translate(locale, 'my-details', 'back')()}</span></a
+    >`,
     main: html`
       <form method="post" action="${format(changeLocationVisibilityMatch.formatter, {})}" novalidate>
         <fieldset role="group">
           <legend>
-            <h1>Who can see your location?</h1>
+            <h1>${translate(locale, 'my-details', 'seeLocation')()}</h1>
           </legend>
 
           <ol>
@@ -31,9 +32,11 @@ export const createFormPage = ({ locale, location }: { locale: SupportedLocale; 
                     .with('public', () => 'checked')
                     .otherwise(() => '')}
                 />
-                <span>Everyone</span>
+                <span>${translate(locale, 'my-details', 'everyone')()}</span>
               </label>
-              <p id="location-visibility-tip-public" role="note">We’ll show it on your public profile.</p>
+              <p id="location-visibility-tip-public" role="note">
+                ${translate(locale, 'my-details', 'showOnPublic')()}
+              </p>
             </li>
             <li>
               <label>
@@ -47,14 +50,16 @@ export const createFormPage = ({ locale, location }: { locale: SupportedLocale; 
                     .with('restricted', () => 'checked')
                     .otherwise(() => '')}
                 />
-                <span>Only PREreview</span>
+                <span>${translate(locale, 'my-details', 'onlyPrereview')()}</span>
               </label>
-              <p id="location-visibility-tip-restricted" role="note">We won’t share it with anyone else.</p>
+              <p id="location-visibility-tip-restricted" role="note">
+                ${translate(locale, 'my-details', 'willNotShare')()}
+              </p>
             </li>
           </ol>
         </fieldset>
 
-        <button>Save and continue</button>
+        <button>${translate(locale, 'my-details', 'saveAndContinueButton')()}</button>
       </form>
     `,
     skipToLabel: 'form',
