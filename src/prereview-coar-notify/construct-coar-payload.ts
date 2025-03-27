3c097def08dd7bd2fab1d6b8e8be43871c0eb334
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-03-27 15:00:26 +0000

    Extract pure code to named function for re-use by effectfied code
    Refs: #2322

--- src/prereview-coar-notify/construct-coar-payload.ts
+++ src/prereview-coar-notify/construct-coar-payload.ts
@@ -6,9 +6,52 @@ import { match } from 'ts-pattern'
 import type { ReviewRequestPreprintId } from '../review-request.js'
 import { profileMatch } from '../routes.js'
 import { ProfileId } from '../types/index.js'
-import { generateUuid, type GenerateUuidEnv } from '../types/uuid.js'
+import { generateUuid, type GenerateUuidEnv, type Uuid } from '../types/uuid.js'
 import type { User } from '../user.js'
 import type { CoarReviewActionOfferPayload } from './coar-review-action-offer-payload.js'
+
+interface PayloadInputs {
+  coarNotifyUrl: URL
+  persona: 'public' | 'pseudonym'
+  preprint: ReviewRequestPreprintId
+  user: User
+  uuid: Uuid
+}
+
+const constructPayload = ({ uuid, coarNotifyUrl, preprint, persona, user }: PayloadInputs) =>
+  ({
+    id: uuid,
+    '@context': ['https://www.w3.org/ns/activitystreams', 'https://purl.org/coar/notify'],
+    type: ['Offer', 'coar-notify:ReviewAction'],
+    origin: {
+      id: coarNotifyUrl.href,
+      inbox: new URL('/inbox', coarNotifyUrl).href,
+      type: 'Service',
+    },
+    target: {
+      id: coarNotifyUrl.href,
+      inbox: new URL('/inbox', coarNotifyUrl).href,
+      type: 'Service',
+    },
+    object: {
+      id: preprint.value,
+      'ietf:cite-as': toUrl(preprint.value).href,
+    },
+    actor: {
+      id: `https://prereview.org${format(profileMatch.formatter, {
+        profile: match(persona)
+          .returnType<ProfileId.ProfileId>()
+          .with('public', () => ProfileId.forOrcid(user.orcid))
+          .with('pseudonym', () => ProfileId.forPseudonym(user.pseudonym))
+          .exhaustive(),
+      })}`,
+      type: 'Person',
+      name: match(persona)
+        .with('public', () => user.name)
+        .with('pseudonym', () => user.pseudonym)
+        .exhaustive(),
+    },
+  }) satisfies CoarReviewActionOfferPayload
 
 export const constructCoarPayload = ({
   coarNotifyUrl,
@@ -23,37 +66,5 @@ export const constructCoarPayload = ({
 }): RIO.ReaderIO<GenerateUuidEnv, CoarReviewActionOfferPayload> =>
   pipe(
     generateUuid,
-    RIO.map(uuid => ({
-      id: uuid,
-      '@context': ['https://www.w3.org/ns/activitystreams', 'https://purl.org/coar/notify'],
-      type: ['Offer', 'coar-notify:ReviewAction'],
-      origin: {
-        id: coarNotifyUrl.href,
-        inbox: new URL('/inbox', coarNotifyUrl).href,
-        type: 'Service',
-      },
-      target: {
-        id: coarNotifyUrl.href,
-        inbox: new URL('/inbox', coarNotifyUrl).href,
-        type: 'Service',
-      },
-      object: {
-        id: preprint.value,
-        'ietf:cite-as': toUrl(preprint.value).href,
-      },
-      actor: {
-        id: `https://prereview.org${format(profileMatch.formatter, {
-          profile: match(persona)
-            .returnType<ProfileId.ProfileId>()
-            .with('public', () => ProfileId.forOrcid(user.orcid))
-            .with('pseudonym', () => ProfileId.forPseudonym(user.pseudonym))
-            .exhaustive(),
-        })}`,
-        type: 'Person',
-        name: match(persona)
-          .with('public', () => user.name)
-          .with('pseudonym', () => user.pseudonym)
-          .exhaustive(),
-      },
-    })),
+    RIO.map(uuid => constructPayload({ coarNotifyUrl, persona, preprint, user, uuid })),
   )
