e8d69e9b8aa0cb7ebff034d912284846614677f1
Chris Wilkinson <chris@prereview.org>
2025-04-02 13:47:31 +0100

    Use future-proof config format
    Refs #2325, https://eslint.org/docs/v8.x/use/configure/configuration-files-new

---
+++ eslint.config.mjs
@@ -0,0 +1,142 @@
+import eslintJs from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginImport from 'eslint-plugin-import'
+import pluginNoComments from 'eslint-plugin-no-comments'
+import * as pluginWc from 'eslint-plugin-wc'
+import typescriptEslint from 'typescript-eslint'
+
+export default typescriptEslint.config([
+  {
+    ignores: [
+      '.cache/',
+      '.dev/',
+      'assets/locales/',
+      'data/',
+      'dist/',
+      'integration-results/',
+      'src/locales/',
+      '*.cjs',
+      '*.mjs',
+    ],
+  },
+  eslintJs.configs.recommended,
+  typescriptEslint.configs.strictTypeChecked,
+  typescriptEslint.configs.stylisticTypeChecked,
+  pluginImport.flatConfigs.recommended,
+  pluginImport.flatConfigs.typescript,
+  pluginWc.configs['flat/best-practice'],
+  eslintConfigPrettier,
+  { plugins: { 'no-comments': pluginNoComments } },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    settings: {
+      'import/core-modules': ['@jest/globals'],
+      'import/resolver': { typescript: true },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+      '@typescript-eslint/consistent-type-exports': ['error', { fixMixedExportsWithInlineTypeSpecifier: true }],
+      '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
+      '@typescript-eslint/no-confusing-void-expression': 'off',
+      '@typescript-eslint/no-deprecated': 'off',
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+      '@typescript-eslint/no-misused-spread': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': ['error', { ignoreConditionalTests: true }],
+      '@typescript-eslint/restrict-template-expressions': ['error', { allowNumber: true }],
+      '@typescript-eslint/strict-boolean-expressions': 'error',
+      'import/no-cycle': 'error',
+      'import/no-duplicates': ['error', { 'prefer-inline': true }],
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: [
+            'assets/**/*.ts',
+            'integration/**/*.ts',
+            'test/**/*.ts',
+            'visual-regression/**/*.ts',
+            '*.ts',
+          ],
+        },
+      ],
+      'import/no-internal-modules': [
+        'error',
+        {
+          allow: [
+            'fp-ts/lib/!(Array|boolean|Eq|function|NonEmptyArray|Option|Ord|Ordering|Predicate|ReadonlyRecord|ReadonlySet|ReadonlyTuple|Refinement|string).js',
+            'io-ts/lib/*',
+            'iso-639-3/to-*.json',
+            'http-proxy-middleware/dist/types',
+            'hyper-ts/lib/*',
+            'logging-ts/lib/*',
+            'nanohtml/*',
+            'remixicon/**/*.svg',
+            'tinyld/heavy',
+            'integration/base',
+            'types/*',
+            '*/index.js',
+          ],
+        },
+      ],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-comments/disallowComments': [
+        'error',
+        { allow: ['\\* @deprecated', 'eslint', 'https://', 'Do', 'Unfortunately', 'Refs'] },
+      ],
+      'object-shorthand': 'error',
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'wc/no-child-traversal-in-connectedcallback': 'off',
+    },
+  },
+  {
+    files: ['**/*.test.ts'],
+    rules: {
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/prefer-promise-reject-errors': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      'no-comments/disallowComments': 'off',
+    },
+  },
+  {
+    files: ['test/assets/**/*.test.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
+  },
+  {
+    files: ['integration/**/*.ts', 'visual-regression/**/*.ts'],
+    rules: {
+      '@typescript-eslint/unbound-method': 'off',
+      'no-empty-pattern': 'off',
+    },
+  },
+  {
+    files: ['integration/**/*.ts', 'test/**/*.ts', 'visual-regression/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-base-to-string': 'off',
+      'import/no-internal-modules': ['error', { forbid: ['**/assets/**/*'] }],
+    },
+  },
+  {
+    files: ['test/assets/** /*.ts'],
+    rules: {
+      'import/no-internal-modules': 'off',
+    },
+  },
+])
