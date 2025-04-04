5dbcdbb5b0a3dd1844539bda72f97b1a9213c2b0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-04-04 12:23:07 +0100

    Extract strings for request-review-flow/carry-on-page
    Refs: #1817

--- src/request-review-flow/start-page/carry-on-page.ts
+++ src/request-review-flow/start-page/carry-on-page.ts
@@ -1,26 +1,30 @@
+import { pipe } from 'effect'
 import { format } from 'fp-ts-routing'
 import { html, plainText } from '../../html.js'
-import type { SupportedLocale } from '../../locales/index.js'
+import { translate, type SupportedLocale } from '../../locales/index.js'
 import { StreamlinePageResponse } from '../../response.js'
 import type { ReviewRequestPreprintId } from '../../review-request.js'
 import { preprintReviewsMatch, requestReviewCheckMatch, requestReviewStartMatch } from '../../routes.js'
 
-export const carryOnPage = (locale: SupportedLocale, preprint: ReviewRequestPreprintId) =>
-  StreamlinePageResponse({
-    title: plainText`Request a PREreview`,
+export const carryOnPage = (locale: SupportedLocale, preprint: ReviewRequestPreprintId) => {
+  const t = translate(locale, 'request-review-flow')
+
+  return StreamlinePageResponse({
+    title: pipe(t('requestAPrereview')(), plainText),
     nav: html`
       <a href="${format(preprintReviewsMatch.formatter, { id: preprint })}" class="back"
-        ><span>Back to preprint</span></a
+        ><span>${t('backToPreprint')()}</span></a
       >
     `,
     main: html`
-      <h1>Request a PREreview</h1>
+      <h1>${t('requestAPrereview')()}</h1>
 
-      <p>As you’ve already started, we’ll take you to the next step so you can carry&nbsp;on.</p>
+      <p>${t('asYouHaveAlreadyStarted')()}</p>
 
       <a href="${format(requestReviewCheckMatch.formatter, { id: preprint })}" role="button" draggable="false"
-        >Continue</a
+        >${t('continueWord')()}</a
       >
     `,
     canonical: format(requestReviewStartMatch.formatter, { id: preprint }),
   })
+}
