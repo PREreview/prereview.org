63463f544b1afaadaa5e46a71e8befb75ef65560
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-04-04 11:40:14 +0100

    Extract strings for request-review-flow/published-page
    Refs: #1817

--- src/request-review-flow/published-page/published-page.ts
+++ src/request-review-flow/published-page/published-page.ts
@@ -1,25 +1,35 @@
+import { pipe } from 'effect'
 import { format } from 'fp-ts-routing'
-import { html, plainText } from '../../html.js'
-import type { SupportedLocale } from '../../locales/index.js'
+import { html, plainText, rawHtml } from '../../html.js'
+import { translate, type SupportedLocale } from '../../locales/index.js'
 import { StreamlinePageResponse } from '../../response.js'
 import type { ReviewRequestPreprintId } from '../../review-request.js'
 import { requestReviewPublishedMatch, reviewRequestsMatch } from '../../routes.js'
 
-export const publishedPage = (locale: SupportedLocale, preprint: ReviewRequestPreprintId) =>
-  StreamlinePageResponse({
-    title: plainText`Request published`,
+const listOfRequestsLink = (text: string) => `<a href="${format(reviewRequestsMatch.formatter, {})}">${text}</a>`
+const communitySlackLink = (text: string) => `<a href="https://bit.ly/PREreview-Slack">${text}</a>`
+
+export const publishedPage = (locale: SupportedLocale, preprint: ReviewRequestPreprintId) => {
+  const t = translate(locale, 'request-review-flow')
+
+  return StreamlinePageResponse({
+    title: pipe(t('requestPublished')(), plainText),
     main: html`
       <div class="panel">
-        <h1>Request published</h1>
+        <h1>${t('requestPublished')()}</h1>
       </div>
 
-      <h2>What happens next</h2>
+      <h2>${t('whatHappensNext')()}</h2>
 
       <p>
-        You’ll be able to see your request on our
-        <a href="${format(reviewRequestsMatch.formatter, {})}">list of requests</a> and
-        <a href="https://bit.ly/PREreview-Slack">Community Slack</a> shortly.
+        ${rawHtml(
+          t('whereYouCanSeeYourRequest')({
+            listOfRequestsLink,
+            communitySlackLink,
+          }),
+        )}
       </p>
     `,
     canonical: format(requestReviewPublishedMatch.formatter, { id: preprint }),
   })
+}
