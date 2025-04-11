fa8496cf45752467c2db1f4cd5c316da139c4a13
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-04-11 09:57:28 +0100

    Prepare support biorxiv preprints
    Refs: #2322

--- test/Crossref/WorkResponseToPreprint.test.ts
+++ test/Crossref/WorkResponseToPreprint.test.ts
@@ -54,6 +54,62 @@ test.each([
       url: new URL('https://neurolibre.org/papers/10.55458/neurolibre.00031'),
     }),
   },
+  {
+    response: 'biorxiv.json',
+    expected: Preprint({
+      authors: [
+        {
+          name: 'Sydney L Miles',
+          orcid: Orcid('0000-0003-2291-4105'),
+        },
+        {
+          name: 'Dilys Santillo',
+          orcid: Orcid('0009-0004-3966-4952'),
+        },
+        {
+          name: 'Vincenzo Torraca',
+          orcid: Orcid('0000-0001-7340-0249'),
+        },
+        {
+          name: 'Ana Teresa López Jiménez',
+          orcid: Orcid('0000-0002-0289-738X'),
+        },
+        {
+          name: 'Claire Jenkins',
+          orcid: Orcid('0000-0001-8600-9169'),
+        },
+        {
+          name: 'Stephen Baker',
+          orcid: Orcid('0000-0003-1308-5755'),
+        },
+        {
+          name: 'Kate S Baker',
+          orcid: Orcid('0000-0001-5850-1949'),
+        },
+        {
+          name: 'Vanessa Sancho-Shimizu',
+          orcid: Orcid('0000-0002-3519-0727'),
+        },
+        {
+          name: 'Kathryn E Holt',
+          orcid: Orcid('0000-0003-3949-2471'),
+        },
+        {
+          name: 'Serge Mostowy',
+          orcid: Orcid('0000-0002-7286-6503'),
+        },
+      ],
+      id: { type: 'biorxiv', value: Doi('10.1101/2025.02.05.636615') },
+      posted: Temporal.PlainDate.from({ year: 2025, month: 2, day: 5 }),
+      title: {
+        language: 'en',
+        text: rawHtml(
+          'Enhanced virulence and stress tolerance are signatures of epidemiologically successful&lt;i&gt;Shigella sonnei&lt;/i&gt;',
+        ),
+      },
+      url: new URL('http://biorxiv.org/lookup/doi/10.1101/2025.02.05.636615'),
+    }),
+  },
 ])('turns a Crossref work response into a preprint ($response)', ({ response, expected }) =>
   Effect.gen(function* () {
     const actual = yield* pipe(
