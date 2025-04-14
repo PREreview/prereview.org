5d185679fa4172ac66dfb0128c8ac33519d33560
Chris Wilkinson <chris@prereview.org>
2025-04-14 15:26:52 +0100

    Extract ready-for-attention strings
    Refs #1817

--- src/write-review/write-review-ready-full-review.ts
+++ src/write-review/write-review-ready-full-review.ts
@@ -16,7 +16,7 @@ import {
   requiredDecoder,
 } from '../form.js'
 import { html, plainText, rawHtml, sendHtml } from '../html.js'
-import { DefaultLocale } from '../locales/index.js'
+import { DefaultLocale, translate } from '../locales/index.js'
 import { getMethod, notFound, seeOther, serviceUnavailable } from '../middleware.js'
 import { templatePage } from '../page.js'
 import { type PreprintTitle, getPreprintTitle } from '../preprint.js'
@@ -26,9 +26,11 @@ import {
   writeReviewReviewTypeMatch,
   writeReviewShouldReadMatch,
 } from '../routes.js'
+import { errorPrefix } from '../shared-translation-elements.js'
 import { NonEmptyStringC } from '../types/string.js'
 import { type User, getUser } from '../user.js'
 import { type Form, getForm, redirectToNextForm, saveForm, updateForm } from './form.js'
+import { prereviewOfSuffix } from './shared-elements.js'
 
 export const writeReviewReadyFullReview = flow(
   RM.fromReaderTaskEitherK(getPreprintTitle),
@@ -130,17 +132,21 @@ const FormToFieldsE: Encoder<ReadyFullReviewForm, Form> = {
 
 type ReadyFullReviewForm = Fields<typeof readyFullReviewFields>
 
-function readyFullReviewForm(preprint: PreprintTitle, form: ReadyFullReviewForm, user: User) {
+function readyFullReviewForm(preprint: PreprintTitle, form: ReadyFullReviewForm, user: User, locale = DefaultLocale) {
   const error = hasAnError(form)
+  const t = translate(locale, 'write-review')
 
   return templatePage({
-    title: plainText`${
-      error ? 'Error: ' : ''
-    }Is it ready for attention from an editor, publisher or broader audience? – PREreview of “${preprint.title}”`,
+    title: pipe(
+      t('readyForAttention')(),
+      prereviewOfSuffix(locale, preprint.title),
+      errorPrefix(locale, error),
+      plainText,
+    ),
     content: html`
       <nav>
         <a href="${format(writeReviewShouldReadMatch.formatter, { id: preprint.id })}" class="back"
-          ><span>Back</span></a
+          ><span>${t('backNav')()}</span></a
         >
       </nav>
 
@@ -153,18 +159,14 @@ function readyFullReviewForm(preprint: PreprintTitle, form: ReadyFullReviewForm,
           ${error
             ? html`
                 <error-summary aria-labelledby="error-summary-title" role="alert">
-                  <h2 id="error-summary-title">There is a problem</h2>
+                  <h2 id="error-summary-title">${t('thereIsAProblem')()}</h2>
                   <ul>
                     ${E.isLeft(form.readyFullReview)
                       ? html`
                           <li>
                             <a href="#ready-full-review-yes">
                               ${match(form.readyFullReview.left)
-                                .with(
-                                  { _tag: 'MissingE' },
-                                  () =>
-                                    'Select yes if it is ready for attention from an editor, publisher or broader audience',
-                                )
+                                .with({ _tag: 'MissingE' }, () => t('selectReadyForAttention')())
                                 .exhaustive()}
                             </a>
                           </li>
@@ -186,19 +188,15 @@ function readyFullReviewForm(preprint: PreprintTitle, form: ReadyFullReviewForm,
                 )}
               >
                 <legend>
-                  <h1>Is it ready for attention from an editor, publisher or broader audience?</h1>
+                  <h1>${t('readyForAttention')()}</h1>
                 </legend>
 
                 ${E.isLeft(form.readyFullReview)
                   ? html`
                       <div class="error-message" id="ready-full-review-error">
-                        <span class="visually-hidden">Error:</span>
+                        <span class="visually-hidden">${t('error')()}:</span>
                         ${match(form.readyFullReview.left)
-                          .with(
-                            { _tag: 'MissingE' },
-                            () =>
-                              'Select yes if it is ready for attention from an editor, publisher or broader audience',
-                          )
+                          .with({ _tag: 'MissingE' }, () => t('selectReadyForAttention')())
                           .exhaustive()}
                       </div>
                     `
@@ -217,11 +215,13 @@ function readyFullReviewForm(preprint: PreprintTitle, form: ReadyFullReviewForm,
                           .with({ right: 'yes' }, () => 'checked')
                           .otherwise(() => '')}
                       />
-                      <span>Yes, as it is</span>
+                      <span>${t('readyForAttentionYes')()}</span>
                     </label>
                     <div class="conditional" id="ready-full-review-yes-control">
                       <div>
-                        <label for="ready-full-review-yes-details" class="textarea">Why is it ready? (optional)</label>
+                        <label for="ready-full-review-yes-details" class="textarea"
+                          >${t('readyForAttentionYesWhy')()}</label
+                        >
 
                         <textarea name="readyFullReviewYesDetails" id="ready-full-review-yes-details" rows="5">
 ${match(form.readyFullReviewYesDetails)
@@ -242,12 +242,12 @@ ${match(form.readyFullReviewYesDetails)
                           .with({ right: 'yes-changes' }, () => 'checked')
                           .otherwise(() => '')}
                       />
-                      <span>Yes, after minor changes</span>
+                      <span>${t('readyForAttentionMinorChanges')()}</span>
                     </label>
                     <div class="conditional" id="ready-full-review-yes-changes-control">
                       <div>
                         <label for="ready-full-review-yes-changes-details" class="textarea"
-                          >What needs tweaking? (optional)</label
+                          >${t('readyForAttentionMinorChangesWhy')()}</label
                         >
 
                         <textarea
@@ -273,12 +273,12 @@ ${match(form.readyFullReviewYesChangesDetails)
                           .with({ right: 'no' }, () => 'checked')
                           .otherwise(() => '')}
                       />
-                      <span>No, it needs a major revision</span>
+                      <span>${t('readyForAttentionNo')()}</span>
                     </label>
                     <div class="conditional" id="ready-full-review-no-control">
                       <div>
                         <label for="ready-full-review-no-details" class="textarea"
-                          >What needs to change? (optional)</label
+                          >${t('readyForAttentionNoWhy')()}</label
                         >
 
                         <textarea name="readyFullReviewNoDetails" id="ready-full-review-no-details" rows="5">
@@ -294,14 +294,14 @@ ${match(form.readyFullReviewNoDetails)
             </conditional-inputs>
           </div>
 
-          <button>Save and continue</button>
+          <button>${t('saveAndContinueButton')()}</button>
         </form>
       </main>
     `,
     js: ['conditional-inputs.js', 'error-summary.js'],
     skipLinks: [[html`Skip to form`, '#form']],
     type: 'streamline',
-    locale: DefaultLocale,
+    locale,
     user,
   })
 }
