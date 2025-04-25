f692436512f572c14adbc5f2ebc39cfc98606340
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-04-25 10:43:30 +0100

    Fill in a test list
    Refs: #2412

--- test/Zenodo/ConstructUrlsToInvalidatePrereview.test.ts
+++ test/Zenodo/ConstructUrlsToInvalidatePrereview.test.ts
@@ -1,18 +1,34 @@
 import { describe, expect, it } from '@jest/globals'
 import { Array, Effect } from 'effect'
+import { Orcid } from 'orcid-id-ts'
+import * as Doi from '../../src/types/Doi.js'
+import type { Pseudonym } from '../../src/types/pseudonym.js'
 import { ZenodoOrigin } from '../../src/Zenodo/CommunityRecords.js'
 import * as _ from '../../src/Zenodo/ConstructUrlsToInvalidatePrereview.js'
 import * as EffectTest from '../EffectTest.js'
 
 describe('constructUrlsToInvalidatePrereview', () => {
-  it('constructs valid urls', () =>
+  it.failing('constructs valid urls', () =>
     Effect.gen(function* () {
       const prereviewId = 12345
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const preprintDoi = Doi.Doi('10.1101/12345')
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const user = {
+        name: 'Josiah Carberry',
+        orcid: Orcid('0000-0002-1825-0097'),
+        pseudonym: 'Orange Panda' as Pseudonym,
+      }
 
-      const expectedUrls = ['http://zenodo.test/api/records/12345']
+      const expectedUrls = [
+        'http://zenodo.test/api/records/12345',
+        'http://zenodo.test/api/communities/prereview-reviews/records?q=related.identifier%3A%2210.1101%2F12345%22&size=100&sort=publication-desc&resource_type=publication%3A%3Apublication-peerreview&access_status=open',
+        'http://zenodo.test/api/communities/prereview-reviews/records?q=metadata.creators.person_or_org.identifiers.identifier%3A0000-0002-1825-0097+metadata.creators.person_or_org.name%3A%22Orange+Panda%22&size=100&sort=publication-desc&resource_type=publication%3A%3Apublication-peerreview&access_status=open',
+      ]
 
       const results = yield* _.constructUrlsToInvalidatePrereview(prereviewId)
 
       expect(Array.map(results, result => result.href)).toStrictEqual(expectedUrls)
-    }).pipe(Effect.provideService(ZenodoOrigin, new URL('http://zenodo.test')), EffectTest.run))
+    }).pipe(Effect.provideService(ZenodoOrigin, new URL('http://zenodo.test')), EffectTest.run),
+  )
 })
