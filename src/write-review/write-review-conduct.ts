69a532138d0649021b17810144b45197b7f7823b
Chris Wilkinson <chris@prereview.org>
2025-05-13 10:56:54 +0100

    Push up knowledge of hyper-ts
    Refs #2436, #2434

--- src/write-review/write-review-conduct.ts
+++ src/write-review/write-review-conduct.ts
@@ -1,20 +1,24 @@
 import { Match, Struct, flow, pipe } from 'effect'
 import { format } from 'fp-ts-routing'
 import * as E from 'fp-ts/lib/Either.js'
-import { Status } from 'hyper-ts'
+import { StatusCodes } from 'http-status-codes'
+import type { ResponseEnded, StatusOpen } from 'hyper-ts'
 import * as RM from 'hyper-ts/lib/ReaderMiddleware.js'
 import * as D from 'io-ts/lib/Decoder.js'
 import { P, match } from 'ts-pattern'
 import { type MissingE, hasAnError, missingE } from '../form.js'
-import { html, plainText, rawHtml, sendHtml } from '../html.js'
+import { html, plainText, rawHtml } from '../html.js'
 import { type SupportedLocale, translate } from '../locales/index.js'
 import { getMethod, notFound, seeOther, serviceUnavailable } from '../middleware.js'
-import { templatePage } from '../page.js'
+import type { TemplatePageEnv } from '../page.js'
 import { type PreprintTitle, getPreprintTitle } from '../preprint.js'
+import type { PublicUrlEnv } from '../public-url.js'
+import { StreamlinePageResponse, handlePageResponse } from '../response.js'
 import * as Routes from '../routes.js'
 import { writeReviewCompetingInterestsMatch, writeReviewConductMatch, writeReviewMatch } from '../routes.js'
 import { errorPrefix, errorSummary, saveAndContinueButton } from '../shared-translation-elements.js'
-import { type User, getUser } from '../user.js'
+import type { GetUserOnboardingEnv } from '../user-onboarding.js'
+import { type GetUserEnv, type User, getUser } from '../user.js'
 import { type Form, getForm, redirectToNextForm, saveForm, updateForm } from './form.js'
 import { backNav, prereviewOfSuffix } from './shared-elements.js'
 
@@ -56,20 +60,42 @@ export const writeReviewConduct = flow(
   ),
 )
 
-const showCodeOfConductForm = flow(
-  RM.fromReaderK(
-    ({ form, preprint, user, locale }: { form: Form; preprint: PreprintTitle; user: User; locale: SupportedLocale }) =>
-      codeOfConductForm(preprint, { conduct: E.right(form.conduct) }, user, locale),
-  ),
-  RM.ichainFirst(() => RM.status(Status.OK)),
-  RM.ichainMiddlewareK(sendHtml),
-)
+const showCodeOfConductForm = ({
+  form,
+  preprint,
+  user,
+  locale,
+}: {
+  form: Form
+  preprint: PreprintTitle
+  user: User
+  locale: SupportedLocale
+}) =>
+  pipe(
+    RM.of({}),
+    RM.apS('user', RM.of(user)),
+    RM.apS('locale', RM.of(locale)),
+    RM.apS('response', RM.of(codeOfConductForm(preprint, { conduct: E.right(form.conduct) }, locale))),
+    RM.ichainW(handlePageResponse),
+  )
 
-const showCodeOfConductErrorForm = (preprint: PreprintTitle, user: User, locale: SupportedLocale) =>
-  flow(
-    RM.fromReaderK((form: CodeOfConductForm) => codeOfConductForm(preprint, form, user, locale)),
-    RM.ichainFirst(() => RM.status(Status.BadRequest)),
-    RM.ichainMiddlewareK(sendHtml),
+const showCodeOfConductErrorForm = ({
+  form,
+  preprint,
+  user,
+  locale,
+}: {
+  form: CodeOfConductForm
+  preprint: PreprintTitle
+  user: User
+  locale: SupportedLocale
+}) =>
+  pipe(
+    RM.of({}),
+    RM.apS('user', RM.of(user)),
+    RM.apS('locale', RM.of(locale)),
+    RM.apS('response', RM.of(codeOfConductForm(preprint, form, locale))),
+    RM.ichainW(handlePageResponse),
   )
 
 const handleCodeOfConductForm = ({
@@ -97,8 +123,17 @@ const handleCodeOfConductForm = ({
     RM.ichainMiddlewareKW(redirectToNextForm(preprint.id)),
     RM.orElseW(error =>
       match(error)
+        .returnType<
+          RM.ReaderMiddleware<
+            GetUserEnv & GetUserOnboardingEnv & { locale: SupportedLocale } & PublicUrlEnv & TemplatePageEnv,
+            StatusOpen,
+            ResponseEnded,
+            never,
+            void
+          >
+        >()
         .with('form-unavailable', () => serviceUnavailable)
-        .with({ conduct: P.any }, showCodeOfConductErrorForm(preprint, user, locale))
+        .with({ conduct: P.any }, form => showCodeOfConductErrorForm({ form, preprint, user, locale }))
         .exhaustive(),
     ),
   )
@@ -115,103 +150,98 @@ interface CodeOfConductForm {
 }
 const codeOfConductLink = (text: string) => `<a href="${Routes.CodeOfConduct}">${text}</a>`.toString()
 
-function codeOfConductForm(preprint: PreprintTitle, form: CodeOfConductForm, user: User, locale: SupportedLocale) {
+function codeOfConductForm(preprint: PreprintTitle, form: CodeOfConductForm, locale: SupportedLocale) {
   const error = hasAnError(form)
   const t = translate(locale)
 
-  return templatePage({
+  return StreamlinePageResponse({
+    status: error ? StatusCodes.BAD_REQUEST : StatusCodes.OK,
     title: pipe(
       t('write-review', 'codeOfConduct')(),
       prereviewOfSuffix(locale, preprint.title),
       errorPrefix(locale, error),
       plainText,
     ),
-    content: html`
-      <nav>${backNav(locale, format(writeReviewCompetingInterestsMatch.formatter, { id: preprint.id }))}</nav>
+    nav: backNav(locale, format(writeReviewCompetingInterestsMatch.formatter, { id: preprint.id })),
+    main: html`
+      <form method="post" action="${format(writeReviewConductMatch.formatter, { id: preprint.id })}" novalidate>
+        ${error ? pipe(form, toErrorItems(locale), errorSummary(locale)) : ''}
 
-      <main id="form">
-        <form method="post" action="${format(writeReviewConductMatch.formatter, { id: preprint.id })}" novalidate>
-          ${error ? pipe(form, toErrorItems(locale), errorSummary(locale)) : ''}
+        <div ${rawHtml(E.isLeft(form.conduct) ? 'class="error"' : '')}>
+          <fieldset
+            role="group"
+            aria-describedby="conduct-tip"
+            ${rawHtml(E.isLeft(form.conduct) ? 'aria-invalid="true" aria-errormessage="conduct-error"' : '')}
+          >
+            <legend>
+              <h1>${t('write-review', 'codeOfConduct')()}</h1>
+            </legend>
 
-          <div ${rawHtml(E.isLeft(form.conduct) ? 'class="error"' : '')}>
-            <fieldset
-              role="group"
-              aria-describedby="conduct-tip"
-              ${rawHtml(E.isLeft(form.conduct) ? 'aria-invalid="true" aria-errormessage="conduct-error"' : '')}
-            >
-              <legend>
-                <h1>${t('write-review', 'codeOfConduct')()}</h1>
-              </legend>
+            <p id="conduct-tip" role="note">
+              ${rawHtml(t('write-review', 'expectYouToAbideByCodeOfConduct')({ link: codeOfConductLink }))}
+            </p>
 
-              <p id="conduct-tip" role="note">
-                ${rawHtml(t('write-review', 'expectYouToAbideByCodeOfConduct')({ link: codeOfConductLink }))}
-              </p>
+            <details>
+              <summary><span>${t('write-review', 'examplesOfExpectedBehaviour')()}</span></summary>
 
-              <details>
-                <summary><span>${t('write-review', 'examplesOfExpectedBehaviour')()}</span></summary>
+              <div>
+                <ul>
+                  <li>${t('write-review', 'expectedBehaviourLanguage')()}</li>
+                  <li>${t('write-review', 'expectedBehaviourFeedback')()}</li>
+                  <li>${t('write-review', 'expectedBehaviourRespect')()}</li>
+                  <li>${t('write-review', 'expectedBehaviourGracefulAcceptance')()}</li>
+                  <li>${t('write-review', 'expectedBehaviourBestOfCommunity')()}</li>
+                  <li>${t('write-review', 'expectedBehaviourEmpathy')()}</li>
+                </ul>
+              </div>
+            </details>
 
-                <div>
-                  <ul>
-                    <li>${t('write-review', 'expectedBehaviourLanguage')()}</li>
-                    <li>${t('write-review', 'expectedBehaviourFeedback')()}</li>
-                    <li>${t('write-review', 'expectedBehaviourRespect')()}</li>
-                    <li>${t('write-review', 'expectedBehaviourGracefulAcceptance')()}</li>
-                    <li>${t('write-review', 'expectedBehaviourBestOfCommunity')()}</li>
-                    <li>${t('write-review', 'expectedBehaviourEmpathy')()}</li>
-                  </ul>
-                </div>
-              </details>
+            <details>
+              <summary><span>${t('write-review', 'examplesOfUnacceptableBehaviour')()}</span></summary>
 
-              <details>
-                <summary><span>${t('write-review', 'examplesOfUnacceptableBehaviour')()}</span></summary>
+              <div>
+                <ul>
+                  <li>${t('write-review', 'unacceptableBehaviourTrollingEtc')()}</li>
+                  <li>${t('write-review', 'unacceptableBehaviourUnconstructive')()}</li>
+                  <li>${t('write-review', 'unacceptableBehaviourHarassment')()}</li>
+                  <li>${t('write-review', 'unacceptableBehaviourPublishingConfidentialInformation')()}</li>
+                  <li>${t('write-review', 'unacceptableBehaviourSexualisedLanguage')()}</li>
+                  <li>${t('write-review', 'unacceptableBehaviourInappropriate')()}</li>
+                </ul>
+              </div>
+            </details>
 
-                <div>
-                  <ul>
-                    <li>${t('write-review', 'unacceptableBehaviourTrollingEtc')()}</li>
-                    <li>${t('write-review', 'unacceptableBehaviourUnconstructive')()}</li>
-                    <li>${t('write-review', 'unacceptableBehaviourHarassment')()}</li>
-                    <li>${t('write-review', 'unacceptableBehaviourPublishingConfidentialInformation')()}</li>
-                    <li>${t('write-review', 'unacceptableBehaviourSexualisedLanguage')()}</li>
-                    <li>${t('write-review', 'unacceptableBehaviourInappropriate')()}</li>
-                  </ul>
-                </div>
-              </details>
+            ${E.isLeft(form.conduct)
+              ? html`
+                  <div class="error-message" id="conduct-error">
+                    <span class="visually-hidden">${t('forms', 'errorPrefix')()}:</span>
+                    ${Match.valueTags(form.conduct.left, {
+                      MissingE: t('write-review', 'confirmCodeOfConduct'),
+                    })}
+                  </div>
+                `
+              : ''}
 
-              ${E.isLeft(form.conduct)
-                ? html`
-                    <div class="error-message" id="conduct-error">
-                      <span class="visually-hidden">${t('forms', 'errorPrefix')()}:</span>
-                      ${Match.valueTags(form.conduct.left, {
-                        MissingE: t('write-review', 'confirmCodeOfConduct'),
-                      })}
-                    </div>
-                  `
-                : ''}
+            <label>
+              <input
+                name="conduct"
+                id="conduct-yes"
+                type="checkbox"
+                value="yes"
+                ${match(form.conduct)
+                  .with({ right: 'yes' }, () => 'checked')
+                  .otherwise(() => '')}
+              />
+              <span>${t('write-review', 'iAmFollowingCodeOfConduct')()}</span>
+            </label>
+          </fieldset>
+        </div>
 
-              <label>
-                <input
-                  name="conduct"
-                  id="conduct-yes"
-                  type="checkbox"
-                  value="yes"
-                  ${match(form.conduct)
-                    .with({ right: 'yes' }, () => 'checked')
-                    .otherwise(() => '')}
-                />
-                <span>${t('write-review', 'iAmFollowingCodeOfConduct')()}</span>
-              </label>
-            </fieldset>
-          </div>
-
-          ${saveAndContinueButton(locale)}
-        </form>
-      </main>
+        ${saveAndContinueButton(locale)}
+      </form>
     `,
     js: error ? ['error-summary.js'] : [],
-    skipLinks: [[html`Skip to form`, '#form']],
-    type: 'streamline',
-    locale,
-    user,
+    skipToLabel: 'form',
   })
 }
 
--- test/write-review/write-review-conduct.test.ts
+++ test/write-review/write-review-conduct.test.ts
@@ -6,7 +6,6 @@ import * as TE from 'fp-ts/lib/TaskEither.js'
 import { MediaType, Status } from 'hyper-ts'
 import * as M from 'hyper-ts/lib/Middleware.js'
 import Keyv from 'keyv'
-import { rawHtml } from '../../src/html.js'
 import type { TemplatePageEnv } from '../../src/page.js'
 import { PreprintIsNotFound, PreprintIsUnavailable } from '../../src/preprint.js'
 import { writeReviewMatch, writeReviewPublishMatch } from '../../src/routes.js'
@@ -34,7 +33,9 @@ describe('writeReviewConduct', () => {
         formStore,
         getPreprintTitle: () => TE.right(preprintTitle),
         getUser: () => M.of(user),
+        getUserOnboarding: shouldNotBeCalled,
         locale,
+        publicUrl: new URL('http://example.com'),
         templatePage: shouldNotBeCalled,
       }),
       connection,
@@ -70,7 +71,9 @@ describe('writeReviewConduct', () => {
         formStore,
         getPreprintTitle: () => TE.right(preprintTitle),
         getUser: () => M.of(user),
+        getUserOnboarding: shouldNotBeCalled,
         locale,
+        publicUrl: new URL('http://example.com'),
         templatePage: shouldNotBeCalled,
       }),
       connection,
@@ -98,7 +101,9 @@ describe('writeReviewConduct', () => {
           formStore: new Keyv(),
           getPreprintTitle: () => TE.right(preprintTitle),
           getUser: () => M.of(user),
+          getUserOnboarding: shouldNotBeCalled,
           locale,
+          publicUrl: new URL('http://example.com'),
           templatePage: shouldNotBeCalled,
         }),
         connection,
@@ -128,7 +133,9 @@ describe('writeReviewConduct', () => {
           formStore: new Keyv(),
           getPreprintTitle: () => TE.left(new PreprintIsUnavailable({})),
           getUser: () => M.of(user),
+          getUserOnboarding: shouldNotBeCalled,
           locale,
+          publicUrl: new URL('http://example.com'),
           templatePage,
         }),
         connection,
@@ -162,7 +169,9 @@ describe('writeReviewConduct', () => {
           formStore: new Keyv(),
           getPreprintTitle: () => TE.left(new PreprintIsNotFound({})),
           getUser: () => M.of(user),
+          getUserOnboarding: shouldNotBeCalled,
           locale,
+          publicUrl: new URL('http://example.com'),
           templatePage,
         }),
         connection,
@@ -194,7 +203,9 @@ describe('writeReviewConduct', () => {
           formStore: new Keyv(),
           getPreprintTitle: () => TE.right(preprintTitle),
           getUser: () => M.left('no-session'),
+          getUserOnboarding: shouldNotBeCalled,
           locale,
+          publicUrl: new URL('http://example.com'),
           templatePage: shouldNotBeCalled,
         }),
         connection,
@@ -237,7 +248,9 @@ describe('writeReviewConduct', () => {
           formStore,
           getPreprintTitle: () => TE.right(preprintTitle),
           getUser: () => M.of(user),
+          getUserOnboarding: () => TE.left('unavailable'),
           locale,
+          publicUrl: new URL('http://example.com'),
           templatePage,
         }),
         connection,
@@ -246,6 +259,7 @@ describe('writeReviewConduct', () => {
       expect(actual).toStrictEqual(
         E.right([
           { type: 'setStatus', status: Status.BadRequest },
+          { type: 'setHeader', name: 'Cache-Control', value: 'no-store, must-revalidate' },
           { type: 'setHeader', name: 'Content-Type', value: MediaType.textHTML },
           { type: 'setBody', body: page.toString() },
         ]),
@@ -253,7 +267,7 @@ describe('writeReviewConduct', () => {
       expect(templatePage).toHaveBeenCalledWith({
         title: expect.anything(),
         content: expect.anything(),
-        skipLinks: [[rawHtml('Skip to form'), '#form']],
+        skipLinks: [[expect.anything(), '#form']],
         js: ['error-summary.js'],
         type: 'streamline',
         locale,
