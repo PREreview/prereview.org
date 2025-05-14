9b95355cf9af5cea5be7eca90b2321b4df67d630
Chris Wilkinson <chris@prereview.org>
2025-05-14 11:03:33 +0100

    Remove knowledge of hyper-ts from a page
    Refs #2436, #2434

--- src/app-router.ts
+++ src/app-router.ts
@@ -1551,7 +1551,26 @@ const router: P.Parser<RM.ReaderMiddleware<RouterEnv, StatusOpen, ResponseEnded,
     ),
     pipe(
       writeReviewShouldReadMatch.parser,
-      P.map(({ id }) => writeReviewShouldRead(id)),
+      P.map(
+        flow(
+          RM.of,
+          RM.apS(
+            'body',
+            RM.gets(c => c.getBody()),
+          ),
+          RM.apS(
+            'method',
+            RM.gets(c => c.getMethod()),
+          ),
+          RM.apS('user', maybeGetUser),
+          RM.apSW(
+            'locale',
+            RM.asks((env: RouterEnv) => env.locale),
+          ),
+          RM.bindW('response', RM.fromReaderTaskK(writeReviewShouldRead)),
+          RM.ichainW(handleResponse),
+        ),
+      ),
     ),
     pipe(
       writeReviewReadyFullReviewMatch.parser,
