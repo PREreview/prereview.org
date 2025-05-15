f2b1c7b8f9b09ad70847c2f3927cd2092f5e4d49
Chris Wilkinson <chris@prereview.org>
2025-05-15 11:43:59 +0100

    Remove unused code
    Refs #2436

--- src/http-error.ts
+++ src/http-error.ts
@@ -1,80 +1,9 @@
-import { Effect, pipe } from 'effect'
-import type { HttpError } from 'http-errors'
-import { Status } from 'hyper-ts'
-import * as RM from 'hyper-ts/lib/ReaderMiddleware.js'
-import { match } from 'ts-pattern'
+import { Effect } from 'effect'
 import { Locale } from './Context.js'
 import { HavingProblemsPage } from './HavingProblemsPage/index.js'
-import { html, plainText, rawHtml, sendHtml } from './html.js'
-import { type SupportedLocale, translate } from './locales/index.js'
+import type { SupportedLocale } from './locales/index.js'
 import { NoPermissionPage } from './NoPermissionPage/index.js'
-import { templatePage } from './page.js'
 import { PageNotFound } from './PageNotFound/index.js'
-import { type User, maybeGetUser } from './user.js'
-
-export function handleError(error: HttpError<typeof Status.NotFound | typeof Status.ServiceUnavailable>) {
-  return pipe(
-    RM.of({}),
-    RM.apS('user', maybeGetUser),
-    RM.apSW(
-      'locale',
-      RM.asks((env: { locale: SupportedLocale }) => env.locale),
-    ),
-    RM.chainReaderKW(
-      match(error)
-        .with({ status: Status.NotFound }, () => notFoundPage)
-        .with({ status: Status.ServiceUnavailable }, () => problemsPage)
-        .exhaustive(),
-    ),
-    RM.ichainFirst(() => RM.status(error.status)),
-    RM.ichainFirst(() => RM.header('Cache-Control', 'no-store, must-revalidate')),
-    RM.ichainMiddlewareK(sendHtml),
-  )
-}
-
-function notFoundPage({ locale, user }: { locale: SupportedLocale; user?: User }) {
-  return templatePage({
-    title: plainText(translate(locale, 'page-not-found', 'pageNotFoundTitle')()),
-    content: html`
-      <main id="main-content">
-        <h1>${translate(locale, 'page-not-found', 'pageNotFoundTitle')()}</h1>
-
-        <p>${translate(locale, 'page-not-found', 'checkCorrect')()}</p>
-
-        <p>${translate(locale, 'page-not-found', 'checkEntire')()}</p>
-
-        <p>
-          ${rawHtml(
-            translate(
-              locale,
-              'page-not-found',
-              'contactUs',
-            )({ contact: text => html`<a href="mailto:help@prereview.org">${text}</a>`.toString() }),
-          )}
-        </p>
-      </main>
-    `,
-    skipLinks: [[html`${translate(locale, 'skip-links', 'main')()}`, '#main-content']],
-    locale,
-    user,
-  })
-}
-
-function problemsPage({ locale, user }: { locale: SupportedLocale; user?: User }) {
-  return templatePage({
-    title: plainText(translate(locale, 'having-problems-page', 'havingProblemsTitle')()),
-    content: html`
-      <main id="main-content">
-        <h1>${translate(locale, 'having-problems-page', 'havingProblemsTitle')()}</h1>
-
-        <p>${translate(locale, 'having-problems-page', 'tryAgainLater')()}</p>
-      </main>
-    `,
-    skipLinks: [[html`${translate(locale, 'skip-links', 'main')()}`, '#main-content']],
-    locale,
-    user,
-  })
-}
 
 /** @deprecated */
 export const pageNotFound = (locale: SupportedLocale) =>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,3 @@
-import { pipe } from 'effect'
-import httpErrors from 'http-errors'
-import { type ResponseEnded, Status, type StatusOpen } from 'hyper-ts'
 import * as M from 'hyper-ts/lib/Middleware.js'
-import { handleError } from './http-error.js'
-
-export const seeOther: <E = never>(location: string) => M.Middleware<StatusOpen, ResponseEnded, E, void> = location =>
-  pipe(
-    M.status(Status.SeeOther),
-    M.ichain(() => M.header('Location', location)),
-    M.ichain(() => M.closeHeaders()),
-    M.ichain(() => M.end()),
-  )
-
-export const movedPermanently: <E = never>(
-  location: string,
-) => M.Middleware<StatusOpen, ResponseEnded, E, void> = location =>
-  pipe(
-    M.status(Status.MovedPermanently),
-    M.ichain(() => M.header('Location', location)),
-    M.ichain(() => M.closeHeaders()),
-    M.ichain(() => M.end()),
-  )
-
-export const notFound = handleError(new httpErrors.NotFound())
-
-export const serviceUnavailable = handleError(new httpErrors.ServiceUnavailable())
 
 export const getMethod = M.gets(c => c.getMethod())
