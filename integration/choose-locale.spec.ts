9b25de70a46bebd50d5b11effcf3994ba1016fee
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-05-16 11:27:49 +0100

    Reflect that an integration test is now passing
    Refs: #2434, cae337883ba0afb34e9a779bafafe92d5a735a5a

--- integration/choose-locale.spec.ts
+++ integration/choose-locale.spec.ts
@@ -1,45 +1,40 @@
 import { canChooseLocale, expect, test } from './base.js'
 
-test.extend(canChooseLocale)(
-  'can choose a locale through picker and path',
-  async ({ fetch, javaScriptEnabled, page }, testInfo) => {
-    await page.goto('/')
+test.extend(canChooseLocale)('can choose a locale through picker and path', async ({ fetch, page }) => {
+  await page.goto('/')
 
-    await expect(page.getByRole('heading', { level: 1 })).toContainText('Open preprint reviews.')
+  await expect(page.getByRole('heading', { level: 1 })).toContainText('Open preprint reviews.')
 
-    await page.getByRole('link', { name: 'português (Brasil)' }).click()
+  await page.getByRole('link', { name: 'português (Brasil)' }).click()
 
-    testInfo.fail(!javaScriptEnabled)
+  await expect(page.getByRole('heading', { level: 1 })).toContainText('Avaliações abertas de preprints.')
 
-    await expect(page.getByRole('heading', { level: 1 })).toContainText('Avaliações abertas de preprints.')
+  fetch.get(
+    { url: 'https://content.prereview.org/ghost/api/content/pages/6154aa157741400e8722bb14/', query: { key: 'key' } },
+    { body: { pages: [{ html: '<p>Some information about us.</p>' }] } },
+  )
 
-    fetch.get(
-      { url: 'https://content.prereview.org/ghost/api/content/pages/6154aa157741400e8722bb14/', query: { key: 'key' } },
-      { body: { pages: [{ html: '<p>Some information about us.</p>' }] } },
-    )
+  await page.getByRole('link', { name: 'Sobre nós' }).click()
 
-    await page.getByRole('link', { name: 'Sobre nós' }).click()
+  await expect(page.getByRole('heading', { level: 1 })).toContainText('Sobre nós')
 
-    await expect(page.getByRole('heading', { level: 1 })).toContainText('Sobre nós')
+  await page.goto('/en-us')
 
-    await page.goto('/en-us')
+  await expect(page.getByRole('heading', { level: 1 })).toContainText('Open preprint reviews.')
 
-    await expect(page.getByRole('heading', { level: 1 })).toContainText('Open preprint reviews.')
+  await page.getByRole('link', { name: 'About' }).click()
 
-    await page.getByRole('link', { name: 'About' }).click()
+  await expect(page.getByRole('heading', { level: 1 })).toContainText('About')
 
-    await expect(page.getByRole('heading', { level: 1 })).toContainText('About')
+  await page.goto('/pt-br')
 
-    await page.goto('/pt-br')
+  await expect(page.getByRole('heading', { level: 1 })).toContainText('Avaliações abertas de preprints.')
 
-    await expect(page.getByRole('heading', { level: 1 })).toContainText('Avaliações abertas de preprints.')
+  await page.getByRole('link', { name: 'Sobre nós' }).click()
 
-    await page.getByRole('link', { name: 'Sobre nós' }).click()
+  await expect(page.getByRole('heading', { level: 1 })).toContainText('Sobre nós')
 
-    await expect(page.getByRole('heading', { level: 1 })).toContainText('Sobre nós')
+  await page.getByRole('link', { name: 'English (US)' }).click()
 
-    await page.getByRole('link', { name: 'English (US)' }).click()
-
-    await expect(page.getByRole('heading', { level: 1 })).toContainText('About')
-  },
-)
+  await expect(page.getByRole('heading', { level: 1 })).toContainText('About')
+})
