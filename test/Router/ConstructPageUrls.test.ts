3dd76145fef229f9a5274289143c040d4a40da5d
Chris Wilkinson <chris@prereview.org>
2025-05-19 10:56:21 +0100

    Separate test responsibilities and expose a bug
    Refs #2434

--- test/Router/ConstructPageUrls.test.ts
+++ test/Router/ConstructPageUrls.test.ts
@@ -1,17 +1,47 @@
-import { describe, expect, it } from '@jest/globals'
-import { HashMap, HashSet } from 'effect'
+import { it } from '@fast-check/jest'
+import { describe, expect } from '@jest/globals'
+import { HashMap, HashSet, Tuple } from 'effect'
 import { constructPageUrls } from '../../src/Router/ConstructPageUrls.js'
 import { SupportedLocales } from '../../src/locales/index.js'
 import type { PageResponse } from '../../src/response.js'
+import * as fc from '../fc.js'
 
 describe('constructPageUrls', () => {
   describe('localeUrls', () => {
-    it('constructs a url for each supported locale', () => {
-      const pageUrls = constructPageUrls({} as unknown as PageResponse, 'http://example.com', '/')
+    it.prop([fc.url().map(url => Tuple.make(url.origin, `${url.pathname}${url.search}`))])(
+      'constructs a url for each supported locale',
+      ([origin, pathAndQueryString]) => {
+        const pageUrls = constructPageUrls({} as unknown as PageResponse, origin, pathAndQueryString)
 
-      expect(HashMap.size(pageUrls.localeUrls)).toBe(HashSet.size(SupportedLocales))
-      expect(HashMap.unsafeGet(pageUrls.localeUrls, 'en-US').pathname).toStrictEqual(expect.stringMatching(/^\/en-us/))
-      expect(HashMap.unsafeGet(pageUrls.localeUrls, 'pt-BR').pathname).toStrictEqual(expect.stringMatching(/^\/pt-br/))
+        expect(HashMap.size(pageUrls.localeUrls)).toBe(HashSet.size(SupportedLocales))
+      },
+    )
+
+    it.failing.prop(
+      [
+        fc
+          .tuple(fc.supportedLocale(), fc.url())
+          .map(([locale, url]) =>
+            Tuple.make(
+              locale,
+              url.origin,
+              `${url.pathname}${url.search}`,
+              `${url.origin}/${locale.toLowerCase()}${url.pathname}${url.search}`,
+            ),
+          ),
+      ],
+      {
+        examples: [
+          [['en-US', 'http://example.com', '/about', 'http://example.com/en-us/about']],
+          [['pt-BR', 'http://example.com', '/about', 'http://example.com/pt-br/about']],
+          [['en-US', 'http://example.com', '/reviews?page=2', 'http://example.com/en-us/reviews?page=2']],
+          [['en-US', 'http://example.com', '/?foo=bar', 'http://example.com/en-us?foo=bar']],
+        ],
+      },
+    )('constructs the url for each supported locale', ([locale, origin, pathAndQueryString, expected]) => {
+      const pageUrls = constructPageUrls({} as unknown as PageResponse, origin, pathAndQueryString)
+
+      expect(HashMap.unsafeGet(pageUrls.localeUrls, locale).href).toStrictEqual(expected)
     })
   })
 })
