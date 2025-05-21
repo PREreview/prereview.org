4dfc9881ec1428ffd0de3880fb680ab9151d3a64
Chris Wilkinson <chris@prereview.org>
2025-05-21 21:06:15 +0100

    Separate rendering into a testable module
    Refs #2436

---
+++ src/write-review/enter-email-address-page/enter-email-address-page.ts
@@ -0,0 +1,111 @@
+import { Match, pipe } from 'effect'
+import { format } from 'fp-ts-routing'
+import * as E from 'fp-ts/lib/Either.js'
+import { StatusCodes } from 'http-status-codes'
+import { match, P } from 'ts-pattern'
+import { hasAnError, type InvalidE, type MissingE } from '../../form.js'
+import { html, plainText } from '../../html.js'
+import { type SupportedLocale, translate } from '../../locales/index.js'
+import type { PreprintTitle } from '../../preprint.js'
+import { StreamlinePageResponse } from '../../response.js'
+import { writeReviewConductMatch, writeReviewEnterEmailAddressMatch } from '../../routes.js'
+import { errorPrefix } from '../../shared-translation-elements.js'
+import type { EmailAddress } from '../../types/email-address.js'
+import { prereviewOfSuffix } from '../shared-elements.js'
+
+export interface EnterEmailAddressForm {
+  readonly emailAddress: E.Either<MissingE | InvalidE, EmailAddress | undefined>
+}
+
+export const enterEmailAddressPage = (
+  preprint: PreprintTitle,
+  form: EnterEmailAddressForm,
+  locale: SupportedLocale,
+) => {
+  const error = hasAnError(form)
+  const t = translate(locale, 'write-review')
+
+  return StreamlinePageResponse({
+    status: error ? StatusCodes.BAD_REQUEST : StatusCodes.OK,
+    title: pipe(
+      t('contactDetails')(),
+      prereviewOfSuffix(locale, preprint.title),
+      errorPrefix(locale, error),
+      plainText,
+    ),
+    nav: html`<a href="${format(writeReviewConductMatch.formatter, { id: preprint.id })}" class="back"
+      ><span>${translate(locale, 'forms', 'backLink')()}</span></a
+    >`,
+    main: html`
+      <form
+        method="post"
+        action="${format(writeReviewEnterEmailAddressMatch.formatter, { id: preprint.id })}"
+        novalidate
+      >
+        ${error
+          ? html`
+              <error-summary aria-labelledby="error-summary-title" role="alert">
+                <h2 id="error-summary-title">${translate(locale, 'forms', 'errorSummaryTitle')()}</h2>
+                <ul>
+                  ${E.isLeft(form.emailAddress)
+                    ? html`
+                        <li>
+                          <a href="#email-address">
+                            ${Match.valueTags(form.emailAddress.left, {
+                              MissingE: () => t('enterEmailAddressError')(),
+                              InvalidE: () => t('enterEmailAddressFormatError')(),
+                            })}
+                          </a>
+                        </li>
+                      `
+                    : ''}
+                </ul>
+              </error-summary>
+            `
+          : ''}
+
+        <h1>${t('contactDetails')()}</h1>
+
+        <p>${t('confirmEmailAddress')()}</p>
+
+        <p>${t('onlyUseContact')()}</p>
+
+        <div ${error ? html`class="error"` : ''}>
+          <h2><label for="email-address">${t('whatIsYourEmail')()}</label></h2>
+
+          ${E.isLeft(form.emailAddress)
+            ? html`
+                <div class="error-message" id="email-address-error">
+                  <span class="visually-hidden">${translate(locale, 'forms', 'errorPrefix')()}:</span>
+                  ${Match.valueTags(form.emailAddress.left, {
+                    MissingE: () => t('enterEmailAddressError')(),
+                    InvalidE: () => t('enterEmailAddressFormatError')(),
+                  })}
+                </div>
+              `
+            : ''}
+
+          <input
+            name="emailAddress"
+            id="email-address"
+            type="text"
+            inputmode="email"
+            spellcheck="false"
+            autocomplete="email"
+            ${match(form.emailAddress)
+              .with({ right: P.select(P.string) }, value => html`value="${value}"`)
+              .with({ right: undefined }, () => '')
+              .with({ left: { _tag: 'MissingE' } }, () => '')
+              .with({ left: { _tag: 'InvalidE', actual: P.select() } }, value => html`value="${value}"`)
+              .exhaustive()}
+            ${E.isLeft(form.emailAddress) ? html`aria-invalid="true" aria-errormessage="email-address-error"` : ''}
+          />
+        </div>
+
+        <button>${translate(locale, 'forms', 'saveContinueButton')()}</button>
+      </form>
+    `,
+    js: error ? ['error-summary.js'] : [],
+    skipToLabel: 'form',
+  })
+}
---
+++ visual-regression/write-review/enter-email-address-page/enter-email-address-page.spec.ts
@@ -0,0 +1,43 @@
+import { Doi } from 'doi-ts'
+import * as E from 'fp-ts/lib/Either.js'
+import { invalidE, missingE } from '../../../src/form.js'
+import { html } from '../../../src/html.js'
+import { DefaultLocale } from '../../../src/locales/index.js'
+import type { PreprintTitle } from '../../../src/preprint.js'
+import { enterEmailAddressPage } from '../../../src/write-review/enter-email-address-page/enter-email-address-page.js'
+import { expect, test } from '../../base.js'
+
+test('content looks right', async ({ showPage }) => {
+  const response = enterEmailAddressPage(preprint, { emailAddress: E.right(undefined) }, locale)
+
+  const content = await showPage(response)
+
+  await expect(content).toHaveScreenshot()
+})
+
+test('content looks right when the email is missing', async ({ showPage }) => {
+  const response = enterEmailAddressPage(preprint, { emailAddress: E.left(missingE()) }, locale)
+
+  const content = await showPage(response)
+
+  await expect(content).toHaveScreenshot()
+})
+
+test('content looks right when the email is invalid', async ({ showPage }) => {
+  const response = enterEmailAddressPage(preprint, { emailAddress: E.left(invalidE('invalid review')) }, locale)
+
+  const content = await showPage(response)
+
+  await expect(content).toHaveScreenshot()
+})
+
+const locale = DefaultLocale
+
+const preprint = {
+  id: {
+    _tag: 'biorxiv',
+    value: Doi('10.1101/2022.01.13.476201'),
+  },
+  title: html`The role of LHCBM1 in non-photochemical quenching in <i>Chlamydomonas reinhardtii</i>`,
+  language: 'en',
+} satisfies PreprintTitle
