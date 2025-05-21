9273443b235f4a8981c1085dd7118eb2097c379a
Chris Wilkinson <chris@prereview.org>
2025-05-21 20:44:04 +0100

    Separate rendering into a testable module
    Refs #2436

--- src/types/Orcid.ts
+++ src/types/Orcid.ts
@@ -2,7 +2,7 @@ import { ParseResult, pipe, Schema } from 'effect'
 import * as Orcid from 'orcid-id-ts'
 import * as FptsToEffect from '../FptsToEffect.js'
 
-export { toUrl, type Orcid } from 'orcid-id-ts'
+export { Orcid, toUrl } from 'orcid-id-ts'
 
 export const OrcidSchema = pipe(Schema.String, Schema.filter(Orcid.isOrcid))
 
---
+++ visual-regression/write-review/persona/persona-form.spec.ts
@@ -0,0 +1,42 @@
+import * as E from 'fp-ts/lib/Either.js'
+import { missingE } from '../../../src/form.js'
+import { html } from '../../../src/html.js'
+import { DefaultLocale } from '../../../src/locales/index.js'
+import type { PreprintTitle } from '../../../src/preprint.js'
+import { Doi, Orcid, type Pseudonym } from '../../../src/types/index.js'
+import type { User } from '../../../src/user.js'
+import { personaForm } from '../../../src/write-review/persona/persona-form.js'
+import { expect, test } from '../../base.js'
+
+test('content looks right', async ({ showPage }) => {
+  const response = personaForm(preprint, { persona: E.right(undefined) }, undefined, user, locale)
+
+  const content = await showPage(response)
+
+  await expect(content).toHaveScreenshot()
+})
+
+test('content looks right when fields are missing', async ({ showPage }) => {
+  const response = personaForm(preprint, { persona: E.left(missingE()) }, undefined, user, locale)
+
+  const content = await showPage(response)
+
+  await expect(content).toHaveScreenshot()
+})
+
+const locale = DefaultLocale
+
+const preprint = {
+  id: {
+    _tag: 'biorxiv',
+    value: Doi.Doi('10.1101/2022.01.13.476201'),
+  },
+  title: html`The role of LHCBM1 in non-photochemical quenching in <i>Chlamydomonas reinhardtii</i>`,
+  language: 'en',
+} satisfies PreprintTitle
+
+const user = {
+  name: 'Josiah Carberry',
+  orcid: Orcid.Orcid('0000-0002-1825-0097'),
+  pseudonym: 'Orange Panda' as Pseudonym.Pseudonym,
+} satisfies User
