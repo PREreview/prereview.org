260bb55845c0ed8c2b940f892d09d4d5e625abe4
Chris Wilkinson <chris@prereview.org>
2025-05-27 20:04:20 +0100

    Separate rendering into a testable module
    Refs #2436

---
+++ visual-regression/write-review/review-type-page/review-type-form.spec.ts
@@ -0,0 +1,35 @@
+import { Doi } from 'doi-ts'
+import * as E from 'fp-ts/lib/Either.js'
+import { missingE } from '../../../src/form.js'
+import { html } from '../../../src/html.js'
+import { DefaultLocale } from '../../../src/locales/index.js'
+import type { PreprintTitle } from '../../../src/preprint.js'
+import { reviewTypeForm } from '../../../src/write-review/review-type-page/review-type-form.js'
+import { expect, test } from '../../base.js'
+
+test('content looks right', async ({ showPage }) => {
+  const response = reviewTypeForm(preprint, { reviewType: E.right(undefined) }, locale)
+
+  const content = await showPage(response)
+
+  await expect(content).toHaveScreenshot()
+})
+
+test('content looks right when the answer is missing', async ({ showPage }) => {
+  const response = reviewTypeForm(preprint, { reviewType: E.left(missingE()) }, locale)
+
+  const content = await showPage(response)
+
+  await expect(content).toHaveScreenshot()
+})
+
+const locale = DefaultLocale
+
+const preprint = {
+  id: {
+    _tag: 'biorxiv',
+    value: Doi('10.1101/2022.01.13.476201'),
+  },
+  title: html`The role of LHCBM1 in non-photochemical quenching in <i>Chlamydomonas reinhardtii</i>`,
+  language: 'en',
+} satisfies PreprintTitle
