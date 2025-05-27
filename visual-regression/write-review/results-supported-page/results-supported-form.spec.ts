5999b2376310d03d40403ca3462d88f99192c9c0
Chris Wilkinson <chris@prereview.org>
2025-05-27 16:34:06 +0100

    Separate rendering into a testable module
    Refs #2436

---
+++ visual-regression/write-review/results-supported-page/results-supported-form.spec.ts
@@ -0,0 +1,57 @@
+import { Doi } from 'doi-ts'
+import * as E from 'fp-ts/lib/Either.js'
+import { missingE } from '../../../src/form.js'
+import { html } from '../../../src/html.js'
+import { DefaultLocale } from '../../../src/locales/index.js'
+import type { PreprintTitle } from '../../../src/preprint.js'
+import { resultsSupportedForm } from '../../../src/write-review/results-supported-page/results-supported-form.js'
+import { expect, test } from '../../base.js'
+
+test('content looks right', async ({ showPage }) => {
+  const response = resultsSupportedForm(
+    preprint,
+    {
+      resultsSupported: E.right(undefined),
+      resultsSupportedNotSupportedDetails: E.right(undefined),
+      resultsSupportedPartiallySupportedDetails: E.right(undefined),
+      resultsSupportedNeutralDetails: E.right(undefined),
+      resultsSupportedWellSupportedDetails: E.right(undefined),
+      resultsSupportedStronglySupportedDetails: E.right(undefined),
+    },
+    locale,
+  )
+
+  const content = await showPage(response)
+
+  await expect(content).toHaveScreenshot()
+})
+
+test('content looks right when the answer is missing', async ({ showPage }) => {
+  const response = resultsSupportedForm(
+    preprint,
+    {
+      resultsSupported: E.left(missingE()),
+      resultsSupportedNotSupportedDetails: E.right(undefined),
+      resultsSupportedPartiallySupportedDetails: E.right(undefined),
+      resultsSupportedNeutralDetails: E.right(undefined),
+      resultsSupportedWellSupportedDetails: E.right(undefined),
+      resultsSupportedStronglySupportedDetails: E.right(undefined),
+    },
+    locale,
+  )
+
+  const content = await showPage(response)
+
+  await expect(content).toHaveScreenshot()
+})
+
+const locale = DefaultLocale
+
+const preprint = {
+  id: {
+    _tag: 'biorxiv',
+    value: Doi('10.1101/2022.01.13.476201'),
+  },
+  title: html`The role of LHCBM1 in non-photochemical quenching in <i>Chlamydomonas reinhardtii</i>`,
+  language: 'en',
+} satisfies PreprintTitle
