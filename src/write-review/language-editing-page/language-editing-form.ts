89c3c6cfafcf6abea79ea6c876b0a97a5ed5b5c1
Chris Wilkinson <chris@prereview.org>
2025-05-27 15:54:35 +0100

    Separate rendering into a testable module
    Refs #2436

--- src/write-review/language-editing-page/language-editing-form.ts
+++ src/write-review/language-editing-page/language-editing-form.ts
@@ -1,164 +1,25 @@
-import { Match, flow, identity, pipe } from 'effect'
+import { Match, identity, pipe } from 'effect'
 import { format } from 'fp-ts-routing'
 import * as E from 'fp-ts/lib/Either.js'
-import * as RT from 'fp-ts/lib/ReaderTask.js'
-import * as RTE from 'fp-ts/lib/ReaderTaskEither.js'
 import { StatusCodes } from 'http-status-codes'
-import * as D from 'io-ts/lib/Decoder.js'
-import type { Encoder } from 'io-ts/lib/Encoder.js'
 import { P, match } from 'ts-pattern'
-import {
-  type FieldDecoders,
-  type MissingE,
-  type ValidFields,
-  decodeFields,
-  hasAnError,
-  optionalDecoder,
-  requiredDecoder,
-} from '../form.js'
-import { html, plainText, rawHtml } from '../html.js'
-import { havingProblemsPage, pageNotFound } from '../http-error.js'
-import { type SupportedLocale, translate } from '../locales/index.js'
-import { type GetPreprintTitleEnv, type PreprintTitle, getPreprintTitle } from '../preprint.js'
-import { type PageResponse, RedirectResponse, StreamlinePageResponse } from '../response.js'
-import {
-  writeReviewLanguageEditingMatch,
-  writeReviewMatch,
-  writeReviewNovelMatch,
-  writeReviewReviewTypeMatch,
-} from '../routes.js'
-import { errorPrefix } from '../shared-translation-elements.js'
-import type { IndeterminatePreprintId } from '../types/preprint-id.js'
-import { type NonEmptyString, NonEmptyStringC } from '../types/string.js'
-import type { User } from '../user.js'
-import { type Form, type FormStoreEnv, getForm, nextFormMatch, saveForm, updateForm } from './form.js'
-import { prereviewOfSuffix } from './shared-elements.js'
+import { type MissingE, hasAnError } from '../../form.js'
+import { html, plainText, rawHtml } from '../../html.js'
+import { type SupportedLocale, translate } from '../../locales/index.js'
+import type { PreprintTitle } from '../../preprint.js'
+import { StreamlinePageResponse } from '../../response.js'
+import { writeReviewLanguageEditingMatch, writeReviewNovelMatch } from '../../routes.js'
+import { errorPrefix } from '../../shared-translation-elements.js'
+import type { NonEmptyString } from '../../types/string.js'
+import { prereviewOfSuffix } from '../shared-elements.js'
 
-export const writeReviewLanguageEditing = ({
-  body,
-  id,
-  locale,
-  method,
-  user,
-}: {
-  body: unknown
-  id: IndeterminatePreprintId
-  locale: SupportedLocale
-  method: string
-  user?: User
-}): RT.ReaderTask<GetPreprintTitleEnv & FormStoreEnv, PageResponse | RedirectResponse | StreamlinePageResponse> =>
-  pipe(
-    getPreprintTitle(id),
-    RTE.matchEW(
-      Match.valueTags({
-        PreprintIsNotFound: () => RT.of(pageNotFound(locale)),
-        PreprintIsUnavailable: () => RT.of(havingProblemsPage(locale)),
-      }),
-      preprint =>
-        pipe(
-          RTE.Do,
-          RTE.let('locale', () => locale),
-          RTE.let('preprint', () => preprint),
-          RTE.apS('user', RTE.fromNullable('no-session' as const)(user)),
-          RTE.bindW('form', ({ user }) => getForm(user.orcid, preprint.id)),
-          RTE.let('method', () => method),
-          RTE.let('body', () => body),
-          RTE.matchE(
-            error =>
-              RT.of(
-                match(error)
-                  .with('no-form', 'no-session', () =>
-                    RedirectResponse({ location: format(writeReviewMatch.formatter, { id: preprint.id }) }),
-                  )
-                  .with('form-unavailable', () => havingProblemsPage(locale))
-                  .exhaustive(),
-              ),
-            state =>
-              match(state)
-                .with(
-                  { form: P.union({ alreadyWritten: P.optional('yes') }, { reviewType: P.optional('freeform') }) },
-                  () =>
-                    RT.of(
-                      RedirectResponse({ location: format(writeReviewReviewTypeMatch.formatter, { id: preprint.id }) }),
-                    ),
-                )
-                .with({ method: 'POST' }, handleLanguageEditingForm)
-                .otherwise(state => RT.of(showLanguageEditingForm(state))),
-          ),
-        ),
-    ),
-  )
-
-const showLanguageEditingForm = ({
-  form,
-  locale,
-  preprint,
-}: {
-  form: Form
-  locale: SupportedLocale
-  preprint: PreprintTitle
-}) => languageEditingForm(preprint, FormToFieldsE.encode(form), locale)
-
-const handleLanguageEditingForm = ({
-  body,
-  form,
-  locale,
-  preprint,
-  user,
-}: {
-  body: unknown
-  form: Form
-  locale: SupportedLocale
-  preprint: PreprintTitle
-  user: User
-}) =>
-  pipe(
-    RTE.fromEither(decodeFields(languageEditingFields)(body)),
-    RTE.map(updateFormWithFields(form)),
-    RTE.chainFirstW(saveForm(user.orcid, preprint.id)),
-    RTE.matchW(
-      error =>
-        match(error)
-          .with('form-unavailable', () => havingProblemsPage(locale))
-          .with({ languageEditing: P.any }, form => languageEditingForm(preprint, form, locale))
-          .exhaustive(),
-      form => RedirectResponse({ location: format(nextFormMatch(form).formatter, { id: preprint.id }) }),
-    ),
-  )
-
-const languageEditingFields = {
-  languageEditing: requiredDecoder(D.literal('no', 'yes')),
-  languageEditingNoDetails: optionalDecoder(NonEmptyStringC),
-  languageEditingYesDetails: optionalDecoder(NonEmptyStringC),
-} satisfies FieldDecoders
-
-const updateFormWithFields = (form: Form) => flow(FieldsToFormE.encode, updateForm(form))
-
-const FieldsToFormE: Encoder<Form, ValidFields<typeof languageEditingFields>> = {
-  encode: fields => ({
-    languageEditing: fields.languageEditing,
-    languageEditingDetails: {
-      no: fields.languageEditingNoDetails,
-      yes: fields.languageEditingYesDetails,
-    },
-  }),
-}
-
-const FormToFieldsE: Encoder<LanguageEditingForm, Form> = {
-  encode: form => ({
-    languageEditing: E.right(form.languageEditing),
-    languageEditingNoDetails: E.right(form.languageEditingDetails?.no),
-    languageEditingYesDetails: E.right(form.languageEditingDetails?.yes),
-  }),
-}
-
-interface LanguageEditingForm {
+export interface LanguageEditingForm {
   readonly languageEditing: E.Either<MissingE, 'no' | 'yes' | undefined>
   readonly languageEditingNoDetails: E.Either<never, NonEmptyString | undefined>
   readonly languageEditingYesDetails: E.Either<never, NonEmptyString | undefined>
 }
 
-function languageEditingForm(preprint: PreprintTitle, form: LanguageEditingForm, locale: SupportedLocale) {
+export function languageEditingForm(preprint: PreprintTitle, form: LanguageEditingForm, locale: SupportedLocale) {
   const error = hasAnError(form)
   const t = translate(locale, 'write-review')
 
