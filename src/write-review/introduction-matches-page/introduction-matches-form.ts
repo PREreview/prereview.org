af83727ae5b198b2157d6bf03629bea3858b14fc
Chris Wilkinson <chris@prereview.org>
2025-05-27 16:52:38 +0100

    Separate rendering into a testable module
    Refs #2436

--- src/write-review/introduction-matches-page/introduction-matches-form.ts
+++ src/write-review/introduction-matches-page/introduction-matches-form.ts
@@ -1,163 +1,30 @@
-import { Match, flow, identity, pipe } from 'effect'
+import { Match, identity, pipe } from 'effect'
 import { format } from 'fp-ts-routing'
 import * as E from 'fp-ts/lib/Either.js'
-import * as RT from 'fp-ts/lib/ReaderTask.js'
-import * as RTE from 'fp-ts/lib/ReaderTaskEither.js'
 import { StatusCodes } from 'http-status-codes'
-import * as D from 'io-ts/lib/Decoder.js'
-import type { Encoder } from 'io-ts/lib/Encoder.js'
 import { P, match } from 'ts-pattern'
-import {
-  type FieldDecoders,
-  type MissingE,
-  type ValidFields,
-  decodeFields,
-  hasAnError,
-  optionalDecoder,
-  requiredDecoder,
-} from '../form.js'
-import { html, plainText, rawHtml } from '../html.js'
-import { havingProblemsPage, pageNotFound } from '../http-error.js'
-import { type SupportedLocale, translate } from '../locales/index.js'
-import { type GetPreprintTitleEnv, type PreprintTitle, getPreprintTitle } from '../preprint.js'
-import { type PageResponse, RedirectResponse, StreamlinePageResponse } from '../response.js'
-import { writeReviewIntroductionMatchesMatch, writeReviewMatch, writeReviewReviewTypeMatch } from '../routes.js'
-import { errorPrefix } from '../shared-translation-elements.js'
-import type { IndeterminatePreprintId } from '../types/preprint-id.js'
-import { type NonEmptyString, NonEmptyStringC } from '../types/string.js'
-import type { User } from '../user.js'
-import { type Form, type FormStoreEnv, getForm, nextFormMatch, saveForm, updateForm } from './form.js'
-import { prereviewOfSuffix } from './shared-elements.js'
+import { type MissingE, hasAnError } from '../../form.js'
+import { html, plainText, rawHtml } from '../../html.js'
+import { type SupportedLocale, translate } from '../../locales/index.js'
+import type { PreprintTitle } from '../../preprint.js'
+import { StreamlinePageResponse } from '../../response.js'
+import { writeReviewIntroductionMatchesMatch, writeReviewReviewTypeMatch } from '../../routes.js'
+import { errorPrefix } from '../../shared-translation-elements.js'
+import type { NonEmptyString } from '../../types/string.js'
+import { prereviewOfSuffix } from '../shared-elements.js'
 
-export const writeReviewIntroductionMatches = ({
-  body,
-  id,
-  locale,
-  method,
-  user,
-}: {
-  body: unknown
-  id: IndeterminatePreprintId
-  locale: SupportedLocale
-  method: string
-  user?: User
-}): RT.ReaderTask<GetPreprintTitleEnv & FormStoreEnv, PageResponse | RedirectResponse | StreamlinePageResponse> =>
-  pipe(
-    getPreprintTitle(id),
-    RTE.matchEW(
-      Match.valueTags({
-        PreprintIsNotFound: () => RT.of(pageNotFound(locale)),
-        PreprintIsUnavailable: () => RT.of(havingProblemsPage(locale)),
-      }),
-      preprint =>
-        pipe(
-          RTE.Do,
-          RTE.let('locale', () => locale),
-          RTE.let('preprint', () => preprint),
-          RTE.apS('user', RTE.fromNullable('no-session' as const)(user)),
-          RTE.bindW('form', ({ user }) => getForm(user.orcid, preprint.id)),
-          RTE.let('method', () => method),
-          RTE.let('body', () => body),
-          RTE.matchE(
-            error =>
-              RT.of(
-                match(error)
-                  .with('no-form', 'no-session', () =>
-                    RedirectResponse({ location: format(writeReviewMatch.formatter, { id: preprint.id }) }),
-                  )
-                  .with('form-unavailable', () => havingProblemsPage(locale))
-                  .exhaustive(),
-              ),
-            state =>
-              match(state)
-                .with(
-                  { form: P.union({ alreadyWritten: P.optional('yes') }, { reviewType: P.optional('freeform') }) },
-                  () =>
-                    RT.of(
-                      RedirectResponse({ location: format(writeReviewReviewTypeMatch.formatter, { id: preprint.id }) }),
-                    ),
-                )
-                .with({ method: 'POST' }, handleIntroductionMatchesForm)
-                .otherwise(state => RT.of(showIntroductionMatchesForm(state))),
-          ),
-        ),
-    ),
-  )
-
-const showIntroductionMatchesForm = ({
-  form,
-  locale,
-  preprint,
-}: {
-  form: Form
-  locale: SupportedLocale
-  preprint: PreprintTitle
-}) => introductionMatchesForm(preprint, FormToFieldsE.encode(form), locale)
-
-const handleIntroductionMatchesForm = ({
-  body,
-  form,
-  locale,
-  preprint,
-  user,
-}: {
-  body: unknown
-  form: Form
-  locale: SupportedLocale
-  preprint: PreprintTitle
-  user: User
-}) =>
-  pipe(
-    RTE.fromEither(decodeFields(introductionMatchesFields)(body)),
-    RTE.map(updateFormWithFields(form)),
-    RTE.chainFirstW(saveForm(user.orcid, preprint.id)),
-    RTE.matchW(
-      error =>
-        match(error)
-          .with('form-unavailable', () => havingProblemsPage(locale))
-          .with({ introductionMatches: P.any }, form => introductionMatchesForm(preprint, form, locale))
-          .exhaustive(),
-      form => RedirectResponse({ location: format(nextFormMatch(form).formatter, { id: preprint.id }) }),
-    ),
-  )
-
-const introductionMatchesFields = {
-  introductionMatches: requiredDecoder(D.literal('yes', 'partly', 'no', 'skip')),
-  introductionMatchesYesDetails: optionalDecoder(NonEmptyStringC),
-  introductionMatchesPartlyDetails: optionalDecoder(NonEmptyStringC),
-  introductionMatchesNoDetails: optionalDecoder(NonEmptyStringC),
-} satisfies FieldDecoders
-
-const updateFormWithFields = (form: Form) => flow(FieldsToFormE.encode, updateForm(form))
-
-const FieldsToFormE: Encoder<Form, ValidFields<typeof introductionMatchesFields>> = {
-  encode: fields => ({
-    introductionMatches: fields.introductionMatches,
-    introductionMatchesDetails: {
-      yes: fields.introductionMatchesYesDetails,
-      partly: fields.introductionMatchesPartlyDetails,
-      no: fields.introductionMatchesNoDetails,
-    },
-  }),
-}
-
-const FormToFieldsE: Encoder<IntroductionMatchesForm, Form> = {
-  encode: form => ({
-    introductionMatches: E.right(form.introductionMatches),
-    introductionMatchesYesDetails: E.right(form.introductionMatchesDetails?.yes),
-    introductionMatchesPartlyDetails: E.right(form.introductionMatchesDetails?.partly),
-    introductionMatchesNoDetails: E.right(form.introductionMatchesDetails?.no),
-  }),
-}
-
-interface IntroductionMatchesForm {
+export interface IntroductionMatchesForm {
   readonly introductionMatches: E.Either<MissingE, 'yes' | 'partly' | 'no' | 'skip' | undefined>
   readonly introductionMatchesYesDetails: E.Either<never, NonEmptyString | undefined>
   readonly introductionMatchesPartlyDetails: E.Either<never, NonEmptyString | undefined>
   readonly introductionMatchesNoDetails: E.Either<never, NonEmptyString | undefined>
 }
 
-function introductionMatchesForm(preprint: PreprintTitle, form: IntroductionMatchesForm, locale: SupportedLocale) {
+export function introductionMatchesForm(
+  preprint: PreprintTitle,
+  form: IntroductionMatchesForm,
+  locale: SupportedLocale,
+) {
   const error = hasAnError(form)
   const t = translate(locale, 'write-review')
 
---
+++ visual-regression/write-review/introduction-matches-page/introduction-matches-form.spec.ts
@@ -0,0 +1,53 @@
+import { Doi } from 'doi-ts'
+import * as E from 'fp-ts/lib/Either.js'
+import { missingE } from '../../../src/form.js'
+import { html } from '../../../src/html.js'
+import { DefaultLocale } from '../../../src/locales/index.js'
+import type { PreprintTitle } from '../../../src/preprint.js'
+import { introductionMatchesForm } from '../../../src/write-review/introduction-matches-page/introduction-matches-form.js'
+import { expect, test } from '../../base.js'
+
+test('content looks right', async ({ showPage }) => {
+  const response = introductionMatchesForm(
+    preprint,
+    {
+      introductionMatches: E.right(undefined),
+      introductionMatchesYesDetails: E.right(undefined),
+      introductionMatchesPartlyDetails: E.right(undefined),
+      introductionMatchesNoDetails: E.right(undefined),
+    },
+    locale,
+  )
+
+  const content = await showPage(response)
+
+  await expect(content).toHaveScreenshot()
+})
+
+test('content looks right when the answer is missing', async ({ showPage }) => {
+  const response = introductionMatchesForm(
+    preprint,
+    {
+      introductionMatches: E.left(missingE()),
+      introductionMatchesYesDetails: E.right(undefined),
+      introductionMatchesPartlyDetails: E.right(undefined),
+      introductionMatchesNoDetails: E.right(undefined),
+    },
+    locale,
+  )
+
+  const content = await showPage(response)
+
+  await expect(content).toHaveScreenshot()
+})
+
+const locale = DefaultLocale
+
+const preprint = {
+  id: {
+    _tag: 'biorxiv',
+    value: Doi('10.1101/2022.01.13.476201'),
+  },
+  title: html`The role of LHCBM1 in non-photochemical quenching in <i>Chlamydomonas reinhardtii</i>`,
+  language: 'en',
+} satisfies PreprintTitle
