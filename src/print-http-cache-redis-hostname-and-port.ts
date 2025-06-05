b101352a05d5cd263b88d06e703e7bde98fe2763
Chris Wilkinson <chris@prereview.org>
2025-06-05 13:48:14 +0100

    Make the local Redis a replica of the HTTP cache
    Note the replica is unused.
    
    Refs #2477

---
+++ src/print-http-cache-redis-hostname-and-port.ts
@@ -0,0 +1,23 @@
+import { Config, Console, Effect, Exit, pipe } from 'effect'
+
+const httpCacheRedisUri = Config.url('HTTP_CACHE_REDIS_URI').pipe(
+  Config.orElse(() =>
+    Config.all({
+      uriTemplate: Config.string('HTTP_CACHE_REDIS_URI_TEMPLATE'),
+      region: Config.nonEmptyString('FLY_REGION'),
+    }).pipe(Config.map(({ uriTemplate, region }) => new URL(uriTemplate.replace('{region}', region)))),
+  ),
+)
+
+const program = pipe(
+  httpCacheRedisUri,
+  Effect.tapBoth({ onSuccess: uri => Console.log(`${uri.hostname} ${uri.port}`), onFailure: Console.log }),
+)
+
+const result = Effect.runSyncExit(program)
+
+if (Exit.isFailure(result)) {
+  process.exit(1)
+}
+
+process.exit(0)
