518a45f45b74151e6275cb6c9b749dcdf33aea26
Chris Wilkinson <chris@prereview.org>
2025-06-09 12:07:05 +0100

    Remove duplication
    Refs #2436

--- src/Router/NonEffectRouter/MyDetailsRouter.ts
+++ src/Router/NonEffectRouter/MyDetailsRouter.ts
@@ -15,82 +15,78 @@ export const MyDetailsRouter = pipe(
   [
     pipe(
       Routes.myDetailsMatch.parser,
-      P.map(
-        () => (env: Env) =>
-          myDetails({ locale: env.locale, user: env.loggedInUser })({
-            getUserOnboarding: withEnv(Keyv.getUserOnboarding, {
-              userOnboardingStore: env.users.userOnboardingStore,
-              ...env.logger,
-            }),
-            getOrcidToken: withEnv(Keyv.getOrcidToken, {
-              orcidTokenStore: env.users.orcidTokenStore,
-              ...env.logger,
-            }),
-            getAvatar: withEnv(getAvatarFromCloudinary, {
-              getCloudinaryAvatar: withEnv(Keyv.getAvatar, { avatarStore: env.users.avatarStore, ...env.logger }),
-              cloudinaryApi: {
-                cloudName: env.cloudinaryApiConfig.cloudName,
-                key: Redacted.value(env.cloudinaryApiConfig.key),
-                secret: Redacted.value(env.cloudinaryApiConfig.secret),
-              },
-            }),
-            getSlackUser: withEnv(getSlackUser, {
-              ...env.logger,
-              slackUserIdStore: env.users.slackUserIdStore,
-              slackApiToken: Redacted.value(env.slackApiConfig.apiToken),
-              fetch: env.fetch,
-            }),
-            getContactEmailAddress: withEnv(Keyv.getContactEmailAddress, {
-              contactEmailAddressStore: env.users.contactEmailAddressStore,
-              ...env.logger,
-            }),
-            isOpenForRequests: withEnv(Keyv.isOpenForRequests, {
-              isOpenForRequestsStore: env.users.isOpenForRequestsStore,
-              ...env.logger,
-            }),
-            getCareerStage: withEnv(Keyv.getCareerStage, {
-              careerStageStore: env.users.careerStageStore,
-              ...env.logger,
-            }),
-            getResearchInterests: withEnv(Keyv.getResearchInterests, {
-              researchInterestsStore: env.users.researchInterestsStore,
-              ...env.logger,
-            }),
-            getLocation: withEnv(Keyv.getLocation, {
-              locationStore: env.users.locationStore,
-              ...env.logger,
-            }),
-            getLanguages: withEnv(Keyv.getLanguages, {
-              languagesStore: env.users.languagesStore,
-              ...env.logger,
-            }),
-            saveUserOnboarding: withEnv(Keyv.saveUserOnboarding, {
-              userOnboardingStore: env.users.userOnboardingStore,
-              ...env.logger,
-            }),
-          }),
-      ),
+      P.map(() => (env: Env) => myDetails({ locale: env.locale, user: env.loggedInUser })),
     ),
     pipe(
       Routes.changeCareerStageMatch.parser,
       P.map(
         () => (env: Env) =>
-          changeCareerStage({ body: env.body, method: env.method, locale: env.locale, user: env.loggedInUser })({
-            getCareerStage: withEnv(Keyv.getCareerStage, {
-              careerStageStore: env.users.careerStageStore,
-              ...env.logger,
-            }),
-            deleteCareerStage: withEnv(Keyv.deleteCareerStage, {
-              careerStageStore: env.users.careerStageStore,
-              ...env.logger,
-            }),
-            saveCareerStage: withEnv(Keyv.saveCareerStage, {
-              careerStageStore: env.users.careerStageStore,
-              ...env.logger,
-            }),
-          }),
+          changeCareerStage({ body: env.body, method: env.method, locale: env.locale, user: env.loggedInUser }),
       ),
     ),
   ],
   concatAll(P.getParserMonoid()),
+  P.map(
+    handler => (env: Env) =>
+      handler(env)({
+        deleteCareerStage: withEnv(Keyv.deleteCareerStage, {
+          careerStageStore: env.users.careerStageStore,
+          ...env.logger,
+        }),
+        getUserOnboarding: withEnv(Keyv.getUserOnboarding, {
+          userOnboardingStore: env.users.userOnboardingStore,
+          ...env.logger,
+        }),
+        getOrcidToken: withEnv(Keyv.getOrcidToken, {
+          orcidTokenStore: env.users.orcidTokenStore,
+          ...env.logger,
+        }),
+        getAvatar: withEnv(getAvatarFromCloudinary, {
+          getCloudinaryAvatar: withEnv(Keyv.getAvatar, { avatarStore: env.users.avatarStore, ...env.logger }),
+          cloudinaryApi: {
+            cloudName: env.cloudinaryApiConfig.cloudName,
+            key: Redacted.value(env.cloudinaryApiConfig.key),
+            secret: Redacted.value(env.cloudinaryApiConfig.secret),
+          },
+        }),
+        getSlackUser: withEnv(getSlackUser, {
+          ...env.logger,
+          slackUserIdStore: env.users.slackUserIdStore,
+          slackApiToken: Redacted.value(env.slackApiConfig.apiToken),
+          fetch: env.fetch,
+        }),
+        getContactEmailAddress: withEnv(Keyv.getContactEmailAddress, {
+          contactEmailAddressStore: env.users.contactEmailAddressStore,
+          ...env.logger,
+        }),
+        isOpenForRequests: withEnv(Keyv.isOpenForRequests, {
+          isOpenForRequestsStore: env.users.isOpenForRequestsStore,
+          ...env.logger,
+        }),
+        getCareerStage: withEnv(Keyv.getCareerStage, {
+          careerStageStore: env.users.careerStageStore,
+          ...env.logger,
+        }),
+        getResearchInterests: withEnv(Keyv.getResearchInterests, {
+          researchInterestsStore: env.users.researchInterestsStore,
+          ...env.logger,
+        }),
+        getLocation: withEnv(Keyv.getLocation, {
+          locationStore: env.users.locationStore,
+          ...env.logger,
+        }),
+        getLanguages: withEnv(Keyv.getLanguages, {
+          languagesStore: env.users.languagesStore,
+          ...env.logger,
+        }),
+        saveCareerStage: withEnv(Keyv.saveCareerStage, {
+          careerStageStore: env.users.careerStageStore,
+          ...env.logger,
+        }),
+        saveUserOnboarding: withEnv(Keyv.saveUserOnboarding, {
+          userOnboardingStore: env.users.userOnboardingStore,
+          ...env.logger,
+        }),
+      }),
+  ),
 ) satisfies P.Parser<(env: Env) => T.Task<Response.Response>>
