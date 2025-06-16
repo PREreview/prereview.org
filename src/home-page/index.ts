7a98644c46db4a358627f7a54abab2f04febc8ef
Chad Sansing <csansing@gmail.com>
2025-06-16 04:16:28 -0400

    Updated stats on index.ts

--- src/home-page/index.ts
+++ src/home-page/index.ts
@@ -22,7 +22,7 @@ export const home = ({
     RT.Do,
     RT.apS('recentPrereviews', getRecentPrereviews()),
     RT.apSW('recentReviewRequests', getRecentReviewRequests()),
-    RT.let('statistics', () => ({ prereviews: 1153, servers: 30, users: 3311 })),
+    RT.let('statistics', () => ({ prereviews: 1237, servers: 30, users: 3453 })),
     RT.let('locale', () => locale),
     RT.let('canSeeDesignTweaks', () => canSeeDesignTweaks),
     RT.let('canSeeHomePageChanges', () => canSeeHomePageChanges),
