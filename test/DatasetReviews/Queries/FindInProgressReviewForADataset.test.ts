fb30d9fc056529728865dfdd565f949f1f437c63
Chris Wilkinson <chris@prereview.org>
2025-06-25 14:36:18 +0100

    Describe expected behaviour
    Refs #2524

--- test/DatasetReviews/Queries/FindInProgressReviewForADataset.test.ts
+++ test/DatasetReviews/Queries/FindInProgressReviewForADataset.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals'
-import { Option } from 'effect'
+import { Array, Option } from 'effect'
 import * as _ from '../../../src/DatasetReviews/Queries/FindInProgressReviewForADataset.js'
 import * as DatasetReviews from '../../../src/DatasetReviews/index.js'
 import * as Datasets from '../../../src/Datasets/index.js'
@@ -9,6 +9,8 @@ const authorId = Orcid.Orcid('0000-0002-1825-0097')
 const datasetId = new Datasets.DryadDatasetId({ value: Doi.Doi('10.5061/dryad.wstqjq2n3') })
 const resourceId = Uuid.Uuid('b005c394-b9a5-4713-b6a9-bafc8467c3f4')
 const datasetReviewWasStarted = new DatasetReviews.DatasetReviewWasStarted({ authorId, datasetId })
+const publicationWasRequested = new DatasetReviews.PublicationWasRequested()
+const datasetReviewWasPublished = new DatasetReviews.DatasetReviewWasPublished()
 
 describe('FindInProgressReviewForADataset', () => {
   describe('when at least one review needs further user input', () => {
@@ -60,6 +62,22 @@ describe('FindInProgressReviewForADataset', () => {
       ]
 
       const actual = _.FindInProgressReviewForADataset(events)(authorId, datasetId)
+
+      expect(actual).toStrictEqual(Option.none())
+    })
+  })
+
+  describe.each([
+    ['completed', [datasetReviewWasStarted, publicationWasRequested, datasetReviewWasPublished]],
+    // ['has datasetReviewWasStarted', [datasetReviewWasStarted]],
+    // ['has publicationWasRequested', [publicationWasRequested]],
+    // ['has datasetReviewWasPublished', [datasetReviewWasPublished]],
+  ])('when no user input is needed for a comment (%s)', (_name, events) => {
+    test.failing('returns nothing', () => {
+      const actual = _.FindInProgressReviewForADataset(Array.map(events, event => ({ event, resourceId })))(
+        authorId,
+        datasetId,
+      )
 
       expect(actual).toStrictEqual(Option.none())
     })
