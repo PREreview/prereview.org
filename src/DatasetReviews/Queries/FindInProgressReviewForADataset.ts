72d6774291a263a089d6a35cb2ef0a805934b31b
Chris Wilkinson <chris@prereview.org>
2025-06-26 20:35:38 +0100

    Prepare to not pass in all events
    Refs #2518

--- src/DatasetReviews/Queries/FindInProgressReviewForADataset.ts
+++ src/DatasetReviews/Queries/FindInProgressReviewForADataset.ts
@@ -1,4 +1,4 @@
-import { Array, Equal, Option, pipe, Predicate, Record, Tuple } from 'effect'
+import { Array, Boolean, Equal, Match, Option, pipe, Record, Tuple } from 'effect'
 import type * as Datasets from '../../Datasets/index.js'
 import type { Orcid, Uuid } from '../../types/index.js'
 import type { DatasetReviewEvent } from '../Events.js'
@@ -7,34 +7,24 @@ export const FindInProgressReviewForADataset =
   (events: ReadonlyArray<{ readonly event: DatasetReviewEvent; readonly resourceId: Uuid.Uuid }>) =>
   (authorId: Orcid.Orcid, datasetId: Datasets.DatasetId): Option.Option<Uuid.Uuid> =>
     pipe(
-      Array.reduce(
-        events,
-        Record.empty<Uuid.Uuid, ReadonlyArray<DatasetReviewEvent>>(),
-        (candidates, { event, resourceId }) =>
-          pipe(
-            Record.modifyOption(candidates, resourceId, Array.append(event)),
-            Option.getOrElse(() => {
-              if (
-                event._tag === 'DatasetReviewWasStarted' &&
-                Equal.equals(event.authorId, authorId) &&
-                Equal.equals(event.datasetId, datasetId)
-              ) {
-                return Record.set(candidates, resourceId, Array.of(event))
-              }
+      Array.reduce(events, Record.empty<Uuid.Uuid, boolean>(), (candidates, { event, resourceId }) =>
+        Match.valueTags(event, {
+          DatasetReviewWasStarted: () => {
+            if (
+              event._tag === 'DatasetReviewWasStarted' &&
+              Equal.equals(event.authorId, authorId) &&
+              Equal.equals(event.datasetId, datasetId)
+            ) {
+              return Record.set(candidates, resourceId, false)
+            }
 
-              return candidates
-            }),
-          ),
+            return candidates
+          },
+          PublicationWasRequested: () => Record.replace(candidates, resourceId, true),
+          DoiWasAssigned: () => candidates,
+          DatasetReviewWasPublished: () => Record.replace(candidates, resourceId, true),
+        }),
       ),
-      Record.findFirst(
-        Array.every(
-          Predicate.not(
-            Predicate.some([
-              Predicate.isTagged('PublicationWasRequested'),
-              Predicate.isTagged('DatasetReviewWasPublished'),
-            ]),
-          ),
-        ),
-      ),
+      Record.findFirst(Boolean.not),
       Option.map(Tuple.getFirst),
     )
