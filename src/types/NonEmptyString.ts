8ae4f834d89442aab003512d4a390143ffd43487
Chris Wilkinson <chris@prereview.org>
2025-07-01 11:43:00 +0100

    Prefer Effect's branded types
    Refs #1834

--- src/types/NonEmptyString.ts
+++ src/types/NonEmptyString.ts
@@ -1,28 +1,34 @@
-import { pipe, Schema } from 'effect'
+import { type Brand, Either, pipe, Schema } from 'effect'
 import * as C from 'io-ts/lib/Codec.js'
 import * as D from 'io-ts/lib/Decoder.js'
+import * as EffectToFpts from '../EffectToFpts.js'
 
-export type NonEmptyString = string & NonEmptyStringBrand
+const NonEmptyStringBrand: unique symbol = Symbol.for('NonEmptyString')
 
-export const NonEmptyStringC = C.fromDecoder(pipe(D.string, D.refine(isNonEmptyString, 'NonEmptyString')))
+export type NonEmptyString = string & Brand.Brand<typeof NonEmptyStringBrand>
 
-export const NonEmptyStringSchema: Schema.Schema<NonEmptyString, string> = pipe(
+export const NonEmptyStringC = C.fromDecoder(
+  pipe(
+    D.string,
+    D.parse(s =>
+      EffectToFpts.either(
+        Either.try({
+          try: () => NonEmptyString(s),
+          catch: () => D.error(s, 'NonEmptyString'),
+        }),
+      ),
+    ),
+  ),
+)
+
+export const NonEmptyStringSchema = pipe(
   Schema.String,
   Schema.filter(isNonEmptyString, { message: () => 'string is empty' }),
+  Schema.brand(NonEmptyStringBrand),
 )
 
 export function isNonEmptyString(value: string): value is NonEmptyString {
   return value.trim().length > 0
 }
 
-interface NonEmptyStringBrand {
-  readonly NonEmptyString: unique symbol
-}
-
-export const NonEmptyString = (nonEmptyString: string): NonEmptyString => {
-  if (!isNonEmptyString(nonEmptyString)) {
-    throw new Error('String is empty')
-  }
-
-  return nonEmptyString
-}
+export const NonEmptyString = (nonEmptyString: string) => NonEmptyStringSchema.make(nonEmptyString)
