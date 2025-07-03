27e36a97ddd41d8035b5d483e19d72246b948792
Chris Wilkinson <chris@prereview.org>
2025-07-03 11:17:47 +0100

    Describe expectation
    Refs #2524

--- integration/reviewing-a-dataset.spec.ts
+++ integration/reviewing-a-dataset.spec.ts
@@ -67,3 +67,28 @@ test.extend(canLogIn).extend(areLoggedIn)("aren't told about ORCID when already 
 
   await expect(page.getByRole('heading', { level: 1 })).toHaveText('Does this dataset follow FAIR and CARE principles?')
 })
+
+test.extend(canLogIn).extend(areLoggedIn)(
+  'have to say if the dataset follows FAIR and CARE principles',
+  async ({ javaScriptEnabled, page }, testInfo) => {
+    await page.goto('/datasets/doi-10.5061-dryad.wstqjq2n3/review-this-dataset', { waitUntil: 'commit' })
+    await page.getByRole('button', { name: 'Start now' }).click()
+
+    await page.getByRole('button', { name: 'Save and continue' }).click()
+
+    testInfo.fail()
+
+    if (javaScriptEnabled) {
+      await expect(page.getByRole('alert', { name: 'There is a problem' })).toBeFocused()
+    } else {
+      await expect(page.getByRole('alert', { name: 'There is a problem' })).toBeInViewport()
+    }
+    await expect(
+      page.getByRole('group', { name: 'Does this dataset follow FAIR and CARE principles?' }),
+    ).toHaveAttribute('aria-invalid', 'true')
+
+    await page.getByRole('link', { name: 'Select if the dataset follows FAIR and CARE principles' }).click()
+
+    await expect(page.getByLabel('Yes')).toBeFocused()
+  },
+)
