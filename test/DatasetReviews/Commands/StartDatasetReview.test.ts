e19c3574cacf766dbd9728bbc099883d5d964f88
Chris Wilkinson <chris@prereview.org>
2025-07-03 14:17:28 +0100

    Use consistent imports
    Refs #2524

--- test/DatasetReviews/Commands/StartDatasetReview.test.ts
+++ test/DatasetReviews/Commands/StartDatasetReview.test.ts
@@ -1,15 +1,14 @@
 import { test } from '@fast-check/jest'
 import { describe, expect } from '@jest/globals'
 import { Either } from 'effect'
-import * as Errors from '../../../src/DatasetReviews/Commands/Errors.js'
 import * as _ from '../../../src/DatasetReviews/Commands/StartDatasetReview.js'
-import * as Events from '../../../src/DatasetReviews/Events.js'
+import * as DatasetReviews from '../../../src/DatasetReviews/index.js'
 import * as Datasets from '../../../src/Datasets/index.js'
 import { Doi, Orcid } from '../../../src/types/index.js'
 
 const authorId = Orcid.Orcid('0000-0002-1825-0097')
 const datasetId = new Datasets.DryadDatasetId({ value: Doi.Doi('10.5061/dryad.wstqjq2n3') })
-const started = new Events.DatasetReviewWasStarted({ authorId, datasetId })
+const started = new DatasetReviews.DatasetReviewWasStarted({ authorId, datasetId })
 
 describe('foldState', () => {
   test('not yet started', () => {
@@ -31,12 +30,12 @@ describe('decide', () => {
   test('has not been started', () => {
     const result = _.decide(new _.NotStarted(), { authorId, datasetId })
 
-    expect(result).toStrictEqual(Either.right([new Events.DatasetReviewWasStarted({ authorId, datasetId })]))
+    expect(result).toStrictEqual(Either.right([new DatasetReviews.DatasetReviewWasStarted({ authorId, datasetId })]))
   })
 
   test('has already been started', () => {
     const result = _.decide(new _.HasBeenStarted(), { authorId, datasetId })
 
-    expect(result).toStrictEqual(Either.left(new Errors.DatasetReviewWasAlreadyStarted()))
+    expect(result).toStrictEqual(Either.left(new DatasetReviews.DatasetReviewWasAlreadyStarted()))
   })
 })
