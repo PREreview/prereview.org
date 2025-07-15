4b1dfdf76fe6fd1b7d7455a3eab3d33af12194d2
Chris Wilkinson <chris@prereview.org>
2025-07-15 16:26:36 +0100

    Improve code readability
    Refs #2611

--- src/GhostPage/index.ts
+++ src/GhostPage/index.ts
@@ -1,5 +1,5 @@
 import type { HttpClient } from '@effect/platform'
-import { Context, Data, Effect, identity, Layer, pipe } from 'effect'
+import { Context, Data, Effect, flow, identity, Layer } from 'effect'
 import type { Locale } from '../Context.js'
 import type { Html } from '../html.js'
 import type { SupportedLocale } from '../locales/index.js'
@@ -8,33 +8,31 @@ import { getGhostIdAndLocaleForPage, type PageId } from './PageIds.js'
 
 export { GhostApi } from './GetPage.js'
 
-export class PageIsUnavailable extends Data.TaggedError('PageIsUnavailable') {}
-
 export class GetPageFromGhost extends Context.Tag('GetPageFromGhost')<
   GetPageFromGhost,
   (page: PageId) => Effect.Effect<GhostPage, PageIsUnavailable, Locale>
 >() {}
-
-export const getPageFromGhost = Effect.serviceFunctionEffect(GetPageFromGhost, identity)
 
 export interface GhostPage {
   readonly html: Html
   readonly locale: SupportedLocale
 }
 
-const loadWithCachingClient = (page: PageId) =>
-  pipe(
-    getGhostIdAndLocaleForPage(page),
-    Effect.bind('html', ({ id }) => getPage(id)),
-    Effect.tapError(error => Effect.logError('Failed to load ghost page').pipe(Effect.annotateLogs({ error }))),
-    Effect.catchTag('GhostPageNotFound', 'GhostPageUnavailable', () => new PageIsUnavailable()),
-  )
+export class PageIsUnavailable extends Data.TaggedError('PageIsUnavailable') {}
+
+export const getPageFromGhost = Effect.serviceFunctionEffect(GetPageFromGhost, identity)
 
 export const layer = Layer.effect(
   GetPageFromGhost,
   Effect.gen(function* () {
     const context = yield* Effect.context<GhostApi | HttpClient.HttpClient>()
 
-    return id => pipe(loadWithCachingClient(id), Effect.provide(context))
+    return flow(
+      getGhostIdAndLocaleForPage,
+      Effect.bind('html', ({ id }) => getPage(id)),
+      Effect.tapError(error => Effect.logError('Failed to load ghost page').pipe(Effect.annotateLogs({ error }))),
+      Effect.catchTag('GhostPageNotFound', 'GhostPageUnavailable', () => new PageIsUnavailable()),
+      Effect.provide(context),
+    )
   }),
 )
