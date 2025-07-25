f2ac89a0f3c0dc31635550f93c86ebdadbb3e018
Chris Wilkinson <chris@prereview.org>
2025-07-25 11:35:29 +0100

    Make it easier to add queries
    Refs #2524

--- src/DatasetReviews/Queries/index.ts
+++ src/DatasetReviews/Queries/index.ts
@@ -1,8 +1,8 @@
-import { Context, Data, Effect, Layer, type Option } from 'effect'
+import { Context, Data, Effect, type Either, Layer } from 'effect'
 import { EventStore } from '../../EventStore.js'
-import type { Orcid, Uuid } from '../../types/index.js'
+import type { Uuid } from '../../types/index.js'
 import * as Errors from '../Errors.js'
-import { DatasetReviewEventTypes, type AnsweredIfTheDatasetFollowsFairAndCarePrinciples } from '../Events.js'
+import { DatasetReviewEventTypes } from '../Events.js'
 import { CheckIfReviewIsInProgress } from './CheckIfReviewIsInProgress.js'
 import { FindInProgressReviewForADataset } from './FindInProgressReviewForADataset.js'
 import { GetAnswerToIfTheDatasetFollowsFairAndCarePrinciples } from './GetAnswerToIfTheDatasetFollowsFairAndCarePrinciples.js'
@@ -12,13 +12,13 @@ export class DatasetReviewQueries extends Context.Tag('DatasetReviewQueries')<
   DatasetReviewQueries,
   {
     checkIfReviewIsInProgress: Query<
-      (datasetReviewId: Uuid.Uuid) => void,
-      Errors.DatasetReviewIsBeingPublished | Errors.DatasetReviewHasBeenPublished | Errors.UnknownDatasetReview
+      (datasetReviewId: Uuid.Uuid) => ReturnType<typeof CheckIfReviewIsInProgress>,
+      Errors.UnknownDatasetReview
     >
     findInProgressReviewForADataset: Query<ReturnType<typeof FindInProgressReviewForADataset>>
-    getAuthor: Query<(datasetReviewId: Uuid.Uuid) => Orcid.Orcid, Errors.UnknownDatasetReview>
+    getAuthor: Query<(datasetReviewId: Uuid.Uuid) => ReturnType<typeof GetAuthor>, Errors.UnknownDatasetReview>
     getAnswerToIfTheDatasetFollowsFairAndCarePrinciples: Query<
-      (datasetReviewId: Uuid.Uuid) => Option.Option<AnsweredIfTheDatasetFollowsFairAndCarePrinciples['answer']>,
+      (datasetReviewId: Uuid.Uuid) => ReturnType<typeof GetAnswerToIfTheDatasetFollowsFairAndCarePrinciples>,
       Errors.UnknownDatasetReview
     >
   }
@@ -26,7 +26,9 @@ export class DatasetReviewQueries extends Context.Tag('DatasetReviewQueries')<
 
 type Query<F extends (...args: never) => unknown, E = never> = (
   ...args: Parameters<F>
-) => Effect.Effect<ReturnType<F>, UnableToQuery | E>
+) => ReturnType<F> extends Either.Either<infer R, infer L>
+  ? Effect.Effect<R, UnableToQuery | E | Exclude<L, { _tag: 'UnexpectedSequenceOfEvents' }>>
+  : Effect.Effect<ReturnType<F>, UnableToQuery | E>
 
 export class UnableToQuery extends Data.TaggedError('UnableToQuery')<{ cause?: unknown }> {}
 
