0d7b57ec22df17752f452eac2fdd2a3ee2ee6afd
Chris Wilkinson <chris@prereview.org>
2025-08-05 10:57:56 +0100

    Write a test list
    Refs #2524

---
+++ test/DatasetReviews/Reactions/MarkDatasetReviewAsPublished.test.ts
@@ -0,0 +1,41 @@
+import { test } from '@fast-check/jest'
+import { describe, expect } from '@jest/globals'
+import { Effect, Either, Layer, pipe } from 'effect'
+import * as DatasetReviews from '../../../src/DatasetReviews/index.js'
+import * as _ from '../../../src/DatasetReviews/Reactions/MarkDatasetReviewAsPublished.js'
+import * as EffectTest from '../../EffectTest.js'
+import * as fc from '../../fc.js'
+
+describe('MarkDatasetReviewAsPublished', () => {
+  test.failing.prop([fc.uuid()])('when the command can be completed', datasetReviewId =>
+    Effect.gen(function* () {
+      const actual = yield* pipe(_.MarkDatasetReviewAsPublished(datasetReviewId), Effect.either)
+
+      expect(actual).toStrictEqual(Either.void)
+    }).pipe(
+      Effect.provide(
+        Layer.mock(DatasetReviews.DatasetReviewCommands, { markDatasetReviewAsPublished: () => Effect.void }),
+      ),
+      EffectTest.run,
+    ),
+  )
+
+  test.prop([
+    fc.uuid(),
+    fc.constantFrom(
+      new DatasetReviews.DatasetReviewHasNotBeenStarted(),
+      new DatasetReviews.PublicationOfDatasetReviewWasNotRequested(),
+      new DatasetReviews.DatasetReviewNotReadyToBeMarkedAsPublished({ missing: ['DatasetReviewWasAssignedADoi'] }),
+      new DatasetReviews.UnableToHandleCommand({}),
+    ),
+  ])("when the command can't be completed", (datasetReviewId, error) =>
+    Effect.gen(function* () {
+      const actual = yield* pipe(_.MarkDatasetReviewAsPublished(datasetReviewId), Effect.either)
+
+      expect(actual).toStrictEqual(Either.left(new DatasetReviews.FailedToMarkDatasetReviewAsPublished({})))
+    }).pipe(
+      Effect.provide(Layer.mock(DatasetReviews.DatasetReviewCommands, { markDatasetReviewAsPublished: () => error })),
+      EffectTest.run,
+    ),
+  )
+})
