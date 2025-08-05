7279a6693c1daad5b1094d7506a4dfc5eba86108
Chris Wilkinson <chris@prereview.org>
2025-08-05 10:27:30 +0100

    Write a test list
    Refs #2524

--- src/DatasetReviews/Commands/MarkDatasetReviewAsPublished.ts
+++ src/DatasetReviews/Commands/MarkDatasetReviewAsPublished.ts
@@ -1,5 +1,7 @@
+import { type Array, Data, Either, Function, type Option } from 'effect'
 import type { Uuid } from '../../types/index.js'
-import type * as Errors from '../Errors.js'
+import * as Errors from '../Errors.js'
+import type * as Events from '../Events.js'
 
 export interface Command {
   readonly datasetReviewId: Uuid.Uuid
@@ -9,3 +11,30 @@ export type Error =
   | Errors.DatasetReviewHasNotBeenStarted
   | Errors.PublicationOfDatasetReviewWasNotRequested
   | Errors.DatasetReviewNotReadyToBeMarkedAsPublished
+
+export type State = NotStarted | NotRequested | NotReady | IsReady | AlreadyMarkedAsPublished
+
+export class NotStarted extends Data.TaggedClass('NotStarted') {}
+
+export class NotRequested extends Data.TaggedClass('NotRequested') {}
+
+export class NotReady extends Data.TaggedClass('NotReady')<{
+  missing: Array.NonEmptyReadonlyArray<'DatasetReviewWasAssignedADoi'>
+}> {}
+
+export class IsReady extends Data.TaggedClass('IsReady') {}
+
+export class AlreadyMarkedAsPublished extends Data.TaggedClass('AlreadyMarkedAsPublished') {}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const foldState = (events: ReadonlyArray<Events.DatasetReviewEvent>): State => new NotStarted()
+
+export const decide: {
+  (state: State, command: Command): Either.Either<Option.Option<Events.DatasetReviewEvent>, Error>
+  (command: Command): (state: State) => Either.Either<Option.Option<Events.DatasetReviewEvent>, Error>
+} = Function.dual(
+  2,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (state: State, command: Command): Either.Either<Option.Option<Events.DatasetReviewEvent>, Error> =>
+    Either.left(new Errors.DatasetReviewHasNotBeenStarted()),
+)
---
+++ test/DatasetReviews/Commands/MarkDatasetReviewAsPublished.test.ts
@@ -0,0 +1,151 @@
+import { test } from '@fast-check/jest'
+import { describe, expect } from '@jest/globals'
+import { Array, Either, identity, Option, Predicate } from 'effect'
+import * as _ from '../../../src/DatasetReviews/Commands/MarkDatasetReviewAsPublished.js'
+import * as DatasetReviews from '../../../src/DatasetReviews/index.js'
+import * as Datasets from '../../../src/Datasets/index.js'
+import { Doi, Orcid, Uuid } from '../../../src/types/index.js'
+import * as fc from '../../fc.js'
+
+const datasetReviewId = Uuid.Uuid('73b481b8-f33f-43f2-a29e-5be10401c09d')
+const authorId = Orcid.Orcid('0000-0002-1825-0097')
+const datasetId = new Datasets.DryadDatasetId({ value: Doi.Doi('10.5061/dryad.wstqjq2n3') })
+const started = new DatasetReviews.DatasetReviewWasStarted({ authorId, datasetId, datasetReviewId })
+const answered = new DatasetReviews.AnsweredIfTheDatasetFollowsFairAndCarePrinciples({ answer: 'no', datasetReviewId })
+const publicationOfDatasetReviewWasRequested = new DatasetReviews.PublicationOfDatasetReviewWasRequested({
+  datasetReviewId,
+})
+const datasetReviewWasAssignedADoi = new DatasetReviews.DatasetReviewWasAssignedADoi({
+  doi: Doi.Doi('10.1101/12345'),
+  datasetReviewId,
+})
+const datasetReviewWasPublished = new DatasetReviews.DatasetReviewWasPublished({ datasetReviewId })
+
+describe.only('foldState', () => {
+  test.prop([fc.array(fc.datasetReviewEvent().filter(Predicate.not(Predicate.isTagged('DatasetReviewWasStarted'))))], {
+    examples: [
+      [[]], // no events
+      [[answered, datasetReviewWasPublished]], // with events
+    ],
+  })('not started', () => {
+    const state = _.foldState([])
+
+    expect(state).toStrictEqual(new _.NotStarted())
+  })
+
+  test.failing.prop([fc.datasetReviewWasStarted().map(Array.of<DatasetReviews.DatasetReviewEvent>)], {
+    examples: [
+      [[started]], // was started
+      [[started, answered]], // answered
+    ],
+  })('publication has not been requested', events => {
+    const state = _.foldState(events)
+
+    expect(state).toStrictEqual(new _.NotRequested())
+  })
+
+  test.failing.prop(
+    [
+      fc
+        .tuple(fc.datasetReviewWasStarted(), fc.publicationOfDatasetReviewWasRequested())
+        .map(identity<Array.NonEmptyReadonlyArray<DatasetReviews.DatasetReviewEvent>>),
+    ],
+    {
+      examples: [
+        [[started, publicationOfDatasetReviewWasRequested]], // was requested
+        [[started, answered, publicationOfDatasetReviewWasRequested]], // also answered
+        [[started, publicationOfDatasetReviewWasRequested, answered]], // different order
+      ],
+    },
+  )('not ready', events => {
+    const state = _.foldState(events)
+
+    expect(state).toStrictEqual(new _.NotReady({ missing: ['DatasetReviewWasAssignedADoi'] }))
+  })
+
+  test.failing.prop(
+    [
+      fc
+        .tuple(
+          fc.datasetReviewWasStarted(),
+          fc.publicationOfDatasetReviewWasRequested(),
+          fc.datasetReviewWasAssignedADoi(),
+        )
+        .map(identity<Array.NonEmptyReadonlyArray<DatasetReviews.DatasetReviewEvent>>),
+    ],
+    {
+      examples: [
+        [[started, publicationOfDatasetReviewWasRequested, datasetReviewWasAssignedADoi]],
+        [[started, answered, publicationOfDatasetReviewWasRequested, datasetReviewWasAssignedADoi]], // other events
+        [[started, datasetReviewWasAssignedADoi, publicationOfDatasetReviewWasRequested]], // different order
+      ],
+    },
+  )('is ready', events => {
+    const state = _.foldState(events)
+
+    expect(state).toStrictEqual(new _.IsReady())
+  })
+
+  test.failing.prop(
+    [
+      fc
+        .tuple(fc.datasetReviewWasStarted(), fc.datasetReviewWasPublished())
+        .map(identity<Array.NonEmptyReadonlyArray<DatasetReviews.DatasetReviewEvent>>),
+    ],
+    {
+      examples: [
+        [[started, answered, datasetReviewWasPublished]], // was published
+        [
+          [
+            started,
+            answered,
+            publicationOfDatasetReviewWasRequested,
+            datasetReviewWasAssignedADoi,
+            datasetReviewWasPublished,
+          ],
+        ], // other events
+        [[started, datasetReviewWasPublished]], // different order
+      ],
+    },
+  )('already marked as published', events => {
+    const state = _.foldState(events)
+
+    expect(state).toStrictEqual(new _.AlreadyMarkedAsPublished())
+  })
+})
+
+describe('decide', () => {
+  test('has not been started', () => {
+    const result = _.decide(new _.NotStarted(), { datasetReviewId })
+
+    expect(result).toStrictEqual(Either.left(new DatasetReviews.DatasetReviewHasNotBeenStarted()))
+  })
+
+  test('has not been requested', () => {
+    const result = _.decide(new _.NotRequested(), { datasetReviewId })
+
+    expect(result).toStrictEqual(Either.left(new DatasetReviews.PublicationOfDatasetReviewWasNotRequested()))
+  })
+
+  test.prop([fc.nonEmptyArray(fc.constant('DatasetReviewWasAssignedADoi'))])('is not ready', missing => {
+    const result = _.decide(new _.NotReady({ missing }), { datasetReviewId })
+
+    expect(result).toStrictEqual(
+      Either.left(new DatasetReviews.DatasetReviewNotReadyToBeMarkedAsPublished({ missing })),
+    )
+  })
+
+  test('is ready', () => {
+    const result = _.decide(new _.IsReady(), { datasetReviewId })
+
+    expect(result).toStrictEqual(
+      Either.right(Option.some(new DatasetReviews.DatasetReviewWasPublished({ datasetReviewId }))),
+    )
+  })
+
+  test('already marked as published', () => {
+    const result = _.decide(new _.AlreadyMarkedAsPublished(), { datasetReviewId })
+
+    expect(result).toStrictEqual(Either.right(Option.none()))
+  })
+})
