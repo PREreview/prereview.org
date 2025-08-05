68468e62c1b9bb554388c8ee27d57e22d3d326ec
Chris Wilkinson <chris@prereview.org>
2025-08-05 15:52:52 +0100

    Implement publishing a new record
    Refs #2524

--- src/Zenodo/PublishRecord/index.ts
+++ src/Zenodo/PublishRecord/index.ts
@@ -1,8 +1,16 @@
-import { Data } from 'effect'
+import { Data, Effect } from 'effect'
+import { GetDeposition } from '../GetDeposition/index.js'
+import { PublishDeposition } from '../PublishDeposition/index.js'
 
 export class FailedToPublishRecord extends Data.TaggedError('FailedToPublishRecord')<{
   cause?: unknown
 }> {}
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const PublishRecord = (recordId: number) => new FailedToPublishRecord({ cause: 'not implemented' })
+export const PublishRecord = Effect.fn(
+  function* (recordId: number) {
+    const deposition = yield* GetDeposition(recordId)
+
+    yield* PublishDeposition(deposition)
+  },
+  Effect.catchAll(error => new FailedToPublishRecord({ cause: error })),
+)
