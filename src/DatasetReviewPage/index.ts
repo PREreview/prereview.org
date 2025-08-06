49f7c60f3ffccd61ba0c9d62c8d9e609e62cfd49
Chris Wilkinson <chris@prereview.org>
2025-08-06 15:15:27 +0100

    Use the query
    Note that the query has not been implemented.
    
    Refs #2658

--- src/DatasetReviewPage/index.ts
+++ src/DatasetReviewPage/index.ts
@@ -1,8 +1,19 @@
 import { Effect } from 'effect'
+import * as DatasetReviews from '../DatasetReviews/index.js'
 import { HavingProblemsPage } from '../HavingProblemsPage/index.js'
+import { PageNotFound } from '../PageNotFound/index.js'
 import type { Uuid } from '../types/index.js'
+import { createDatasetReviewPage } from './DatasetReviewPage.js'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const DatasetReviewPage = Effect.fn(function* ({ datasetReviewId }: { datasetReviewId: Uuid.Uuid }) {
-  return yield* HavingProblemsPage
-})
+export const DatasetReviewPage = Effect.fn(
+  function* ({ datasetReviewId }: { datasetReviewId: Uuid.Uuid }) {
+    const datasetReview = yield* DatasetReviews.getPublishedReview(datasetReviewId)
+
+    return createDatasetReviewPage({ datasetReview })
+  },
+  Effect.catchTags({
+    DatasetReviewHasNotBeenPublished: () => PageNotFound,
+    UnableToQuery: () => HavingProblemsPage,
+    UnknownDatasetReview: () => PageNotFound,
+  }),
+)
--- test/DatasetReviewPage.test.ts
+++ test/DatasetReviewPage.test.ts
@@ -1,23 +1,124 @@
 import { test } from '@fast-check/jest'
-import { expect } from '@jest/globals'
-import { Effect } from 'effect'
-import { Status } from 'hyper-ts'
+import { describe, expect } from '@jest/globals'
+import { Effect, Layer } from 'effect'
+import { StatusCodes } from 'http-status-codes'
 import { Locale } from '../src/Context.js'
 import * as _ from '../src/DatasetReviewPage/index.js'
+import * as DatasetReviews from '../src/DatasetReviews/index.js'
+import * as Routes from '../src/routes.js'
 import * as EffectTest from './EffectTest.js'
 import * as fc from './fc.js'
 
-test.prop([fc.supportedLocale(), fc.uuid()])('DatasetReviewPage', (locale, datasetReviewId) =>
-  Effect.gen(function* () {
-    const actual = yield* _.DatasetReviewPage({ datasetReviewId })
+describe('DatasetReviewPage', () => {
+  test.prop([
+    fc.supportedLocale(),
+    fc.uuid(),
+    fc.record({
+      author: fc.record(
+        {
+          name: fc.string(),
+          orcid: fc.orcid(),
+        },
+        { requiredKeys: ['name'] },
+      ),
+      doi: fc.doi(),
+      id: fc.uuid(),
+      questions: fc.record({
+        answerToIfTheDatasetFollowsFairAndCarePrinciples: fc.constantFrom('yes', 'partly', 'no', 'unsure'),
+      }),
+      published: fc.plainDate(),
+    }),
+  ])('when the review can be loaded', (locale, datasetReviewId, publishedReview) =>
+    Effect.gen(function* () {
+      const actual = yield* _.DatasetReviewPage({ datasetReviewId })
 
-    expect(actual).toStrictEqual({
-      _tag: 'PageResponse',
-      status: Status.ServiceUnavailable,
-      title: expect.anything(),
-      main: expect.anything(),
-      skipToLabel: 'main',
-      js: [],
-    })
-  }).pipe(Effect.provideService(Locale, locale), EffectTest.run),
-)
+      expect(actual).toStrictEqual({
+        _tag: 'PageResponse',
+        canonical: Routes.DatasetReview.href({ datasetReviewId: publishedReview.id }),
+        status: StatusCodes.OK,
+        title: expect.anything(),
+        description: expect.anything(),
+        main: expect.anything(),
+        skipToLabel: 'prereview',
+        js: [],
+      })
+    }).pipe(
+      Effect.provide(
+        Layer.mock(DatasetReviews.DatasetReviewQueries, {
+          getPublishedReview: () => Effect.succeed(publishedReview),
+        }),
+      ),
+      Effect.provideService(Locale, locale),
+      EffectTest.run,
+    ),
+  )
+
+  test.prop([fc.supportedLocale(), fc.uuid()])('when the review is not found', (locale, datasetReviewId) =>
+    Effect.gen(function* () {
+      const actual = yield* _.DatasetReviewPage({ datasetReviewId })
+
+      expect(actual).toStrictEqual({
+        _tag: 'PageResponse',
+        status: StatusCodes.NOT_FOUND,
+        title: expect.anything(),
+        main: expect.anything(),
+        skipToLabel: 'main',
+        js: [],
+      })
+    }).pipe(
+      Effect.provide(
+        Layer.mock(DatasetReviews.DatasetReviewQueries, {
+          getPublishedReview: () => new DatasetReviews.UnknownDatasetReview({}),
+        }),
+      ),
+      Effect.provideService(Locale, locale),
+      EffectTest.run,
+    ),
+  )
+
+  test.prop([fc.supportedLocale(), fc.uuid()])('when the review is not published', (locale, datasetReviewId) =>
+    Effect.gen(function* () {
+      const actual = yield* _.DatasetReviewPage({ datasetReviewId })
+
+      expect(actual).toStrictEqual({
+        _tag: 'PageResponse',
+        status: StatusCodes.NOT_FOUND,
+        title: expect.anything(),
+        main: expect.anything(),
+        skipToLabel: 'main',
+        js: [],
+      })
+    }).pipe(
+      Effect.provide(
+        Layer.mock(DatasetReviews.DatasetReviewQueries, {
+          getPublishedReview: () => new DatasetReviews.DatasetReviewHasNotBeenPublished({}),
+        }),
+      ),
+      Effect.provideService(Locale, locale),
+      EffectTest.run,
+    ),
+  )
+
+  test.prop([fc.supportedLocale(), fc.uuid()])("when the review can't be loaded", (locale, datasetReviewId) =>
+    Effect.gen(function* () {
+      const actual = yield* _.DatasetReviewPage({ datasetReviewId })
+
+      expect(actual).toStrictEqual({
+        _tag: 'PageResponse',
+        status: StatusCodes.SERVICE_UNAVAILABLE,
+        title: expect.anything(),
+        main: expect.anything(),
+        skipToLabel: 'main',
+        js: [],
+      })
+    }).pipe(
+      Effect.provide(
+        Layer.mock(DatasetReviews.DatasetReviewQueries, {
+          getPublishedReview: () => new DatasetReviews.UnableToQuery({}),
+        }),
+      ),
+      Effect.provideService(Locale, locale),
+      EffectTest.run,
+    ),
+  )
+})
