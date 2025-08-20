55c2fbb25f4f5cbb3a69d0d2fa53b2598be074f0
Chris Wilkinson <chris@prereview.org>
2025-08-20 13:54:38 +0100

    Check that the dataset review is by the user
    Refs #2684

--- src/ReviewADatasetFlow/HasEnoughMetadataQuestion/index.ts
+++ src/ReviewADatasetFlow/HasEnoughMetadataQuestion/index.ts
@@ -1,23 +1,54 @@
 import type { UrlParams } from '@effect/platform'
-import type { Effect } from 'effect'
+import { Effect, Equal } from 'effect'
 import type { Locale } from '../../Context.js'
+import * as DatasetReviews from '../../DatasetReviews/index.js'
 import { HavingProblemsPage } from '../../HavingProblemsPage/index.js'
+import { PageNotFound } from '../../PageNotFound/index.js'
 import type * as Response from '../../response.js'
 import type { Uuid } from '../../types/index.js'
+import { LoggedInUser } from '../../user.js'
 
 export const HasEnoughMetadataQuestion = ({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   datasetReviewId,
 }: {
   datasetReviewId: Uuid.Uuid
-}): Effect.Effect<Response.Response, never, Locale> => HavingProblemsPage
+}): Effect.Effect<Response.Response, never, DatasetReviews.DatasetReviewQueries | Locale | LoggedInUser> =>
+  Effect.gen(function* () {
+    const user = yield* LoggedInUser
+    const author = yield* DatasetReviews.getAuthor(datasetReviewId)
+
+    if (!Equal.equals(user.orcid, author)) {
+      return yield* PageNotFound
+    }
+
+    return yield* HavingProblemsPage
+  }).pipe(
+    Effect.catchTags({
+      UnableToQuery: () => HavingProblemsPage,
+      UnknownDatasetReview: () => PageNotFound,
+    }),
+  )
 
 export const HasEnoughMetadataSubmission = ({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   body,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   datasetReviewId,
 }: {
   body: UrlParams.UrlParams
   datasetReviewId: Uuid.Uuid
-}): Effect.Effect<Response.Response, never, Locale> => HavingProblemsPage
+}): Effect.Effect<Response.Response, never, DatasetReviews.DatasetReviewQueries | Locale | LoggedInUser> =>
+  Effect.gen(function* () {
+    const user = yield* LoggedInUser
+    const author = yield* DatasetReviews.getAuthor(datasetReviewId)
+
+    if (!Equal.equals(user.orcid, author)) {
+      return yield* PageNotFound
+    }
+
+    return yield* HavingProblemsPage
+  }).pipe(
+    Effect.catchTags({
+      UnableToQuery: () => HavingProblemsPage,
+      UnknownDatasetReview: () => PageNotFound,
+    }),
+  )
