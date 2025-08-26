96557f308ef015d057b4fc3b316058cd349db6b1
Chris Wilkinson <chris@prereview.org>
2025-08-26 15:02:55 +0100

    Write a test list
    Refs #2684

--- src/DatasetReviews/Commands/AnswerIfTheDatasetHasTrackedChanges.ts
+++ src/DatasetReviews/Commands/AnswerIfTheDatasetHasTrackedChanges.ts
@@ -1,5 +1,7 @@
+import { Data, type Either, Function, type Option } from 'effect'
 import type { Uuid } from '../../types/index.js'
 import type * as Errors from '../Errors.js'
+import type * as Events from '../Events.js'
 
 export interface Command {
   readonly answer: 'yes' | 'partly' | 'no' | 'unsure'
@@ -10,3 +12,33 @@ export type Error =
   | Errors.DatasetReviewHasNotBeenStarted
   | Errors.DatasetReviewIsBeingPublished
   | Errors.DatasetReviewHasBeenPublished
+
+export type State = NotStarted | NotAnswered | HasBeenAnswered | IsBeingPublished | HasBeenPublished
+
+export class NotStarted extends Data.TaggedClass('NotStarted') {}
+
+export class NotAnswered extends Data.TaggedClass('NotAnswered') {}
+
+export class HasBeenAnswered extends Data.TaggedClass('HasBeenAnswered')<{
+  answer: Events.AnsweredIfTheDatasetHasTrackedChanges['answer']
+}> {}
+
+export class IsBeingPublished extends Data.TaggedClass('IsBeingPublished') {}
+
+export class HasBeenPublished extends Data.TaggedClass('HasBeenPublished') {}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const foldState = (events: ReadonlyArray<Events.DatasetReviewEvent>): State => {
+  throw new Error('not implemented')
+}
+
+export const decide: {
+  (state: State, command: Command): Either.Either<Option.Option<Events.DatasetReviewEvent>, Error>
+  (command: Command): (state: State) => Either.Either<Option.Option<Events.DatasetReviewEvent>, Error>
+} = Function.dual(
+  2,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (state: State, command: Command): Either.Either<Option.Option<Events.DatasetReviewEvent>, Error> => {
+    throw new Error('not implemented')
+  },
+)
---
+++ test/DatasetReviews/Commands/AnswerIfTheDatasetHasTrackedChanges.test.ts
@@ -0,0 +1,155 @@
+import { test } from '@fast-check/jest'
+import { describe, expect } from '@jest/globals'
+import { Temporal } from '@js-temporal/polyfill'
+import { Array, Either, Equal, identity, Option, Predicate, Tuple } from 'effect'
+import * as _ from '../../../src/DatasetReviews/Commands/AnswerIfTheDatasetHasTrackedChanges.js'
+import * as DatasetReviews from '../../../src/DatasetReviews/index.js'
+import * as Datasets from '../../../src/Datasets/index.js'
+import { Doi, Orcid, Uuid } from '../../../src/types/index.js'
+import * as fc from '../../fc.js'
+
+const datasetReviewId = Uuid.Uuid('73b481b8-f33f-43f2-a29e-5be10401c09d')
+const authorId = Orcid.Orcid('0000-0002-1825-0097')
+const datasetId = new Datasets.DryadDatasetId({ value: Doi.Doi('10.5061/dryad.wstqjq2n3') })
+const started = new DatasetReviews.DatasetReviewWasStarted({ authorId, datasetId, datasetReviewId })
+const answered1 = new DatasetReviews.AnsweredIfTheDatasetHasTrackedChanges({ answer: 'no', datasetReviewId })
+const answered2 = new DatasetReviews.AnsweredIfTheDatasetHasTrackedChanges({ answer: 'yes', datasetReviewId })
+const publicationOfDatasetReviewWasRequested = new DatasetReviews.PublicationOfDatasetReviewWasRequested({
+  datasetReviewId,
+})
+const datasetReviewWasPublished = new DatasetReviews.DatasetReviewWasPublished({
+  datasetReviewId,
+  publicationDate: Temporal.PlainDate.from('2025-01-01'),
+})
+
+describe('foldState', () => {
+  test.failing.prop(
+    [fc.array(fc.datasetReviewEvent().filter(Predicate.not(Predicate.isTagged('DatasetReviewWasStarted'))))],
+    {
+      examples: [
+        [[]], // no events
+        [[answered1, datasetReviewWasPublished]], // with events
+      ],
+    },
+  )('not started', () => {
+    const state = _.foldState([])
+
+    expect(state).toStrictEqual(new _.NotStarted())
+  })
+
+  test.failing.prop([fc.datasetReviewWasStarted().map(Array.of<DatasetReviews.DatasetReviewEvent>)], {
+    examples: [
+      [[started]], // was started
+    ],
+  })('not answered', events => {
+    const state = _.foldState(events)
+
+    expect(state).toStrictEqual(new _.NotAnswered())
+  })
+
+  test.failing.prop(
+    [
+      fc
+        .tuple(fc.datasetReviewWasStarted(), fc.datasetReviewAnsweredIfTheDatasetHasTrackedChanges())
+        .map(([started, answered]) => Tuple.make([started, answered], answered.answer)),
+    ],
+    {
+      examples: [
+        [[[started, answered1], answered1.answer]], // one answer
+        [[[started, answered1, answered2], answered2.answer]], // two answers
+      ],
+    },
+  )('has been answered', ([events, expected]) => {
+    const state = _.foldState(events)
+
+    expect(state).toStrictEqual(new _.HasBeenAnswered({ answer: expected }))
+  })
+
+  test.failing.prop(
+    [
+      fc
+        .tuple(fc.datasetReviewWasStarted(), fc.publicationOfDatasetReviewWasRequested())
+        .map(identity<Array.NonEmptyReadonlyArray<DatasetReviews.DatasetReviewEvent>>),
+    ],
+    {
+      examples: [
+        [[started, publicationOfDatasetReviewWasRequested]], // was requested
+        [[started, answered1, publicationOfDatasetReviewWasRequested]], // also answered
+        [[started, publicationOfDatasetReviewWasRequested, answered1]], // different order
+      ],
+    },
+  )('is being published', events => {
+    const state = _.foldState(events)
+
+    expect(state).toStrictEqual(new _.IsBeingPublished())
+  })
+
+  test.failing.prop(
+    [
+      fc
+        .tuple(fc.datasetReviewWasStarted(), fc.datasetReviewWasPublished())
+        .map(identity<Array.NonEmptyReadonlyArray<DatasetReviews.DatasetReviewEvent>>),
+    ],
+    {
+      examples: [
+        [[started, answered1, datasetReviewWasPublished]], // was published
+        [[started, answered1, publicationOfDatasetReviewWasRequested, datasetReviewWasPublished]], // also requested
+        [[started, datasetReviewWasPublished, answered1]], // different order
+      ],
+    },
+  )('has been published', events => {
+    const state = _.foldState(events)
+
+    expect(state).toStrictEqual(new _.HasBeenPublished())
+  })
+})
+
+describe('decide', () => {
+  test.failing.prop([fc.constantFrom('yes', 'partly', 'no', 'unsure')])('has not been started', answer => {
+    const result = _.decide(new _.NotStarted(), { answer, datasetReviewId })
+
+    expect(result).toStrictEqual(Either.left(new DatasetReviews.DatasetReviewHasNotBeenStarted()))
+  })
+
+  test.failing.prop([fc.constantFrom('yes', 'partly', 'no', 'unsure')])('has not been answered', answer => {
+    const result = _.decide(new _.NotAnswered(), { answer, datasetReviewId })
+
+    expect(result).toStrictEqual(
+      Either.right(Option.some(new DatasetReviews.AnsweredIfTheDatasetHasTrackedChanges({ answer, datasetReviewId }))),
+    )
+  })
+
+  describe('has been answered', () => {
+    test.failing.prop([
+      fc
+        .tuple(fc.constantFrom('yes', 'partly', 'no', 'unsure'), fc.constantFrom('yes', 'partly', 'no', 'unsure'))
+        .filter(([answer1, answer2]) => !Equal.equals(answer1, answer2)),
+    ])('with a different answer', ([answer1, answer2]) => {
+      const result = _.decide(new _.HasBeenAnswered({ answer: answer1 }), { answer: answer2, datasetReviewId })
+
+      expect(result).toStrictEqual(
+        Either.right(
+          Option.some(new DatasetReviews.AnsweredIfTheDatasetHasTrackedChanges({ answer: answer2, datasetReviewId })),
+        ),
+      )
+    })
+
+    test.failing.prop([fc.constantFrom('yes', 'partly', 'no', 'unsure')])('with the same answer', answer => {
+      const result = _.decide(new _.HasBeenAnswered({ answer }), { answer, datasetReviewId })
+
+      expect(result).toStrictEqual(Either.right(Option.none()))
+    })
+  })
+
+  test.failing.prop([fc.constantFrom('yes', 'partly', 'no', 'unsure')])('is being published', answer => {
+    const result = _.decide(new _.IsBeingPublished(), { answer, datasetReviewId })
+
+    expect(result).toStrictEqual(Either.left(new DatasetReviews.DatasetReviewIsBeingPublished()))
+  })
+
+  test.failing.prop([fc.constantFrom('yes', 'partly', 'no', 'unsure')])('has been published', answer => {
+    const result = _.decide(new _.HasBeenPublished(), { answer, datasetReviewId })
+
+    expect(result).toStrictEqual(Either.left(new DatasetReviews.DatasetReviewHasBeenPublished()))
+  })
+})
