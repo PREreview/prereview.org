a97e083d7fb1749aedc1cd021d35c046c1ec9d25
Chris Wilkinson <chris@prereview.org>
2025-08-26 13:58:06 +0100

    Check that the dataset review is by the user
    Refs #2684

--- test/ReviewADatasetFlow/HasTrackedChanges.test.ts
+++ test/ReviewADatasetFlow/HasTrackedChanges.test.ts
@@ -1,40 +1,231 @@
 import { test } from '@fast-check/jest'
-import { expect } from '@jest/globals'
-import { Effect } from 'effect'
+import { describe, expect } from '@jest/globals'
+import { Effect, Equal, Layer } from 'effect'
 import { Locale } from '../../src/Context.js'
+import * as DatasetReviews from '../../src/DatasetReviews/index.js'
 import * as _ from '../../src/ReviewADatasetFlow/HasTrackedChangesQuestion/index.js'
 import * as StatusCodes from '../../src/StatusCodes.js'
+import { LoggedInUser } from '../../src/user.js'
 import * as EffectTest from '../EffectTest.js'
 import * as fc from '../fc.js'
 
-test.prop([fc.uuid(), fc.supportedLocale()])('HasTrackedChangesQuestion', (datasetReviewId, locale) =>
-  Effect.gen(function* () {
-    const actual = yield* _.HasTrackedChangesQuestion({ datasetReviewId })
+describe('HasTrackedChangesQuestion', () => {
+  test.prop([fc.uuid(), fc.supportedLocale(), fc.user()])(
+    'when the dataset review is by the user',
+    (datasetReviewId, locale, user) =>
+      Effect.gen(function* () {
+        const actual = yield* _.HasTrackedChangesQuestion({ datasetReviewId })
 
-    expect(actual).toStrictEqual({
-      _tag: 'PageResponse',
-      status: StatusCodes.ServiceUnavailable,
-      title: expect.anything(),
-      main: expect.anything(),
-      skipToLabel: 'main',
-      js: [],
-    })
-  }).pipe(Effect.provideService(Locale, locale), EffectTest.run),
-)
+        expect(actual).toStrictEqual({
+          _tag: 'PageResponse',
+          status: StatusCodes.ServiceUnavailable,
+          title: expect.anything(),
+          main: expect.anything(),
+          skipToLabel: 'main',
+          js: [],
+        })
+      }).pipe(
+        Effect.provide(
+          Layer.mock(DatasetReviews.DatasetReviewQueries, {
+            getAuthor: () => Effect.succeed(user.orcid),
+          }),
+        ),
+        Effect.provideService(Locale, locale),
+        Effect.provideService(LoggedInUser, user),
+        EffectTest.run,
+      ),
+  )
 
-test.prop([fc.uuid(), fc.urlParams(), fc.supportedLocale()])(
-  'HasTrackedChangesSubmission',
-  (datasetReviewId, body, locale) =>
+  test.prop([
+    fc.uuid(),
+    fc.supportedLocale(),
+    fc
+      .tuple(fc.user(), fc.orcid())
+      .filter(([user, datasetReviewAuthor]) => !Equal.equals(user.orcid, datasetReviewAuthor)),
+  ])('when the dataset review is by a different user', (datasetReviewId, locale, [user, datasetReviewAuthor]) =>
     Effect.gen(function* () {
-      const actual = yield* _.HasTrackedChangesSubmission({ datasetReviewId, body })
+      const actual = yield* _.HasTrackedChangesQuestion({ datasetReviewId })
 
       expect(actual).toStrictEqual({
         _tag: 'PageResponse',
-        status: StatusCodes.ServiceUnavailable,
+        status: StatusCodes.NotFound,
         title: expect.anything(),
         main: expect.anything(),
         skipToLabel: 'main',
         js: [],
       })
-    }).pipe(Effect.provideService(Locale, locale), EffectTest.run),
-)
+    }).pipe(
+      Effect.provide(
+        Layer.mock(DatasetReviews.DatasetReviewQueries, {
+          getAuthor: () => Effect.succeed(datasetReviewAuthor),
+        }),
+      ),
+      Effect.provideService(Locale, locale),
+      Effect.provideService(LoggedInUser, user),
+      EffectTest.run,
+    ),
+  )
+
+  test.prop([fc.uuid(), fc.supportedLocale(), fc.user()])(
+    "when the dataset review hasn't been started",
+    (datasetReviewId, locale, user) =>
+      Effect.gen(function* () {
+        const actual = yield* _.HasTrackedChangesQuestion({ datasetReviewId })
+
+        expect(actual).toStrictEqual({
+          _tag: 'PageResponse',
+          status: StatusCodes.NotFound,
+          title: expect.anything(),
+          main: expect.anything(),
+          skipToLabel: 'main',
+          js: [],
+        })
+      }).pipe(
+        Effect.provide(
+          Layer.mock(DatasetReviews.DatasetReviewQueries, {
+            getAuthor: () => new DatasetReviews.UnknownDatasetReview({}),
+          }),
+        ),
+        Effect.provideService(Locale, locale),
+        Effect.provideService(LoggedInUser, user),
+        EffectTest.run,
+      ),
+  )
+
+  test.prop([fc.uuid(), fc.supportedLocale(), fc.user()])(
+    "when the dataset review can't been queried",
+    (datasetReviewId, locale, user) =>
+      Effect.gen(function* () {
+        const actual = yield* _.HasTrackedChangesQuestion({ datasetReviewId })
+
+        expect(actual).toStrictEqual({
+          _tag: 'PageResponse',
+          status: StatusCodes.ServiceUnavailable,
+          title: expect.anything(),
+          main: expect.anything(),
+          skipToLabel: 'main',
+          js: [],
+        })
+      }).pipe(
+        Effect.provide(
+          Layer.mock(DatasetReviews.DatasetReviewQueries, {
+            getAuthor: () => new DatasetReviews.UnableToQuery({}),
+          }),
+        ),
+        Effect.provideService(Locale, locale),
+        Effect.provideService(LoggedInUser, user),
+        EffectTest.run,
+      ),
+  )
+})
+
+describe('HasTrackedChangesSubmission', () => {
+  test.prop([fc.uuid(), fc.urlParams(), fc.supportedLocale(), fc.user()])(
+    'when the dataset review is by the user',
+    (datasetReviewId, body, locale, user) =>
+      Effect.gen(function* () {
+        const actual = yield* _.HasTrackedChangesSubmission({ datasetReviewId, body })
+
+        expect(actual).toStrictEqual({
+          _tag: 'PageResponse',
+          status: StatusCodes.ServiceUnavailable,
+          title: expect.anything(),
+          main: expect.anything(),
+          skipToLabel: 'main',
+          js: [],
+        })
+      }).pipe(
+        Effect.provide(
+          Layer.mock(DatasetReviews.DatasetReviewQueries, {
+            getAuthor: () => Effect.succeed(user.orcid),
+          }),
+        ),
+        Effect.provideService(Locale, locale),
+        Effect.provideService(LoggedInUser, user),
+        EffectTest.run,
+      ),
+  )
+
+  test.prop([
+    fc.uuid(),
+    fc.urlParams(),
+    fc.supportedLocale(),
+    fc
+      .tuple(fc.user(), fc.orcid())
+      .filter(([user, datasetReviewAuthor]) => !Equal.equals(user.orcid, datasetReviewAuthor)),
+  ])('when the dataset review is by a different user', (datasetReviewId, body, locale, [user, datasetReviewAuthor]) =>
+    Effect.gen(function* () {
+      const actual = yield* _.HasTrackedChangesSubmission({ datasetReviewId, body })
+
+      expect(actual).toStrictEqual({
+        _tag: 'PageResponse',
+        status: StatusCodes.NotFound,
+        title: expect.anything(),
+        main: expect.anything(),
+        skipToLabel: 'main',
+        js: [],
+      })
+    }).pipe(
+      Effect.provide(
+        Layer.mock(DatasetReviews.DatasetReviewQueries, {
+          getAuthor: () => Effect.succeed(datasetReviewAuthor),
+        }),
+      ),
+      Effect.provideService(Locale, locale),
+      Effect.provideService(LoggedInUser, user),
+      EffectTest.run,
+    ),
+  )
+
+  test.prop([fc.uuid(), fc.urlParams(), fc.supportedLocale(), fc.user()])(
+    "when the dataset review hasn't been started",
+    (datasetReviewId, body, locale, user) =>
+      Effect.gen(function* () {
+        const actual = yield* _.HasTrackedChangesSubmission({ body, datasetReviewId })
+
+        expect(actual).toStrictEqual({
+          _tag: 'PageResponse',
+          status: StatusCodes.NotFound,
+          title: expect.anything(),
+          main: expect.anything(),
+          skipToLabel: 'main',
+          js: [],
+        })
+      }).pipe(
+        Effect.provide(
+          Layer.mock(DatasetReviews.DatasetReviewQueries, {
+            getAuthor: () => new DatasetReviews.UnknownDatasetReview({}),
+          }),
+        ),
+        Effect.provideService(Locale, locale),
+        Effect.provideService(LoggedInUser, user),
+        EffectTest.run,
+      ),
+  )
+
+  test.prop([fc.uuid(), fc.urlParams(), fc.supportedLocale(), fc.user()])(
+    "when the dataset review can't been queried",
+    (datasetReviewId, body, locale, user) =>
+      Effect.gen(function* () {
+        const actual = yield* _.HasTrackedChangesSubmission({ body, datasetReviewId })
+
+        expect(actual).toStrictEqual({
+          _tag: 'PageResponse',
+          status: StatusCodes.ServiceUnavailable,
+          title: expect.anything(),
+          main: expect.anything(),
+          skipToLabel: 'main',
+          js: [],
+        })
+      }).pipe(
+        Effect.provide(
+          Layer.mock(DatasetReviews.DatasetReviewQueries, {
+            getAuthor: () => new DatasetReviews.UnableToQuery({}),
+          }),
+        ),
+        Effect.provideService(Locale, locale),
+        Effect.provideService(LoggedInUser, user),
+        EffectTest.run,
+      ),
+  )
+})
