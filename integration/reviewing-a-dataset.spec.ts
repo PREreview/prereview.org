a36a10616cb4ec578bb97886097f2ba5ac92ceb1
Chris Wilkinson <chris@prereview.org>
2025-09-02 14:22:37 +0100

    Process the answer
    Refs #2684

--- integration/reviewing-a-dataset.spec.ts
+++ integration/reviewing-a-dataset.spec.ts
@@ -447,3 +447,27 @@ test.extend(canLogIn).extend(areLoggedIn)(
     await expect(page.getByLabel('Yes')).toBeFocused()
   },
 )
+
+test.extend(canLogIn).extend(areLoggedIn)(
+  'have to say if the dataset supports related conclusions',
+  async ({ javaScriptEnabled, page }) => {
+    await page.goto('/datasets/doi-10.5061-dryad.wstqjq2n3/review-this-dataset', { waitUntil: 'commit' })
+    await page.getByRole('button', { name: 'Start now' }).click()
+    await page.goto(`${page.url()}/../supports-related-conclusions`, { waitUntil: 'commit' })
+
+    await page.getByRole('button', { name: 'Save and continue' }).click()
+
+    if (javaScriptEnabled) {
+      await expect(page.getByRole('alert', { name: 'There is a problem' })).toBeFocused()
+    } else {
+      await expect(page.getByRole('alert', { name: 'There is a problem' })).toBeInViewport()
+    }
+    await expect(
+      page.getByRole('group', { name: 'Does this dataset support the researcher’s stated conclusions?' }),
+    ).toHaveAttribute('aria-invalid', 'true')
+
+    await page.getByRole('link', { name: 'Select if the dataset supports the conclusions' }).click()
+
+    await expect(page.getByLabel('Yes')).toBeFocused()
+  },
+)
--- src/ReviewADatasetFlow/SupportsRelatedConclusionsQuestion/SupportsRelatedConclusionsForm.ts
+++ src/ReviewADatasetFlow/SupportsRelatedConclusionsQuestion/SupportsRelatedConclusionsForm.ts
@@ -1,4 +1,5 @@
-import { Data, type Either, Option } from 'effect'
+import { UrlParams } from '@effect/platform'
+import { Data, Effect, Either, Option, Schema } from 'effect'
 
 export type SupportsRelatedConclusionsForm = EmptyForm | InvalidForm | CompletedForm
 
@@ -14,8 +15,25 @@ export class CompletedForm extends Data.TaggedClass('CompletedForm')<{
   supportsRelatedConclusions: 'yes' | 'partly' | 'no' | 'unsure'
 }> {}
 
+export const fromBody = Effect.fn(
+  function* (body: UrlParams.UrlParams) {
+    const { supportsRelatedConclusions } = yield* Schema.decode(SupportsRelatedConclusionsSchema)(body)
+
+    return new CompletedForm({ supportsRelatedConclusions })
+  },
+  Effect.catchTag('ParseError', () =>
+    Effect.succeed(new InvalidForm({ supportsRelatedConclusions: Either.left(new Missing()) })),
+  ),
+)
+
 export const fromAnswer: (answer: Option.Option<'yes' | 'partly' | 'no' | 'unsure'>) => SupportsRelatedConclusionsForm =
   Option.match({
     onNone: () => new EmptyForm(),
     onSome: answer => new CompletedForm({ supportsRelatedConclusions: answer }),
   })
+
+const SupportsRelatedConclusionsSchema = UrlParams.schemaRecord(
+  Schema.Struct({
+    supportsRelatedConclusions: Schema.Literal('yes', 'partly', 'no', 'unsure'),
+  }),
+)
