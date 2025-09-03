c4fcd964630278df268143163775c10eb70d03e9
Chris Wilkinson <chris@prereview.org>
2025-09-03 14:45:31 +0100

    Set a better expectation for the length of the answer
    Refs #2684, 4a964fab2f8bb2f17a6cf5e5f4b03eb85e18dddc

--- src/ReviewADatasetFlow/IsMissingAnythingQuestion/IsMissingAnythingQuestion.ts
+++ src/ReviewADatasetFlow/IsMissingAnythingQuestion/IsMissingAnythingQuestion.ts
@@ -25,9 +25,9 @@ export const IsMissingAnythingQuestion = ({
           </h1>
 
           ${Match.valueTags(form, {
-            EmptyForm: () => html`<textarea id="is-missing-anything" name="is-missing-anything" rows="20"></textarea>`,
+            EmptyForm: () => html`<textarea id="is-missing-anything" name="is-missing-anything" rows="5"></textarea>`,
             CompletedForm: form =>
-              html`<textarea id="is-missing-anything" name="is-missing-anything" rows="20">
+              html`<textarea id="is-missing-anything" name="is-missing-anything" rows="5">
 ${Option.getOrElse(form.isMissingAnything, () => String.empty)}</textarea
               >`,
           })}
