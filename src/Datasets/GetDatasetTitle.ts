888a58ffecba9192405859a6f25c1905ba0b1d31
Chris Wilkinson <chris@prereview.org>
2025-09-19 10:19:47 +0100

    Push up hardcoding
    Refs #2733

--- src/Datasets/GetDatasetTitle.ts
+++ src/Datasets/GetDatasetTitle.ts
@@ -1,20 +1,13 @@
-import { Effect, Equal } from 'effect'
-import { html } from '../html.js'
-import { Doi } from '../types/index.js'
+import { Effect } from 'effect'
 import * as Dataset from './Dataset.js'
 import type * as DatasetId from './DatasetId.js'
+import { GetDataset } from './GetDataset.js'
 
 export const GetDatasetTitle = (
   id: DatasetId.DatasetId,
 ): Effect.Effect<Dataset.DatasetTitle, Dataset.DatasetIsNotFound | Dataset.DatasetIsUnavailable> =>
-  Effect.if(Equal.equals(id.value, Doi.Doi('10.5061/dryad.wstqjq2n3')), {
-    onFalse: () => new Dataset.DatasetIsUnavailable({ cause: 'not implemented' }),
-    onTrue: () =>
-      Effect.succeed(
-        new Dataset.DatasetTitle({
-          id,
-          title: html`Metadata collected from 500 articles in the field of ecology and evolution`,
-          language: 'en',
-        }),
-      ),
-  })
+  Effect.andThen(
+    GetDataset(id),
+    dataset =>
+      new Dataset.DatasetTitle({ id: dataset.id, language: dataset.title.language, title: dataset.title.text }),
+  )
