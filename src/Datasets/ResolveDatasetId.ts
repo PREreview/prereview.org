8ee2ae9b22deac8fef6ce87539408eb24410b3f7
Chris Wilkinson <chris@prereview.org>
2025-09-23 11:00:48 +0100

    Prepare to remove hardcoding
    Refs #2733

--- src/Datasets/ResolveDatasetId.ts
+++ src/Datasets/ResolveDatasetId.ts
@@ -1,12 +1,9 @@
-import { Effect, Equal } from 'effect'
-import { Doi } from '../types/index.js'
-import * as Dataset from './Dataset.js'
+import { Effect, Struct } from 'effect'
+import { GetDatasetFromDatacite } from './Datacite/index.js'
+import type * as Dataset from './Dataset.js'
 import type * as DatasetId from './DatasetId.js'
 
 export const ResolveDatasetId = (
   id: DatasetId.DatasetId,
 ): Effect.Effect<DatasetId.DatasetId, Dataset.DatasetIsNotFound | Dataset.DatasetIsUnavailable | Dataset.NotADataset> =>
-  Effect.if(Equal.equals(id.value, Doi.Doi('10.5061/dryad.wstqjq2n3')), {
-    onFalse: () => new Dataset.DatasetIsUnavailable({ cause: 'not implemented', datasetId: id }),
-    onTrue: () => Effect.succeed(id),
-  })
+  Effect.andThen(GetDatasetFromDatacite(id), Struct.get('id'))
