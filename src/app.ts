f9aecce937b8a1cdf20e7d449d2140cd6365108c
Chris Wilkinson <chris@prereview.org>
2025-09-30 10:40:16 +0100

    Stop proxying to the legacy API as it’s not being used externally
    Refs #2436

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import { Function, pipe, type Runtime } from 'effect'
 import express from 'express'
 import asyncHandler from 'express-async-handler'
-import type { Json } from 'fp-ts/lib/Json.js'
 import * as R from 'fp-ts/lib/Reader.js'
 import type { HttpError } from 'http-errors'
-import { createProxyMiddleware } from 'http-proxy-middleware'
 import type { ResponseEnded, StatusOpen } from 'hyper-ts'
 import * as RM from 'hyper-ts/lib/ReaderMiddleware.js'
 import { toRequestHandler } from 'hyper-ts/lib/express.js'
-import * as L from 'logger-fp-ts'
+import type * as L from 'logger-fp-ts'
 import * as EffectToFpts from './EffectToFpts.ts'
 import { withEnv } from './Fpts.ts'
 import type * as Keyv from './keyv.ts'
@@ -60,39 +58,6 @@ export type AppContext = Preprints.Preprints
 type AppRuntime = Runtime.Runtime<AppContext>
 
 export const app = (config: ConfigEnv) => {
-  const proxy = createProxyMiddleware<Express.Request, Express.Response>({
-    target: config.legacyPrereviewApi.url,
-    changeOrigin: true,
-    pathFilter: '/api/v2/',
-    on: {
-      proxyReq: (proxyReq, req) => {
-        const logger = req.logger
-
-        const payload = {
-          url: `${proxyReq.protocol}//${proxyReq.host}${proxyReq.path}`,
-          method: proxyReq.method,
-        }
-
-        L.debugP('Sending proxy HTTP request')(payload)({ ...config, logger })()
-
-        proxyReq.once('response', response => {
-          L.debugP('Received proxy HTTP response')({
-            ...payload,
-            status: response.statusCode as Json,
-            headers: response.headers as Json,
-          })({ ...config, logger })()
-        })
-
-        proxyReq.once('error', error => {
-          L.warnP('Did not receive a proxy HTTP response')({
-            ...payload,
-            error: error.message,
-          })({ ...config, logger })()
-        })
-      },
-    },
-  })
-
   return ({
     locale,
     logger,
@@ -106,8 +71,6 @@ export const app = (config: ConfigEnv) => {
   }) => {
     return express()
       .use((req, res, next) => {
-        req.logger = logger
-
         if (!config.allowSiteCrawlers) {
           res.header('X-Robots-Tag', 'none, noarchive')
         }
@@ -129,7 +92,6 @@ export const app = (config: ConfigEnv) => {
 
         next()
       })
-      .use(asyncHandler(proxy))
       .use(express.urlencoded({ extended: true }))
       .use((req, res, next) => {
         res.set('Cache-Control', 'no-cache, private')
