a1ed44a591982f1ed5cac3303016daf2a6dd55a1
Chris Wilkinson <chris@prereview.org>
2025-10-15 12:18:09 +0100

    Commit missing change
    Refs #2805, e9fc99fb7d3df9f450b2e714a4298a06b45d6c39

--- src/ReviewADatasetFlow/ReviewPublishedPage/ReviewPublishedPage.ts
+++ src/ReviewADatasetFlow/ReviewPublishedPage/ReviewPublishedPage.ts
@@ -16,6 +16,43 @@ export const ReviewPublishedPage = ({ datasetReviewId, doi }: { datasetReviewId:
         </div>
       </div>
 
+      <h2>What happens next</h2>
+
+      <div class="inset">
+        <p>We’re working on showing your dataset review on PREreview. It’ll appear around the site soon.</p>
+      </div>
+
+      <p>
+        If you reviewed the dataset with others, please get in touch so we can invite them to appear on the review. Our
+        email address is
+        <a href="mailto:help@prereview.org" target="_blank" rel="noopener noreferrer"
+          >help@prereview.org<span class="visually-hidden"> (opens in a new tab)</span></a
+        >.
+      </p>
+
+      <h2>Let us know how it went</h2>
+
+      <p>
+        <a
+          href="https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUw4R0F6MVo1ZWhyfGRlZmF1bHR8ZGM2YTU1OTNhYzNhY2RiN2YzNTBlYTdmZTBmMzNmNDA"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Schedule an interview<span class="visually-hidden"> (opens in a new tab)</span></a
+        >
+        with our product team to discuss your experience on PREreview. We gladly compensate interviewees in appreciation
+        for their help!
+      </p>
+
+      <p>
+        You can also share your feedback about PREreview.org with us by completing a
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSfynZ25_toGP6pnTrEyKE-Fv-7z7pK2h9AlNksKI9_DVJMnng/viewform"
+          target="_blank"
+          rel="noopener noreferrer"
+          >brief anonymous survey<span class="visually-hidden"> (opens in a new tab)</span></a
+        >.
+      </p>
+
       <a href="${Routes.DatasetReview.href({ datasetReviewId })}" class="button">See your review</a>
     `,
     canonical: Routes.ReviewADatasetReviewPublished.href({ datasetReviewId }),
