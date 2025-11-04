f33a2cf14a8f77080dd7a5ec4be3a704613e3b82
Chris Wilkinson <chris@prereview.org>
2025-11-04 11:01:05 +0000

    Make it easier to test by push up knowledge of the channel
    Refs #2837, #2017

--- src/ExternalInteractions/CommunitySlack/ShareDatasetReview/index.ts
+++ src/ExternalInteractions/CommunitySlack/ShareDatasetReview/index.ts
@@ -1,5 +1,6 @@
 import { Data, Effect, flow } from 'effect'
 import { Slack } from '../../../ExternalApis/index.ts'
+import { shareAReviewChannelId } from '../ChannelIds.ts'
 import { DatasetReviewToChatPostMessageInput } from './DatasetReviewToChatPostMessageInput.ts'
 
 export type { DatasetReview } from './DatasetReviewToChatPostMessageInput.ts'
@@ -10,6 +11,8 @@ export class FailedToShareDatasetReview extends Data.TaggedError('FailedToShareD
 
 export const ShareDatasetReview = flow(
   DatasetReviewToChatPostMessageInput,
+  Effect.succeed,
+  Effect.bind('channel', () => shareAReviewChannelId),
   Effect.andThen(Slack.chatPostMessage),
   Effect.catchAll(error => new FailedToShareDatasetReview({ cause: error })),
 )
