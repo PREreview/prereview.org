44da148ca9fc6e8a9f56ac44e0626e8208a137bd
Chris Wilkinson <chris@prereview.org>
2025-12-01 11:55:54 +0000

    Implement the command
    Ref #2891

--- src/ReviewRequests/Commands/index.ts
+++ src/ReviewRequests/Commands/index.ts
@@ -1,7 +1,8 @@
-import { Context, Data, Effect, Layer } from 'effect'
-import type * as EventStore from '../../EventStore.ts'
+import { Context, Data, Effect, Layer, Option, pipe } from 'effect'
+import type * as Events from '../../Events.ts'
+import * as EventStore from '../../EventStore.js'
 import type { Uuid } from '../../types/index.ts'
-import type * as AcceptReviewRequest from './AcceptReviewRequest.ts'
+import * as AcceptReviewRequest from './AcceptReviewRequest.js'
 
 export class ReviewRequestCommands extends Context.Tag('ReviewRequestCommands')<
   ReviewRequestCommands,
@@ -19,9 +20,54 @@ export class UnableToHandleCommand extends Data.TaggedError('UnableToHandleComma
 export const { acceptReviewRequest } = Effect.serviceFunctions(ReviewRequestCommands)
 
 const makeReviewRequestCommands: Effect.Effect<typeof ReviewRequestCommands.Service, never, EventStore.EventStore> =
-  Effect.sync(() => {
+  Effect.gen(function* () {
+    const context = yield* Effect.context<EventStore.EventStore>()
+
+    const handleCommand = <
+      Event extends Events.ReviewRequestEvent['_tag'],
+      State,
+      Command extends { reviewRequestId: Uuid.Uuid },
+    >(
+      createFilter: (reviewRequestId: Uuid.Uuid) => Events.EventFilter<Event>,
+      foldState: (events: ReadonlyArray<Extract<Events.Event, { _tag: Event }>>, reviewRequestId: Uuid.Uuid) => State,
+      decide: (command: Command) => (state: State) => Option.Option<Events.ReviewRequestEvent>,
+    ): CommandHandler<Command> =>
+      Effect.fn(
+        function* (command) {
+          const filter = createFilter(command.reviewRequestId)
+
+          const { events, lastKnownEvent } = yield* pipe(
+            EventStore.query(filter),
+            Effect.catchTag('NoEventsFound', () => Effect.succeed({ events: [], lastKnownEvent: undefined })),
+          )
+
+          yield* pipe(
+            Effect.succeed(foldState(events, command.reviewRequestId)),
+            Effect.map(decide(command)),
+            Effect.tap(
+              Option.match({
+                onNone: () => Effect.void,
+                onSome: event =>
+                  EventStore.append(event, { filter, lastKnownEvent: Option.fromNullable(lastKnownEvent) }),
+              }),
+            ),
+          )
+        },
+        Effect.catchTag(
+          'FailedToCommitEvent',
+          'FailedToGetEvents',
+          'NewEventsFound',
+          cause => new UnableToHandleCommand({ cause }),
+        ),
+        Effect.provide(context),
+      )
+
     return {
-      acceptReviewRequest: () => new UnableToHandleCommand({ cause: 'not implemented' }),
+      acceptReviewRequest: handleCommand(
+        AcceptReviewRequest.createFilter,
+        AcceptReviewRequest.foldState,
+        AcceptReviewRequest.decide,
+      ),
     }
   })
 
---
+++ test/ReviewRequests/Commands/AcceptReviewRequest.test.ts
@@ -0,0 +1,101 @@
+import { test } from '@fast-check/jest'
+import { describe, expect } from '@jest/globals'
+import { Temporal } from '@js-temporal/polyfill'
+import { Array, Equal, Option, Tuple } from 'effect'
+import * as Preprints from '../../../src/Preprints/index.ts'
+import * as _ from '../../../src/ReviewRequests/Commands/AcceptReviewRequest.ts'
+import * as ReviewRequests from '../../../src/ReviewRequests/index.ts'
+import { Doi, NonEmptyString, Uuid } from '../../../src/types/index.ts'
+import * as fc from '../../fc.ts'
+
+const reviewRequestId = Uuid.Uuid('475434b4-3c0d-4b70-a5f4-8af7baf55753')
+const otherReviewRequestId = Uuid.Uuid('7bb629bd-9616-4e0f-bab7-f2ab07b95340')
+const preprintId = new Preprints.BiorxivPreprintId({ value: Doi.Doi('10.1101/12345') })
+const reviewRequestForAPreprintWasAccepted = new ReviewRequests.ReviewRequestForAPreprintWasAccepted({
+  acceptedAt: Temporal.Now.instant().subtract({ hours: 1 }),
+  receivedAt: Temporal.Now.instant().subtract({ hours: 2 }),
+  preprintId,
+  requester: { name: NonEmptyString.NonEmptyString('Josiah Carberry') },
+  reviewRequestId,
+})
+const otherReviewRequestForAPreprintWasAccepted = new ReviewRequests.ReviewRequestForAPreprintWasAccepted({
+  acceptedAt: Temporal.Now.instant().subtract({ hours: 1 }),
+  receivedAt: Temporal.Now.instant().subtract({ hours: 2 }),
+  preprintId,
+  requester: { name: NonEmptyString.NonEmptyString('Josiah Carberry') },
+  reviewRequestId: otherReviewRequestId,
+})
+
+const command = (): fc.Arbitrary<_.Command> =>
+  fc.record({
+    receivedAt: fc.instant(),
+    acceptedAt: fc.instant(),
+    preprintId: fc.preprintId(),
+    reviewRequestId: fc.uuid(),
+    requester: fc.record({
+      name: fc.nonEmptyString(),
+    }),
+  })
+
+describe('foldState', () => {
+  test.prop(
+    [
+      fc
+        .uuid()
+        .chain(reviewRequestId =>
+          fc.tuple(
+            fc.array(fc.reviewRequestEvent().filter(event => !Equal.equals(event.reviewRequestId, reviewRequestId))),
+            fc.constant(reviewRequestId),
+          ),
+        ),
+    ],
+    {
+      examples: [
+        [[[], reviewRequestId]], // no events
+        [[[otherReviewRequestForAPreprintWasAccepted], reviewRequestId]], // for other review request
+      ],
+    },
+  )('not yet accepted', ([events, reviewRequestId]) => {
+    const state = _.foldState(events, reviewRequestId)
+
+    expect(state).toStrictEqual(new _.NotAccepted())
+  })
+
+  test.prop(
+    [fc.reviewRequestForAPreprintWasAccepted().map(event => Tuple.make(Array.make(event), event.reviewRequestId))],
+    {
+      examples: [
+        [[[reviewRequestForAPreprintWasAccepted], reviewRequestId]], // was accepted
+        [[[reviewRequestForAPreprintWasAccepted, otherReviewRequestForAPreprintWasAccepted], reviewRequestId]], // other review request too
+      ],
+    },
+  )('already accepted', ([events, reviewRequestId]) => {
+    const state = _.foldState(events, reviewRequestId)
+
+    expect(state).toStrictEqual(new _.HasBeenAccepted())
+  })
+})
+
+describe('decide', () => {
+  test.prop([command()])('has not been accepted', command => {
+    const result = _.decide(new _.NotAccepted(), command)
+
+    expect(result).toStrictEqual(
+      Option.some(
+        new ReviewRequests.ReviewRequestForAPreprintWasAccepted({
+          receivedAt: command.receivedAt,
+          acceptedAt: command.acceptedAt,
+          preprintId: command.preprintId,
+          reviewRequestId: command.reviewRequestId,
+          requester: command.requester,
+        }),
+      ),
+    )
+  })
+
+  test.prop([command()])('has already been accepted', command => {
+    const result = _.decide(new _.HasBeenAccepted(), command)
+
+    expect(result).toStrictEqual(Option.none())
+  })
+})
