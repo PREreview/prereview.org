3ec252e867a1eaf6608aa6cfd8845c9fbffcb238
Chris Wilkinson <chris@prereview.org>
2025-12-01 09:48:16 +0000

    Fix an import
    Ref #2891, 4844aa7911ef2c620d233636cf719cb3906fa2ab

--- src/Inbox/ProcessCoarNotifyMessage.ts
+++ src/Inbox/ProcessCoarNotifyMessage.ts
@@ -2,7 +2,7 @@ import { Effect, Either, pipe } from 'effect'
 import type { CoarNotify } from '../ExternalApis/index.ts'
 import * as Preprints from '../Preprints/index.ts'
 import * as ReviewRequests from '../ReviewRequests/index.ts'
-import { Temporal, type Uuid } from '../types/index.js'
+import { Temporal, type Uuid } from '../types/index.ts'
 import * as Errors from './Errors.ts'
 
 export const ProcessCoarNotifyMessage = Effect.fn(
