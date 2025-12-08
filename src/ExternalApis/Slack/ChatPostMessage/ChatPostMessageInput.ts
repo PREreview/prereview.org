85185dd826b96acba2f669ef4c3330e40d87b824
Chris Wilkinson <chris@prereview.org>
2025-12-08 15:25:50 +0000

    Prepare to include more content in messages
    Refs #2891

--- src/ExternalApis/Slack/ChatPostMessage/ChatPostMessageInput.ts
+++ src/ExternalApis/Slack/ChatPostMessage/ChatPostMessageInput.ts
@@ -3,6 +3,11 @@ import { ChannelId, Timestamp, UserId } from '../Types.ts'
 
 export type ChatPostMessageInput = Schema.Schema.Type<typeof ChatPostMessageInput>
 
+const PlainText = Schema.Struct({
+  type: Schema.tag('plain_text'),
+  text: Schema.NonEmptyString,
+})
+
 const Text = Schema.Struct({
   type: Schema.tag('text'),
   text: Schema.NonEmptyString,
@@ -18,6 +23,11 @@ const Link = Schema.Struct({
   url: Schema.URL,
 })
 
+const RichTextQuote = Schema.Struct({
+  type: Schema.tag('rich_text_quote'),
+  elements: Schema.NonEmptyArray(Schema.Union(Text, Link)),
+})
+
 const RichTextSection = Schema.Struct({
   type: Schema.tag('rich_text_section'),
   elements: Schema.NonEmptyArray(Schema.Union(Text, Link)),
@@ -25,13 +35,30 @@ const RichTextSection = Schema.Struct({
 
 const RichTextBlock = Schema.Struct({
   type: Schema.tag('rich_text'),
-  elements: Schema.Tuple(RichTextSection),
+  elements: Schema.NonEmptyArray(Schema.Union(RichTextQuote, RichTextSection)),
+})
+
+const ButtonElement = Schema.Struct({
+  type: Schema.tag('button'),
+  text: PlainText,
+  style: Schema.optional(Schema.Literal('primary', 'danger')),
+  url: Schema.URL,
+})
+
+const ActionsBlock = Schema.Struct({
+  type: Schema.tag('actions'),
+  elements: Schema.NonEmptyArray(ButtonElement),
+})
+
+const MarkdownBlock = Schema.Struct({
+  type: Schema.tag('markdown'),
+  text: Schema.NonEmptyString,
 })
 
 export const ChatPostMessageInput = Schema.Struct({
   channel: Schema.Union(ChannelId, UserId),
   threadTs: Schema.optional(Timestamp).pipe(Schema.fromKey('thread_ts')),
-  blocks: Schema.Tuple(RichTextBlock),
+  blocks: Schema.NonEmptyArray(Schema.Union(ActionsBlock, MarkdownBlock, RichTextBlock)),
   unfurlLinks: Schema.optional(Schema.Boolean).pipe(Schema.fromKey('unfurl_links')),
   unfurlMedia: Schema.optional(Schema.Boolean).pipe(Schema.fromKey('unfurl_media')),
 })
