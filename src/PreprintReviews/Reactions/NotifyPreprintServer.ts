98fd6d4ece5878bca8ca86a8c2289c302f9f749c
Chris Wilkinson <chris@prereview.org>
2025-12-09 11:39:39 +0000

    Load review data
    Refs #2891

--- src/PreprintReviews/Reactions/NotifyPreprintServer.ts
+++ src/PreprintReviews/Reactions/NotifyPreprintServer.ts
@@ -1,15 +1,18 @@
 import { Effect } from 'effect'
 import * as FeatureFlags from '../../FeatureFlags.ts'
+import * as Prereviews from '../../Prereviews/index.ts'
 import * as Errors from '../Errors.ts'
 
 export const NotifyPreprintServer = Effect.fn(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   function* (reviewId: number) {
     const sendCoarNotifyMessages = yield* FeatureFlags.sendCoarNotifyMessages
 
     if (sendCoarNotifyMessages === false) {
       return
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const prereview = yield* Prereviews.getPrereview(reviewId)
 
     return yield* Effect.fail('not implemented')
   },
--- test/PreprintReviews/Reactions/NotifyPreprintServer.test.ts
+++ test/PreprintReviews/Reactions/NotifyPreprintServer.test.ts
@@ -1,32 +1,71 @@
 import { test } from '@fast-check/jest'
 import { describe, expect } from '@jest/globals'
-import { Effect, Either, pipe } from 'effect'
+import { Effect, Either, Layer, pipe } from 'effect'
 import * as FeatureFlags from '../../../src/FeatureFlags.ts'
 import * as PreprintReviews from '../../../src/PreprintReviews/index.ts'
 import * as _ from '../../../src/PreprintReviews/Reactions/NotifyPreprintServer.ts'
+import * as Prereviews from '../../../src/Prereviews/index.ts'
 import * as EffectTest from '../../EffectTest.ts'
 import * as fc from '../../fc.ts'
 import { shouldNotBeCalled } from '../../should-not-be-called.ts'
 
 describe('NotifyPreprintServer', () => {
-  test.prop([fc.integer()])('when the feature is enabled', reviewId =>
+  describe('when the review can be loaded', () => {
+    test.prop([fc.integer(), fc.prereview()])('when the feature is enabled', (reviewId, review) =>
+      Effect.gen(function* () {
+        const actual = yield* pipe(_.NotifyPreprintServer(reviewId), Effect.either)
+
+        expect(actual).toStrictEqual(
+          Either.left(new PreprintReviews.FailedToNotifyPreprintServer({ cause: 'not implemented' })),
+        )
+      }).pipe(
+        Effect.provide(
+          Layer.mergeAll(
+            featureFlagsLayer(true),
+            Layer.mock(Prereviews.Prereviews, { getPrereview: () => Effect.succeed(review) }),
+          ),
+        ),
+        EffectTest.run,
+      ),
+    )
+
+    test.prop([fc.integer(), fc.prereview()])('when the feature is sandboxed', (reviewId, review) =>
+      Effect.gen(function* () {
+        const actual = yield* pipe(_.NotifyPreprintServer(reviewId), Effect.either)
+
+        expect(actual).toStrictEqual(
+          Either.left(new PreprintReviews.FailedToNotifyPreprintServer({ cause: 'not implemented' })),
+        )
+      }).pipe(
+        Effect.provide(
+          Layer.mergeAll(
+            featureFlagsLayer(true),
+            Layer.mock(Prereviews.Prereviews, { getPrereview: () => Effect.succeed(review) }),
+          ),
+        ),
+        EffectTest.run,
+      ),
+    )
+  })
+
+  test.prop([
+    fc.integer(),
+    fc.constantFrom(
+      new Prereviews.PrereviewIsNotFound(),
+      new Prereviews.PrereviewIsUnavailable(),
+      new Prereviews.PrereviewWasRemoved(),
+    ),
+  ])("when the review can't be loaded", (reviewId, error) =>
     Effect.gen(function* () {
       const actual = yield* pipe(_.NotifyPreprintServer(reviewId), Effect.either)
 
-      expect(actual).toStrictEqual(
-        Either.left(new PreprintReviews.FailedToNotifyPreprintServer({ cause: 'not implemented' })),
-      )
-    }).pipe(Effect.provide(featureFlagsLayer(true)), EffectTest.run),
-  )
-
-  test.prop([fc.integer()])('when the feature is sandboxed', reviewId =>
-    Effect.gen(function* () {
-      const actual = yield* pipe(_.NotifyPreprintServer(reviewId), Effect.either)
-
-      expect(actual).toStrictEqual(
-        Either.left(new PreprintReviews.FailedToNotifyPreprintServer({ cause: 'not implemented' })),
-      )
-    }).pipe(Effect.provide(featureFlagsLayer('sandbox')), EffectTest.run),
+      expect(actual).toStrictEqual(Either.left(new PreprintReviews.FailedToNotifyPreprintServer({ cause: error })))
+    }).pipe(
+      Effect.provide(
+        Layer.mergeAll(featureFlagsLayer('sandbox'), Layer.mock(Prereviews.Prereviews, { getPrereview: () => error })),
+      ),
+      EffectTest.run,
+    ),
   )
 
   test.prop([fc.integer()])('when the feature is not enabled', reviewId =>
@@ -34,7 +73,10 @@ describe('NotifyPreprintServer', () => {
       const actual = yield* pipe(_.NotifyPreprintServer(reviewId), Effect.either)
 
       expect(actual).toStrictEqual(Either.void)
-    }).pipe(Effect.provide(featureFlagsLayer(false)), EffectTest.run),
+    }).pipe(
+      Effect.provide(Layer.mergeAll(featureFlagsLayer(false), Layer.mock(Prereviews.Prereviews, {}))),
+      EffectTest.run,
+    ),
   )
 })
 
