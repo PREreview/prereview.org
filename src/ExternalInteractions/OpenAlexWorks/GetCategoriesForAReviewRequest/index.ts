feb80d3d0b8d0ae66e27100743f0a8a0480a78df
Chris Wilkinson <chris@prereview.org>
2025-12-11 13:27:06 +0000

    Create a way to read OpenAlex categories for a review request
    Refs #2891

---
+++ src/ExternalInteractions/OpenAlexWorks/GetCategoriesForAReviewRequest/index.ts
@@ -0,0 +1,30 @@
+import { Array, Data, Effect, Option, pipe, Schema } from 'effect'
+import type { LanguageCode } from 'iso-639-1'
+import { OpenAlex } from '../../../ExternalApis/index.ts'
+import type * as Preprints from '../../../Preprints/index.ts'
+import { KeywordIdFromOpenAlexUrlSchema, type KeywordId } from '../../../types/Keyword.ts'
+import { TopicIdFromOpenAlexUrlSchema, type TopicId } from '../../../types/Topic.ts'
+
+export interface CategoriesForAReviewRequest {
+  readonly language: Option.Option<LanguageCode>
+  readonly topics: ReadonlyArray<TopicId>
+  readonly keywords: ReadonlyArray<KeywordId>
+}
+
+export class CategoriesAreNotAvailable extends Data.TaggedError('CategoriesAreNotAvailable')<{
+  cause?: unknown
+}> {}
+
+export const GetCategoriesForAReviewRequest = (preprintId: Preprints.IndeterminatePreprintIdWithDoi) =>
+  pipe(
+    OpenAlex.getWork(preprintId.value),
+    Effect.andThen(
+      work =>
+        ({
+          language: Option.fromNullable(work.language),
+          topics: Array.filterMap(work.topics, ({ id }) => Schema.decodeOption(TopicIdFromOpenAlexUrlSchema)(id)),
+          keywords: Array.filterMap(work.keywords, ({ id }) => Schema.decodeOption(KeywordIdFromOpenAlexUrlSchema)(id)),
+        }) satisfies CategoriesForAReviewRequest,
+    ),
+    Effect.catchAll(error => new CategoriesAreNotAvailable({ cause: error })),
+  )
--- src/ExternalInteractions/OpenAlexWorks/index.ts
+++ src/ExternalInteractions/OpenAlexWorks/index.ts
@@ -1,6 +1,9 @@
 import { Context, Effect, flow, Layer, Scope } from 'effect'
 import type { OpenAlex } from '../../ExternalApis/index.ts'
 import { GetCategories } from './GetCategories/index.ts'
+import { GetCategoriesForAReviewRequest } from './GetCategoriesForAReviewRequest/index.ts'
+
+export { CategoriesAreNotAvailable, type CategoriesForAReviewRequest } from './GetCategoriesForAReviewRequest/index.ts'
 
 export class OpenAlexWorks extends Context.Tag('OpenAlexWorks')<
   OpenAlexWorks,
@@ -11,16 +14,23 @@ export class OpenAlexWorks extends Context.Tag('OpenAlexWorks')<
       Effect.Effect.Success<ReturnType<typeof GetCategories>>,
       Effect.Effect.Error<ReturnType<typeof GetCategories>>
     >
+    getCategoriesForAReviewRequest: (
+      ...args: Parameters<typeof GetCategoriesForAReviewRequest>
+    ) => Effect.Effect<
+      Effect.Effect.Success<ReturnType<typeof GetCategoriesForAReviewRequest>>,
+      Effect.Effect.Error<ReturnType<typeof GetCategoriesForAReviewRequest>>
+    >
   }
 >() {}
 
-export const { getCategories } = Effect.serviceFunctions(OpenAlexWorks)
+export const { getCategories, getCategoriesForAReviewRequest } = Effect.serviceFunctions(OpenAlexWorks)
 
 const make: Effect.Effect<typeof OpenAlexWorks.Service, never, OpenAlex.OpenAlex> = Effect.gen(function* () {
   const context = yield* Effect.andThen(Effect.context<OpenAlex.OpenAlex>(), Context.omit(Scope.Scope))
 
   return {
     getCategories: flow(GetCategories, Effect.provide(context)),
+    getCategoriesForAReviewRequest: flow(GetCategoriesForAReviewRequest, Effect.provide(context)),
   }
 })
 
