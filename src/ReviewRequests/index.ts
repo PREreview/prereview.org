7d2261f3e9e4f84267c9fb61f69b84c08245f76e
Chris Wilkinson <chris@prereview.org>
2025-12-12 11:26:44 +0000

    Use the queries when the feature is enabled
    Refs #2891

--- src/ReviewRequests/index.ts
+++ src/ReviewRequests/index.ts
@@ -2,21 +2,23 @@ import type { HttpClient } from '@effect/platform'
 import type { Temporal } from '@js-temporal/polyfill'
 import { Array, Context, Effect, flow, Layer, pipe, Scope } from 'effect'
 import type { LanguageCode } from 'iso-639-1'
+import * as FeatureFlags from '../FeatureFlags.ts'
 import type { Html } from '../html.ts'
-import type * as Preprints from '../Preprints/index.ts'
 import type { PreprintId } from '../Preprints/index.ts'
+import * as Preprints from '../Preprints/index.ts'
 import * as PrereviewCoarNotify from '../prereview-coar-notify/index.ts'
 import {
   getReviewRequestsFromPrereviewCoarNotify,
   type PrereviewCoarNotifyConfig,
 } from '../prereview-coar-notify/index.ts'
-import type {
-  ReviewRequests as PageOfReviewRequests,
+import {
   ReviewRequestsAreUnavailable,
   ReviewRequestsNotFound,
+  type ReviewRequests as PageOfReviewRequests,
 } from '../review-requests-page/index.ts'
 import type { FieldId } from '../types/field.ts'
 import type { SubfieldId } from '../types/subfield.ts'
+import { getTopicField, getTopicSubfield } from '../types/Topic.ts'
 import * as Commands from './Commands/index.ts'
 import * as Queries from './Queries/index.ts'
 
@@ -54,30 +56,92 @@ export const { getFiveMostRecent } = Effect.serviceConstants(ReviewRequests)
 
 export const { isReviewRequested, search } = Effect.serviceFunctions(ReviewRequests)
 
-export const layer = Layer.mergeAll(
+export const layer = Layer.provideMerge(
   Layer.effect(
     ReviewRequests,
     Effect.gen(function* () {
       const context = yield* Effect.andThen(
-        Effect.context<HttpClient.HttpClient | Preprints.Preprints | PrereviewCoarNotifyConfig>(),
+        Effect.context<
+          HttpClient.HttpClient | Preprints.Preprints | PrereviewCoarNotifyConfig | Queries.ReviewRequestQueries
+        >(),
         Context.omit(Scope.Scope),
       )
 
-      return {
-        getFiveMostRecent: pipe(
-          PrereviewCoarNotify.getFirstPageOfReviewRequestsFromPrereviewCoarNotify,
-          Effect.orElseSucceed(Array.empty),
-          Effect.provide(context),
-        ),
-        isReviewRequested: flow(
-          PrereviewCoarNotify.isReviewRequested,
-          Effect.orElseSucceed(() => false),
-          Effect.provide(context),
-        ),
-        search: flow(getReviewRequestsFromPrereviewCoarNotify, Effect.provide(context)),
-      }
+      return yield* Effect.if(FeatureFlags.enableCoarNotifyInbox, {
+        onTrue: () =>
+          Effect.succeed({
+            getFiveMostRecent: pipe(
+              Queries.getFiveMostRecentReviewRequests(),
+              Effect.andThen(
+                Effect.forEach(
+                  reviewRequest =>
+                    Effect.gen(function* () {
+                      return {
+                        ...reviewRequest,
+                        published: reviewRequest.published.toZonedDateTimeISO('UTC').toPlainDate(),
+                        preprint: yield* Preprints.getPreprintTitle(reviewRequest.preprintId),
+                        fields: Array.dedupe(Array.map(reviewRequest.topics, getTopicField)),
+                        subfields: Array.dedupe(Array.map(reviewRequest.topics, getTopicSubfield)),
+                      }
+                    }),
+                  { concurrency: 'inherit' },
+                ),
+              ),
+              Effect.orElseSucceed(Array.empty),
+              Effect.provide(context),
+            ),
+            isReviewRequested: flow(
+              (preprintId: Preprints.IndeterminatePreprintId) =>
+                Queries.doesAPreprintHaveAReviewRequest({ preprintId }),
+              Effect.orElseSucceed(() => false),
+              Effect.provide(context),
+            ),
+            search: flow(
+              Queries.searchForPublishedReviewRequests,
+              Effect.andThen(pageOfReviewRequests =>
+                Effect.andThen(
+                  Effect.forEach(
+                    pageOfReviewRequests.reviewRequests,
+                    reviewRequest =>
+                      Effect.gen(function* () {
+                        return {
+                          ...reviewRequest,
+                          published: reviewRequest.published.toZonedDateTimeISO('UTC').toPlainDate(),
+                          preprint: yield* Preprints.getPreprintTitle(reviewRequest.preprintId),
+                          fields: Array.dedupe(Array.map(reviewRequest.topics, getTopicField)),
+                          subfields: Array.dedupe(Array.map(reviewRequest.topics, getTopicSubfield)),
+                        }
+                      }),
+                    { concurrency: 'inherit' },
+                  ),
+                  reviewRequests => ({ ...pageOfReviewRequests, reviewRequests }),
+                ),
+              ),
+              Effect.catchTags({
+                NoReviewRequestsFound: error => new ReviewRequestsNotFound({ cause: error }),
+                PreprintIsNotFound: error => new ReviewRequestsAreUnavailable({ cause: error }),
+                PreprintIsUnavailable: error => new ReviewRequestsAreUnavailable({ cause: error }),
+                UnableToQuery: error => new ReviewRequestsAreUnavailable({ cause: error }),
+              }),
+              Effect.provide(context),
+            ),
+          }),
+        onFalse: () =>
+          Effect.succeed({
+            getFiveMostRecent: pipe(
+              PrereviewCoarNotify.getFirstPageOfReviewRequestsFromPrereviewCoarNotify,
+              Effect.orElseSucceed(Array.empty),
+              Effect.provide(context),
+            ),
+            isReviewRequested: flow(
+              PrereviewCoarNotify.isReviewRequested,
+              Effect.orElseSucceed(() => false),
+              Effect.provide(context),
+            ),
+            search: flow(getReviewRequestsFromPrereviewCoarNotify, Effect.provide(context)),
+          }),
+      })
     }),
   ),
-  Commands.commandsLayer,
-  Queries.queriesLayer,
+  Layer.mergeAll(Commands.commandsLayer, Queries.queriesLayer),
 )
