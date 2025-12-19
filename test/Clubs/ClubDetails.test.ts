31b33c040f59faaa640f9f469672bb12005b232b
Chad Sansing <csansing@gmail.com>
2025-12-19 05:15:17 -0500

    Updated HHMI club name
    Co-authored-by: Chris Wilkinson <chris@prereview.org>

--- test/Clubs/ClubDetails.test.ts
+++ test/Clubs/ClubDetails.test.ts
@@ -9,6 +9,8 @@ describe('getClubByName', () => {
   test.each([
     ['ASAPbio Cancer Biology Crowd', 'asapbio-cancer-biology'],
     ['ASAPbio Neurobiology Crowd', 'asapbio-neurobiology'],
+    ['HHMI Transparent and Accountable Peer Review Training Pilot', 'hhmi-training-pilot'],
+    ['HHMI Transparent and Accountable Peer Review Training Program', 'hhmi-training-pilot'],
   ])('with a club name (%s)', (name, expected) => {
     const actual = _.getClubByName(name)
 
