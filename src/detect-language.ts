d91398087a4bb4e43120b1b7b3e8aefa86e61141
Chris Wilkinson <chris@prereview.org>
2025-12-19 13:51:21 +0000

    Support detecting languages from other types
    Refs #2919

--- src/detect-language.ts
+++ src/detect-language.ts
@@ -1,20 +1,21 @@
 import { Array, flow, Option, pipe } from 'effect'
 import iso6391, { type LanguageCode } from 'iso-639-1'
 import { detect, detectAll } from 'tinyld/heavy'
-import { type Html, plainText } from './html.ts'
+import { type Html, PlainText, plainText } from './html.ts'
 
-export function detectLanguage(html: Html): Option.Option<LanguageCode> {
+export function detectLanguage(text: Html | PlainText | string): Option.Option<LanguageCode> {
   return pipe(
-    detectAll(plainText(html).toString()),
+    detectAll(text instanceof PlainText ? text.toString() : plainText(text).toString()),
     Array.findFirst(({ lang }) => Option.liftPredicate(lang, iso6391.validate)),
   )
 }
 
 export function detectLanguageFrom<L extends LanguageCode>(
   ...languages: ReadonlyArray<L>
-): (html: Html) => Option.Option<L> {
+): (text: Html | PlainText | string) => Option.Option<L> {
   return flow(
-    html => detect(plainText(html).toString(), { only: [...languages] }) as L,
+    text =>
+      detect(text instanceof PlainText ? text.toString() : plainText(text).toString(), { only: [...languages] }) as L,
     Option.liftPredicate(detected => Array.contains(languages, detected)),
   )
 }
