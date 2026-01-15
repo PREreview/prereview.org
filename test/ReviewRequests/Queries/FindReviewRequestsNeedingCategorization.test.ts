c19f1a24509bde9d01dfce1c25c09ece27f12b3e
Chris Wilkinson <chris@prereview.org>
2026-01-15 14:56:13 +0000

    Write a more exhaustive test list
    Refs #2891

--- test/ReviewRequests/Queries/FindReviewRequestsNeedingCategorization.test.ts
+++ test/ReviewRequests/Queries/FindReviewRequestsNeedingCategorization.test.ts
@@ -8,10 +8,13 @@ import * as _ from '../../../src/ReviewRequests/Queries/FindReviewRequestsNeedin
 import { Doi, NonEmptyString, Uuid } from '../../../src/types/index.ts'
 
 const requester1 = { name: NonEmptyString.NonEmptyString('Josiah Carberry') }
+const requester2 = { name: NonEmptyString.NonEmptyString('Jean-Baptiste Botul') }
 
 const request1Id = Uuid.Uuid('475434b4-3c0d-4b70-a5f4-8af7baf55753')
+const request2Id = Uuid.Uuid('7bb629bd-9616-4e0f-bab7-f2ab07b95340')
 
 const preprintId1 = new Preprints.BiorxivPreprintId({ value: Doi.Doi('10.1101/12345') })
+const preprintId2 = new Preprints.MedrxivPreprintId({ value: Doi.Doi('10.1101/67890') })
 
 const now = Temporal.Now.instant()
 
@@ -27,6 +30,12 @@ const request1Categorized = new ReviewRequests.ReviewRequestForAPreprintWasCateg
   topics: ['13741', '12422'],
   reviewRequestId: request1Id,
 })
+const request2Imported = new ReviewRequests.ReviewRequestForAPreprintWasImported({
+  publishedAt: now.subtract({ hours: 1 }),
+  preprintId: preprintId2,
+  requester: Option.some(requester2),
+  reviewRequestId: request2Id,
+})
 
 test('query', () => {
   const actual = _.query([request1Imported, request1Categorized])
@@ -34,8 +43,12 @@ test('query', () => {
   expect(actual).toStrictEqual([])
 })
 
-test.failing('query', () => {
-  const actual = _.query([request1Imported])
+test.failing.each<[string, ReadonlyArray<ReviewRequests.ReviewRequestEvent>, ReadonlyArray<Uuid.Uuid>]>([
+  ['single not categorized', [request1Imported], [request1Id]],
+  ['two not categorized', [request1Imported, request2Imported], [request1Id, request2Id]],
+  ['partially categorized', [request1Imported, request2Imported, request1Categorized], [request2Id]],
+])('query (%s)', (_name, events, expected) => {
+  const actual = _.query(events)
 
-  expect(actual).toStrictEqual([request1Id])
+  expect(actual).toStrictEqual(expected)
 })
