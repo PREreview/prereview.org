85d7ecaa14165073a35ff9fdcd639aaeab9039be
Chris Wilkinson <chris@prereview.org>
2026-01-15 15:51:01 +0000

    Make a logged error easier to understand by expanding the context to include the input
    Refs #2891

--- src/ExternalApis/OpenAlex/GetWork/index.ts
+++ src/ExternalApis/OpenAlex/GetWork/index.ts
@@ -1,17 +1,17 @@
 import { HttpClient } from '@effect/platform'
-import { Effect, flow } from 'effect'
+import { Effect, pipe } from 'effect'
 import type { Doi } from '../../../types/index.ts'
 import { type Work, type WorkIsNotFound, WorkIsUnavailable } from '../Work.ts'
 import { CreateRequest } from './CreateRequest.ts'
 import { HandleResponse } from './HandleResponse.ts'
 
-export const GetWork: (doi: Doi.Doi) => Effect.Effect<Work, WorkIsNotFound | WorkIsUnavailable, HttpClient.HttpClient> =
-  flow(
-    CreateRequest,
+export const GetWork = (doi: Doi.Doi): Effect.Effect<Work, WorkIsNotFound | WorkIsUnavailable, HttpClient.HttpClient> =>
+  pipe(
+    CreateRequest(doi),
     HttpClient.execute,
     Effect.andThen(HandleResponse),
     Effect.catchTag('RequestError', 'ResponseError', error => new WorkIsUnavailable({ cause: error })),
     Effect.tapErrorTag('WorkIsUnavailable', error =>
-      Effect.logError('Failed to get work from OpenAlex').pipe(Effect.annotateLogs({ error })),
+      Effect.logError('Failed to get work from OpenAlex').pipe(Effect.annotateLogs({ doi, error })),
     ),
   )
