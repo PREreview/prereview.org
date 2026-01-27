7afedb43994509c43c11141eb6cd3c0db826ba6b
Chris Wilkinson <chris@prereview.org>
2026-01-27 15:32:25 +0000

    Load sensitive data in batches to remove an n+1 problem
    Refs #2891, https://effect.website/docs/batching/, 3c77423792473a73582e364aeb11d66553477011

--- src/SensitiveDataStore.ts
+++ src/SensitiveDataStore.ts
@@ -1,4 +1,4 @@
-import { type Array, Context, Data, Effect, type Option, type Record } from 'effect'
+import { Array, Context, Data, Effect, type Option, pipe, Record, Request, RequestResolver, Struct } from 'effect'
 import type { Uuid } from './types/index.ts'
 
 export const SensitiveDataStore = Context.GenericTag<SensitiveDataStore>('SensitiveDataStore')
@@ -17,4 +17,28 @@ export interface SensitiveDataStore {
   readonly add: (value: string) => Effect.Effect<Uuid.Uuid, FailedToAddSensitiveData>
 }
 
-export const { add, get, getMany } = Effect.serviceFunctions(SensitiveDataStore)
+export const { add, getMany } = Effect.serviceFunctions(SensitiveDataStore)
+
+export const get = (id: Uuid.Uuid) => Effect.request(new GetSensitiveData({ id }), GetSensitiveDataResolver)
+
+class GetSensitiveData extends Request.TaggedClass('GetSensitiveData')<
+  Option.Option<string>,
+  FailedToGetSensitiveData,
+  { readonly id: Uuid.Uuid }
+> {}
+
+const GetSensitiveDataResolver = RequestResolver.makeBatched(
+  (requests: Array.NonEmptyReadonlyArray<GetSensitiveData>) =>
+    pipe(
+      Array.map(requests, Struct.get('id')),
+      getMany,
+      Effect.andThen(results =>
+        Effect.forEach(requests, request =>
+          Request.completeEffect(request, Effect.succeed(Record.get(results, request.id))),
+        ),
+      ),
+      Effect.catchAll(error =>
+        Effect.forEach(requests, request => Request.completeEffect(request, Effect.fail(error))),
+      ),
+    ),
+).pipe(RequestResolver.contextFromServices(SensitiveDataStore))
