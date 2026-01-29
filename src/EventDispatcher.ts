d2d7a1626e6401a008c53f7dac968dc5f602474b
Chris Wilkinson <chris@prereview.org>
2026-01-29 13:31:12 +0000

    Create a seam for dispatching new events
    Refs #2891

--- src/EventDispatcher.ts
+++ src/EventDispatcher.ts
@@ -1,9 +1,11 @@
 import { Console, Effect, Layer, Schedule } from 'effect'
 import * as FeatureFlags from './FeatureFlags.ts'
 
+const dispatchNewEvents = Console.log('hello')
+
 export const worker = Layer.effectDiscard(
   Effect.if(FeatureFlags.enableCoarNotifyInbox, {
-    onTrue: () => Effect.repeat(Console.log('hello'), Schedule.fixed('2 seconds')),
+    onTrue: () => Effect.repeat(dispatchNewEvents, Schedule.fixed('2 seconds')),
     onFalse: () => Effect.void,
   }),
 )
