bdfac02e8b456077b2d05662abe2fa6e7536a253
Chris Wilkinson <chris@prereview.org>
2026-01-29 13:49:33 +0000

    Start to recognise when there are new events
    Refs #2891

--- src/EventDispatcher.ts
+++ src/EventDispatcher.ts
@@ -1,10 +1,21 @@
-import { Console, Effect, Layer, Schedule } from 'effect'
+import { Array, Console, Effect, Layer, Schedule } from 'effect'
 import * as EventStore from './EventStore.ts'
 import * as FeatureFlags from './FeatureFlags.ts'
 
+let numberOfKnownEvents = 0
+
 const dispatchNewEvents = Effect.gen(function* () {
   const events = yield* EventStore.all
-  yield* Console.log(`Found ${events.length} events`)
+
+  if (events.length <= numberOfKnownEvents) {
+    return
+  }
+
+  const slice = Array.takeRight(events, events.length - numberOfKnownEvents)
+
+  numberOfKnownEvents = events.length
+
+  yield* Console.log(`Found ${slice.length} new event${slice.length > 1 ? 's' : ''}`)
 })
 
 export const worker = Layer.effectDiscard(
