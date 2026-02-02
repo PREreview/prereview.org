0ff6a14a7021856304c088ca3f303d372cf24cdc
Chris Wilkinson <chris@prereview.org>
2026-02-02 13:21:08 +0000

    Create a script for seeing review requests that haven’t been categorised yet
    Refs #2891

---
+++ src/print-status.ts
@@ -0,0 +1,43 @@
+import { NodeRuntime } from '@effect/platform-node'
+import { PgClient } from '@effect/sql-pg'
+import { Array, Config, Console, Effect, flow, Layer, pipe } from 'effect'
+import * as EventDispatcher from './EventDispatcher.ts'
+import * as Events from './Events.ts'
+import * as ReviewRequest from './ReviewRequests/index.ts'
+import * as SqlEventStore from './SqlEventStore.ts'
+import * as SqlSensitiveDataStore from './SqlSensitiveDataStore.ts'
+import { Uuid } from './types/index.ts'
+
+const program = pipe(
+  Console.log('Review requests needing categorization'),
+  Effect.andThen(ReviewRequest.findReviewRequestsNeedingCategorization),
+  Effect.tapBoth({
+    onSuccess: flow(
+      Array.map(result => ({
+        ID: result,
+      })),
+      Console.table,
+    ),
+    onFailure: Console.log,
+  }),
+)
+
+pipe(
+  program,
+  Effect.provide(
+    pipe(
+      ReviewRequest.queriesLayer,
+      Layer.provide(SqlEventStore.layer),
+      Layer.provide(Layer.mergeAll(Events.layer, SqlSensitiveDataStore.layer, EventDispatcher.EventDispatcherLayer)),
+      Layer.provide(
+        Layer.mergeAll(
+          Uuid.layer,
+          PgClient.layerConfig({
+            url: Config.redacted(Config.string('POSTGRES_URL')),
+          }),
+        ),
+      ),
+    ),
+  ),
+  NodeRuntime.runMain,
+)
