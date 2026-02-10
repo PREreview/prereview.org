4c350e7589efd6910dc1883cd0d080e30c61ecd7
Chris Wilkinson <chris@prereview.org>
2026-02-10 12:08:18 +0000

    Remove a redundant setting
    cld includes a ‘.node’ file, so rather than updating this list we can use the default which contains it.
    
    Refs #3025

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,7 +5,6 @@ export default {
   setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
   roots: ['./src/', './test/'],
   transformIgnorePatterns: ['<rootDir>/node_modules/(?!@arendjr/text-clipper/|case-anything|cdigit|normalize-url)'],
-  moduleFileExtensions: ['js', 'ts'],
   moduleNameMapper: { '^(\\.{1,2}/.*)\\.js$': '$1' },
   transform: {
     '^.+\\.[jt]s$': '@swc/jest',
