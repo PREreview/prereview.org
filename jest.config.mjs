e77b6b1ac27c387344efbc5dbdc0b3cd7ca0ad1f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-02-12 14:44:35 +0000

    Fix formatting
    Refs: #3031, 40ec4f87748521a39d8a5cad40e1988cdcd6c3ba

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,7 +4,9 @@ export default {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
   roots: ['./src/', './test/'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/(.pnpm/)?(?!.*text-clipper.*|case-anything|cdigit|normalize-url|uuid)[@/]'],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(.pnpm/)?(?!.*text-clipper.*|case-anything|cdigit|normalize-url|uuid)[@/]',
+  ],
   moduleNameMapper: { '^(\\.{1,2}/.*)\\.js$': '$1' },
   transform: {
     '^.+\\.[jt]s$': '@swc/jest',
