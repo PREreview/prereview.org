16123aaa8b916059832d72f621a6cce3325eb6f7
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-02-27 11:32:25 +0000

    Sketch a test list for the command
    Refs #3070

---
+++ test/ReviewRequests/Commands/WithdrawReviewRequest.test.ts
@@ -0,0 +1,14 @@
+import { it } from '@fast-check/jest'
+import { describe } from '@jest/globals'
+
+describe('review request has not been published', () => {
+  it.todo('rejects the command with an error')
+})
+
+describe('review request has been published', () => {
+  it.todo('withdraws the review request')
+})
+
+describe('review request has been withdrawn', () => {
+  it.todo('does nothing')
+})
