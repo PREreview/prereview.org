7b1866a45590e85643ff53513ef7b606162f2c0c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-02-27 12:06:53 +0000

    Add a test case for another possible event sequence
    Refs #3070

--- test/ReviewRequests/Commands/WithdrawReviewRequest.test.ts
+++ test/ReviewRequests/Commands/WithdrawReviewRequest.test.ts
@@ -1,16 +1,35 @@
 import { it } from '@fast-check/jest'
 import { describe, expect } from '@jest/globals'
 import { Temporal } from '@js-temporal/polyfill'
-import { Either } from 'effect'
+import { Either, Option } from 'effect'
+import * as Preprints from '../../../src/Preprints/index.ts'
 import * as _ from '../../../src/ReviewRequests/Commands/WithdrawReviewRequest.ts'
 import * as ReviewRequests from '../../../src/ReviewRequests/index.ts'
-import { Uuid } from '../../../src/types/index.ts'
+import { Doi, NonEmptyString, Uuid } from '../../../src/types/index.ts'
 
-describe('review request has not been published', () => {
-  const events = [] satisfies ReadonlyArray<ReviewRequests.ReviewRequestEvent>
+const requester1 = { name: NonEmptyString.NonEmptyString('Josiah Carberry') }
+
+const request1Id = Uuid.Uuid('475434b4-3c0d-4b70-a5f4-8af7baf55753')
+
+const preprintId1 = new Preprints.BiorxivPreprintId({ value: Doi.Doi('10.1101/12345') })
+
+const now = Temporal.Now.instant()
+
+const received = new ReviewRequests.ReviewRequestForAPreprintWasReceived({
+  receivedAt: now.subtract({ hours: 2 }),
+  receivedFrom: new URL('http://example.com'),
+  preprintId: preprintId1,
+  requester: Option.some(requester1),
+  reviewRequestId: request1Id,
+})
+
+describe.each<[string, ReadonlyArray<ReviewRequests.ReviewRequestEvent>]>([
+  ['no events', []],
+  ['received but not accepted', [received]],
+])('review request has not been published (%s)', (_case, events) => {
   const command = {
     withdrawnAt: Temporal.Now.instant(),
-    reviewRequestId: Uuid.Uuid('475434b4-3c0d-4b70-a5f4-8af7baf55753'),
+    reviewRequestId: request1Id,
     reason: 'preprint-withdrawn-from-preprint-server',
   } satisfies _.Command
 
