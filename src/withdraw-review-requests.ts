a9ff48747b7f455f19d99ebdb85c0870ec5e52fa
Chris Wilkinson <chris@prereview.org>
2026-02-27 19:12:44 +0000

    Withdraw known requests for withdrawn preprints
    Refs #3070

--- src/withdraw-review-requests.ts
+++ src/withdraw-review-requests.ts
@@ -7,16 +7,29 @@ import * as SqlEventStore from './SqlEventStore.ts'
 import * as SqlSensitiveDataStore from './SqlSensitiveDataStore.ts'
 import { Temporal, Uuid } from './types/index.ts'
 
-const reviewRequestId = Uuid.Uuid('a1c02f8b-1400-4303-9b03-f631b6c22ee5')
+const reviewRequestIds = [
+  Uuid.Uuid('a1c02f8b-1400-4303-9b03-f631b6c22ee5'),
+  Uuid.Uuid('2f4ecac1-428a-526c-99ec-ff21cba42ba5'),
+  Uuid.Uuid('f8bf2dbe-d10e-5e5e-9e20-b06b869b0303'),
+  Uuid.Uuid('fee3c5d8-83f2-5f40-98ac-fddb33e3b733'),
+  Uuid.Uuid('556f36e7-5be1-5ea2-8edf-0371fefd75ca'),
+  Uuid.Uuid('2c90ed30-3b37-5fbb-a008-3cf02ace810d'),
+  Uuid.Uuid('59657ff3-edd9-5ba4-b7f0-35c4d522d3e0'),
+]
 
 const program = Effect.gen(function* () {
   const withdrawnAt = yield* Temporal.currentInstant
 
-  yield* ReviewRequests.withdrawReviewRequest({
-    reviewRequestId,
-    withdrawnAt,
-    reason: 'preprint-withdrawn-from-preprint-server',
-  })
+  yield* Effect.forEach(
+    reviewRequestIds,
+    reviewRequestId =>
+      ReviewRequests.withdrawReviewRequest({
+        reviewRequestId,
+        withdrawnAt,
+        reason: 'preprint-withdrawn-from-preprint-server',
+      }),
+    { concurrency: 10 },
+  )
 })
 
 pipe(
