cc6112ad68b9e837b5907c533dff02fef6ff1083
Chris Wilkinson <chris@prereview.org>
2026-03-09 14:24:51 +0000

    Don’t use mutable variables
    Refs https://effect.website/docs/state-management/ref/

--- src/EventDispatcher.ts
+++ src/EventDispatcher.ts
@@ -1,11 +1,18 @@
-import { Array, Context, Effect, Layer, Option, pipe, Schedule } from 'effect'
+import { Array, Context, Effect, Layer, Option, pipe, Ref, Schedule } from 'effect'
 import type * as Events from './Events.ts'
 import * as EventStore from './EventStore.ts'
 
 type Subscriber = (event: Events.Event) => void
 
-let lastKnownPosition = Option.none<EventStore.Position>()
-const subscribers: Array<Subscriber> = []
+class LastKnownPosition extends Context.Tag('EventDispatcher/LastKnownPosition')<
+  LastKnownPosition,
+  Ref.Ref<Option.Option<EventStore.Position>>
+>() {}
+
+class Subscribers extends Context.Tag('EventDispatcher/Subscribers')<
+  Subscribers,
+  Ref.Ref<ReadonlyArray<Subscriber>>
+>() {}
 
 export class EventDispatcher extends Context.Tag('EventDispatcher')<
   EventDispatcher,
@@ -14,12 +21,34 @@ export class EventDispatcher extends Context.Tag('EventDispatcher')<
   }
 >() {}
 
-export const EventDispatcherLayer = Layer.succeed(EventDispatcher, {
-  addSubscriber: subscriber => Effect.sync(() => subscribers.push(subscriber)),
-})
+export const EventDispatcherLayer = Layer.mergeAll(
+  Layer.provideMerge(
+    Layer.effect(
+      EventDispatcher,
+      Effect.gen(function* () {
+        const subscribers = yield* Subscribers
+
+        return {
+          addSubscriber: subscriber => Ref.update(subscribers, Array.append(subscriber)),
+        }
+      }),
+    ),
+    Layer.effect(Subscribers, Ref.make([] as ReadonlyArray<Subscriber>)),
+  ),
+  Layer.effect(LastKnownPosition, Ref.make(Option.none())),
+)
 
 const dispatchNewEvents = Effect.gen(function* () {
-  const result = yield* Option.match(lastKnownPosition, { onNone: () => EventStore.all, onSome: EventStore.since })
+  const lastKnownPosition = yield* LastKnownPosition
+  const subscribers = yield* Subscribers
+
+  const result = yield* Effect.andThen(
+    Ref.get(lastKnownPosition),
+    Option.match({
+      onNone: () => EventStore.all,
+      onSome: EventStore.since,
+    }),
+  )
 
   if (Option.isNone(result)) {
     return
@@ -27,12 +56,14 @@ const dispatchNewEvents = Effect.gen(function* () {
 
   const { events: newEvents, lastKnownPosition: newLastKnownPosition } = result.value
 
-  lastKnownPosition = Option.some(newLastKnownPosition)
+  const allSubscribers = yield* Ref.get(subscribers)
+
+  yield* Ref.set(lastKnownPosition, Option.some(newLastKnownPosition))
 
   yield* Effect.forEach(
     Array.chunksOf(newEvents, 100),
     events =>
-      Effect.sync(() => Array.forEach(events, event => Array.forEach(subscribers, subscriber => subscriber(event)))),
+      Effect.sync(() => Array.forEach(events, event => Array.forEach(allSubscribers, subscriber => subscriber(event)))),
     { discard: true },
   )
 })
