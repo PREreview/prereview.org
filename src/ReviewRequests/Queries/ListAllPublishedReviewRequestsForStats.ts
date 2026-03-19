9fd18bda0ee56fea732a15c4a261c9a724f01092
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-03-19 10:07:25 +0000

    Prepare to include request ID in endpoint response Refs #3125

--- src/ReviewRequests/Queries/ListAllPublishedReviewRequestsForStats.ts
+++ src/ReviewRequests/Queries/ListAllPublishedReviewRequestsForStats.ts
@@ -3,13 +3,14 @@ import * as Preprints from '../../Preprints/index.ts'
 import * as Queries from '../../Queries.ts'
 import { DomainIdSchema } from '../../types/domain.ts'
 import { FieldIdSchema } from '../../types/field.ts'
-import { Iso639, Temporal, type Uuid } from '../../types/index.ts'
+import { Iso639, Temporal, Uuid } from '../../types/index.ts'
 import { SubfieldIdSchema } from '../../types/subfield.ts'
 import * as shared from './sharedLogic.ts'
 
 export type ReviewRequestForStats = typeof ReviewRequestForStats.Type
 
 export const ReviewRequestForStats = Schema.Struct({
+  requestId: Uuid.UuidSchema,
   published: Temporal.InstantSchema,
   subfields: Schema.Array(SubfieldIdSchema),
   fields: Schema.Array(FieldIdSchema),
@@ -21,13 +22,10 @@ export const ReviewRequestForStats = Schema.Struct({
 export type Result = ReadonlyArray<ReviewRequestForStats>
 
 const query = (state: shared.State): Result => {
-  const filteredReviewRequests = HashMap.filter(
-    state,
-    reviewRequest => reviewRequest._tag === 'PublishedReviewRequest',
-  ) as HashMap.HashMap<Uuid.Uuid, ReviewRequestForStats>
+  const filteredReviewRequests = HashMap.filter(state, reviewRequest => reviewRequest._tag === 'PublishedReviewRequest')
 
   const sortedReviewRequests = Array.sortWith(
-    HashMap.values(filteredReviewRequests),
+    Array.map(Array.fromIterable(filteredReviewRequests), ([requestId, properties]) => ({ ...properties, requestId })),
     Struct.get('published'),
     Temporal.OrderInstant,
   )
