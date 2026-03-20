9b4c69b4d3ec30484a8623554da91c4eb4cfd2d5
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-03-20 12:37:38 +0000

    Extract strings for ReviewADatasetFlow/ChooseYourPersona Refs #1816

--- src/WebApp/ReviewADatasetFlow/ChooseYourPersonaPage/ChooseYourPersonaPage.ts
+++ src/WebApp/ReviewADatasetFlow/ChooseYourPersonaPage/ChooseYourPersonaPage.ts
@@ -1,19 +1,21 @@
 import { Either, Match, pipe } from 'effect'
 import { html, plainText, rawHtml } from '../../../html.ts'
-import type { SupportedLocale } from '../../../locales/index.ts'
+import { translate, type SupportedLocale } from '../../../locales/index.ts'
 import type * as Personas from '../../../Personas/index.ts'
 import * as Routes from '../../../routes.ts'
+import { errorPrefix, errorSummary, saveAndContinueButton } from '../../../shared-translation-elements.ts'
 import * as StatusCodes from '../../../StatusCodes.ts'
 import type { Uuid } from '../../../types/index.ts'
 import { StreamlinePageResponse } from '../../Response/index.ts'
 import type * as ChooseYourPersonaForm from './ChooseYourPersonaForm.ts'
+
+const definition = (text: string) => `<dfn>${text}</dfn>`
 
 export const ChooseYourPersonaPage = ({
   datasetReviewId,
   form,
   publicPersona,
   pseudonymPersona,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   locale,
 }: {
   datasetReviewId: Uuid.Uuid
@@ -22,36 +24,20 @@ export const ChooseYourPersonaPage = ({
   pseudonymPersona: Personas.PseudonymPersona
   locale: SupportedLocale
 }) => {
+  const hasAnError = form._tag === 'InvalidForm'
+  const t = translate(locale, 'review-a-dataset-flow')
+
   return StreamlinePageResponse({
     status: form._tag === 'InvalidForm' ? StatusCodes.BadRequest : StatusCodes.OK,
-    title: plainText`${form._tag === 'InvalidForm' ? 'Error: ' : ''}What name would you like to use?`,
+    title: pipe(t('whatNameWouldYouLikeToUse')(), errorPrefix(locale, hasAnError), plainText),
     nav: html`
-      <a href="${Routes.ReviewADatasetIsMissingAnything.href({ datasetReviewId })}" class="back"><span>Back</span></a>
+      <a href="${Routes.ReviewADatasetIsMissingAnything.href({ datasetReviewId })}" class="back"
+        ><span>${t('back')()}</span></a
+      >
     `,
     main: html`
       <form method="post" action="${Routes.ReviewADatasetChooseYourPersona.href({ datasetReviewId })}" novalidate>
-        ${form._tag === 'InvalidForm'
-          ? html`
-              <error-summary aria-labelledby="error-summary-title" role="alert">
-                <h2 id="error-summary-title">There is a problem</h2>
-                <ul>
-                  ${Either.isLeft(form.chooseYourPersona)
-                    ? html`
-                        <li>
-                          <a href="#choose-your-persona-public">
-                            ${pipe(
-                              Match.value(form.chooseYourPersona.left),
-                              Match.tag('Missing', () => 'Select the name that you would like to use'),
-                              Match.exhaustive,
-                            )}
-                          </a>
-                        </li>
-                      `
-                    : ''}
-                </ul>
-              </error-summary>
-            `
-          : ''}
+        ${hasAnError ? pipe(form, toErrorItems(locale), errorSummary(locale)) : ''}
 
         <div ${form._tag === 'InvalidForm' ? 'class="error"' : ''}>
           <fieldset
@@ -64,38 +50,35 @@ export const ChooseYourPersonaPage = ({
             )}
           >
             <legend>
-              <h1>What name would you like to use?</h1>
+              <h1>${t('whatNameWouldYouLikeToUse')()}</h1>
             </legend>
 
-            <p id="choose-your-persona-tip" role="note">
-              You can choose between the name on your ORCID&nbsp;record or your PREreview&nbsp;pseudonym.
-            </p>
+            <p id="choose-your-persona-tip" role="note">${t('youCanChooseOrcidOrPseudonym')()}</p>
 
             <details>
-              <summary><span>What is a PREreview&nbsp;pseudonym?</span></summary>
+              <summary><span>${t('whatIsAPrereviewPseudonym')()}</span></summary>
 
               <div>
                 <p>
-                  A <dfn>PREreview&nbsp;pseudonym</dfn> is an alternate name you can use instead of your real&nbsp;name.
-                  It is unique and combines a random color and animal. Your pseudonym is
-                  ‘${rawHtml(pseudonymPersona.pseudonym.replace(' ', '&nbsp;'))}’.
+                  ${rawHtml(
+                    t('prereviewPseudonymnExplainer')({
+                      definition,
+                      pseudonym: pseudonymPersona.pseudonym.replace(' ', '&nbsp;'),
+                    }),
+                  )}
                 </p>
 
-                <p>
-                  Using your pseudonym, you can contribute to open dataset review without fearing retribution or
-                  judgment that may occur when using your real name. However, using a pseudonym retains an element of
-                  accountability.
-                </p>
+                <p>${t('whyUseAPseudonym')()}</p>
               </div>
             </details>
 
             ${form._tag === 'InvalidForm' && Either.isLeft(form.chooseYourPersona)
               ? html`
                   <div class="error-message" id="choose-your-persona-error">
-                    <span class="visually-hidden">Error:</span>
+                    <span class="visually-hidden">${t('error')()}:</span>
                     ${pipe(
                       Match.value(form.chooseYourPersona.left),
-                      Match.tag('Missing', () => 'Select the name that you would like to use'),
+                      Match.tag('Missing', t('selectNameYouWouldLikeToUse')),
                       Match.exhaustive,
                     )}
                   </div>
@@ -122,7 +105,7 @@ export const ChooseYourPersonaPage = ({
                   />
                   <span>${publicPersona.name}</span>
                 </label>
-                <p id="choose-your-persona-tip-public" role="note">We’ll link your PREreview to your ORCID&nbsp;iD.</p>
+                <p id="choose-your-persona-tip-public" role="note">${t('weWillLinkToYourOrcid')()}</p>
               </li>
               <li>
                 <label>
@@ -142,15 +125,13 @@ export const ChooseYourPersonaPage = ({
                   />
                   <span>${pseudonymPersona.pseudonym}</span>
                 </label>
-                <p id="choose-your-persona-tip-pseudonym" role="note">
-                  We’ll only link your PREreview to others that also use your pseudonym.
-                </p>
+                <p id="choose-your-persona-tip-pseudonym" role="note">${t('weWillOnlyLinkToYourPseudonym')()}</p>
               </li>
             </ol>
           </fieldset>
         </div>
 
-        <button>Save and continue</button>
+        ${saveAndContinueButton(locale)}
       </form>
     `,
     canonical: Routes.ReviewADatasetChooseYourPersona.href({ datasetReviewId }),
@@ -158,3 +139,19 @@ export const ChooseYourPersonaPage = ({
     skipToLabel: 'form',
   })
 }
+
+const toErrorItems = (locale: SupportedLocale) => (form: ChooseYourPersonaForm.InvalidForm) => html`
+  ${Either.isLeft(form.chooseYourPersona)
+    ? html`
+        <li>
+          <a href="#choose-your-persona-public">
+            ${pipe(
+              Match.value(form.chooseYourPersona.left),
+              Match.tag('Missing', translate(locale, 'review-a-dataset-flow', 'selectNameYouWouldLikeToUse')),
+              Match.exhaustive,
+            )}
+          </a>
+        </li>
+      `
+    : ''}
+`
