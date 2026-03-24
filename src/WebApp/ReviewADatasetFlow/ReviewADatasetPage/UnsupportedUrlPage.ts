13b565f6dd13f32fde1ce53492d9c6f5fa4f2bb4
Chris Wilkinson <chris@prereview.org>
2026-03-24 14:05:56 +0000

    Translate a page
    Refs #2805

--- src/WebApp/ReviewADatasetFlow/ReviewADatasetPage/UnsupportedUrlPage.ts
+++ src/WebApp/ReviewADatasetFlow/ReviewADatasetPage/UnsupportedUrlPage.ts
@@ -1,27 +1,27 @@
-import { html, plainText } from '../../../html.ts'
+import { html, plainText, rawHtml } from '../../../html.ts'
 import { translate, type SupportedLocale } from '../../../locales/index.ts'
 import * as Routes from '../../../routes.ts'
 import * as StatusCodes from '../../../StatusCodes.ts'
 import { PageResponse } from '../../Response/index.ts'
 
 export const UnsupportedUrlPage = ({ locale }: { locale: SupportedLocale }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const t = translate(locale, 'review-a-dataset-flow')
 
   return PageResponse({
     status: StatusCodes.BadRequest,
-    title: plainText('Sorry, we don’t support this URL'),
+    title: plainText(t('unsupportedUrl')()),
     main: html`
-      <h1>Sorry, we don’t support this URL</h1>
+      <h1>${t('unsupportedUrl')()}</h1>
 
-      <p>We support datasets from Dryad.</p>
+      <p>${t('supportDatasetsFrom')()}</p>
 
       <p>
-        If this URL is for a dataset on a repository we don’t support, please
-        <a href="mailto:help@prereview.org">get in touch</a>.
+        ${rawHtml(
+          t('urlIsForDataset')({ contact: text => html`<a href="mailto:help@prereview.org">${text}</a>`.toString() }),
+        )}
       </p>
 
-      <p>Otherwise, if the dataset has a DOI, please try using that instead.</p>
+      <p>${t('tryDoi')()}</p>
 
       <a href="${Routes.ReviewADataset}" class="button">${translate(locale, 'forms', 'backLink')()}</a>
     `,
