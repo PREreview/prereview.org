09d8ec207ea0c8d6079afc9b43d4436d46edc1d4
Chris Wilkinson <chris@prereview.org>
2026-03-24 14:31:38 +0000

    Translate a page
    Refs #2805

--- src/WebApp/ReviewADatasetFlow/StartNow/CarryOnPage.ts
+++ src/WebApp/ReviewADatasetFlow/StartNow/CarryOnPage.ts
@@ -1,6 +1,6 @@
 import rtlDetect from 'rtl-detect'
 import type * as Datasets from '../../../Datasets/index.ts'
-import { html, plainText } from '../../../html.ts'
+import { html, plainText, rawHtml } from '../../../html.ts'
 import { translate, type SupportedLocale } from '../../../locales/index.ts'
 import * as Routes from '../../../routes.ts'
 import type { Uuid } from '../../../types/index.ts'
@@ -17,18 +17,21 @@ export const CarryOnPage = ({
   locale: SupportedLocale
   nextRoute: Routes.Route<{ datasetReviewId: Uuid.Uuid }>
 }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const t = translate(locale, 'review-a-dataset-flow')
 
   return PageResponse({
-    title: plainText`Review a dataset`,
+    title: plainText(t('reviewADataset')()),
     main: html`
-      <h1>Review a dataset</h1>
+      <h1>${t('reviewADataset')()}</h1>
 
       <p>
-        As you’ve already started a PREreview of
-        <cite lang="${dataset.language}" dir="${rtlDetect.getLangDir(dataset.language)}">${dataset.title}</cite>, we’ll
-        take you to the next step so you can carry&nbsp;on.
+        ${rawHtml(
+          t('asYouHaveAlreadyStarted')({
+            dataset: html`<cite lang="${dataset.language}" dir="${rtlDetect.getLangDir(dataset.language)}"
+              >${dataset.title}</cite
+            >`.toString(),
+          }),
+        )}
       </p>
 
       <a href="${nextRoute.href({ datasetReviewId })}" role="button" draggable="false"
