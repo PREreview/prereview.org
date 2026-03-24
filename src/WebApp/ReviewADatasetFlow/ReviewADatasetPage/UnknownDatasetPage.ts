dd2496f2e3e02ba9b56fd7ebecb147931aa62464
Chris Wilkinson <chris@prereview.org>
2026-03-24 13:17:35 +0000

    Translate a page
    Refs #2805

--- src/WebApp/ReviewADatasetFlow/ReviewADatasetPage/UnknownDatasetPage.ts
+++ src/WebApp/ReviewADatasetFlow/ReviewADatasetPage/UnknownDatasetPage.ts
@@ -1,34 +1,37 @@
 import { Match } from 'effect'
 import type * as Datasets from '../../../Datasets/index.ts'
-import { html, plainText } from '../../../html.ts'
+import { html, plainText, rawHtml } from '../../../html.ts'
 import { translate, type SupportedLocale } from '../../../locales/index.ts'
 import * as Routes from '../../../routes.ts'
 import * as StatusCodes from '../../../StatusCodes.ts'
 import { PageResponse } from '../../Response/index.ts'
 
 export const UnknownDatasetPage = ({ dataset, locale }: { dataset: Datasets.DatasetId; locale: SupportedLocale }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const t = translate(locale, 'review-a-dataset-flow')
 
   return PageResponse({
     status: StatusCodes.BadRequest,
-    title: plainText('Sorry, we don’t know this dataset'),
+    title: plainText(t('dontKnowDataset')()),
     main: html`
-      <h1>Sorry, we don’t know this dataset</h1>
+      <h1>${t('dontKnowDataset')()}</h1>
 
       <p>
-        ${Match.valueTags(dataset, {
-          DryadDatasetId: () =>
-            html`We think the DOI <q class="select-all" translate="no">${dataset.value}</q> could be a Dryad dataset,
-              but we can’t find any details.`,
-        })}
+        ${rawHtml(
+          Match.valueTags(dataset, {
+            DryadDatasetId: () => t('doiCouldBeDryad'),
+          })({ doi: html`<q class="select-all" translate="no">${dataset.value}</q>`.toString() }),
+        )}
       </p>
 
-      <p>If you typed the DOI, check it is correct.</p>
+      <p>${t('checkCorrectDoi')()}</p>
 
-      <p>If you pasted the DOI, check you copied the entire address.</p>
+      <p>${t('checkPastedDoi')()}</p>
 
-      <p>If the DOI is correct, please <a href="mailto:help@prereview.org">get in touch</a>.</p>
+      <p>
+        ${rawHtml(
+          t('doiIsCorrect')({ contact: text => html`<a href="mailto:help@prereview.org">${text}</a>`.toString() }),
+        )}
+      </p>
 
       <a href="${Routes.ReviewADataset}" class="button">${translate(locale, 'forms', 'backLink')()}</a>
     `,
