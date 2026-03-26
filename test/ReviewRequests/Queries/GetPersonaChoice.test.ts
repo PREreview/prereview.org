9ec708dd6dbffda99513507c2308d1d76f48af2e
Chris Wilkinson <chris@prereview.org>
2026-03-26 14:30:57 +0000

    Add test list
    Refs #3108

--- test/ReviewRequests/Queries/GetPersonaChoice.test.ts
+++ test/ReviewRequests/Queries/GetPersonaChoice.test.ts
@@ -1,14 +1,43 @@
 import { test } from '@fast-check/jest'
 import { expect } from '@jest/globals'
-import { Array, Either } from 'effect'
+import { Temporal } from '@js-temporal/polyfill'
+import { Array, Either, Option } from 'effect'
 import * as Preprints from '../../../src/Preprints/index.ts'
 import * as ReviewRequests from '../../../src/ReviewRequests/index.ts'
 import * as _ from '../../../src/ReviewRequests/Queries/GetPersonaChoice.ts'
-import { Doi, OrcidId } from '../../../src/types/index.ts'
+import { Doi, OrcidId, Uuid } from '../../../src/types/index.ts'
 
 const requesterId1 = OrcidId.OrcidId('0000-0002-1825-0097')
+const requesterId2 = OrcidId.OrcidId('0000-0002-6109-0367')
 
 const preprintId1 = new Preprints.BiorxivPreprintId({ value: Doi.Doi('10.1101/12345') })
+const preprintId2 = new Preprints.MedrxivPreprintId({ value: Doi.Doi('10.1101/67890') })
+
+const reviewRequestId1 = Uuid.Uuid('123e4567-e89b-12d3-a456-426614174000')
+
+const now = Temporal.Now.instant()
+
+const started = new ReviewRequests.ReviewRequestForAPreprintWasStarted({
+  reviewRequestId: reviewRequestId1,
+  requesterId: requesterId1,
+  preprintId: preprintId1,
+  startedAt: now,
+})
+
+const publicChosen = new ReviewRequests.PersonaForAReviewRequestForAPreprintWasChosen({
+  reviewRequestId: reviewRequestId1,
+  persona: 'public',
+})
+
+const pseudonymChosen = new ReviewRequests.PersonaForAReviewRequestForAPreprintWasChosen({
+  reviewRequestId: reviewRequestId1,
+  persona: 'pseudonym',
+})
+
+const published = new ReviewRequests.ReviewRequestForAPreprintWasPublished({
+  reviewRequestId: reviewRequestId1,
+  publishedAt: now,
+})
 
 test.each<[string, _.Input, ReadonlyArray<ReviewRequests.ReviewRequestEvent>, _.Result]>([
   [
@@ -16,6 +45,56 @@ test.each<[string, _.Input, ReadonlyArray<ReviewRequests.ReviewRequestEvent>, _.
     { requesterId: requesterId1, preprintId: preprintId1 },
     [],
     Either.left(new ReviewRequests.UnknownReviewRequest({})),
+  ],
+  [
+    'different requester',
+    { requesterId: requesterId2, preprintId: preprintId1 },
+    [started],
+    Either.left(new ReviewRequests.UnknownReviewRequest({})),
+  ],
+  [
+    'different preprint',
+    { requesterId: requesterId1, preprintId: preprintId2 },
+    [started],
+    Either.left(new ReviewRequests.UnknownReviewRequest({})),
+  ],
+])('%s', (_name, input, events, expected) => {
+  const { initialState, updateStateWithEvents, query } = _.GetPersonaChoice
+
+  const state = Array.match(events, {
+    onNonEmpty: events => updateStateWithEvents(initialState, events),
+    onEmpty: () => initialState,
+  })
+
+  const actual = query(state, input)
+
+  expect(actual).toStrictEqual(expected)
+})
+
+test.failing.each<[string, _.Input, ReadonlyArray<ReviewRequests.ReviewRequestEvent>, _.Result]>([
+  [
+    'incomplete without choice',
+    { requesterId: requesterId1, preprintId: preprintId1 },
+    [started],
+    Either.right({ reviewRequestId: reviewRequestId1, personaChoice: Option.none() }),
+  ],
+  [
+    'incomplete with choice',
+    { requesterId: requesterId1, preprintId: preprintId1 },
+    [started, publicChosen],
+    Either.right({ reviewRequestId: reviewRequestId1, personaChoice: Option.some('public') }),
+  ],
+  [
+    'incomplete with multiples choice',
+    { requesterId: requesterId1, preprintId: preprintId1 },
+    [started, publicChosen, pseudonymChosen],
+    Either.right({ reviewRequestId: reviewRequestId1, personaChoice: Option.some('pseudonym') }),
+  ],
+  [
+    'has been published',
+    { requesterId: requesterId1, preprintId: preprintId1 },
+    [started, publicChosen, published],
+    Either.left(new ReviewRequests.ReviewRequestHasBeenPublished({})),
   ],
 ])('%s', (_name, input, events, expected) => {
   const { initialState, updateStateWithEvents, query } = _.GetPersonaChoice
