4fa395be89371a5d8e0e39f464b759b3cc9ec4be
Chris Wilkinson <chris@prereview.org>
2026-03-26 14:02:35 +0000

    Make the query easier to use in the existing page by returning other required information
    Refs #3108

--- src/ReviewRequests/Queries/GetPersonaChoice.ts
+++ src/ReviewRequests/Queries/GetPersonaChoice.ts
@@ -1,6 +1,6 @@
 import type { Either, Option } from 'effect'
 import type * as Preprints from '../../Preprints/index.ts'
-import type { OrcidId } from '../../types/index.ts'
+import type { OrcidId, Uuid } from '../../types/index.ts'
 import type * as Errors from '../Errors.ts'
 
 export interface Input {
@@ -9,6 +9,6 @@ export interface Input {
 }
 
 export type Result = Either.Either<
-  Option.Option<'public' | 'pseudonym'>,
+  { personaChoice: Option.Option<'public' | 'pseudonym'>; reviewRequestId: Uuid.Uuid },
   Errors.UnknownReviewRequest | Errors.ReviewRequestHasBeenPublished
 >
