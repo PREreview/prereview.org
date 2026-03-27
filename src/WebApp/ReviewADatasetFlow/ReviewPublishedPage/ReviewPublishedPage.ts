5ba771fd4e9494fbe53143825c3a1581d7bc252c
Chris Wilkinson <chris@prereview.org>
2026-03-27 17:10:00 +0000

    Translate a page
    Refs #2805

--- src/WebApp/ReviewADatasetFlow/ReviewPublishedPage/ReviewPublishedPage.ts
+++ src/WebApp/ReviewADatasetFlow/ReviewPublishedPage/ReviewPublishedPage.ts
@@ -1,5 +1,5 @@
 import type * as DatasetReviews from '../../../DatasetReviews/index.ts'
-import { html, plainText } from '../../../html.ts'
+import { html, plainText, rawHtml } from '../../../html.ts'
 import { translate, type SupportedLocale } from '../../../locales/index.ts'
 import * as Routes from '../../../routes.ts'
 import { StreamlinePageResponse } from '../../Response/index.ts'
@@ -11,65 +11,73 @@ export const ReviewPublishedPage = ({
   datasetReview: DatasetReviews.PublishedReviewDetails
   locale: SupportedLocale
 }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const t = translate(locale, 'review-a-dataset-flow')
 
   return StreamlinePageResponse({
-    title: plainText('PREreview published'),
+    title: plainText(t('prereviewPublished')()),
     main: html`
       <div class="panel">
-        <h1>PREreview published</h1>
+        <h1>${t('prereviewPublished')()}</h1>
 
         <div>
-          Your DOI
+          ${t('yourDoi')()}
           <div><strong class="doi" translate="no">${datasetReview.doi}</strong></div>
         </div>
       </div>
 
-      <h2>What happens next</h2>
+      <h2>${t('whatHappensNext')()}</h2>
 
       <p>
-        If you reviewed the dataset with others, please get in touch so we can invite them to appear on the review. Our
-        email address is
-        <a href="mailto:help@prereview.org" target="_blank" rel="noopener noreferrer"
-          >help@prereview.org<span class="visually-hidden"> (opens in a new tab)</span></a
-        >.
+        ${rawHtml(
+          t('inviteOthers')({
+            emailAddress: html`<a href="mailto:help@prereview.org" target="_blank" rel="noopener noreferrer"
+              >help@prereview.org<span class="visually-hidden"> ${t('opensNewTabSuffix')()}</span></a
+            >`.toString(),
+          }),
+        )}
       </p>
 
       ${datasetReview.persona === 'public'
         ? html`
-            <h2>Share your review</h2>
+            <h2>${t('shareYourReview')()}</h2>
 
-            <p>
-              Please let the authors know that you published your review. They may have contact details on the dataset.
-            </p>
+            <p>${t('shareYourReviewText')()}</p>
           `
         : ''}
 
-      <h2>Let us know how it went</h2>
+      <h2>${t('letUsKnowHowItWent')()}</h2>
 
       <p>
-        <a
-          href="https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUw4R0F6MVo1ZWhyfGRlZmF1bHR8ZGM2YTU1OTNhYzNhY2RiN2YzNTBlYTdmZTBmMzNmNDA"
-          target="_blank"
-          rel="noopener noreferrer"
-          >Schedule an interview<span class="visually-hidden"> (opens in a new tab)</span></a
-        >
-        with our product team to discuss your experience on PREreview. We gladly compensate interviewees in appreciation
-        for their help!
+        ${rawHtml(
+          t('scheduleInterview')({
+            link: text =>
+              html`<a
+                href="https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUw4R0F6MVo1ZWhyfGRlZmF1bHR8ZGM2YTU1OTNhYzNhY2RiN2YzNTBlYTdmZTBmMzNmNDA"
+                target="_blank"
+                rel="noopener noreferrer"
+                >${text}<span class="visually-hidden"> ${t('opensNewTabSuffix')()}</span></a
+              >`.toString(),
+          }),
+        )}
       </p>
 
       <p>
-        You can also share your feedback about PREreview.org with us by completing a
-        <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSfynZ25_toGP6pnTrEyKE-Fv-7z7pK2h9AlNksKI9_DVJMnng/viewform"
-          target="_blank"
-          rel="noopener noreferrer"
-          >brief anonymous survey<span class="visually-hidden"> (opens in a new tab)</span></a
-        >.
+        ${rawHtml(
+          t('feedbackForm')({
+            link: text =>
+              html`<a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSfynZ25_toGP6pnTrEyKE-Fv-7z7pK2h9AlNksKI9_DVJMnng/viewform"
+                target="_blank"
+                rel="noopener noreferrer"
+                >${text}<span class="visually-hidden"> ${t('opensNewTabSuffix')()}</span></a
+              >`.toString(),
+          }),
+        )}
       </p>
 
-      <a href="${Routes.DatasetReview.href({ datasetReviewId: datasetReview.id })}" class="button">See your review</a>
+      <a href="${Routes.DatasetReview.href({ datasetReviewId: datasetReview.id })}" class="button"
+        >${t('seeYourReview')()}</a
+      >
     `,
     canonical: Routes.ReviewADatasetReviewPublished.href({ datasetReviewId: datasetReview.id }),
   })
