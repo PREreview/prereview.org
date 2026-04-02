1df7a6776bdb537bb8b170ddcced5749cba3692c
Chris Wilkinson <chris@prereview.org>
2026-04-02 10:12:11 +0100

    Extract strings
    Refs #2891

--- src/ExternalInteractions/Email/AcknowledgeReviewRequest/CreateEmail.ts
+++ src/ExternalInteractions/Email/AcknowledgeReviewRequest/CreateEmail.ts
@@ -1,75 +1,84 @@
 import type { Nodemailer } from '../../../ExternalApis/index.ts'
-import { html, mjmlToHtml } from '../../../html.ts'
+import { html, mjmlToHtml, rawHtml } from '../../../html.ts'
+import { DefaultLocale, translate } from '../../../locales/index.ts'
 import type * as ReviewRequests from '../../../ReviewRequests/index.ts'
 import { EmailAddress } from '../../../types/index.ts'
 
-export const CreateEmail = (reviewRequest: ReviewRequests.ReviewRequestToAcknowledge): Nodemailer.Email => ({
-  from: { name: 'PREreview', address: EmailAddress.EmailAddress('help@prereview.org') },
-  to: { name: reviewRequest.requester.name, address: reviewRequest.requester.emailAddress },
-  subject: 'Review requested from the PREreview community',
-  html: mjmlToHtml(html`
-    <mjml>
-      <mj-body>
-        <mj-section>
-          <mj-column>
-            <mj-text>Hi ${reviewRequest.requester.name},</mj-text>
-            <mj-text>Thank you for requesting a review from PREreview.</mj-text>
-            <mj-text>
-              While we cannot guarantee a review, we’ve shared your request with our PREreview community on our
-              #request-a-review Slack channel.
-            </mj-text>
-            <mj-text>
-              You can join our Slack Community and add further details to your review request by signing up at
-              <a href="https://bit.ly/PREreview-Slack">bit.ly/PREreview-Slack</a>.
-            </mj-text>
-            <mj-text>
-              If you have any questions, please let us know at
-              <a href="mailto:help@prereview.org">help@prereview.org</a>.
-            </mj-text>
-            <mj-text>All the best,<br />PREreview</mj-text>
-          </mj-column>
-        </mj-section>
-        <mj-section padding-bottom="0" border-top="1px solid lightgrey">
-          <mj-column width="25%" vertical-align="middle">
-            <mj-image
-              href="https://prereview.org"
-              src="https://res.cloudinary.com/prereview/image/upload/f_auto,q_auto,w_300/emails/logo_tbhi5b"
-              padding="0"
-            />
-          </mj-column>
-          <mj-column width="75%" vertical-align="middle">
-            <mj-text font-size="11px">PREreview is a platform, resource center, and convener.</mj-text>
-            <mj-text font-size="11px">
-              We provide ways for feedback to preprints to be done openly, rapidly, constructively, and by a global
-              community of peers.
-            </mj-text>
-            <mj-text font-size="11px">
-              Join us at <a href="https://prereview.org">prereview.org</a> and
-              <a href="https://bit.ly/PREreview-Slack">sign up to our vibrant Slack community</a>.
-            </mj-text>
-          </mj-column>
-        </mj-section>
-      </mj-body>
-    </mjml>
-  `),
-  text: `
-Hi ${reviewRequest.requester.name},
+export const CreateEmail = (reviewRequest: ReviewRequests.ReviewRequestToAcknowledge): Nodemailer.Email => {
+  const t = translate(DefaultLocale, 'email')
 
-Thank you for requesting a review from PREreview.
+  return {
+    from: { name: 'PREreview', address: EmailAddress.EmailAddress('help@prereview.org') },
+    to: { name: reviewRequest.requester.name, address: reviewRequest.requester.emailAddress },
+    subject: t('acknowledgeReviewRequestTitle')(),
+    html: mjmlToHtml(html`
+      <mjml>
+        <mj-body>
+          <mj-section>
+            <mj-column>
+              <mj-text>${t('hiName')({ name: reviewRequest.requester.name })}</mj-text>
+              <mj-text>${t('thanksReviewRequest')()}</mj-text>
+              <mj-text>${t('reviewRequestSharedWithCommunity')({ slackChannel: '#request-a-review' })}</mj-text>
+              <mj-text>
+                ${rawHtml(
+                  t('reviewRequestSlackCommunity')({
+                    slackLink: html`<a href="https://bit.ly/PREreview-Slack">bit.ly/PREreview-Slack</a>`.toString(),
+                  }),
+                )}
+              </mj-text>
+              <mj-text>
+                ${rawHtml(
+                  t('haveAnyQuestions')({
+                    emailAddress: html`<a href="mailto:help@prereview.org">help@prereview.org</a>`.toString(),
+                  }),
+                )}
+              </mj-text>
+              <mj-text>${t('allTheBest')()}<br />PREreview</mj-text>
+            </mj-column>
+          </mj-section>
+          <mj-section padding-bottom="0" border-top="1px solid lightgrey">
+            <mj-column width="25%" vertical-align="middle">
+              <mj-image
+                href="https://prereview.org"
+                src="https://res.cloudinary.com/prereview/image/upload/f_auto,q_auto,w_300/emails/logo_tbhi5b"
+                padding="0"
+              />
+            </mj-column>
+            <mj-column width="75%" vertical-align="middle">
+              <mj-text font-size="11px">${t('footerIntro')()}</mj-text>
+              <mj-text font-size="11px">${t('footerCommunity')()}</mj-text>
+              <mj-text font-size="11px"
+                >${rawHtml(
+                  t('footerJoinHtml')({
+                    prereviewLink: text => html`<a href="https://prereview.org">${text}</a>`.toString(),
+                    slackLink: text => html`<a href="https://bit.ly/PREreview-Slack">${text}</a>`.toString(),
+                  }),
+                )}</mj-text
+              >
+            </mj-column>
+          </mj-section>
+        </mj-body>
+      </mjml>
+    `),
+    text: `
+${t('hiName')({ name: reviewRequest.requester.name })}
 
-While we cannot guarantee a review, we’ve shared your request with our PREreview community on our #request-a-review Slack channel.
+${t('thanksReviewRequest')()}
 
-You can join our Slack Community and add further details to your review request by signing up at https://bit.ly/PREreview-Slack.
+${t('reviewRequestSharedWithCommunity')({ slackChannel: '#request-a-review' })}
 
-If you have any questions, please let us know at help@prereview.org.
+${t('reviewRequestSlackCommunity')({ slackLink: 'https://bit.ly/PREreview-Slack' })}
 
-All the best,
+${t('haveAnyQuestions')({ emailAddress: 'help@prereview.org' })}
+
+${t('allTheBest')()}
 PREreview
 
 ---
 
-PREreview is a platform, resource center, and convener.
-We provide ways for feedback to preprints to be done openly, rapidly, constructively, and by a global community of peers.
-Join us at https://prereview.org and sign up to our vibrant Slack community at https://bit.ly/PREreview-Slack.
+${t('footerIntro')()}
+${t('footerCommunity')()}
+${t('footerJoinText')({ prereviewLink: 'https://prereview.org', slackLink: 'https://bit.ly/PREreview-Slack' })}
 `.trim(),
-})
+  }
+}
