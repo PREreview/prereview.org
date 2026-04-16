b16261de79f3e2e3b8ef2b025fe28b40c859d289
Chris Wilkinson <chris@prereview.org>
2026-04-16 14:42:06 +0100

    Fix a signature to allow failure
    Refs #3188

--- src/Prereviewers/index.ts
+++ src/Prereviewers/index.ts
@@ -1,11 +1,12 @@
 import { Context, Effect, Layer } from 'effect'
+import type { UnableToQuery } from '../Queries.ts'
 import type { OrcidId } from '../types/index.ts'
 
 export class Prereviewers extends Context.Tag('Prereviewers')<
   Prereviewers,
   {
     register: (orcidId: OrcidId.OrcidId) => Effect.Effect<void>
-    isRegistered: (orcidId: OrcidId.OrcidId) => Effect.Effect<boolean>
+    isRegistered: (orcidId: OrcidId.OrcidId) => Effect.Effect<boolean, UnableToQuery>
   }
 >() {}
 
