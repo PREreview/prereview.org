42da656a7a0be2cedd0e10e5ef8513118a388184
Chris Wilkinson <chris@prereview.org>
2026-04-16 14:35:30 +0100

    Prepare to use the service with stubbed implementations
    Refs #3188

--- src/Prereviewers/index.ts
+++ src/Prereviewers/index.ts
@@ -1,6 +1,15 @@
-import { Context, type Effect } from 'effect'
+import { Context, Effect, Layer } from 'effect'
+import type { OrcidId } from '../types/index.ts'
 
 export class Prereviewers extends Context.Tag('Prereviewers')<
   Prereviewers,
-  { register: () => Effect.Effect<void>; isRegistered: () => Effect.Effect<boolean> }
+  {
+    register: (orcidId: OrcidId.OrcidId) => Effect.Effect<void>
+    isRegistered: (orcidId: OrcidId.OrcidId) => Effect.Effect<boolean>
+  }
 >() {}
+
+export const layer = Layer.succeed(Prereviewers, {
+  register: () => Effect.void,
+  isRegistered: () => Effect.succeed(false),
+})
