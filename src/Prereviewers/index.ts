64ad86ac35fbf5db84e3b9c126f9aed2538fb98d
Chris Wilkinson <chris@prereview.org>
2026-04-17 11:34:28 +0100

    Wire up the command so that newly registered PREreviewers are imported
    Refs #3188

--- src/Prereviewers/index.ts
+++ src/Prereviewers/index.ts
@@ -1,17 +1,21 @@
 import { FetchHttpClient } from '@effect/platform'
 import { Context, Effect, flow, Layer, Match, pipe, Redacted } from 'effect'
+import * as Commands from '../Commands.ts'
 import { UnableToHandleCommand } from '../Commands.ts'
 import * as LegacyPrereview from '../legacy-prereview.ts'
 import { UnableToQuery } from '../Queries.ts'
 import { FptsToEffect } from '../RefactoringUtilities/index.ts'
 import type { OrcidId } from '../types/index.ts'
+import { ImportRegisteredPrereviewer } from './ImportRegisteredPrereviewer.ts'
 
 export class Prereviewers extends Context.Tag('Prereviewers')<
   Prereviewers,
   {
     register: (orcidId: OrcidId.OrcidId) => Effect.Effect<void, UnableToHandleCommand>
     isRegistered: (orcidId: OrcidId.OrcidId) => Effect.Effect<boolean, UnableToQuery>
-    importRegisteredOrcidId: (orcidId: OrcidId.OrcidId) => Effect.Effect<void, UnableToHandleCommand>
+    importRegisteredOrcidId: (
+      orcidId: OrcidId.OrcidId,
+    ) => ReturnType<Commands.FromCommand<typeof ImportRegisteredPrereviewer>>
   }
 >() {}
 
@@ -20,6 +24,8 @@ export const layer = Layer.effect(
   Effect.gen(function* () {
     const fetch = yield* FetchHttpClient.Fetch
     const legacyPrereviewApi = yield* LegacyPrereview.LegacyPrereviewApi
+
+    const importRegisteredPrereviewer = yield* Commands.makeCommand(ImportRegisteredPrereviewer)
 
     return {
       register: orcid =>
@@ -65,8 +71,10 @@ export const layer = Layer.effect(
               url: legacyPrereviewApi.origin,
             },
           }),
-          Effect.asVoid,
           Effect.mapError(() => new UnableToHandleCommand({ cause: 'Legacy user API unavailable' })),
+          Effect.andThen(({ pseudonym, createdAt }) =>
+            importRegisteredPrereviewer({ orcidId: orcid, pseudonym, registeredAt: createdAt }),
+          ),
         ),
     }
   }),
