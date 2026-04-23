352d9d2f9d68b1faa86223e8717a4c661b03814f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-04-23 15:48:30 +0100

    Sketch a new command and testlist Refs #3188

---
+++ src/Prereviewers/RegisterPrereviewer.ts
@@ -0,0 +1,26 @@
+import { Data } from 'effect'
+import type * as Commands from '../Commands.ts'
+import type { Pseudonym, Temporal } from '../types/index.ts'
+import type { OrcidId } from '../types/OrcidId.ts'
+
+export interface Input {
+  readonly orcidId: OrcidId
+  readonly registeredAt: Temporal.Instant
+  readonly pseudonym: Pseudonym.Pseudonym
+}
+
+type State = unknown
+
+export class PseudonymAlreadyInUse extends Data.TaggedError('PseudonymAlreadyInUse') {}
+
+export class MismatchWithExistingDataForOrcid extends Data.TaggedError('MismatchWithExistingDataForOrcid')<{
+  existingPseudonym: Pseudonym.Pseudonym
+  existingRegisteredAt: Temporal.Instant | 'not available from import source'
+}> {}
+
+export declare const RegisterPrereviewer: Commands.Command<
+  'RegisteredPrereviewerImported' | 'PrereviewerRegistered',
+  [Input],
+  State,
+  PseudonymAlreadyInUse | MismatchWithExistingDataForOrcid
+>
