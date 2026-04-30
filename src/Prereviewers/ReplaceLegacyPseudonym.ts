0837e9cdf3f4a62366c1f75bed07a06d4ac0350f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-04-30 11:15:58 +0100

    Prepare for writing a test list for new command by introducing a stub Refs #3220

---
+++ src/Prereviewers/ReplaceLegacyPseudonym.ts
@@ -0,0 +1,26 @@
+import { Data } from 'effect'
+import type * as Command from '../Commands.ts'
+import type { OrcidId, Pseudonym, Temporal } from '../types/index.ts'
+
+export interface Input {
+  readonly orcidId: OrcidId.OrcidId
+  readonly replacedAt: Temporal.Instant
+  readonly pseudonym: Pseudonym.Pseudonym
+}
+
+type State = unknown
+
+type Error = PseudonymAlreadyInUse | PrereviewerDoesNotHaveLegacyPseudonym | PrereviewerNotRegistered
+
+export class PseudonymAlreadyInUse extends Data.TaggedError('PseudonymAlreadyInUse') {}
+
+export class PrereviewerDoesNotHaveLegacyPseudonym extends Data.TaggedError('PrereviewerDoesNotHaveLegacyPseudonym') {}
+
+export class PrereviewerNotRegistered extends Data.TaggedError('PrereviewerNotRegistered') {}
+
+export declare const ReplaceLegacyPseudonym: Command.Command<
+  'RegisteredPrereviewerImported' | 'PrereviewerRegistered' | 'LegacyPseudonymReplaced',
+  [Input],
+  State,
+  Error
+>
