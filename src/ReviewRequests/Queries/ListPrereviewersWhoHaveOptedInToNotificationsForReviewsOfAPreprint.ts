a1a6e60d068f9d093be8ed5925484b88487e2a59
Chris Wilkinson <chris@prereview.org>
2026-05-11 14:00:38 +0100

    Define a type
    Refs #3237

--- src/ReviewRequests/Queries/ListPrereviewersWhoHaveOptedInToNotificationsForReviewsOfAPreprint.ts
+++ src/ReviewRequests/Queries/ListPrereviewersWhoHaveOptedInToNotificationsForReviewsOfAPreprint.ts
@@ -1,13 +1,35 @@
+import type { Array, HashMap } from 'effect'
 import type * as Preprints from '../../Preprints/index.ts'
 import type * as Queries from '../../Queries.ts'
-import type { OrcidId } from '../../types/index.ts'
+import type { OrcidId, Uuid } from '../../types/index.ts'
 
 export type Input = Preprints.IndeterminatePreprintId
 
 export type Result = ReadonlyArray<OrcidId.OrcidId>
 
+interface PublishedReviewRequest {
+  _tag: 'PublishedReviewRequest'
+  preprintId: Preprints.IndeterminatePreprintId
+  optedIn: boolean
+  requesterId: OrcidId.OrcidId
+}
+
+interface ReviewRequestPendingPublication {
+  _tag: 'ReviewRequestPendingPublication'
+  preprintId: Preprints.IndeterminatePreprintId
+  optedIn: boolean
+  requesterId: OrcidId.OrcidId
+}
+
+type ReviewRequest = PublishedReviewRequest | ReviewRequestPendingPublication
+
+interface State {
+  readonly reviewRequests: HashMap.HashMap<Uuid.Uuid, ReviewRequest>
+  readonly reviewRequestsByPreprintId: HashMap.HashMap<string, Array.NonEmptyReadonlyArray<Uuid.Uuid>>
+}
+
 export declare const ListPrereviewersWhoHaveOptedInToNotificationsForReviewsOfAPreprint: Queries.StatefulQuery<
-  unknown,
+  State,
   [Input],
   Result
 >
