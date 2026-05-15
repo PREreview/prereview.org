36d70282426496ce98ecb23f33651daa1cc17e13
Chris Wilkinson <chris@prereview.org>
2026-05-15 16:29:32 +0100

    Make it easier to see when emails haven’t been sent
    Refs #2017

--- src/ExternalApis/Nodemailer/SendEmail.ts
+++ src/ExternalApis/Nodemailer/SendEmail.ts
@@ -2,15 +2,21 @@ import { Effect, Schema } from 'effect'
 import { Email, UnableToSendEmail } from './Email.ts'
 import { NodemailerTransporter } from './Nodemailer.ts'
 
-export const SendEmail = Effect.fn('Nodemailer.sendEmail')(function* (
-  email: Email,
-): Effect.fn.Return<void, UnableToSendEmail, NodemailerTransporter> {
-  const transporter = yield* NodemailerTransporter
+export const SendEmail = Effect.fn('Nodemailer.sendEmail')(
+  function* (email: Email): Effect.fn.Return<void, UnableToSendEmail, NodemailerTransporter> {
+    const transporter = yield* NodemailerTransporter
 
-  const encoded = yield* Effect.mapError(Schema.encode(Email)(email), error => new UnableToSendEmail({ cause: error }))
+    const encoded = yield* Effect.mapError(
+      Schema.encode(Email)(email),
+      error => new UnableToSendEmail({ cause: error }),
+    )
 
-  yield* Effect.tryPromise({
-    try: () => transporter.sendMail(encoded),
-    catch: error => new UnableToSendEmail({ cause: error }),
-  })
-})
+    yield* Effect.tryPromise({
+      try: () => transporter.sendMail(encoded),
+      catch: error => new UnableToSendEmail({ cause: error }),
+    })
+  },
+  Effect.tapErrorTag('UnableToSendEmail', error =>
+    Effect.logError('Unable to send email').pipe(Effect.annotateLogs({ cause: error.cause })),
+  ),
+)
