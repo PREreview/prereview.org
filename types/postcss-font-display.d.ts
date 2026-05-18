1f3051f26dbd4fcf485ed6bfc56109379b739c8b
Chris Wilkinson <chris@prereview.org>
2026-05-18 15:55:42 +0100

    Ensure a module is typed
    Refs #3256

---
+++ types/postcss-font-display.d.ts
@@ -0,0 +1,12 @@
+declare module 'postcss-font-display' {
+  import type { PluginCreator } from 'postcss'
+
+  export interface PostcssFontDisplayOptions {
+    display?: 'auto' | 'block' | 'swap' | 'fallback' | 'optional' | (string & {})
+    replace?: boolean
+  }
+
+  declare const postcssFontDisplay: PluginCreator<PostcssFontDisplayOptions>
+
+  export default postcssFontDisplay
+}
