1954a7e989de05cf0ecfca78edc7beb39d140e47
Daniel Haarhoff <daniel@rknt.de>
2026-05-21 15:52:02 +0100

    Start showing the form Refs #3268

--- src/WebApp/ReviewADatasetFlow/OthersNeedToBeListedOnTheReviewPage/index.ts
+++ src/WebApp/ReviewADatasetFlow/OthersNeedToBeListedOnTheReviewPage/index.ts
@@ -1,19 +1,25 @@
 import type { UrlParams } from '@effect/platform'
-import type { Effect } from 'effect'
-import type { Locale } from '../../../Context.ts'
+import { Effect } from 'effect'
+import { Locale } from '../../../Context.ts'
 import type * as DatasetReviews from '../../../DatasetReviews/index.ts'
 import type { Uuid } from '../../../types/index.ts'
 import type { LoggedInUser } from '../../../user.ts'
 import { HavingProblemsPage } from '../../HavingProblemsPage/index.ts'
 import type * as Response from '../../Response/index.ts'
+import * as OthersNeedToBeListedForm from './OthersNeedToBeListedForm.ts'
+import { OthersNeedToBeListedPage } from './OthersNeedToBeListedPage.ts'
 
 export const OthersNeedToBeListedOnTheReviewPage = ({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   datasetReviewId,
 }: {
   datasetReviewId: Uuid.Uuid
 }): Effect.Effect<Response.Response, never, DatasetReviews.DatasetReviewQueries | Locale | LoggedInUser> =>
-  HavingProblemsPage
+  Effect.gen(function* () {
+    const locale = yield* Locale
+    const form = new OthersNeedToBeListedForm.EmptyForm()
+
+    return OthersNeedToBeListedPage({ datasetReviewId, form, locale })
+  })
 
 export const OthersNeedToBeListedOnTheReviewSubmission = ({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
