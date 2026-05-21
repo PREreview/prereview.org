ad793d9b466b631857b86fe03c5deddc0c3500de
Chris Wilkinson <chris@prereview.org>
2026-05-21 16:28:42 +0100

    Implement the query
    Refs #3268

--- src/DatasetReviews/Queries/CheckIfUserCanAnswerIfOthersNeedToBeListedOnTheReview.ts
+++ src/DatasetReviews/Queries/CheckIfUserCanAnswerIfOthersNeedToBeListedOnTheReview.ts
@@ -1,8 +1,8 @@
-import type { Either, Option } from 'effect'
-import type * as Events from '../../Events.ts'
-import type * as Queries from '../../Queries.ts'
+import { Array, Either, Equal, Option, Struct, type Types } from 'effect'
+import * as Events from '../../Events.ts'
+import * as Queries from '../../Queries.ts'
 import type { OrcidId, Uuid } from '../../types/index.ts'
-import type * as Errors from '../Errors.ts'
+import * as Errors from '../Errors.ts'
 
 export interface Input {
   datasetReviewId: Uuid.Uuid
@@ -17,12 +17,52 @@ export type Result = Either.Either<
   | Errors.DatasetReviewHasBeenPublished
 >
 
-export declare const CheckIfUserCanAnswerIfOthersNeedToBeListedOnTheReview: Queries.OnDemandQuery<
-  | 'DatasetReviewWasStarted'
-  | 'AnsweredIfOthersNeedToBeListedOnTheReview'
-  | 'PublicationOfDatasetReviewWasRequested'
-  | 'DatasetReviewWasPublished',
-  [Input],
-  Either.Either.Right<Result>,
-  Either.Either.Left<Result>
->
+const createFilter = ({ datasetReviewId }: Input) =>
+  Events.EventFilter({
+    types: [
+      'DatasetReviewWasStarted',
+      'AnsweredIfOthersNeedToBeListedOnTheReview',
+      'PublicationOfDatasetReviewWasRequested',
+      'DatasetReviewWasPublished',
+    ],
+    predicates: { datasetReviewId },
+  })
+
+const query = (events: ReadonlyArray<Events.Event>, input: Input): Result =>
+  Either.gen(function* () {
+    const filter = createFilter(input)
+
+    const filteredEvents = Array.filter(events, Events.matches(filter))
+
+    const started = yield* Either.fromOption(
+      Array.findLast(filteredEvents, hasTag('DatasetReviewWasStarted')),
+      () => new Errors.DatasetReviewHasNotBeenStarted(),
+    )
+
+    if (!Equal.equals(started.authorId, input.authorId)) {
+      return yield* Either.left(new Errors.DatasetReviewWasStartedByAnotherUser())
+    }
+
+    if (Array.some(filteredEvents, hasTag('DatasetReviewWasPublished'))) {
+      return yield* Either.left(new Errors.DatasetReviewHasBeenPublished())
+    }
+
+    if (Array.some(filteredEvents, hasTag('PublicationOfDatasetReviewWasRequested'))) {
+      return yield* Either.left(new Errors.DatasetReviewIsBeingPublished())
+    }
+
+    return Option.map(
+      Array.findLast(filteredEvents, hasTag('AnsweredIfOthersNeedToBeListedOnTheReview')),
+      Struct.get('answer'),
+    )
+  })
+
+export const CheckIfUserCanAnswerIfOthersNeedToBeListedOnTheReview = Queries.OnDemandQuery({
+  name: 'DatasetReviewQueries.checkIfUserCanAnswerIfOthersNeedToBeListedOnTheReview',
+  createFilter,
+  query,
+})
+
+function hasTag<Tag extends Types.Tags<T>, T extends { _tag: string }>(...tags: ReadonlyArray<Tag>) {
+  return (tagged: T): tagged is Types.ExtractTag<T, Tag> => Array.contains(tags, tagged._tag)
+}
