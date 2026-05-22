f552bae2550f8c20a06278b11560a1a1adce5851
Chris Wilkinson <chris@prereview.org>
2026-05-22 15:49:27 +0100

    Add authorization to the type
    Refs #3268, c4edbec2c035b3066056ab95bac17cecc483cdd9

--- src/Commands.ts
+++ src/Commands.ts
@@ -17,27 +17,41 @@ export type FromStatelessCommand<T extends StatelessCommand<any>> = [T] extends 
 
 export class UnableToHandleCommand extends Data.TaggedError('UnableToHandleCommand')<{ cause?: unknown }> {}
 
-export interface Command<
+export type Command<
   EventTags extends Types.Tags<Events.Event>,
   Input extends ReadonlyArray<unknown>,
   State,
   Error,
-> {
-  name: string
-  createFilter: (...input: Input) => Events.EventFilter<EventTags>
-  foldState: (events: ReadonlyArray<Events.Event>, ...input: Input) => State
-  authorize?: (state: State, ...input: Input) => boolean
-  decide: (state: State, ...input: Input) => Either.Either<Option.Option<Events.Event>, Error>
-}
+  Authorize extends boolean = false,
+> = Authorize extends true
+  ? {
+      name: string
+      createFilter: (...input: Input) => Events.EventFilter<EventTags>
+      foldState: (events: ReadonlyArray<Events.Event>, ...input: Input) => State
+      authorize: (state: State, ...input: Input) => boolean
+      decide: (state: State, ...input: Input) => Either.Either<Option.Option<Events.Event>, Error>
+    }
+  : {
+      name: string
+      createFilter: (...input: Input) => Events.EventFilter<EventTags>
+      foldState: (events: ReadonlyArray<Events.Event>, ...input: Input) => State
+      decide: (state: State, ...input: Input) => Either.Either<Option.Option<Events.Event>, Error>
+    }
 
 export interface StatelessCommand<Input extends ReadonlyArray<unknown>> {
   name: string
   decide: (...input: Input) => Events.Event
 }
 
-export const Command: <EventTags extends Types.Tags<Events.Event>, Input extends ReadonlyArray<unknown>, State, Error>(
-  command: Command<EventTags, Input, State, Error>,
-) => Command<EventTags, Input, State, Error> = Data.struct
+export const Command: <
+  EventTags extends Types.Tags<Events.Event>,
+  Input extends ReadonlyArray<unknown>,
+  State,
+  Error,
+  Authorize extends boolean = false,
+>(
+  command: Command<EventTags, Input, State, Error, Authorize>,
+) => Command<EventTags, Input, State, Error, Authorize> = Data.struct as never
 
 export const StatelessCommand: <Input extends ReadonlyArray<unknown>>(
   command: StatelessCommand<Input>,
@@ -48,18 +62,18 @@ export const makeCommand = <
   Input extends ReadonlyArray<unknown>,
   State,
   Error,
->({
-  name,
-  createFilter,
-  foldState,
-  authorize = () => true,
-  decide,
-}: Command<EventTags, Input, State, Error>): Effect.Effect<
+  Authorize extends boolean = false,
+>(
+  command: Command<EventTags, Input, State, Error, Authorize>,
+): Effect.Effect<
   (...input: Input) => Effect.Effect<void, Error | UnableToHandleCommand>,
   never,
   EventStore.EventStore
 > =>
   Effect.gen(function* () {
+    const { name, createFilter, foldState, decide } = command
+    const authorize = 'authorize' in command ? command.authorize : () => true
+
     const context = yield* Effect.andThen(Effect.context<EventStore.EventStore>(), Context.omit(Scope.Scope))
 
     return Effect.fn(name)(
