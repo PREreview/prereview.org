afc385525beacbfa982859a6ac1699d2dbcc7f54
Daniel Haarhoff <daniel@rknt.de>
2026-05-28 11:59:52 +0100

    Pull a hardcoded viewmodel into existence by writing HTML Refs #3283

--- src/WebApp/AuthorInviteFlow/StartNowPage/StartNowPage.ts
+++ src/WebApp/AuthorInviteFlow/StartNowPage/StartNowPage.ts
@@ -1,10 +1,127 @@
-import { html, plainText } from '../../../html.ts'
+import { Array, flow } from 'effect'
+import { format } from 'fp-ts-routing'
+import type { LanguageCode } from 'iso-639-1'
+import rtlDetect from 'rtl-detect'
+import type * as DatasetReviews from '../../../DatasetReviews/index.ts'
+import type * as Datasets from '../../../Datasets/index.ts'
+import { html, plainText, rawHtml, type Html } from '../../../html.ts'
+import { translate, type SupportedLocale } from '../../../locales/index.ts'
+import * as Personas from '../../../Personas/index.ts'
+import * as Routes from '../../../routes.ts'
 import * as StatusCodes from '../../../StatusCodes.ts'
+import { renderDate } from '../../../time.ts'
+import { Doi, ProfileId } from '../../../types/index.ts'
 import { PageResponse } from '../../Response/index.ts'
 
-export const renderStartNowPage = () =>
-  PageResponse({
+export type DatasetReview = Omit<
+  DatasetReviews.PublishedReview,
+  'author' | 'otherAuthors' | 'dataset' | 'questions' | 'competingInterests'
+> & {
+  readonly author: Personas.Persona
+  readonly otherAuthors: ReadonlyArray<Personas.Persona>
+  readonly anonymousAuthors: number
+  readonly dataset: {
+    readonly id: Datasets.DatasetId
+    readonly language: LanguageCode
+    readonly title: Html
+    readonly url: URL
+  }
+}
+
+export const renderStartNowPage = ({
+  locale,
+  datasetReview,
+}: {
+  locale: SupportedLocale
+  datasetReview: DatasetReview
+}) => {
+  const t = translate(locale, 'dataset-review-page')
+
+  return PageResponse({
     status: StatusCodes.OK,
     title: plainText('Be listed as an author'),
-    main: html` <h1>Be listed as an author</h1> `,
+    main: html`
+      <h1>Be listed as an author</h1>
+      <article class="preview" tabindex="0" aria-labelledby="prereview-title">
+        <header>
+          <h2 id="prereview-title">
+            ${rawHtml(
+              t('structuredReviewTitle')({
+                dataset: html`<cite
+                  lang="${datasetReview.dataset.language}"
+                  dir="${rtlDetect.getLangDir(datasetReview.dataset.language)}"
+                  >${datasetReview.dataset.title}</cite
+                >`.toString(),
+              }),
+            )}
+          </h2>
+
+          <div class="byline">
+            ${rawHtml(
+              t('authoredBy')({
+                author: authorList(datasetReview, locale).toString(),
+                visuallyHidden: text => html`<span class="visually-hidden">${text}</span>`.toString(),
+              }),
+            )}
+          </div>
+
+          <dl>
+            <div>
+              <dt>${t('published')()}</dt>
+              <dd>${renderDate(locale)(datasetReview.published)}</dd>
+            </div>
+            <div>
+              <dt>DOI</dt>
+              <dd>
+                <a href="${Doi.toUrl(datasetReview.doi).href}" class="doi" translate="no">${datasetReview.doi}</a>
+              </dd>
+            </div>
+            <div>
+              <dt>${t('license')()}</dt>
+              <dd>
+                <a href="https://creativecommons.org/licenses/by/4.0/">
+                  <dfn>
+                    <abbr title="${t('licenseCcBy40')()}"><span translate="no">CC BY 4.0</span></abbr>
+                  </dfn>
+                </a>
+              </dd>
+            </div>
+          </dl>
+        </header>
+      </article>
+    `,
   })
+}
+
+const authorList = (datasetReview: DatasetReview, locale: SupportedLocale) => {
+  const list = Array.map(Array.make(datasetReview.author, ...datasetReview.otherAuthors), displayAuthor)
+
+  if (datasetReview.anonymousAuthors > 0) {
+    list.push(html`${datasetReview.anonymousAuthors} other author${datasetReview.anonymousAuthors > 1 ? 's' : ''}`)
+  }
+
+  return formatList(locale)(list)
+}
+
+const displayAuthor = Personas.match({
+  onPublic: persona =>
+    html`<a href="${format(Routes.profileMatch.formatter, { profile: ProfileId.forPersona(persona) })}" class="orcid"
+      >${persona.name}</a
+    >`,
+  onPseudonym: persona =>
+    html`<a href="${format(Routes.profileMatch.formatter, { profile: ProfileId.forPersona(persona) })}"
+      >${persona.pseudonym}</a
+    >`,
+})
+
+function formatList(
+  ...args: ConstructorParameters<typeof Intl.ListFormat>
+): (list: Array.NonEmptyReadonlyArray<Html | string>) => Html {
+  const formatter = new Intl.ListFormat(...args)
+
+  return flow(
+    Array.map(item => html`${item}`.toString()),
+    list => formatter.format(list),
+    rawHtml,
+  )
+}
--- src/WebApp/AuthorInviteFlow/StartNowPage/index.ts
+++ src/WebApp/AuthorInviteFlow/StartNowPage/index.ts
@@ -1,4 +1,35 @@
 import { Effect } from 'effect'
-import { renderStartNowPage } from './StartNowPage.ts'
+import { Locale } from '../../../Context.ts'
+import * as Datasets from '../../../Datasets/index.ts'
+import { html } from '../../../html.ts'
+import * as Personas from '../../../Personas/index.ts'
+import { Doi } from '../../../types/Doi.ts'
+import { NonEmptyString } from '../../../types/NonEmptyString.ts'
+import { OrcidId } from '../../../types/OrcidId.ts'
+import { PlainDate } from '../../../types/Temporal.ts'
+import { Uuid } from '../../../types/Uuid.ts'
+import { renderStartNowPage, type DatasetReview } from './StartNowPage.ts'
 
-export const StartNowPage = () => Effect.sync(renderStartNowPage)
+export const StartNowPage = () =>
+  Effect.gen(function* () {
+    const locale = yield* Locale
+    const datasetReview = {
+      doi: Doi('10.1235/234234'),
+      id: Uuid('6e1cc29e-be34-4bbc-b174-fdb4e5c57327'),
+      published: PlainDate.from('2020-12-01'),
+      author: new Personas.PublicPersona({
+        name: NonEmptyString('Josiah Carberry'),
+        orcidId: OrcidId('0000-0002-1825-0097'),
+      }),
+      otherAuthors: [],
+      anonymousAuthors: 1,
+      dataset: {
+        id: new Datasets.DryadDatasetId({ value: Doi('10.7291/foo') }),
+        language: 'en',
+        title: html``,
+        url: new URL('http://example.com'),
+      },
+    } satisfies DatasetReview
+
+    return renderStartNowPage({ locale, datasetReview })
+  })
