a55b1575bd2c762a2071aa3bc04f67ee417780a8
Daniel Haarhoff <daniel@rknt.de>
2026-05-28 16:17:49 +0100

    Use the command Refs #3283

--- src/WebApp/AuthorInviteFlow/AcceptInvite/index.ts
+++ src/WebApp/AuthorInviteFlow/AcceptInvite/index.ts
@@ -1,14 +1,30 @@
-import type { Effect } from 'effect'
-import type { AuthorInvites } from '../../../AuthorInvites/index.ts'
+import { Effect } from 'effect'
+import { AuthorInvites } from '../../../AuthorInvites/index.ts'
 import type { Locale } from '../../../Context.ts'
+import { Temporal } from '../../../types/index.ts'
 import type { Uuid } from '../../../types/Uuid.ts'
-import type { LoggedInUser } from '../../../user.ts'
+import { LoggedInUser } from '../../../user.ts'
 import { HavingProblemsPage } from '../../HavingProblemsPage/index.ts'
+import { NoPermissionPage } from '../../NoPermissionPage/index.ts'
+import { PageNotFound } from '../../PageNotFound/index.ts'
 import type { Response } from '../../Response/index.ts'
 
 export const AcceptInvite = ({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   invitationId,
 }: {
   invitationId: Uuid
-}): Effect.Effect<Response, never, AuthorInvites | LoggedInUser | Locale> => HavingProblemsPage
+}): Effect.Effect<Response, never, AuthorInvites | LoggedInUser | Locale> =>
+  Effect.gen(function* () {
+    const authorInvites = yield* AuthorInvites
+    const user = yield* LoggedInUser
+    const acceptedAt = yield* Temporal.currentInstant
+
+    yield* authorInvites.acceptInvite({ invitationId, orcidId: user.orcid, acceptedAt })
+    return yield* HavingProblemsPage
+  }).pipe(
+    Effect.catchTags({
+      InvitationNotFound: () => PageNotFound,
+      InvitationHasAlreadyBeenAcceptedByAnotherPrereviewer: () => NoPermissionPage,
+      UnableToHandleCommand: () => HavingProblemsPage,
+    }),
+  )
