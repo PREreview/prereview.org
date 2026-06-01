cb116fe2db6825d919dff072e2e2659c3894d666
Chris Wilkinson <chris@prereview.org>
2026-06-01 13:47:19 +0100

    Create a page for confirming your choices
    Refs #3283

---
+++ src/WebApp/AuthorInviteFlow/ConfirmAuthorChoicesPage/ConfirmAuthorChoicesPage.ts
@@ -0,0 +1,60 @@
+import { format } from 'fp-ts-routing'
+import { html, plainText } from '../../../html.ts'
+import * as Personas from '../../../Personas/index.ts'
+import * as Routes from '../../../routes.ts'
+import { ProfileId, type Uuid } from '../../../types/index.ts'
+import { StreamlinePageResponse } from '../../Response/index.ts'
+
+export const renderConfirmAuthorChoicesPage = ({
+  invitationId,
+  persona,
+}: {
+  invitationId: Uuid.Uuid
+  persona: Personas.Persona
+}) => {
+  return StreamlinePageResponse({
+    title: plainText('Check your details'),
+    main: html`
+      <form method="post" action="${Routes.AuthorInviteConfirmAuthorChoices.href({ invitationId })}" novalidate>
+        <h1>Check your details</h1>
+
+        <div class="summary-card">
+          <div>
+            <h2>Your details</h2>
+          </div>
+
+          <dl class="summary-list">
+            <div>
+              <dt><span>Published name</span></dt>
+              <dd>${displayAuthor(persona)}</dd>
+              <dd>
+                <a href="${Routes.AuthorInviteChooseYourPersona.href({ invitationId })}"
+                  >Change <span class="visually-hidden">name</span></a
+                >
+              </dd>
+            </div>
+          </dl>
+        </div>
+
+        <h2>Now publish your updated PREreview</h2>
+
+        <p>We will add your name to the author list.</p>
+
+        <button>Update PREreview</button>
+      </form>
+    `,
+    canonical: Routes.AuthorInviteConfirmAuthorChoices.href({ invitationId }),
+    skipToLabel: 'form',
+  })
+}
+
+const displayAuthor = Personas.match({
+  onPublic: persona =>
+    html`<a href="${format(Routes.profileMatch.formatter, { profile: ProfileId.forPersona(persona) })}" class="orcid"
+      >${persona.name}</a
+    >`,
+  onPseudonym: persona =>
+    html`<a href="${format(Routes.profileMatch.formatter, { profile: ProfileId.forPersona(persona) })}"
+      >${persona.pseudonym}</a
+    >`,
+})
--- src/WebApp/AuthorInviteFlow/ConfirmAuthorChoicesPage/index.ts
+++ src/WebApp/AuthorInviteFlow/ConfirmAuthorChoicesPage/index.ts
@@ -1,18 +1,39 @@
-import type { Effect } from 'effect'
-import type { AuthorInvites } from '../../../AuthorInvites/index.ts'
+import { Effect } from 'effect'
+import { AuthorInvites } from '../../../AuthorInvites/index.ts'
 import type { Locale } from '../../../Context.ts'
-import type * as Personas from '../../../Personas/index.ts'
+import * as Personas from '../../../Personas/index.ts'
+import * as Routes from '../../../routes.ts'
 import type { Uuid } from '../../../types/Uuid.ts'
-import type { LoggedInUser } from '../../../user.ts'
+import { LoggedInUser } from '../../../user.ts'
 import { HavingProblemsPage } from '../../HavingProblemsPage/index.ts'
-import type { Response } from '../../Response/index.ts'
+import { PageNotFound } from '../../PageNotFound/index.ts'
+import { RedirectResponse, type Response } from '../../Response/index.ts'
+import { renderConfirmAuthorChoicesPage } from './ConfirmAuthorChoicesPage.ts'
 
 export const ConfirmAuthorChoicesPage = ({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   invitationId,
 }: {
   invitationId: Uuid
-}): Effect.Effect<Response, never, Locale | LoggedInUser | Personas.Personas | AuthorInvites> => HavingProblemsPage
+}): Effect.Effect<Response, never, Locale | LoggedInUser | Personas.Personas | AuthorInvites> =>
+  Effect.gen(function* () {
+    const authorInvites = yield* AuthorInvites
+    const user = yield* LoggedInUser
+
+    const choices = yield* authorInvites.getAuthorChoicesToConfirm({ invitationId, orcidId: user.orcid })
+
+    const persona = yield* Personas.getPersona({ orcidId: user.orcid, persona: choices.persona })
+
+    return renderConfirmAuthorChoicesPage({ invitationId, persona })
+  }).pipe(
+    Effect.catchTags({
+      ChoicesHaveBeenConfirmed: () => HavingProblemsPage,
+      PersonaHasNotBeenChosen: () =>
+        Effect.succeed(RedirectResponse({ location: Routes.AuthorInviteChooseYourPersona.href({ invitationId }) })),
+      PrereviewerIsNotListedOnTheReview: () => PageNotFound,
+      UnableToGetPersona: () => HavingProblemsPage,
+      UnableToQuery: () => HavingProblemsPage,
+    }),
+  )
 
 export const ConfirmAuthorChoicesSubmission = ({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
