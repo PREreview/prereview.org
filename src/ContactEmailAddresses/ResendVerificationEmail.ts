33a745fc5416597643ac5fcee0b2908bc7d4d302
Chris Wilkinson <chris@prereview.org>
2026-06-23 11:45:58 +0100

    Implement the command
    Refs #3331

--- src/ContactEmailAddresses/ResendVerificationEmail.ts
+++ src/ContactEmailAddresses/ResendVerificationEmail.ts
@@ -1,10 +1,12 @@
-import type { Effect } from 'effect'
-import { type ContactEmailAddressIsNotFound, ContactEmailAddressIsUnavailable } from '../contact-email-address.ts'
+import { Effect, pipe } from 'effect'
+import { ContactEmailAddressIsNotFound, ContactEmailAddressIsUnavailable } from '../contact-email-address.ts'
 import type { Locale } from '../Context.ts'
-import type { Email, OrcidRecords } from '../ExternalInteractions/index.ts'
-import type * as Keyv from '../keyv.ts'
+import { MakeDeprecatedLoggerEnv } from '../DeprecatedServices.ts'
+import { Email, OrcidRecords } from '../ExternalInteractions/index.ts'
+import * as Keyv from '../keyv.ts'
+import { FptsToEffect } from '../RefactoringUtilities/index.ts'
 import type { OrcidId } from '../types/OrcidId.ts'
-import type { ContactEmailAddressHasAlreadyBeenVerified } from './VerifyContactEmailAddress.ts'
+import { ContactEmailAddressHasAlreadyBeenVerified } from './VerifyContactEmailAddress.ts'
 
 export interface Input {
   readonly orcidId: OrcidId
@@ -18,5 +20,44 @@ export type Error =
 
 export const ResendVerificationEmail: (
   contactEmailAddressStore: (typeof Keyv.KeyvStores.Service)['contactEmailAddressStore'],
-) => (input: Input) => Effect.Effect<void, Error, Email.Email | Locale | OrcidRecords.OrcidRecords> = () => () =>
-  new ContactEmailAddressIsUnavailable({ cause: 'not implemented' })
+) => (
+  input: Input,
+) => Effect.Effect<void, Error, Email.Email | Locale | OrcidRecords.OrcidRecords> = contactEmailAddressStore =>
+  Effect.fn(
+    function* (input) {
+      const email = yield* Email.Email
+      const orcidRecords = yield* OrcidRecords.OrcidRecords
+      const loggerEnv = yield* MakeDeprecatedLoggerEnv
+
+      const contactAddress = yield* pipe(
+        FptsToEffect.readerTaskEither(Keyv.getContactEmailAddress(input.orcidId), {
+          contactEmailAddressStore,
+          ...loggerEnv,
+        }),
+        Effect.filterOrElse(
+          contactAddress => contactAddress._tag !== 'VerifiedContactEmailAddress',
+          () => new ContactEmailAddressHasAlreadyBeenVerified(),
+        ),
+        Effect.catchIf(
+          error => error === 'not-found',
+          () => new ContactEmailAddressIsNotFound(),
+        ),
+        Effect.catchIf(
+          error => error === 'unavailable',
+          () => new ContactEmailAddressIsUnavailable({ cause: 'unknown' }),
+        ),
+      )
+
+      const name = yield* orcidRecords.getName(input.orcidId)
+
+      yield* email.verifyContactEmailAddress({
+        name,
+        emailAddress: contactAddress,
+        redirectTo: input.resumeAt,
+      })
+    },
+    Effect.catchTags({
+      NameIsNotAvailable: error => new ContactEmailAddressIsUnavailable({ cause: error }),
+      UnableToSendEmail: error => new ContactEmailAddressIsUnavailable({ cause: error }),
+    }),
+  )
