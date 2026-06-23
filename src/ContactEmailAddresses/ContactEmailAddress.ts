574f288e027ea41495190ccaf67e802e3c0d34af
Chris Wilkinson <chris@prereview.org>
2026-06-23 17:45:55 +0100

    Remove unused code
    Refs #3331

--- src/ContactEmailAddresses/ContactEmailAddress.ts
+++ src/ContactEmailAddresses/ContactEmailAddress.ts
@@ -4,10 +4,6 @@ import type { Uuid } from '../types/Uuid.ts'
 
 export class ContactEmailAddressIsNotFound extends Data.TaggedError('ContactEmailAddressIsNotFound') {}
 
-export class ContactEmailAddressIsUnavailable extends Data.TaggedError('ContactEmailAddressIsUnavailable')<{
-  cause?: unknown
-}> {}
-
 export type ContactEmailAddress = VerifiedContactEmailAddress | UnverifiedContactEmailAddress
 
 export class VerifiedContactEmailAddress extends Data.TaggedClass('VerifiedContactEmailAddress')<{
