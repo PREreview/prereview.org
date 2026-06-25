f436d74f9551a47dda7f31ef0c4450d53314b594
Chris Wilkinson <chris@prereview.org>
2026-06-25 13:34:12 +0100

    Prepare to append events
    Refs #3331

--- test/ContactEmailAddresses/VerifyContactEmailAddress.test.ts
+++ test/ContactEmailAddresses/VerifyContactEmailAddress.test.ts
@@ -1,13 +1,13 @@
 import { LibsqlClient } from '@effect/sql-libsql'
 import { expect, it } from '@effect/vitest'
-import { Effect, Either, Layer } from 'effect'
+import { Array, Effect, Either, Layer, Option, pipe, Struct } from 'effect'
 import {
   ContactEmailAddressHasAlreadyBeenVerified,
   ContactEmailAddressIsNotFound,
   VerificationTokenInvalid,
 } from '../../src/ContactEmailAddresses/index.ts'
 import * as _ from '../../src/ContactEmailAddresses/VerifyContactEmailAddress.ts'
-import { Events } from '../../src/Events.ts'
+import { type Event, Events } from '../../src/Events.ts'
 import { EventStore } from '../../src/EventStore.ts'
 import { Keyv } from '../../src/keyv.ts'
 import { SensitiveDataStore } from '../../src/SensitiveDataStore.ts'
@@ -22,32 +22,38 @@ const orcidWithNoEmailAddress = OrcidId('0000-0003-4921-6155')
 const validVerificationToken = Uuid('27e501e5-bc25-4975-995a-b0e789ac0865')
 const invalidVerificationToken = Uuid('784f7806-14a2-47dd-9801-00364bdf1829')
 
-it.effect.each<[string, _.Input, Either.Either<void, _.Error>, 'verified' | 'unverified' | 'does-not-exist']>([
+it.effect.each<
+  [string, _.Input, Either.Either<void, _.Error>, 'verified' | 'unverified' | 'does-not-exist', ReadonlyArray<Event>]
+>([
   [
     'currently unverified, valid token',
     { orcid: orcidWithUnverified, verificationToken: validVerificationToken },
     Either.void,
     'verified',
+    [],
   ],
   [
     'currently unverified, invalid token',
     { orcid: orcidWithUnverified, verificationToken: invalidVerificationToken },
     Either.left(new VerificationTokenInvalid()),
     'unverified',
+    [],
   ],
   [
     'no email address',
     { orcid: orcidWithNoEmailAddress, verificationToken: validVerificationToken },
     Either.left(new ContactEmailAddressIsNotFound()),
     'does-not-exist',
+    [],
   ],
   [
     'already verified',
     { orcid: orcidWithVerified, verificationToken: validVerificationToken },
     Either.left(new ContactEmailAddressHasAlreadyBeenVerified()),
     'verified',
+    [],
   ],
-])('%s', ([, input, expectedReturn, expectedState]) =>
+])('%s', ([, input, expectedReturn, expectedState, expectedEvents]) =>
   Effect.gen(function* () {
     const store = new Keyv()
 
@@ -66,11 +72,16 @@ it.effect.each<[string, _.Input, Either.Either<void, _.Error>, 'verified' | 'unv
       _.VerifyContactEmailAddress(store)(input).pipe(Effect.provideService(EventStore, eventStore)),
     )
     const actualState = yield* Effect.promise(() => store.get(input.orcid))
+    const actualEvents = yield* pipe(
+      eventStore.all,
+      Effect.andThen(Option.match({ onNone: Array.empty, onSome: Struct.get('events') })),
+    )
 
     expect(actualReturn).toStrictEqual(expectedReturn)
     expect(actualState).toStrictEqual(
       expectedState === 'does-not-exist' ? undefined : expect.objectContaining({ type: expectedState }),
     )
+    expect(actualEvents).toStrictEqual(expectedEvents)
   }).pipe(
     Effect.provide([
       Layer.mock(GenerateUuid, {}),
