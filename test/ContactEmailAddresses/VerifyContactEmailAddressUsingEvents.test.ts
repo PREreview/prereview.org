d146fee9f83b65b1a34d277e827837c5def9ade2
Daniel Haarhoff <daniel@rknt.de>
2026-06-25 15:31:31 +0100

    Write a testlist for event based command Refs #3331

---
+++ test/ContactEmailAddresses/VerifyContactEmailAddressUsingEvents.test.ts
@@ -0,0 +1,80 @@
+import { expect, test } from '@effect/vitest'
+import { Temporal } from '@js-temporal/polyfill'
+import { Either, Option } from 'effect'
+import {
+  ContactEmailAddressHasAlreadyBeenVerified,
+  ContactEmailAddressIsNotFound,
+  VerificationTokenInvalid,
+} from '../../src/ContactEmailAddresses/Errors.ts'
+import * as _ from '../../src/ContactEmailAddresses/VerifyContactEmailAddressUsingEvents.ts'
+import * as Events from '../../src/Events.ts'
+import { EmailAddress } from '../../src/types/EmailAddress.ts'
+import { OrcidId } from '../../src/types/OrcidId.ts'
+import { Uuid } from '../../src/types/Uuid.ts'
+
+const orcidWithVerified = OrcidId('0000-0002-1825-0097')
+const orcidWithUnverified = OrcidId('0000-0002-6109-0367')
+const orcidWithNoEmailAddress = OrcidId('0000-0003-4921-6155')
+
+const validVerificationToken = Uuid('27e501e5-bc25-4975-995a-b0e789ac0865')
+const invalidVerificationToken = Uuid('784f7806-14a2-47dd-9801-00364bdf1829')
+
+const verifiedImported = new Events.ContactAddressImported({
+  orcidId: orcidWithVerified,
+  contactAddressId: Uuid('4cd3642a-74d4-418d-a41d-d2edcd3dbcad'),
+  emailAddress: Option.some(EmailAddress('josiah@example.com')),
+  verificationStatus: { status: 'verified' },
+})
+
+const unverifiedImported = new Events.ContactAddressImported({
+  orcidId: orcidWithUnverified,
+  contactAddressId: Uuid('1a2c8e69-39c4-40b7-b3db-b941a74ba3e8'),
+  emailAddress: Option.some(EmailAddress('josiah@example.com')),
+  verificationStatus: { status: 'unverified', token: validVerificationToken },
+})
+
+const verifiedPreviouslyUnverified = new Events.ContactAddressVerified({
+  contactAddressId: unverifiedImported.contactAddressId,
+  verifiedAt: Temporal.Now.instant(),
+})
+
+test.fails.each<[string, ReadonlyArray<Events.Event>, _.Input, Either.Either<Option.Option<Events.Event>, _.Error>]>([
+  [
+    'currently unverified, valid token',
+    [unverifiedImported],
+    { orcid: orcidWithUnverified, verificationToken: validVerificationToken },
+    Either.right(Option.some(verifiedPreviouslyUnverified)),
+  ],
+  [
+    'currently unverified, invalid token',
+    [unverifiedImported],
+    { orcid: orcidWithUnverified, verificationToken: invalidVerificationToken },
+    Either.left(new VerificationTokenInvalid()),
+  ],
+  [
+    'no events',
+    [],
+    { orcid: orcidWithNoEmailAddress, verificationToken: validVerificationToken },
+    Either.left(new ContactEmailAddressIsNotFound()),
+  ],
+  [
+    'already verified imported',
+    [verifiedImported],
+    { orcid: orcidWithVerified, verificationToken: validVerificationToken },
+    Either.left(new ContactEmailAddressHasAlreadyBeenVerified()),
+  ],
+  [
+    'already verified after imported unverified',
+    [unverifiedImported, verifiedPreviouslyUnverified],
+    { orcid: orcidWithVerified, verificationToken: validVerificationToken },
+    Either.left(new ContactEmailAddressHasAlreadyBeenVerified()),
+  ],
+])('%s', (_name, events, input, expected) => {
+  const { foldState, decide } = _.VerifyContactEmailAddressUsingEvents
+
+  const state = foldState(events, input)
+
+  const actual = decide(state, input)
+
+  expect(actual).toStrictEqual(expected)
+})
