47fc33973fa26b1a4355bda2626833af9f3c02d7
Chris Wilkinson <chris@prereview.org>
2026-07-06 12:01:43 +0100

    Parse the form
    Refs #3358

--- src/WebApp/RequestAReviewFlow/EnterEmailAddressPage/index.ts
+++ src/WebApp/RequestAReviewFlow/EnterEmailAddressPage/index.ts
@@ -1,4 +1,5 @@
-import { Effect } from 'effect'
+import type { UrlParams } from '@effect/platform'
+import { Effect, Match } from 'effect'
 import { Locale } from '../../../Context.ts'
 import { Preprints, type IndeterminatePreprintId } from '../../../Preprints/index.ts'
 import { ReviewRequestQueries } from '../../../ReviewRequests/index.ts'
@@ -48,7 +49,51 @@ export const EnterEmailAddressPage: ({
 )
 
 export const EnterEmailAddressSubmission: ({
+  body,
   preprintId,
 }: {
+  body: UrlParams.UrlParams
   preprintId: IndeterminatePreprintId
-}) => Effect.Effect<Response, never, Locale> = () => HavingProblemsPage
+}) => Effect.Effect<Response, never, Locale | Preprints | ReviewRequestQueries> = Effect.fn(
+  'RequestAReviewFlow.EnterEmailAddressSubmission',
+)(
+  function* ({ body, preprintId }) {
+    const preprints = yield* Preprints
+    const reviewRequestQueries = yield* ReviewRequestQueries
+    const user = yield* EnsureUserIsLoggedIn
+    const locale = yield* Locale
+
+    const preprint = yield* preprints.getPreprintTitle(preprintId)
+
+    const { contactAddress } = yield* reviewRequestQueries.doesAReviewRequestNeedAContactAddressToBeVerified({
+      requesterId: user.orcid,
+      preprintId,
+    })
+
+    if (contactAddress._tag === 'VerifiedContactAddress') {
+      return RedirectResponse({ location: Routes.RequestAReviewPublished.href({ preprintId: preprint.id }) })
+    }
+
+    const form = EnterEmailAddressForm.fromBody(body)
+
+    return yield* Match.valueTags(form, {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      CompletedForm: Effect.fnUntraced(function* (form: EnterEmailAddressForm.CompletedForm) {
+        return yield* HavingProblemsPage
+      }),
+      InvalidForm: (form: EnterEmailAddressForm.InvalidForm) =>
+        Effect.succeed(renderEnterEmailAddressPage({ preprintId: preprint.id, form, locale })),
+    })
+  },
+  (result, { preprintId }) =>
+    Effect.catchTags(result, {
+      PreprintIsNotFound: () => PageNotFound,
+      PreprintIsUnavailable: () => HavingProblemsPage,
+      ReviewRequestHasBeenPublished: () =>
+        Effect.succeed(RedirectResponse({ location: Routes.RequestAReviewPublished.href({ preprintId }) })),
+      UnableToQuery: () => HavingProblemsPage,
+      UnknownReviewRequest: () => PageNotFound,
+      UserIsNotLoggedIn: () =>
+        Effect.succeed(LogInResponse({ location: Routes.RequestAReviewOfThisPreprint.href({ preprintId }) })),
+    }),
+)
