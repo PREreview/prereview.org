d2259bbf3c34738cc5e457dd425b4ad7db3c2921
Chris Wilkinson <chris@prereview.org>
2026-07-08 13:13:13 +0100

    Extract strings
    Refs #3368

--- src/WebApp/RequestAReviewFlow/NeedToVerifyEmailAddressPage/NeedToVerifyEmailAddressPage.ts
+++ src/WebApp/RequestAReviewFlow/NeedToVerifyEmailAddressPage/NeedToVerifyEmailAddressPage.ts
@@ -1,5 +1,5 @@
 import { html, plainText } from '../../../html.ts'
-import type { SupportedLocale } from '../../../locales/index.ts'
+import { translate, type SupportedLocale } from '../../../locales/index.ts'
 import type { PreprintId } from '../../../Preprints/index.ts'
 import * as Routes from '../../../routes.ts'
 import type { EmailAddress } from '../../../types/index.ts'
@@ -8,26 +8,23 @@ import { StreamlinePageResponse } from '../../Response/index.ts'
 export const renderNeedToVerifyEmailAddressPage = ({
   preprintId,
   emailAddress,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   locale,
 }: {
   preprintId: PreprintId
   emailAddress: EmailAddress.EmailAddress
   locale: SupportedLocale
-}) =>
-  StreamlinePageResponse({
-    title: plainText('Verify your email address'),
+}) => {
+  const t = translate(locale, 'request-review-flow')
+
+  return StreamlinePageResponse({
+    title: plainText(t('verifyYourEmailAddress')()),
     main: html`
-      <h1><span lang="en" dir="ltr">Verify your email address</span></h1>
+      <h1>${t('verifyYourEmailAddress')()}</h1>
 
-      <p>
-        <span lang="en" dir="ltr"
-          >Please open the email we sent to <bdi>${emailAddress}</bdi> and follow the link. You’ll then be able to
-          publish your request.</span
-        >
-      </p>
+      <p>${t('followLinkInEmail')({ emailAddress: html`<bdi>${emailAddress}</bdi>` })}</p>
 
-      <p><span lang="en" dir="ltr">Once your address is verified, you can close this page.</span></p>
+      <p>${t('closePageOnceVerified')()}</p>
     `,
     canonical: Routes.RequestAReviewNeedToVerifyEmailAddress.href({ preprintId }),
   })
+}
