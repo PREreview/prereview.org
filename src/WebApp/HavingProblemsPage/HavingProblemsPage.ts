96be77852b7973b1a58e7a662f208cae2ef22c30
Chris Wilkinson <chris@prereview.org>
2026-07-10 15:15:38 +0100

    Remove duplication
    Refs #2017

--- src/WebApp/HavingProblemsPage/HavingProblemsPage.ts
+++ src/WebApp/HavingProblemsPage/HavingProblemsPage.ts
@@ -3,13 +3,16 @@ import { translate, type SupportedLocale } from '../../locales/index.ts'
 import * as StatusCodes from '../../StatusCodes.ts'
 import { PageResponse } from '../Response/index.ts'
 
-export const createHavingProblemsPage = (locale: SupportedLocale): PageResponse =>
-  PageResponse({
-    status: StatusCodes.ServiceUnavailable,
-    title: plainText(translate(locale, 'having-problems-page', 'havingProblemsTitle')()),
-    main: html`
-      <h1>${translate(locale, 'having-problems-page', 'havingProblemsTitle')()}</h1>
+export const createHavingProblemsPage = (locale: SupportedLocale): PageResponse => {
+  const t = translate(locale, 'having-problems-page')
 
-      <p>${translate(locale, 'having-problems-page', 'tryAgainLater')()}</p>
+  return PageResponse({
+    status: StatusCodes.ServiceUnavailable,
+    title: plainText(t('havingProblemsTitle')()),
+    main: html`
+      <h1>${t('havingProblemsTitle')()}</h1>
+
+      <p>${t('tryAgainLater')()}</p>
     `,
   })
+}
