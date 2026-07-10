1ba8594d97cbfb78dc94152021468d002b8afd88
Chris Wilkinson <chris@prereview.org>
2026-07-10 15:19:38 +0100

    Remove duplication
    Refs #2017

--- src/WebApp/Router/RemovedForNowPage.ts
+++ src/WebApp/Router/RemovedForNowPage.ts
@@ -3,21 +3,18 @@ import { type SupportedLocale, translate } from '../../locales/index.ts'
 import * as StatusCodes from '../../StatusCodes.ts'
 import { PageResponse } from '../Response/index.ts'
 
-export const removedForNowPage = (locale: SupportedLocale) =>
-  PageResponse({
-    title: plainText(translate(locale, 'legacy-routes', 'temporaryTitle')()),
+export const removedForNowPage = (locale: SupportedLocale) => {
+  const t = translate(locale, 'legacy-routes')
+
+  return PageResponse({
+    title: plainText(t('temporaryTitle')()),
     status: StatusCodes.NotFound,
     main: html`
-      <h1>${translate(locale, 'legacy-routes', 'temporaryTitle')()}</h1>
+      <h1>${t('temporaryTitle')()}</h1>
 
-      <p>${translate(locale, 'legacy-routes', 'temporaryMessage')()}</p>
+      <p>${t('temporaryMessage')()}</p>
 
-      <p>
-        ${translate(
-          locale,
-          'legacy-routes',
-          'getInTouch',
-        )({ link: text => html`<a href="mailto:help@prereview.org">${text}</a>` })}
-      </p>
+      <p>${t('getInTouch')({ link: text => html`<a href="mailto:help@prereview.org">${text}</a>` })}</p>
     `,
   })
+}
