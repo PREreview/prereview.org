86a46977b5c5329669b7f31a4068a8a8eb062a9b
Chris Wilkinson <chris@prereview.org>
2026-07-14 10:58:18 +0100

    Write a query
    Refs #3379

---
+++ src/Clubs/GetClubsThatAPrereviewerLeads.ts
@@ -0,0 +1,16 @@
+import { Array, Option } from 'effect'
+import type { OrcidId } from '../types/OrcidId.ts'
+import type { ClubDetails, ClubName } from './Clubs.ts'
+
+export type Input = OrcidId
+
+export type Result = ReadonlyArray<ClubName>
+
+export const GetClubsThatAPrereviewerLeads =
+  (clubs: Array.NonEmptyReadonlyArray<ClubDetails>) =>
+  (input: Input): Result =>
+    Array.filterMap(clubs, club =>
+      Array.contains(club.leads, input)
+        ? Option.some({ id: club.id, language: club.name.language, name: club.name.text })
+        : Option.none(),
+    )
---
+++ test/Clubs/GetClubsThatAPrereviewerLeads.test.ts
@@ -0,0 +1,62 @@
+import { expect, it } from '@effect/vitest'
+import type { Array } from 'effect'
+import * as _ from '../../src/Clubs/GetClubsThatAPrereviewerLeads.ts'
+import type { ClubDetails } from '../../src/Clubs/index.ts'
+import { html } from '../../src/html.ts'
+import { Name } from '../../src/types/Name.ts'
+import { OrcidId } from '../../src/types/OrcidId.ts'
+import { Slug } from '../../src/types/Slug.ts'
+import { PlainDate } from '../../src/types/Temporal.ts'
+import { Uuid } from '../../src/types/Uuid.ts'
+
+const orcidId1Club = OrcidId('0000-0002-1825-0097')
+const orcidId2Clubs = OrcidId('0000-0003-4921-6155')
+const orcidId0Clubs = OrcidId('0000-0002-6109-0367')
+
+const clubs = [
+  {
+    id: Uuid('a0b77186-b242-44ed-a969-49eca634d701'),
+    name: {
+      language: 'en',
+      text: Name('Superheroes Club'),
+    },
+    slug: Slug('superheroes-club'),
+    description: {
+      language: 'en',
+      text: html`<p>Some text.</p>`,
+    },
+    added: PlainDate.from('2024-01-02'),
+    leads: [orcidId1Club, orcidId2Clubs],
+  },
+  {
+    id: Uuid('26a8ce99-8c88-4874-bfe6-71324b8970c9'),
+    name: {
+      language: 'en',
+      text: Name('Villains Club'),
+    },
+    slug: Slug('villains-club'),
+    description: {
+      language: 'en',
+      text: html`<p>Some text.</p>`,
+    },
+    added: PlainDate.from('2025-01-02'),
+    leads: [orcidId2Clubs],
+  },
+] satisfies Array.NonEmptyReadonlyArray<ClubDetails>
+
+it.each<[string, _.Input, _.Result]>([
+  ['1 club', orcidId1Club, [{ id: clubs[0].id, language: clubs[0].name.language, name: clubs[0].name.text }]],
+  [
+    '2 clubs',
+    orcidId2Clubs,
+    [
+      { id: clubs[0].id, language: clubs[0].name.language, name: clubs[0].name.text },
+      { id: clubs[1].id, language: clubs[1].name.language, name: clubs[1].name.text },
+    ],
+  ],
+  ['0 clubs', orcidId0Clubs, []],
+])('%s', (_name, input, expected) => {
+  const actual = _.GetClubsThatAPrereviewerLeads(clubs)(input)
+
+  expect(actual).toStrictEqual(expected)
+})
