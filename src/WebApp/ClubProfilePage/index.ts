5ffe241ee26f0e15b92c117cee7f8eb7ac61e5d7
Chris Wilkinson <chris@prereview.org>
2026-07-14 09:56:57 +0100

    Match PREreviews by using names as-on ORCID records
    Refs #3379

--- src/WebApp/ClubProfilePage/index.ts
+++ src/WebApp/ClubProfilePage/index.ts
@@ -1,15 +1,35 @@
-import { Effect } from 'effect'
+import { Array, Effect, pipe } from 'effect'
 import * as Clubs from '../../Clubs/index.ts'
 import { Locale } from '../../Context.ts'
+import { OrcidRecords } from '../../ExternalInteractions/index.ts'
 import * as Prereviews from '../../Prereviews/index.ts'
 import { HavingProblemsPage } from '../HavingProblemsPage/index.ts'
 import { createPage } from './ClubProfilePage.ts'
 
 export const ClubProfilePage = Effect.fn(
   function* ({ id }: { id: Clubs.ClubId }) {
-    const club = Clubs.getClubDetails(id)
     const locale = yield* Locale
-    const prereviews = yield* Prereviews.getForClub(id)
+
+    const { club, prereviews } = yield* Effect.all(
+      {
+        club: pipe(Clubs.getClubDetails(id), club =>
+          pipe(
+            Array.map(
+              club.leads,
+              Effect.fnUntraced(function* ({ orcid }) {
+                const name = yield* OrcidRecords.getName(orcid)
+
+                return { name, orcid }
+              }),
+            ),
+            Effect.allWith({ concurrency: 'inherit' }),
+            Effect.andThen(leads => ({ ...club, leads })),
+          ),
+        ),
+        prereviews: Prereviews.getForClub(id),
+      },
+      { concurrency: 'inherit' },
+    )
 
     return createPage({ club, id, prereviews, locale })
   },
--- test/WebApp/ClubProfilePage.test.ts
+++ test/WebApp/ClubProfilePage.test.ts
@@ -3,6 +3,7 @@ import { Effect, Layer, pipe } from 'effect'
 import { encode } from 'html-entities'
 import { getClubName } from '../../src/Clubs/index.ts'
 import { Locale } from '../../src/Context.ts'
+import { OrcidRecords } from '../../src/ExternalInteractions/index.ts'
 import * as Prereviews from '../../src/Prereviews/index.ts'
 import * as Routes from '../../src/routes.ts'
 import * as StatusCodes from '../../src/StatusCodes.ts'
@@ -27,8 +28,9 @@ describe('ClubProfilePage', () => {
           .map(args => new Prereviews.RecentPreprintPrereview(args)),
       ),
       fc.supportedLocale(),
+      fc.name(),
     ],
-    ([clubId, prereviews, locale]) =>
+    ([clubId, prereviews, locale, name]) =>
       Effect.gen(function* () {
         const getForClub = vi.fn<(typeof Prereviews.Prereviews.Service)['getForClub']>(_ => Effect.succeed(prereviews))
 
@@ -47,26 +49,35 @@ describe('ClubProfilePage', () => {
           js: [],
         })
         expect(getForClub).toHaveBeenCalledWith(clubId)
-      }).pipe(Effect.provideService(Locale, locale)),
+      }).pipe(
+        Effect.provide(Layer.mock(OrcidRecords.OrcidRecords, { getName: () => Effect.succeed(name) })),
+        Effect.provideService(Locale, locale),
+      ),
   )
 
-  it.effect.prop('when the PREreviews are unavailable', [fc.clubId(), fc.supportedLocale()], ([clubId, locale]) =>
-    Effect.gen(function* () {
-      const actual = yield* pipe(
-        _.ClubProfilePage({ id: clubId }),
-        Effect.provide(
-          Layer.mock(Prereviews.Prereviews, { getForClub: () => new Prereviews.PrereviewsAreUnavailable() }),
-        ),
-      )
+  it.effect.prop(
+    'when the PREreviews are unavailable',
+    [fc.clubId(), fc.supportedLocale(), fc.name()],
+    ([clubId, locale, name]) =>
+      Effect.gen(function* () {
+        const actual = yield* pipe(
+          _.ClubProfilePage({ id: clubId }),
+          Effect.provide(
+            Layer.mock(Prereviews.Prereviews, { getForClub: () => new Prereviews.PrereviewsAreUnavailable() }),
+          ),
+        )
 
-      expect(actual).toStrictEqual({
-        _tag: 'PageResponse',
-        status: StatusCodes.ServiceUnavailable,
-        title: expect.anything(),
-        main: expect.anything(),
-        skipToLabel: 'main',
-        js: [],
-      })
-    }).pipe(Effect.provideService(Locale, locale)),
+        expect(actual).toStrictEqual({
+          _tag: 'PageResponse',
+          status: StatusCodes.ServiceUnavailable,
+          title: expect.anything(),
+          main: expect.anything(),
+          skipToLabel: 'main',
+          js: [],
+        })
+      }).pipe(
+        Effect.provide(Layer.mock(OrcidRecords.OrcidRecords, { getName: () => Effect.succeed(name) })),
+        Effect.provideService(Locale, locale),
+      ),
   )
 })
