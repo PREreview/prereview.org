d5d6a1294e297297dee902ce2e90263d73626ec5
Chris Wilkinson <chris@prereview.org>
2026-07-15 09:42:54 +0100

    Remove knowledge of hardcoded data
    Refs #3379

--- test/WebApp/my-prereviews-page/fc.ts
+++ test/WebApp/my-prereviews-page/fc.ts
@@ -1,6 +1,4 @@
-import { getClubName, getClubSlug } from '../../../src/Clubs/index.ts'
 import { RecentDatasetPrereview, RecentPreprintPrereview } from '../../../src/Prereviews/Prereview.ts'
-import { Uuid } from '../../../src/types/Uuid.ts'
 import type { Prereview } from '../../../src/WebApp/my-prereviews-page/prereviews.ts'
 import * as fc from '../../fc.ts'
 
@@ -12,12 +10,7 @@ export const localPrereview = (): fc.Arbitrary<Prereview> =>
       .record(
         {
           id: fc.integer(),
-          club: fc.clubId().map(id => ({
-            id: Uuid(id),
-            name: getClubName(id).text,
-            language: getClubName(id).language,
-            slug: getClubSlug(id),
-          })),
+          club: fc.clubName(),
           reviewers: fc.record({ named: fc.nonEmptyArray(fc.name()), anonymous: fc.integer({ min: 0 }) }),
           published: fc.plainDate(),
           fields: fc.array(fc.fieldId()),
@@ -30,6 +23,7 @@ export const localPrereview = (): fc.Arbitrary<Prereview> =>
     fc
       .record({
         id: fc.uuid(),
+        club: fc.clubName(),
         doi: fc.doi(),
         author: fc.persona(),
         otherAuthors: fc.array(fc.persona()),
