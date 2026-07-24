99a16b070785dac70af59001533dc66209753016
Chris Wilkinson <chris@prereview.org>
2026-07-24 14:33:09 +0100

    Create a way to turn Contentful entries into spotlight banners
    Refs #3404

---
+++ src/SpotlightBanners/GetCurrentBanner/EntryToSpotlightBanner.ts
@@ -0,0 +1,41 @@
+import { ParseResult, Schema } from 'effect'
+import { ContentfulId, Entry } from '../../ExternalApis/Contentful/index.ts'
+import { SpotlightBanner } from '../Types.ts'
+
+const SpotlightBannerEntry = Schema.Struct({
+  ...Entry.fields,
+  sys: Schema.Struct({
+    ...Entry.fields.sys.fields,
+    contentType: Schema.Struct({
+      ...Entry.fields.sys.fields.contentType.fields,
+      sys: Schema.Struct({
+        ...Entry.fields.sys.fields.contentType.fields.sys.fields,
+        id: Schema.Literal(ContentfulId.make('banner')),
+      }),
+    }),
+  }),
+  fields: Schema.Struct({
+    title: Schema.String,
+    text: Schema.String,
+    callToAction: Schema.String,
+    link: Schema.String,
+  }),
+})
+
+export const EntryToSpotlightBanner = Schema.transformOrFail(Schema.typeSchema(SpotlightBannerEntry), SpotlightBanner, {
+  strict: true,
+  decode: entry =>
+    ParseResult.succeed({
+      id: entry.sys.id,
+      title: entry.fields.title,
+      description: entry.fields.text,
+      callToAction: {
+        text: entry.fields.callToAction,
+        url: entry.fields.link,
+      },
+    }),
+  encode: (spotlightBanner, _, ast) =>
+    ParseResult.fail(
+      new ParseResult.Forbidden(ast, spotlightBanner, 'Encoding spotlight banners back to an entry is forbidden.'),
+    ),
+})
---
+++ test/SpotlightBanners/GetCurrentBanner/EntryToSpotlightBanner.test.ts
@@ -0,0 +1,38 @@
+import { FileSystem } from '@effect/platform'
+import { NodeFileSystem } from '@effect/platform-node'
+import { expect, it } from '@effect/vitest'
+import { Array, Effect, pipe, Schema, Struct } from 'effect'
+import { URL } from 'url'
+import { Entries } from '../../../src/ExternalApis/Contentful/index.ts'
+import { plainText, rawHtml } from '../../../src/html.ts'
+import * as _ from '../../../src/SpotlightBanners/GetCurrentBanner/EntryToSpotlightBanner.ts'
+import { SpotlightBanner } from '../../../src/SpotlightBanners/index.ts'
+
+it.effect.each([
+  {
+    response: 'single-banner',
+    index: 0,
+    expected: new SpotlightBanner({
+      id: '19ku1fGWddXyrFone7Pu62',
+      title: plainText('Matchmaking experiment'),
+      description: rawHtml('Check out our experiment for suggestions about what to review next!'),
+      callToAction: {
+        text: plainText('Find preprints to review'),
+        url: new URL('https://matchmaking-experiment.prereview.org/'),
+      },
+    }),
+  },
+])('can parse a record ($response $index)', ({ response, index, expected }) =>
+  Effect.gen(function* () {
+    const actual = yield* pipe(
+      FileSystem.FileSystem,
+      Effect.andThen(fs => fs.readFileString(`test/ExternalApis/Contentful/Samples/${response}.json`)),
+      Effect.andThen(Schema.decodeUnknown(Schema.parseJson(Entries))),
+      Effect.andThen(Struct.get('items')),
+      Effect.andThen(Array.get(index)),
+      Effect.andThen(Schema.decodeUnknown(_.EntryToSpotlightBanner)),
+    )
+
+    expect(actual).toStrictEqual(expected)
+  }).pipe(Effect.provide(NodeFileSystem.layer)),
+)
