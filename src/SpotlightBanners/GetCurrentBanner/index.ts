dbfee8835279af21c55882f72331bf568a6eb25e
Chris Wilkinson <chris@prereview.org>
2026-07-27 15:03:34 +0100

    Implement the method
    Refs #3404

--- src/SpotlightBanners/GetCurrentBanner/index.ts
+++ src/SpotlightBanners/GetCurrentBanner/index.ts
@@ -1,6 +1,35 @@
-import type { Effect, Option } from 'effect'
+import { Array, Effect, Option, pipe, Schema } from 'effect'
 import type { Locale } from '../../Context.ts'
-import type { UnableToQuery } from '../../Queries.ts'
-import type { SpotlightBanner } from '../index.ts'
+import { Contentful } from '../../ExternalApis/Contentful/index.ts'
+import { UnableToQuery } from '../../Queries.ts'
+import type { SpotlightBanner } from '../Types.ts'
+import { EntryToSpotlightBanner } from './EntryToSpotlightBanner.ts'
 
-export declare const GetCurrentBanner: Effect.Effect<Option.Option<SpotlightBanner>, UnableToQuery, Locale>
+export const GetCurrentBanner: Effect.Effect<
+  Option.Option<SpotlightBanner>,
+  UnableToQuery,
+  Contentful | Locale
+> = Effect.gen(function* () {
+  const contentful = yield* Contentful
+
+  const { items } = yield* contentful.getEntries({
+    content_type: 'banner',
+    limit: 1,
+    locale: '*',
+    order: 'sys.createdAt',
+  })
+
+  if (!Array.isNonEmptyReadonlyArray(items)) {
+    return Option.none()
+  }
+
+  return yield* Effect.asSome(Schema.decodeUnknown(EntryToSpotlightBanner)(items[0]))
+}).pipe(
+  Effect.catchTag('ContentfulIsUnavailable', 'ParseError', error =>
+    pipe(
+      Effect.logError('Unable to get current banner'),
+      Effect.annotateLogs({ error }),
+      Effect.andThen(new UnableToQuery({ cause: error })),
+    ),
+  ),
+)
