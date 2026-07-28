79c0a3c2b8d77b492d0cae82a434d7d9a405e282
Chris Wilkinson <chris@prereview.org>
2026-07-28 15:01:30 +0100

    Implement the method
    Refs #3411

--- src/ExternalApis/Contentful/GetEntry/index.ts
+++ src/ExternalApis/Contentful/GetEntry/index.ts
@@ -1,10 +1,20 @@
-import type { HttpClient, UrlParams } from '@effect/platform'
-import type { Effect } from 'effect'
+import { HttpClient, type UrlParams } from '@effect/platform'
+import { Effect, pipe } from 'effect'
 import type { ContentfulConfig } from '../ContentfulConfig.ts'
-import type { ContentfulIsUnavailable, EntryIsNotFound } from '../Errors.ts'
+import { ContentfulIsUnavailable, type EntryIsNotFound } from '../Errors.ts'
 import type { ContentfulId, Entry } from '../Types.ts'
+import { CreateRequest } from './CreateRequest.ts'
+import { HandleResponse } from './HandleResponse.ts'
 
-export declare const GetEntry: (
+export const GetEntry = (
   id: ContentfulId,
   params?: UrlParams.Input,
-) => Effect.Effect<Entry, EntryIsNotFound | ContentfulIsUnavailable, ContentfulConfig | HttpClient.HttpClient>
+): Effect.Effect<Entry, EntryIsNotFound | ContentfulIsUnavailable, ContentfulConfig | HttpClient.HttpClient> =>
+  pipe(
+    CreateRequest(id, params),
+    Effect.andThen(HttpClient.execute),
+    Effect.catchTag('RequestError', 'ResponseError', error => new ContentfulIsUnavailable({ cause: error })),
+    Effect.andThen(HandleResponse),
+    Effect.tapError(error => Effect.annotateLogs(Effect.logError('Failed to get entries from Contentful'), { error })),
+    Effect.withSpan('Contentful.getEntry', { attributes: { id } }),
+  )
