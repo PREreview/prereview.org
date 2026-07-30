6534eb93d7cbb12825a6e3671406ae43bbca0c51
Chris Wilkinson <chris@prereview.org>
2026-07-30 14:05:25 +0100

    Remove method that we won’t use as it can’t resolve includes
    Refs #3411, c13c58bcf6cc91cff037c1b2947159e7cbb79ad6

--- src/ExternalApis/Contentful/Contentful.ts
+++ src/ExternalApis/Contentful/Contentful.ts
@@ -2,18 +2,10 @@ import type { HttpClient } from '@effect/platform'
 import { Context, Effect, flow, Layer, Scope } from 'effect'
 import type { ContentfulConfig } from './ContentfulConfig.ts'
 import { GetEntries } from './GetEntries/index.ts'
-import { GetEntry } from './GetEntry/index.ts'
 
 export class Contentful extends Context.Tag('Contentful')<
   Contentful,
   {
-    getEntry: (
-      ...args: Parameters<typeof GetEntry>
-    ) => Effect.Effect<
-      Effect.Effect.Success<ReturnType<typeof GetEntry>>,
-      Effect.Effect.Error<ReturnType<typeof GetEntry>>
-    >
-  } & {
     getEntries: (
       ...args: Parameters<typeof GetEntries>
     ) => Effect.Effect<
@@ -31,7 +23,6 @@ export class Contentful extends Context.Tag('Contentful')<
       )
 
       return {
-        getEntry: flow(GetEntry, Effect.provide(context)),
         getEntries: flow(GetEntries, Effect.provide(context)),
       }
     }),
--- src/ExternalApis/Contentful/Errors.ts
+++ src/ExternalApis/Contentful/Errors.ts
@@ -1,5 +1,3 @@
 import { Data } from 'effect'
 
 export class ContentfulIsUnavailable extends Data.TaggedError('ContentfulIsUnavailable')<{ cause?: unknown }> {}
-
-export class EntryIsNotFound extends Data.TaggedError('EntryIsNotFound')<{ cause?: unknown }> {}
