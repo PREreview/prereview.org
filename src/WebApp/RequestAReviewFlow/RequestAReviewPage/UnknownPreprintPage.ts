01b2625d463ccf608c59635760ee656be1f2d5ca
Chris Wilkinson <chris@prereview.org>
2026-08-24 15:14:05 +0100

    Extract strings
    Refs #3440

--- src/WebApp/RequestAReviewFlow/RequestAReviewPage/UnknownPreprintPage.ts
+++ src/WebApp/RequestAReviewFlow/RequestAReviewPage/UnknownPreprintPage.ts
@@ -63,12 +63,7 @@ export const UnknownPreprintPage = (preprint: IndeterminatePreprintId, locale: S
                   PreprintsorgPreprintId: () => t('doiCouldBePreprintsorg'),
                   PsyarxivPreprintId: () => t('doiCouldBePsyarxiv'),
                   PsychArchivesPreprintId: () => t('doiCouldBePsycharchives'),
-                  RadialPreprintId:
-                    () =>
-                    ({ doi }: { doi: Html }) =>
-                      html`<span lang="en" dir="ltr">
-                        >We think the DOI ${doi} could be a Radial preprint, but we can’t find any details.</span
-                      >`,
+                  RadialPreprintId: () => t('doiCouldBeRadial'),
                   ResearchSquarePreprintId: () => t('doiCouldBeResearchSquare'),
                   ScieloPreprintId: () => t('doiCouldBeScielo'),
                   ScienceOpenPreprintId: () => t('doiCouldBeScienceOpen'),
--- src/WebApp/review-a-preprint-page/unknown-preprint-with-doi-page.ts
+++ src/WebApp/review-a-preprint-page/unknown-preprint-with-doi-page.ts
@@ -46,12 +46,7 @@ export function createUnknownPreprintWithDoiPage(preprint: IndeterminatePreprint
           PreprintsorgPreprintId: () => t('doiCouldBePreprintsorg'),
           PsyarxivPreprintId: () => t('doiCouldBePsyarxiv'),
           PsychArchivesPreprintId: () => t('doiCouldBePsycharchives'),
-          RadialPreprintId:
-            () =>
-            ({ doi }: { doi: Html }) =>
-              html`<span lang="en" dir="ltr">
-                >We think the DOI ${doi} could be a Radial preprint, but we can’t find any details.</span
-              >`,
+          RadialPreprintId: () => t('doiCouldBeRadial'),
           ResearchSquarePreprintId: () => t('doiCouldBeResearchSquare'),
           ScieloPreprintId: () => t('doiCouldBeScielo'),
           ScienceOpenPreprintId: () => t('doiCouldBeScienceOpen'),
