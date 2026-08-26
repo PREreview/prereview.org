81b4767dd21fed23b6d915117f734505117d877a
Chris Wilkinson <chris@prereview.org>
2026-08-26 16:42:36 +0100

    Remove unused code
    Refs #2017

--- src/preprint.ts
+++ src/preprint.ts
@@ -3,27 +3,11 @@ import type * as TE from 'fp-ts/lib/TaskEither.js'
 import type * as Preprints from './Preprints/index.ts'
 
 /** @deprecated */
-export interface GetPreprintEnv {
-  getPreprint: (
-    id: Preprints.IndeterminatePreprintId,
-  ) => TE.TaskEither<Preprints.PreprintIsNotFound | Preprints.PreprintIsUnavailable, Preprints.Preprint>
-}
-
-/** @deprecated */
 export interface GetPreprintTitleEnv {
   getPreprintTitle: (
     id: Preprints.IndeterminatePreprintId,
   ) => TE.TaskEither<Preprints.PreprintIsNotFound | Preprints.PreprintIsUnavailable, Preprints.PreprintTitle>
 }
-
-/** @deprecated */
-export const getPreprint = (
-  id: Preprints.IndeterminatePreprintId,
-): RTE.ReaderTaskEither<
-  GetPreprintEnv,
-  Preprints.PreprintIsNotFound | Preprints.PreprintIsUnavailable,
-  Preprints.Preprint
-> => RTE.asksReaderTaskEither(RTE.fromTaskEitherK(({ getPreprint }) => getPreprint(id)))
 
 /** @deprecated */
 export const getPreprintTitle = (
