30919fbd99ef010e1304e553157104544106d2e5
Chad Sansing <csansing@gmail.com>
2026-09-01 15:21:16 -0400

    Update hardcoded statistics

--- src/WebApp/HomePage/index.ts
+++ src/WebApp/HomePage/index.ts
@@ -20,6 +20,6 @@ export const HomePage: Effect.Effect<
     }),
     { concurrency: 'inherit' },
   ),
-  Effect.let('statistics', () => ({ prereviews: 2151, servers: ServerNames.length, users: 4642 })),
+  Effect.let('statistics', () => ({ prereviews: 2180, servers: ServerNames.length, users: 4740 })),
   Effect.andThen(createPage),
 )
